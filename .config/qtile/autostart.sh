a2286009c35d507f32c03a1685894b7961bc562f
Matt Camp <usmcamp0811@gmail.com>
2021-03-16 08:17:29 -0500

    updated ignore

---
+++ .config/qtile/autostart.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+picom -b
+feh --bg-scale /home/mcamp/.background
+# /home/mcamp/.config/polybar/launch &
+redshift-gtk &
+bash ~/.local/bin/swap-capslock-esc.sh
+powertop --auto-tune &
+xautolock -time 10 -locker i3lock-fancy &
+/home/mcamp/.local/bin/lid-action &
+powertop --auto-tune &
+nm-applet &
+ckb-next-daemon &
+ckb-next -b &
+# See https://wiki.archli
