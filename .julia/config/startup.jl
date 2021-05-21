d4daac601daa90a6038eaf6321c8d685e2eed0b3
Matt Camp <matt@matt-camp.com>
2021-05-21 13:32:49 -0500

    Added julia pkg template to autostart script

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -4,3 +4,22 @@ enable_autocomplete_brackets(true)
 # OhMyREPL.input_prompt!(">>> ")
 # OhMyREPL.output_prompt!("\n")
 # head(df::DataFrame) = first(df, 5)
+function template()
+    @eval begin
+        using PkgTemplates
+        Template(;
+           user="mcamp",
+           dir="/home/mcamp/code-home/",
+           authors="Matthew Camp",
+           julia=v"1.6.1",
+           plugins=[
+               License(; name="MIT"),
+               Git(; manifest=true, ssh=true),
+               GitLabCI(),
+               Codecov(),
+               Documenter{GitLabCI}(),
+               Develop(),
+           ],
+       )
+    end
+end
