2ed95c0327756f5d06fadc5db726a3561df2431c
Matt <usmcamp0811@gmail.com>
2021-07-20 14:14:04 -0500

    Update setup-base-pkgs.sh
    bug

--- setup-base-pkgs.sh
+++ setup-base-pkgs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Installing Base Packages from the Arch main repositories"
-sudo pacman -S $(curl https://raw.githubusercontent.com/usmcamp0811/dotfiles/wopr/pkglist-base | cut -d ' ')
+sudo pacman -S $(curl https://raw.githubusercontent.com/usmcamp0811/dotfiles/wopr/pkglist-base)
 
 echo "Installing Base Packages from AUR"
-sudo pacman -S $(curl https://raw.githubusercontent.com/usmcamp0811/dotfiles/wopr/pkglist-aur-base | cut -d ' ')
+sudo pacman -S $(curl https://raw.githubusercontent.com/usmcamp0811/dotfiles/wopr/pkglist-aur-base)
