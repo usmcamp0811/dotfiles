4216c06685a27f1095d03e0674fbf4e263693cb4
Matt Camp <matt@matt-camp.com>
2022-07-18 07:34:06 -0500

    there we go adding lua

---
+++ .config/nvim/lua/user/options.lua
@@ -0,0 +1,77 @@
+
+local options = {
+
+    directory = vim.fn.stdpath('config') .. '/swaps',
+    backupdir = vim.fn.stdpath('config') .. '/backups',
+    undodir = vim.fn.stdpath('config') .. '/undo',
+    guifont = "monospace:h17",               -- the font used in graphical neovim applications
+    completeopt = { "menuone", "noselect" }, -- mostly just for cmp
+    termguicolors = true,
+    clipboard = 'unnamedplus',
+    autoindent = true,
+    diffopt = 'filler',
+    cursorline = true,
+    mouse = 'a', -- enable mouse in all modes
+    wrap = false, -- don't wrp lines
+    magic = true, -- Enable extended regex commands
+    joinspaces = false, -- Only insert single space after a '.', '?' and '!' with a join command
+    showmode = false, -- Going to use airline.vim to handle this for us
+    startofline = false, -- Don't reset cursor to the start of the line when moving around
+    ruler = true, -- Show current position
+    nu = true, -- enable line numbers
+    shell = '/bin/zsh',
+    splitbelow = true, -- New windows go below
+    splitright = true, -- New windows go right
+    title = true, -- Show the filename in the window titlebar
+    undofile = true, -- Persistent Undo
+    wildmenu = true, -- Hitting TAB in command mode will show possible completion above command line
+    wrapscan = true, -- Searches wrap around end of file
+    encoding = 'utf-8', -- nobomb
+    -- vim.o.guioptions = 'a' -- copy on select... requires gvim
+    shortmess = 'atI', -- Don't show the intro message when starting vim
+    showtabline = 2, -- Always show tab bar
+    sidescrolloff = 3, -- Start scrolling three columns before vertical border of window
+    smartcase = true, -- Ignore 'ignorecase' if search patter contains uppercase characters
+    smarttab = true, -- At start of line, <Tab> inserts shiftwidth spaces, <Bs> deletes shiftwidth spaces
+    scrolloff = 3, -- Start scrolling three lines before horizontal border of window
+    lazyredraw = true, -- Don't redraw when we don't have to
+    laststatus = 2, -- Always show status line
+    spell = true, -- turns on spell check
+    relativenumber = true, -- Use relative line numbers. Current line is still in status bar.
+    wildignore = {
+        '.DS_Store', '*.jpg', '*.jpeg', '*.gif', '*.png', '*.gif', '*.psd', '*.o', '*.obj', '*.min.js',
+        '*/bower_components/*', '*/node_modules/*',
+        '*/smarty/*', '*/vendor/*', '*/.git/*', '*/.hg/*', 
+        '*/.svn/*', '*/.sass-cache/*', '*/log/*', '*/tmp/*', 
+        '*/build/*', '*/ckeditor/*', '*/doc/*', '*/source_maps/*', '*/dist/*'
+    },
+    wildmode = 'longest,list,full',
+
+}
+
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
+
+-- TODO: see if its possible to combine into something better
+o = vim.opt
+wo = vim.wo
+bo = vim.bo
+
+o.listchars:append({ eol = '¬' })
+o.listchars:append({ tab = '>·' })
+o.listchars:append({ trail = '~' })
+o.listchars:append({ precedes = '<' })
+o.listchars:append({ space = '␣' })
+o.listchars:append({ extends = '>' })
+o.iskeyword:append("-")
+o.shortmess:append "c"
+
+wo.number = true
+bo.expandtab = true
+bo.shiftwidth = 2
+bo.softtabstop = 2
+bo.tabstop = 2
+vim.cmd "set expandtab ts=2 sw=2 ai"
+
---
+++ .config/nvim/lua/user/plugins.lua
@@ -0,0 +1,124 @@
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  vim.notify("shit broke")
+  return
+end
+
+-- Have packer use a popup window
+packer.init {
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
+}
+
+-- Install your plugins here
+return packer.startup(function(use)
+
+  -- Basic stuff needed
+  use "wbthomason/packer.nvim" -- Have packer manage itself
+  use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
+  use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+  use "lewis6991/impatient.nvim" -- suppose to speed up lua load times
+
+  use "ahmedkhalf/project.nvim"
+  use "kyazdani42/nvim-tree.lua"
+
+  -- Notebook like functions / REPL
+  use "JuliaEditorSupport/julia-vim"
+  use "jpalardy/vim-slime" -- allow vim to send julia / python commands to the repl
+  use({"hanschen/vim-ipython-cell", ft = { "python", "julia", "markdown.pandoc" }})
+  use({"mroavi/vim-julia-cell", ft = { "julia", "jl" }})
+  use "metakirby5/codi.vim"
+
+  -- random plugins
+  use "unblevable/quick-scope" -- easier horizontal jumping
+  use "justinmk/vim-sneak" --  better vertical jumping
+  use "JoosepAlviste/nvim-ts-context-commentstring"
+  use "akinsho/toggleterm.nvim"
+  use "folke/which-key.nvim"
+  use "moll/vim-bbye"
+  use "numToStr/Comment.nvim"
+  use "windwp/nvim-autopairs"
+  use "907th/vim-auto-save"
+
+  -- UI
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+  use "lukas-reineke/indent-blankline.nvim"
+  use "norcalli/nvim-colorizer.lua"
+  use "dhruvasagar/vim-table-mode"
+  use "lunarvim/darkplus.nvim"
+  use "joshdick/onedark.vim"
+  use "goolord/alpha-nvim"
+
+  use "akinsho/bufferline.nvim"
+  use({"kevinhwang91/rnvimr", run = "make sync" }) -- ranger in vima
+
+  -- cmp plugins
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
+
+  -- snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+
+  -- LSP
+  use "neovim/nvim-lspconfig" -- enable LSP
+  use "williamboman/nvim-lsp-installer" -- simple to use language server installer
+  use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
+
+  -- Telescope
+  use "nvim-telescope/telescope.nvim"
+
+  -- Treesitter
+  use({
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+  })
+
+  -- Git
+  use {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup()
+    end
+  }
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
+end)
