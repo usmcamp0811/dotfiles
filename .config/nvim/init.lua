5212f218b5c2b23f855c7a70c65ee42d57df1f76
Matt Camp <matt@matt-camp.com>
2022-07-22 07:30:57 -0500

    more vim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,6 +7,7 @@ require "user.colorscheme"
 
 -- Plugins
 require "user.plugins"
+-- $HOME/.config/nvim/lua/user/plugins.lua
 
 -- Plugin Configurations
 require "user.plug-setting.cmp"
@@ -23,6 +24,8 @@ require "user.plug-setting.impatient"
 require "user.plug-setting.project"
 require "user.plug-setting.nvim-tree"
 require "user.plug-setting.catppuccin"
+require "user.plug-setting.markdown"
+require "user.plug-setting.telekasten"
 
 
 -- TODO: port what I can (below) to Lua
@@ -31,6 +34,28 @@ vim.cmd "source $HOME/.config/nvim/plug-config/ranger.vim"
 
 vim.cmd "source $HOME/.config/nvim/plug-config/slime.vim"
 vim.cmd "source $HOME/.config/nvim/plug-config/codi.vim"
-vim.cmd "source $HOME/.config/nvim/plug-config/jupyter-vim.vim"  -- allows for julia cell jumping
 vim.cmd "source $HOME/.config/nvim/plug-config/autosave.vim"
+vim.cmd "source $HOME/.config/nvim/plug-config/jupyter-vim.vim"  -- allows for julia cell jumping
+vim.cmd "source $HOME/.config/nvim/plug-config/pandoc.vim"
+vim.cmd "source $HOME/.config/nvim/functions.vim" -- has a function for my markdown code blocks
+vim.cmd [[ 
+function! s:isAtStartOfLine(mapping)
+  let text_before_cursor = getline('.')[0 : col('.')-1]
+  let mapping_pattern = '\V' . escape(a:mapping, '\')
+  let comment_pattern = '\V' . escape(substitute(&l:commentstring, '%s.*$', '', ''), '\')
+  return (text_before_cursor =~? '^' . ('\v(' . comment_pattern . '\v)?') . '\s*\v' . mapping_pattern . '\v$')
+endfunction
 
+inoreabbrev <expr> <bar><bar>
+          \ <SID>isAtStartOfLine('\|\|') ?
+          \ '<c-o>:TableModeEnable<cr><bar><space><bar><left><left>' : '<bar><bar>'
+inoreabbrev <expr> __
+          \ <SID>isAtStartOfLine('__') ?
+
+let g:ipython_cell_regex = 1
+let g:ipython_cell_tag = '```( [^[].*)?'
+let g:julia_cell_delimit_cells_by = 'marks'
+let g:ipython_language = "Julia"
+let g:ipython_cell_run_command = 'include("{filepath}")'
+let g:ipython_cell_cell_command = 'include_string(Main, clipboard())'
+]]
