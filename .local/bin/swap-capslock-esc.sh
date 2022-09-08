8df184b1b0581b93d5bee7af01d522cc65e3cece
Matt Camp <matt@matt-camp.com>
2022-09-07 22:35:17 -0500

    fixed julia ohmyrepl

--- .local/bin/swap-capslock-esc.sh
+++ .local/bin/swap-capslock-esc.sh
@@ -3,6 +3,9 @@ xmodmap -e "clear lock" &
 xmodmap -e "keycode 9 = Caps_Lock NoSymbol Caps_Lock" &
 xmodmap -e "keycode 66 = Escape NoSymbol Escape" &
 
+#swap Fn key (151) with Ctrl (37)
+# xmodmap -e "keycode 151 = Control_L" &
+# xmodmap -e "keycode 36 = XF86WakeUp" &
 # wait 30
 notify-send "Keyboard" "$(whoami) your Keyboard was detected and Escape has been swapped with Caps Lock"
 # [[ -f ~/.Xmodmap ]] && xmodmap ~/.Xmodmap
