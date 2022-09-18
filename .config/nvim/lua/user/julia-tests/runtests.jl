d431f26372e702f449c76f3677507dda59777369
Matt Camp <matt@matt-camp.com>
2022-09-18 15:19:29 -0500

    added comments

--- .config/nvim/lua/user/julia-tests/runtests.jl
+++ .config/nvim/lua/user/julia-tests/runtests.jl
@@ -18,12 +18,13 @@ charsets = ["UTF-8", "windows-1250", "UTF-8", "Shift_JIS"]
         @test length(charsets) != 44
     end
     @testset "not group b test" begin 
-        @test length(charsets) != 44
+        @test length(charsets) != 4
+        @test length(charsets) == 4
     end
     @testset "Group B" begin 
         @testset "something good" begin 
             @test 3.14 ≈ π 
-            @test 2.31 == 2.31
+            @test 2.31 == 2.313
         end
     end
 end
