49ad6335e2cc741f4c5695af3bc61605a96b8085
Matt Camp <gitlab@aicampground.com>
2022-11-09 13:37:09 -0600

    vim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,19 @@
+
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system({
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  })
+  print("Installing packer close and reopen Neovim...")
+  vim.cmd([[packadd packer.nvim]])
+end
+
+
 -- Base Settings
 require("user.options")
 require("user.keymaps")
