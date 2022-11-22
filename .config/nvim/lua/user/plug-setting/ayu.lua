d2a0a6b92e573bdaced1fe74a53bc6868ac74f23
Matt Camp <gitlab@aicampground.com>
2022-11-22 07:12:47 -0600

    colors' :

--- .config/nvim/lua/user/plug-setting/ayu.lua
+++ .config/nvim/lua/user/plug-setting/ayu.lua
@@ -54,6 +54,43 @@ colors.vcs_removed_bg = "#2D2220"
 colors.fg_idle = "#3E4B59"
 colors.warning = "#FF8F40"
 
+colors.dark0_hard = "#0A0E14"
+colors.dark0 = "#282828"
+colors.dark0_soft = "#32302f"
+colors.dark1 = "#00010A"
+colors.dark2 = "#504945"
+colors.dark3 = "#665c54"
+colors.dark4 = "#7c6f64"
+colors.light0_hard = "#f9f5d7"
+colors.light0 = "#fbf1c7"
+colors.light0_soft = "#f2e5bc"
+colors.light1 = "#ebdbb2"
+colors.light2 = "#d5c4a1"
+colors.light3 = "#bdae93"
+colors.light4 = "#a89984"
+colors.bright_red = "#fb4934"
+colors.bright_green = "#b8bb26"
+colors.bright_yellow = "#fabd2f"
+colors.bright_blue = "#83a598"
+colors.bright_purple = "#d3869b"
+colors.bright_aqua = "#8ec07c"
+colors.bright_orange = "#fe8019"
+colors.neutral_red = "#cc241d"
+colors.neutral_green = "#98971a"
+colors.neutral_yellow = "#d79921"
+colors.neutral_blue = "#458588"
+colors.neutral_purple = "#b16286"
+colors.neutral_aqua = "#689d6a"
+colors.neutral_orange = "#d65d0e"
+colors.faded_red = "#9d0006"
+colors.faded_green = "#79740e"
+colors.faded_yellow = "#b57614"
+colors.faded_blue = "#076678"
+colors.faded_purple = "#8f3f71"
+colors.faded_aqua = "#427b58"
+colors.faded_orange = "#af3a03"
+colors.gray = "#928374"
+
 local function set_colors(fg, bg)
 	return "guifg=" .. fg .. " guibg=" .. bg
 end
@@ -62,7 +99,7 @@ end
 local api = vim.api
 api.nvim_command("hi StatusDefault " .. set_colors(colors.fg, colors.bg))
 api.nvim_command("hi StatusVimNormal " .. set_colors(colors.bg, colors.accent))
--- api.nvim_command("hi StatusVimInsert " .. set_colors(colors.bg, colors.neutral_blue))
+api.nvim_command("hi StatusVimInsert " .. set_colors(colors.bg, colors.neutral_blue))
 api.nvim_command("hi StatusVimVisual " .. set_colors(colors.bg, colors.func))
 api.nvim_command("hi StatusVimReplace " .. set_colors(colors.bg, colors.entity))
 api.nvim_command("hi StatusVimEnter " .. set_colors(colors.bg, colors.string))
@@ -75,15 +112,15 @@ api.nvim_command("hi StatusModified " .. set_colors(colors.bg, colors.constant))
 api.nvim_command("hi StatusLineNumber " .. set_colors(colors.bg, colors.operator))
 api.nvim_command("hi StatusColumnNumber " .. set_colors(colors.bg, colors.error))
 api.nvim_command("hi StatusFileInfo " .. set_colors(colors.keyword, colors.bg))
--- api.nvim_command("hi StatusGitInfo " .. set_colors(colors.bright_green, colors.bg))
--- api.nvim_command("hi StatusLSPProgress " .. set_colors(colors.neutral_blue, colors.bg))
--- api.nvim_command("hi StatusLSPError " .. set_colors(colors.error, colors.bg))
--- api.nvim_command("hi StatusLSPWarn " .. set_colors(colors.accent, colors.bg))
--- api.nvim_command("hi StatusLSPInfo " .. set_colors(colors.entity, colors.bg))
--- api.nvim_command("hi StatusLSPHin " .. set_colors(colors.tag, colors.bg))
--- api.nvim_command("hi StatusLSPStatus " .. set_colors(colors.tag, colors.bg))
--- api.nvim_command("hi StatusCwd " .. set_colors(colors.keyword, colors.bg))
--- api.nvim_command("hi StatusCursor " .. set_colors(colors.accent, colors.bg))
--- api.nvim_command("hi WhichKeyFloat " .. set_colors(colors.accent, colors.bg))
--- api.nvim_command("hi SessionName " .. set_colors(colors.string, colors.bg))
+api.nvim_command("hi StatusGitInfo " .. set_colors(colors.bright_green, colors.bg))
+api.nvim_command("hi StatusLSPProgress " .. set_colors(colors.neutral_blue, colors.bg))
+api.nvim_command("hi StatusLSPError " .. set_colors(colors.error, colors.bg))
+api.nvim_command("hi StatusLSPWarn " .. set_colors(colors.accent, colors.bg))
+api.nvim_command("hi StatusLSPInfo " .. set_colors(colors.entity, colors.bg))
+api.nvim_command("hi StatusLSPHin " .. set_colors(colors.tag, colors.bg))
+api.nvim_command("hi StatusLSPStatus " .. set_colors(colors.tag, colors.bg))
+api.nvim_command("hi StatusCwd " .. set_colors(colors.keyword, colors.bg))
+api.nvim_command("hi StatusCursor " .. set_colors(colors.accent, colors.bg))
+api.nvim_command("hi WhichKeyFloat " .. set_colors(colors.accent, colors.bg))
+api.nvim_command("hi SessionName " .. set_colors(colors.string, colors.bg))
 
