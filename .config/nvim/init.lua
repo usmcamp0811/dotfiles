324d2cc9520c8f498ad9098cd845dea94a533b44
Matt Camp <gitlab@aicampground.com>
2022-12-04 15:14:04 -0600

    more literate things

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,48 +9,68 @@ require("user.autocommands")
 require("user.plugins")
 require("user.snippets")
 
--- Plugin Configurations
- -- require("user.plug-setting.catppuccin")
- require("user.plug-setting.ayu")
- -- require("user.plug-setting.gruv-box")
- require("user.plug-setting.conjure")
- require("user.plug-setting.cmp")
- require("user.plug-setting.telescope")
- require("user.plug-setting.pandoc")
- require("user.plug-setting.treesitter")
- require("user.plug-setting.whichkey")
+--Vim Fatures
  require("user.plug-setting.toggleterm")
- require("user.plug-setting.alpha")
- require("user.plug-setting.comment")
- require("user.plug-setting.autopairs")
- require("user.plug-setting.lualine")
- require("user.plug-setting.bufferline")
- require("user.plug-setting.impatient")
- require("user.plug-setting.scrollbars")
+
+
  require("user.plug-setting.hlslens")
- require("user.plug-setting.project")
  require("user.plug-setting.nvim-tree")
- require("user.plug-setting.markdown")
- require("user.plug-setting.colorizer")
- require("user.plug-setting.calendar")
- require("user.plug-setting.knap")
- require("user.plug-setting.autosave")
- require("user.plug-setting.vim-ipython-cell")
- require("user.plug-setting.slime")
- require("user.plug-setting.ranger")
  require("user.plug-setting.pretty-fold")
- require("user.plug-setting.gitsigns")
+ require("user.plug-setting.autosave")
+ require("user.plug-setting.ranger")
+ require("user.plug-setting.impatient")
+ require("user.plug-setting.knap")
+ require("user.plug-setting.whichkey")
+
+-- Note taking
  require("user.plug-setting.neorg")
  require("user.plug-setting.vimwiki")
  require("user.plug-setting.literate")
  require("user.plug-setting.zen")
- require("user.plug-setting.navic")
- -- require("user.plug-setting.image")
  require("user.plug-setting.mind")
+ require("user.plug-setting.pandoc")
+ require("user.plug-setting.markdown")
+
+-- Coding
+ require("user.plug-setting.conjure")
+ require("user.plug-setting.comment")
+ require("user.plug-setting.autopairs")
+ require("user.plug-setting.vim-ipython-cell")
+ require("user.plug-setting.slime")
  require("user.plug-setting.julia")
  require("user.plug-setting.codewindow")
- -- require("user.julia_tests")
+ require("user.plug-setting.navic")
+
+-- Git
+ require("user.plug-setting.gitsigns")
+
+-- Treesitter LSP
+ require("user.plug-setting.treesitter")
+
+-- Telescope
+ require("user.plug-setting.telescope")
+
+-- Auto Complete Snipptes
+ require("user.plug-setting.cmp")
+
+-- Movement
  require("user.plug-setting.leap")
+
+-- Themes
+ require("user.plug-setting.ayu")
+ require("user.plug-setting.alpha")
+ require("user.plug-setting.lualine")
+ require("user.plug-setting.bufferline")
+ require("user.plug-setting.scrollbars")
+ require("user.plug-setting.colorizer")
+
+ require("user.plug-setting.project")
+ require("user.plug-setting.calendar")
+-- Plugin Configurations
+ -- require("user.plug-setting.catppuccin")
+ -- require("user.plug-setting.gruv-box")
+ -- require("user.plug-setting.image")
+ -- require("user.julia_tests")
 --  require('hologram').setup{
 --     auto_display = true -- WIP automatic markdown image display, may be prone to breaking
 -- }
