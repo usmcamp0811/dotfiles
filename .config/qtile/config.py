d91bc038fa1f704734ad44b6af506158bfc3eb64
Matt Camp <gitlab@aicampground.com>
2023-01-27 07:19:12 -0600

    qtile

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -1,7 +1,4 @@
-# Copyright (c) 2010 Aldo Cortesi
-# Copyright (c) 2010, 2014 dequis
-# Copyright (c) 2012 Randall Ma
-# Copyright (c) 2012-2014 Tycho Andersen
+# Copyright (c) 2010 Aldo Cortesi Copyright (c) 2010, 2014 dequis Copyright (c) 2012 Randall Ma Copyright (c) 2012-2014 Tycho Andersen
 # Copyright (c) 2012 Craig Barnes
 # Copyright (c) 2013 horsik
 # Copyright (c) 2013 Tao Sauvage
