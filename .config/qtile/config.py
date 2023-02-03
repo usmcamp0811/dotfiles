00027b5cc481f8caf38b239b4f8844fd8c73e711
Matt Camp <gitlab@aicampground.com>
2023-02-02 20:25:42 -0600

    no vsync

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -667,22 +667,22 @@ main = None  # WARNING: this is deprecated and will be removed soon
 follow_mouse_focus = True
 bring_front_click = False
 cursor_warp = False
-floating_layout = layout.Floating(
-    float_rules=[
-        # Run the utility of `xprop` to see the wm class and name of an X client.
-        *layout.Floating.default_float_rules,
-        Match(wm_class="confirmreset"),  # gitk
-        Match(wm_class="makebranch"),  # gitk
-        Match(wm_class="maketag"),  # gitk
-        Match(wm_class="ssh-askpass"),  # ssh-askpass
-        Match(title="branchdialog"),  # gitk
-        Match(title="pinentry"),  # GPG key password entry
-        Match(wm_class="Wine"),
-        Match(wm_class="steam_app_1238810"),  # for Battlefiled V
-        Match(wm_class="steam_app_1238840"),  # for Battlefield 1
-        Match(wm_class="steam_app_1182480"),  # origin thing for steam games
-    ]
-)
+# floating_layout = layout.Floating(
+#     float_rules=[
+#         # Run the utility of `xprop` to see the wm class and name of an X client.
+#         *layout.Floating.default_float_rules,
+#         Match(wm_class="confirmreset"),  # gitk
+#         Match(wm_class="makebranch"),  # gitk
+#         Match(wm_class="maketag"),  # gitk
+#         Match(wm_class="ssh-askpass"),  # ssh-askpass
+#         Match(title="branchdialog"),  # gitk
+#         Match(title="pinentry"),  # GPG key password entry
+#         Match(wm_class="Wine"),
+#         Match(wm_class="steam_app_1238810"),  # for Battlefiled V
+#         Match(wm_class="steam_app_1238840"),  # for Battlefield 1
+#         Match(wm_class="steam_app_1182480"),  # origin thing for steam games
+#     ]
+# )
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 
