0c5dc6a87c6d3cc0e558cf2c27543fbb8beed7ef
Matt Camp <gitlab@aicampground.com>
2023-07-01 18:04:56 -0500

    moving things around

--- .local/bin/import-watch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-while :; do /usr/bin/inotifywait --event create /mnt/campfs/media/phone-pictures && /usr/bin/docker-compose -f /mnt/campfs/docker/hosts/lucas/docker-compose.photoprism.yml exec photoprism photoprism import; done
--- .local/bin/swap-capslock-esc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-xmodmap -e "clear lock" &
-xmodmap -e "keycode 9 = Caps_Lock NoSymbol Caps_Lock" &
-xmodmap -e "keycode 66 = Escape NoSymbol Escape" &
-
-notify-send "Keyboard" "$(whoami) your Keyboard was detected and Escape has been swapped with Caps Lock"
-# [[ -f ~/.Xmodmap ]] && xmodmap ~/.Xmodmap
