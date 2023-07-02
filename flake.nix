8c9a1827a5a90d57737dc7d5711155109f8e62d5
Matt Camp <gitlab@aicampground.com>
2023-07-02 08:26:57 -0500

    dunno

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "The Campground Config";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
     home-manager.url = "github:nix-community/home-manager/release-23.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
