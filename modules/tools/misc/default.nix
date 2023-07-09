9797b8fc08459fa9a4b6bb92d72c5226f39d999a
Matt Camp <gitlab@aicampground.com>
2023-07-09 14:44:22 -0500

    tools

---
+++ modules/tools/misc/default.nix
@@ -0,0 +1,25 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.internal;
+let cfg = config.campground.tools.misc;
+in
+{
+  options.campground.tools.misc = with types; {
+    enable = mkBoolOpt false "Whether or not to enable common utilities.";
+  };
+
+  config = mkIf cfg.enable {
+    campground.home.configFile."wgetrc".text = "";
+
+    environment.systemPackages = with pkgs; [
+      fzf
+      killall
+      unzip
+      file
+      jq
+      clac
+      wget
+    ];
+  };
+}
