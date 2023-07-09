69c7aa9b26560832fab0ee935e791de2b8a802ae
Matt Camp <gitlab@aicampground.com>
2023-07-09 11:49:37 -0500

    wip

--- modules/cache/public/default.nix
+++ modules/cache/public/default.nix
@@ -7,9 +7,9 @@ let
 in
 {
   options.campground.cache.public = {
-    enable = mkEnableOption "Plus Ultra public cache";
+    enable = mkEnableOption "Campground public cache";
   };
-
+# TODO: What is a public cache?
   config = mkIf cfg.enable {
     campground.nix.extra-substituters = {
       "https://attic.ruby.hamho.me/public".key = "public:QUkZTErD8fx9HQ64kuuEUZHO9tXNzws7chV8qy/KLUk=";
