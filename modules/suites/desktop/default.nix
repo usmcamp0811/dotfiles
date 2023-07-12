000b1481de45e0199a3743da7979173f74a2f2d8
Matt Camp <gitlab@aicampground.com>
2023-07-12 00:44:08 +0000

    missed a file

---
+++ modules/suites/desktop/default.nix
@@ -0,0 +1,35 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.internal;
+let
+  cfg = config.campground.suites.desktop;
+in
+{
+  options.campground.suites.desktop = with types; {
+    enable =
+      mkBoolOpt false "Whether or not to enable common desktop configuration.";
+  };
+
+  config = mkIf cfg.enable {
+    campground = {
+      desktop = {
+        qtile = enabled;
+
+        addons = { wallpapers = enabled; };
+      };
+
+      apps = {
+        # _1password = enabled;
+        # firefox = enabled;
+        # vlc = enabled;
+        # logseq = enabled;
+        # hey = enabled;
+        # pocketcasts = enabled;
+        # yt-music = enabled;
+        # twitter = enabled;
+        # gparted = enabled;
+      };
+    };
+  };
+}
