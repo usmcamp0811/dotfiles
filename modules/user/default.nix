3389d01889908038db2443eafe46f6dc94be3830
Matt Camp <gitlab@aicampground.com>
2023-07-13 02:37:11 +0000

    errr

--- modules/user/default.nix
+++ modules/user/default.nix
@@ -50,28 +50,6 @@ in
       lsd
     ];
 
-    programs.zsh = {
-      enable = true; # Enable zsh as the default shell
-      enableCompletion = true; # Enable command completion
-      autosuggestions.enable = true;
-      syntaxHighlighting.enable = true;
-      histFile = "$XDG_CACHE_HOME/zsh.history";
-
-
-      interactiveShellInit = ""; # Extra commands to run at interactive shell initialization
-
-      loginShellInit = ""; # Extra commands to run at login shell initialization
-
-      promptInit = ""; # Extra commands to run at prompt initialization
-
-      # TODO: migrate my theme here
-      ohMyZsh = {
-        enable = false; # Enable Oh My Zsh
-        plugins = [ ]; # Oh My Zsh plugins
-        theme = "fino"; # Oh My Zsh theme
-        custom = ""; # Custom Oh My Zsh configuration
-      };
-     };
     };
 
     campground.home = {
@@ -106,7 +84,28 @@ in
           lclu = "${pkgs.colorls}/bin/colorls -U -1";
         };
 
-        programs.zsh.enable = true;
+        programs.zsh = {
+          enable = true; # Enable zsh as the default shell
+          enableCompletion = true; # Enable command completion
+          autosuggestions.enable = true;
+          syntaxHighlighting.enable = true;
+          histFile = "$XDG_CACHE_HOME/zsh.history";
+
+
+          interactiveShellInit = ""; # Extra commands to run at interactive shell initialization
+
+          loginShellInit = ""; # Extra commands to run at login shell initialization
+
+          promptInit = ""; # Extra commands to run at prompt initialization
+
+          # TODO: migrate my theme here
+          ohMyZsh = {
+            enable = false; # Enable Oh My Zsh
+            plugins = [ ]; # Oh My Zsh plugins
+            theme = "fino"; # Oh My Zsh theme
+            custom = ""; # Custom Oh My Zsh configuration
+          };
+         };
 
         programs.zsh.initExtra = ''
           for file in /home/${cfg.name}/.config/shell/zsh/*.zsh; do
