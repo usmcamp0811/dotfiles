24d04d993b60f8a2b9e694868c8c31e45f1f970b
Matt Camp <gitlab@aicampground.com>
2023-07-13 18:24:15 +0000

    lame

--- systems/x86_64-linux/ata-xps/default.nix
+++ systems/x86_64-linux/ata-xps/default.nix
@@ -67,12 +67,10 @@ in
   }
 
 
-  {
-    users.users = {
-      alice = newUser "alice";
-      bob = newUser "bob";
-    };
-  }
+  users.users = {
+    alice = newUser "alice";
+    bob = newUser "bob";
+  };
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
