9654c387f9323d7b9e57e13ab2bf9e7715801adb
Matt Camp <gitlab@aicampground.com>
2023-07-13 02:24:59 +0000

    this?

--- modules/user/default.nix
+++ modules/user/default.nix
@@ -66,8 +66,6 @@ in
         for file in /home/${cfg.name}/.config/shell/*.shrc; do
             [ -r "$file" ] && source "$file"
         done
-
-        # source /home/${cfg.name}/.config/shell/zsh/theme
       '';
 
       interactiveShellInit = ""; # Extra commands to run at interactive shell initialization
