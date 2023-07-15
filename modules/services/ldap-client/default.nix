c5a4fbaa962060cfa3bb7c4d9f866661eeb2caf2
Matt Camp <gitlab@aicampground.com>
2023-07-15 17:40:40 +0000

    this is looking good hope it works

--- modules/services/ldap-client/default.nix
+++ modules/services/ldap-client/default.nix
@@ -10,15 +10,14 @@ in
   # TODO: modify this to have all the options for LDAP
   options.campground.services.ldap-client = with types; {
     enable = mkBoolOpt false "Whether or not to configure LDAP support.";
-    authorizedKeys =
-      mkOpt (listOf str) [ default-key ] "The public keys to apply.";
-    port = mkOpt port 2222 "The port to listen on (in addition to 22).";
-    manage-other-hosts = mkOpt bool true "Whether or not to add other host configurations to SSH config.";
+    domain = mkOpt str "aicampground" "The domain name.";
+    ldap_uri = mkOpt str "ldap://ldap.campground.lan:389" "The ldap URI to use.";
+    ldap_search_base = mkOpt str "dc=aicampground,dc=com" "The ldap search base.";
+    cache_credentials = mkBoolOpt true "Wheather or not to cache credentials.";
   };
 
-  # TODO: use variables down here
   config = mkIf cfg.enable {
-    # is this where this goes? feel like it needs to be modified
+
     environment.systemPackages = with pkgs; [
       sssd
       openldap
@@ -29,15 +28,15 @@ in
     services.sssd.config = {
       sssd = {
         services = [ "nss" "pam" ];
-        domains = [ "aicampground" ];
+        domains = [ cfg.domain ];
       };
 
       domain/aicampground = {
         id_provider = "ldap";
-        ldap_uri = "ldap://ldap.campground.lan:389";
-        ldap_search_base = "dc=aicampground,dc=com";
+        ldap_uri = cfg.ldap_uri;
+        ldap_search_base = cfg.ldap_search_base;
         ldap_tls_reqcert = "never";
-        cache_credentials = true;
+        cache_credentials = cfg.cache_credentials;
         enumerate = true;
         use_fully_qualified_names = false;
       };
