151daeda03dfcdb7bcae2b6f7ac69de68c03d9bb
Matt Camp <gitlab@aicampground.com>
2023-07-18 02:36:57 +0000

    movesecrets

--- systems/x86_64-linux/ata-xps/default.nix
+++ systems/x86_64-linux/ata-xps/default.nix
@@ -3,11 +3,6 @@
 with lib;
 with lib.internal;
 let
-  mysecrets = builtins.fetchGit {
-    url = "https://gitlab.com/usmcamp0811/campground-secrets.git";
-    ref = "master"; 
-    rev = "57228b7bbd48b88a6660f6d2a9540be893e76976"; 
-  };
   newUser = name: {
     isNormalUser = true;
     createHome = true;
