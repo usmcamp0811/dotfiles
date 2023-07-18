a596c914863e75f3395cd78004b111000713d45f
Matt Camp <gitlab@aicampground.com>
2023-07-18 02:23:27 +0000

    last thing suprisingly worked

--- systems/x86_64-linux/ata-xps/default.nix
+++ systems/x86_64-linux/ata-xps/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, nixos-hardware, agenix, ... }:
+{ pkgs, lib, nixos-hardware, agenix,... }:
 
 with lib;
 with lib.internal;
@@ -19,7 +19,9 @@ in
 {
   imports = [ 
     ./hardware.nix
+     agenix.nixosModules.default
   ];
+
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
   campground = {
     archetypes = {
@@ -53,17 +55,17 @@ in
     
   };
 
-  # agenix.secrets."test" = {
-  #   # wether secrets are symlinked to age.secrets.<name>.path
-  #   symlink = true;
-  #   # target path for decrypted file
-  #   path = "/etc/some-secret-file";
-  #   # encrypted file path
-  #   file =  "${mysecrets}/test.age";  # refer to ./xxx.age located in `mysecrets` repo
-  #   mode = "0400";
-  #   owner = "root";
-  #   group = "root";
-  # };
+  age.secrets."test" = {
+    # wether secrets are symlinked to age.secrets.<name>.path
+    symlink = true;
+    # target path for decrypted file
+    path = "/etc/some-secret-file";
+    # encrypted file path
+    file =  "${mysecrets}/test.age";  # refer to ./xxx.age located in `mysecrets` repo
+    mode = "0400";
+    owner = "root";
+    group = "root";
+  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
