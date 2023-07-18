c086ba7e2bcd6404d4b09c87be796a60c1f9e33d
Matt Camp <gitlab@aicampground.com>
2023-07-18 03:02:15 +0000

    this works?

--- modules/apps/agenix/default.nix
+++ modules/apps/agenix/default.nix
@@ -26,16 +26,16 @@ in
         } + "/pkgs/agenix.nix") {})
       ]; 
 
-      age.secrets."test" = {
-        # wether secrets are symlinked to age.secrets.<name>.path
-        symlink = true;
-        # target path for decrypted file
-        path = "/etc/some-secret-file";
-        # encrypted file path
-        file =  "${mysecrets}/test.age";  # refer to ./xxx.age located in `mysecrets` repo
-        mode = "0400";
-        owner = "root";
-        group = "root";
-      };
+      # age.secrets."test" = {
+      #   # wether secrets are symlinked to age.secrets.<name>.path
+      #   symlink = true;
+      #   # target path for decrypted file
+      #   path = "/etc/some-secret-file";
+      #   # encrypted file path
+      #   file =  "${mysecrets}/test.age";  # refer to ./xxx.age located in `mysecrets` repo
+      #   mode = "0400";
+      #   owner = "root";
+      #   group = "root";
+      # };
     };
 }
