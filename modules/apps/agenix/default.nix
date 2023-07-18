23b3e665a477f59b8db646e0ea01c0240b755ee6
Matt Camp <gitlab@aicampground.com>
2023-07-18 01:11:10 +0000

    test

--- modules/apps/agenix/default.nix
+++ modules/apps/agenix/default.nix
@@ -4,10 +4,11 @@ with lib;
 with lib.internal;
 let 
   cfg = config.campground.apps.agenix;
-  agenix = {
-    url = "github:yaxitech/ragenix";
-    flake = false;
-  };
+  # agenix = {
+    # url = "github:yaxitech/ragenix";
+    # flake = false;
+  # };
+  agenix.url = "github:ryantm/agenix";
 in
 {
   options.campground.apps.agenix = with types; {
@@ -18,10 +19,11 @@ in
   config =
     mkIf cfg.enable { 
       environment.systemPackages = [ 
-        (pkgs.callPackage (builtins.fetchTarball {
-          url = "https://github.com/yaxitech/ragenix/archive/main.tar.gz";
-          sha256 = "1j9yr5q0453wnmn8941vfppwfsqmx98nk1ajqqw4zjgmkc0kjfbn";
-        } + "/pkgs/agenix.nix") {})
+        # (pkgs.callPackage (builtins.fetchTarball {
+        #   url = "https://github.com/yaxitech/ragenix/archive/main.tar.gz";
+        #   sha256 = "1j9yr5q0453wnmn8941vfppwfsqmx98nk1ajqqw4zjgmkc0kjfbn";
+        # } + "/pkgs/agenix.nix") {})
+        agenix.nixosModules.default
       ]; 
     };
 }
