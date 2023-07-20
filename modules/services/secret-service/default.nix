5918efdae87ed1a849ee0bfc6c77f419963ce924
Matt Camp <gitlab@aicampground.com>
2023-07-20 03:12:47 +0000

    echo

--- modules/services/secret-service/default.nix
+++ modules/services/secret-service/default.nix
@@ -16,7 +16,7 @@ in
       wantedBy = [ "multi-user.target" ];
       serviceConfig = {
         # ExecStart = "${pkgs.bash}/bin/bash -c 'for i in {1..5}; do echo $YANKEE_WHITE; sleep 1; done'";
-        ExecStart = "${pkgs.bash}/bin/bash -c 'for i in {1..5}; do FROM A FILE: $(cat /tmp/detsys-vault/my-secret-file) FROM ENV: $YANKEE_WHITE; sleep 1; done'";
+        ExecStart = "${pkgs.bash}/bin/bash -c 'for i in {1..5}; do echo FROM A FILE: $(cat /tmp/detsys-vault/my-secret-file) FROM ENV: $YANKEE_WHITE; sleep 1; done'";
         Type = "oneshot";
       };
     };
