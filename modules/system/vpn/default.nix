c430db3a054b4ae55fa92287ce96cd69ca06841d
Matt Camp <gitlab@aicampground.com>
2023-07-23 03:07:06 +0000

    add vpn options

---
+++ modules/system/vpn/default.nix
@@ -0,0 +1,75 @@
+{ options, config, pkgs, lib, ... }:
+
+with lib;
+with lib.internal;
+let 
+  cfg = config.campground.system.vpn;
+in
+{
+  options.campground.system.vpn = with types; {
+    enable = mkBoolOpt false "Whether or not to enable VPN.";
+    role-id = mkOpt str config.campground.services.vault-agent.settings.vault.role-id "Absolute path to the Vault role-id";
+    secret-id = mkOpt str config.campground.services.vault-agent.settings.vault.secret-id "Absolute path to the Vault secret-id";
+    vault-address = mkOption {
+      type = str;
+      default = config.campground.services.vault-agent.settings.vault.address;
+      description = "The address of your Vault";
+    };
+    networks = mkOption {
+      type = attrsOf (submodule {
+        options = {
+          key = mkOption {
+            type = str;
+            description = "The key of the VPN network.";
+          };
+        };
+      });
+      default = {};
+      description = "A list of VPN networks to connect to.";
+    };
+  };
+  config = mkIf cfg.enable {
+    systemd.services.vpn_configs = {
+      description = "Set/update all VPN configs";
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        ExecStart = "${pkgs.bash}/bin/bash /tmp/detsys-vault/vpn-configs";
+        Type = "oneshot";
+      };
+    };
+    campground.services.vault-agent.services.vpn_configs = {
+      settings = {
+        vault.address = cfg.vault-address;
+        auto_auth = {
+          method = [{
+            type = "approle";
+            config = {
+              role_id_file_path = cfg.role-id;
+              secret_id_file_path = cfg.secret-id;
+              remove_secret_id_file_after_reading = false;
+            };
+          }];
+        };
+      };
+      secrets = {
+        file = {
+          files = { 
+            "vpn-configs" = {
+              text = builtins.concatStringsSep "\n" (lib.mapAttrsToList (name: network: ''
+                #!/bin/sh
+                VPN_NAME="${name}"
+                OVPN_FILE={{ with secret "secret/campground/vpn" }}{{ .Data.${network.key} }}{{ end }}
+                if ! ${pkgs.networkmanager}/bin/nmcli con show | grep -q $VPN_NAME; then
+                  ${pkgs.networkmanager}/bin/nmcli con import type openvpn file $OVPN_FILE
+                fi
+              '') cfg.networks);
+              permissions = "0400";
+              change-action = "restart";
+            };
+          };
+        };
+      };
+    };
+  };
+}
+
