55f2711101e0f62e55c5871c92f583d7f0838e62
Matt Camp <gitlab@aicampground.com>
2023-07-25 23:58:22 +0000

    not it

--- modules/apps/brave/default.nix
+++ modules/apps/brave/default.nix
@@ -18,7 +18,7 @@ let
     mkdir -p $NSS_DB_DIR
     ${pkgs.unzip}/bin/unzip ${cacCertificates} -d $DOD_CERT_DIR
     cd $DOD_CERT_DIR
-    ls -lah ${pkgs.opensc}/lib/
+    ls -lah $DOD_CERT_DIR
     ${pkgs.nssTools}/bin/modutil -dbdir sql:$NSS_DB_DIR -add "CAC Module" -libfile ${pkgs.opensc}/lib/opensc-pkcs11.so -force
     for n in $(ls * | grep Certificates); do
       ${pkgs.nssTools}/bin/certutil -d sql:$NSS_DB_DIR -A -t TC -n "$n" -i "$n"
