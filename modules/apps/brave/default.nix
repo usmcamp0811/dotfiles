779a20d8af106ff9a2866315af3798e1c4da9490
Matt Camp <matt@aicampground.com>
2023-07-26 20:09:07 -0500

    chsnge url

--- modules/apps/brave/default.nix
+++ modules/apps/brave/default.nix
@@ -5,8 +5,8 @@ with lib.internal;
 let
   cfg = config.campground.apps.brave;
   cacCertificates = pkgs.fetchurl {
-    url = "https://dl.dod.cyber.mil/wp-content/uploads/pki-pke/zip/unclass-certificates_pkcs7_WCF.zip";
-    sha256 = "1inbf55mfqi0clsd8ybagfgz90n1h5knvs2rz33f7n6pjy7hcsnm";
+    url = "https://dl.dod.cyber.mil/wp-content/uploads/pki-pke/zip/unclass-certificates_pkcs7_DoD.zip";
+    sha256 = "09w9z6vs8r394wq2xk4gwaii9l04zb5h1q1qixbr9m45vnfs2i1v";
   };
 
   installCACertsScript = pkgs.writeShellScriptBin "installCACerts" ''
@@ -16,15 +16,15 @@ let
     DOD_CERT_DIR=$HOME_DIR/dodcerts
     NSS_DB_DIR=$HOME_DIR/.pki/nssdb
     mkdir -p $NSS_DB_DIR
-    ${pkgs.unzip}/bin/unzip ${cacCertificates} -d $DOD_CERT_DIR
+    rm -rf $DOD_CERT_DIR
+    ${pkgs.unzip}/bin/unzip -o ${cacCertificates} -d $DOD_CERT_DIR
+
     cd $DOD_CERT_DIR/certificates_pkcs7_v5_14_wcf
-    ls -lah $DOD_CERT_DIR/certificates_pkcs7_v5_14_wcf
-    ${pkgs.nssTools}/bin/modutil -dbdir sql:$NSS_DB_DIR -add "CAC Module" -libfile ${pkgs.opensc}/lib/opensc-pkcs11.so -force
-    for n in $(ls * | grep Certificates); do
+#    ${pkgs.nssTools}/bin/modutil -dbdir sql:$NSS_DB_DIR -add "CAC Module" -libfile ${pkgs.opensc}/lib/pkcs11/opensc-pkcs11.so -force
+    for n in $(ls $DOD_CERT_DIR/certificates_pkcs7_v5_14_wcf/*.p7b); do
       echo $n
       ${pkgs.nssTools}/bin/certutil -d sql:$NSS_DB_DIR -A -t TC -n "$n" -i "$n"
     done
-    chown -R ${config.campground.user.name} $HOME_DIR
     rm -rf $DOD_CERT_DIR
   '';
 in
