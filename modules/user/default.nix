4fb1ee7895314cc1ad68272d2fd0a0e34915c3ec
Matt Camp <matt@aicampground.com>
2023-07-31 21:04:29 -0500

    shit is wonky

--- modules/user/default.nix
+++ modules/user/default.nix
@@ -159,22 +159,22 @@ in
 
     # TODO: Make what gets copied here more generic for all users
     # TODO: This needs a zshrc or does it? home-manager needs to be accessible
-    # system.activationScripts.copySkelDotfiles = lib.stringAfter
-    #   [ "users" ]
-    #   ''
-    #     echo "Copying dotfiles to /etc/skel home directory..."
-    #     ${pkgs.bash}/bin/bash -c '
-    #       rm -rf /etc/skel/*
-    #       echo "Dotfiles directory: ${builtins.toString dotfilesDir}"
-    #       for file in ${builtins.toString dotfilesDir}/*; do
-    #         dest="/etc/skel/.config/$(basename $file)"
-    #         echo "Checking $file..."
-    #         echo "Copying $file to $dest..."
-    #         mkdir -p /etc/skel/.config/
-    #         cp -r $file $dest
-    #       done
-    #     '
-    #   '';
+    system.activationScripts.copySkelDotfiles = lib.stringAfter
+      [ "users" ]
+      ''
+        echo "Copying dotfiles to /etc/skel home directory..."
+        ${pkgs.bash}/bin/bash -c '
+          rm -rf /etc/skel/*
+          echo "Dotfiles directory: ${builtins.toString dotfilesDir}"
+          for file in ${builtins.toString dotfilesDir}/*; do
+            dest="/etc/skel/.config/$(basename $file)"
+            echo "Checking $file..."
+            echo "Copying $file to $dest..."
+            mkdir -p /etc/skel/.config/
+            cp -r $file $dest
+          done
+        '
+      '';
   };
 }
 
