30c1dbfa7fa7009990538e146abf6d61a9abe1e7
Matt Camp <matt@aicampground.com>
2023-08-01 13:45:09 -0500

    this enables the gpu

--- modules/hardware/nvidia/default.nix
+++ modules/hardware/nvidia/default.nix
@@ -13,7 +13,7 @@ in
       external-display.configuration = {
         system.nixos.tags = [ "external-display" ];
         hardware.nvidia = {
-          prime.offload.enable = lib.mkForce false;
+          prime.offload.enable = lib.mkForce true;
           powerManagement.enable = lib.mkForce false;
         };
       };
@@ -23,29 +23,20 @@ in
 
     hardware.nvidia.prime = {
       sync = {
-        enable = true;
+        enable = false;
       };
       offload = {
         enable = true;
         enableOffloadCmd = true;
       };
-
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:1:0:0";
     };
 
     hardware.nvidia = {
-      # Modesetting is needed for most Wayland compositors
       modesetting.enable = true;
-
-      # Use the open source version of the kernel module
-      # Only available on driver 515.43.04+
       open = false;
-
-      # Enable the nvidia settings menu
       nvidiaSettings = true;
-
-      # Optionally, you may need to select the appropriate driver version for your specific GPU.
       package = config.boot.kernelPackages.nvidiaPackages.stable;
     };
   };
