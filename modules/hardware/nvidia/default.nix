90db04eecc5da8da2317f6dc913d2ad75a6f2491
Matt Camp <matt@aicampground.com>
2023-07-31 22:16:45 -0500

    chad is an asshole

--- modules/hardware/nvidia/default.nix
+++ modules/hardware/nvidia/default.nix
@@ -9,12 +9,30 @@ in
     enable = mkBoolOpt false "Whether or not to enable Nvidia support";
   };
 
-  services.xserver.videoDrivers = [ "nvidia" ];
   config = mkIf cfg.enable {
-    hardware.nvidia.prime = {
-      sync.enable = true;
-      nvidiaBusId = "PCI:1:0:0";
-      intelBusId = "PCI:0:2:0";
+    hardware.opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+    };
+
+    # Tell Xorg to use the nvidia driver (also valid for Wayland)
+    services.xserver.videoDrivers = ["nvidia"];
+
+    hardware.nvidia = {
+
+      # Modesetting is needed for most Wayland compositors
+      modesetting.enable = true;
+
+      # Use the open source version of the kernel module
+      # Only available on driver 515.43.04+
+      open = false;
+
+      # Enable the nvidia settings menu
+      nvidiaSettings = true;
+
+      # Optionally, you may need to select the appropriate driver version for your specific GPU.
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
     };
   };
 }
