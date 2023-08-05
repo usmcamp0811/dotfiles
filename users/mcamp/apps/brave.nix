4a720fc1510ee78aa30e6a6bbcf5ebfafb654b8e
Matt Camp <gitlab@aicampground.com>
2023-08-05 15:41:11 -0500

    mucking with home-manager

---
+++ users/mcamp/apps/brave.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, ... }:
+
+{
+  programs.brave = {
+    enable = true;
+    package = pkgs.brave;
+    extensions = [
+      { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # uBlock Origin
+      { id = "nngceckbapebfimnlniiiahkandclblb"; } # Bitwarden
+      { id = "eimadpbcbfnmbkopoojfekhnkhdbieeh"; } # Dark Reader
+      { id = "iaddfgegjgjelgkanamleadckkpnjpjc"; } # Auto Quality for YouTube
+      { id = "dbepggeogbaibhgnhhndojpepiihcmeb"; } # Vimium
+      { id = "annfbnbieaamhaimclajlajpijgkdblo"; } # Dark Theme
+    ];
+  };
+}
+
--- users/mcamp/home.nix
+++ users/mcamp/home.nix
@@ -1,12 +1,15 @@
 { config, pkgs, ... }:
-
 {
-  # TODO: Move all things out to modules then import them here using snowfall lib methods
   home.username = "mcamp";
   home.homeDirectory = "/home/mcamp";
   home.stateVersion = "22.11";
 
   nixpkgs.config.allowUnfree = true;
+
+  imports = [
+    ./apps/brave.nix
+    ./apps/firefox.nix
+  ];
 
   home.packages = with pkgs; [
     k9s
@@ -22,65 +25,11 @@
     go-sct
   ];
 
-  programs.firefox = {
-    enable = true;
-    profiles = {
-      default = {
-        id = 0;
-        name = "default";
-        isDefault = true;
-        settings = {
-          "browser.startup.homepage" = "https://searx.aicampground.com";
-          "browser.search.defaultenginename" = "Searx";
-          "browser.search.order.1" = "Searx";
-        };
-        search = {
-          force = true;
-          default = "Searx";
-          order = [ "Searx" "Google" ];
-          engines = {
-            "Nix Packages" = {
-              urls = [{
-                template = "https://search.nixos.org/packages";
-                params = [
-                  { name = "type"; value = "packages"; }
-                  { name = "query"; value = "{searchTerms}"; }
-                ];
-              }];
-              icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-              definedAliases = [ "@np" ];
-            };
-            "NixOS Wiki" = {
-              urls = [{ template = "https://nixos.wiki/index.php?search={searchTerms}"; }];
-              iconUpdateURL = "https://nixos.wiki/favicon.png";
-              updateInterval = 24 * 60 * 60 * 1000; # every day
-              definedAliases = [ "@nw" ];
-            };
-            "Searx" = {
-              urls = [{ template = "https://searx.aicampground.com/?q={searchTerms}"; }];
-              iconUpdateURL = "https://nixos.wiki/favicon.png";
-              updateInterval = 24 * 60 * 60 * 1000; # every day
-              definedAliases = [ "@searx" ];
-            };
-            "Bing".metaData.hidden = true;
-            "Google".metaData.alias = "@g"; # builtin engines only support specifying one additional alias
-          };
-        };
-        extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-          ublock-origin
-          bitwarden
-          darkreader
-          vimium
-        ];
-      };
-    };
-  };
   services = {
     syncthing = {
       enable = true;
     };
   };
-
 
   xsession.windowManager.command = ''
     ${pkgs.dunst}/bin/dunst &
@@ -88,19 +37,6 @@
     ${pkgs.ckb-next}/bin/ckb-next -b &
     ${pkgs.go-sct}/bin/sct &
   '';
-
-  programs.brave = {
-    enable = true;
-    package = pkgs.brave;
-    extensions = [
-      { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # uBlock Origin
-      { id = "nngceckbapebfimnlniiiahkandclblb"; } # Bitwarden
-      { id = "eimadpbcbfnmbkopoojfekhnkhdbieeh"; } # Dark Reader
-      { id = "iaddfgegjgjelgkanamleadckkpnjpjc"; } # Auto Quality for YouTube
-      { id = "dbepggeogbaibhgnhhndojpepiihcmeb"; } # Vimium
-      { id = "annfbnbieaamhaimclajlajpijgkdblo"; } # Dark Theme
-    ];
-  };
 
   home.file = { };
 
