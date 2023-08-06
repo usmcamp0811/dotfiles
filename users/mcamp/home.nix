e4d37bde4f4a259261b521403bf9d693a76cca3b
Matt Camp <gitlab@aicampground.com>
2023-08-06 09:42:01 -0500

    guess this is how it wil lbe

--- users/mcamp/home.nix
+++ users/mcamp/home.nix
@@ -22,34 +22,22 @@
   ];
 
   # Use the function to import all .nix files from the apps directory
-  imports = map (n: "${./apps}/${n}") (builtins.attrNames (builtins.readDir ./apps));
+  #  imports = map (n: "${./apps}/${n}") (builtins.attrNames (builtins.readDir ./apps));
+  imports = [
+    ./apps/brave.nix
+    ./apps/firefox.nix
+  ];
 
-  config = {
-    options = {
-      campground = lib.mkOption {
-        type = lib.types.attrsOf (lib.types.submodule ({ name, ... }: {
-          options.enable = lib.mkEnableOption name;
-        }));
-        default = {};
-        description = "Campground programs";
-      };
-    };
 
-    campground.firefox.enable = true;
-    campground.brave.enable = true;
+  xsession.windowManager.command = ''
+    ${pkgs.dunst}/bin/dunst &
+    ${config.xsession.windowManager.command}
+    ${pkgs.ckb-next}/bin/ckb-next -b &
+    ${pkgs.go-sct}/bin/sct &
+  '';
 
-    xsession.windowManager.command = ''
-      ${pkgs.dunst}/bin/dunst &
-      ${config.xsession.windowManager.command}
-      ${pkgs.ckb-next}/bin/ckb-next -b &
-      ${pkgs.go-sct}/bin/sct &
-    '';
+  home.file = { };
 
-    home.file = { };
-
-    home.sessionVariables = { };
-
-    programs.home-manager.enable = true;
-  };
+  home.sessionVariables = { };
 }
 
