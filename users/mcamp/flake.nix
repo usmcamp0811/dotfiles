3d52bf5bbd47289c51f74e3e70b1b63299342f05
Matt Camp <matt@aicampground.com>
2023-08-07 11:56:21 -0500

    test

--- users/mcamp/flake.nix
+++ users/mcamp/flake.nix
@@ -1,42 +1,36 @@
 {
-  description = "Test";
+  # Import the necessary flake
+  inputs.plusultra.url = "github:jakehamilton/config";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-22.05";
-    unstable.url = "github:nixos/nixpkgs";
-    home-manager.url = "github:nix-community/home-manager";
-    snowfall-lib = {
-      url = "github:snowfallorg/lib";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    plusultra = {
-      url = "github:jakehamilton/config";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.unstable.follows = "unstable";
-    };
-  };
+  outputs = { self, nixpkgs, plusultra, ... }: let
+    system = "x86_64-linux";  # Define the system here
+  in
+  {
+    homeConfigurations = {
+      mcamp = { config, pkgs, ... }: {
+        # Import the home-manager module from the flake
+        imports = [ plusultra.nixosModules.home-manager ];
 
-  outputs = { self, nixpkgs, home-manager, snowfall-lib, plusultra, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      homeConfigurations = {
-        mcamp = home-manager.lib.homeManagerConfiguration {
-          pkgs = pkgs;
-          modules = [
-            ({ pkgs, ... }: {
-              imports = [
-                ./home.nix
-              ];
-              home.username = "mcamp";
-              home.homeDirectory = "/home/mcamp";
-              home.stateVersion = "22.11";
-            })
-          ];
+        # Use the applications defined in the flake
+        programs.firefox.enable = true;
+        programs.vscode.enable = true;
+
+        # Specify the user for home-manager
+        home.username = "mcamp";
+        home.homeDirectory = "/home/mcamp";
+
+        # Use the user packages from the flake
+        home.packages = with plusultra.packages.${system}; [ firefox vscode ];
+
+        # Use the home-manager options from the flake
+        plusultra.home.file = {
+          ".config/somefile".text = "contents of somefile";
+        };
+        plusultra.home.configFile = {
+          "someconfig".text = "contents of someconfig";
         };
       };
     };
+  };
 }
 
