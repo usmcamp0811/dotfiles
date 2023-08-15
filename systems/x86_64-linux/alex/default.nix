d2fab83454e105d3be1e9e2e85632f471bc049be
Matt Camp <mcamp@ata-llc.com>
2023-08-15 13:04:22 +0000

    wip

---
+++ systems/x86_64-linux/alex/default.nix
@@ -0,0 +1,81 @@
+{ pkgs, lib, nixos-hardware, nixosModules, agenix, ... }:
+
+with lib;
+with lib.internal;
+let
+  newUser = name: {
+    isNormalUser = true;
+    createHome = true;
+    home = "/home/${name}";
+    shell = pkgs.zsh;
+  };
+
+in
+{
+  imports = [ 
+    ./hardware.nix
+  ];
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+  services.logind.lidSwitch = "ignore";
+  campground = {
+    archetypes = {
+      workstation = enabled;
+    };
+    desktop.gnome = {
+      enable = true;
+      lightdm = false;
+      gdm = true;
+      wallpaper = {
+        light = pkgs.campground.wallpapers.nord-rainbow-light-nix-ultrawide;
+        dark = pkgs.campground.wallpapers.nord-rainbow-dark-nix-ultrawide;
+      };
+    };
+
+    apps = {
+      firefox = {
+        enable = true;
+        cac = false;
+      };
+      brave = {
+        enable = true;
+        cac = false;
+      };
+      vscode = enabled;
+    };
+
+    system = {
+      boot = enabled;
+    };
+
+    hardware.audio = {
+    };
+
+  };
+
+  campground.home.extraOptions = {
+    home.shellAliases = {
+      la = "lsd -lah";
+      update = "sudo nixos-rebuild switch";
+    };
+  };
+
+  campground.user = {
+    name = "ajames";
+    fullName = "Alex James";
+    email = "ajames@ata-llc.com";
+    extraGroups = ["wheel"];
+  };
+
+  campground.services = {
+  };
+
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+}
+
