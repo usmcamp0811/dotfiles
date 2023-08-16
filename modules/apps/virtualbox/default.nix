533da778c1c885c1e3714cb9d8f2f360543d545e
Matt Camp <mcamp@ata-llc.com>
2023-08-16 13:28:22 +0000

    no

--- modules/apps/virtualbox/default.nix
+++ modules/apps/virtualbox/default.nix
@@ -11,7 +11,7 @@ in
   };
 
   config = mkIf cfg.enable {
-    campground.virtualisation.virtualbox.host = {
+    virtualisation.virtualbox.host = {
       enable = true;
       enableExtensionPack = true;
     };
