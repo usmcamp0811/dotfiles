87a761a83559e0a1569dfe5db947d2c372f3f9bb
Matt Camp <matt@aicampground.com>
2023-08-18 15:21:28 -0500

    ldap?

---
+++ homes/x86_64-linux/mcamp@ldap/default.nix
@@ -0,0 +1,26 @@
+{ lib, pkgs, config, osConfig ? { }, format ? "unknown", ... }:
+
+with lib.campground;
+{
+  campground = {
+    user = {
+      enable = true;
+      # name = config.snowfallorg.user.name;
+      # TODO: I think the above fails because we dont add this user to the system as a local user elsewhere it would probbly work ¯\_(ツ)_/¯
+      name = "mcamp";
+    };
+
+    cli-apps = {
+    #   zsh = enabled;
+    #   neovim = enabled;
+      home-manager = enabled;
+    };
+    #
+    # tools = {
+    #   git = enabled;
+    #   direnv = enabled;
+    # };
+  };
+
+  home.stateVersion = "23.05";
+}
