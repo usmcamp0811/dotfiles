04221c6ec6ac9d7f503d0b7c5da3c5faba5ef5b6
Matt Camp <matt@aicampground.com>
2023-08-19 09:36:03 -0500

    clean up of env and fixing home

--- modules/nixos/user/default.nix
+++ modules/nixos/user/default.nix
@@ -93,18 +93,18 @@ in
 
         programs.zsh.enable = true;
 
-        programs.zsh.initExtra = ''
-          for file in /home/${cfg.name}/.config/shell/zsh/*.zsh; do
-              [ -r "$file" ] && source "$file"
-          done
-
-          # source all the other bash config files
-          for file in /home/${cfg.name}/.config/shell/*.shrc; do
-              [ -r "$file" ] && source "$file"
-          done
-
-          source /home/${cfg.name}/.config/shell/zsh/theme
-        '';
+        # programs.zsh.initExtra = ''
+        #   for file in /home/${cfg.name}/.config/shell/zsh/*.zsh; do
+        #       [ -r "$file" ] && source "$file"
+        #   done
+        #
+        #   # source all the other bash config files
+        #   for file in /home/${cfg.name}/.config/shell/*.shrc; do
+        #       [ -r "$file" ] && source "$file"
+        #   done
+        #
+        #   source /home/${cfg.name}/.config/shell/zsh/theme
+        # '';
 
         programs.zsh.history = {
           size = 10000;
@@ -139,42 +139,42 @@ in
    } // cfg.extraOptions;
 
 
-    system.activationScripts.copyDotfiles = lib.stringAfter
-      [ "users" ]
-      ''
-        echo "Copying dotfiles to ${cfg.name}'s home directory..."
-        ${pkgs.bash}/bin/bash -c '
-          echo "Dotfiles directory: ${builtins.toString dotfilesDir}"
-          for file in ${builtins.toString dotfilesDir}/*; do
-            dest="/home/${builtins.toString cfg.name}/.config/"
-            echo "Checking $file..."
-            echo "Copying $file to $dest..."
-            mkdir -p /home/${builtins.toString cfg.name}/.config
-            chown -R ${builtins.toString cfg.name}:users $dest
-            chmod -R 755 /home/${builtins.toString cfg.name}/.config
-            ${pkgs.rsync}/bin/rsync -a $file $dest
-            chown -R ${builtins.toString cfg.name}:ldap_user $dest
-          done
-        '
-      '';
-    # TODO: Make what gets copied here more generic for all users
-    # TODO: This needs a zshrc or does it? home-manager needs to be accessible
-    system.activationScripts.copySkelDotfiles = lib.stringAfter
-      [ "users" ]
-      ''
-        echo "Copying dotfiles to /etc/skel home directory..."
-        ${pkgs.bash}/bin/bash -c '
-          rm -rf /etc/skel/*
-          echo "Dotfiles directory: ${builtins.toString dotfilesDir}"
-          for file in ${builtins.toString dotfilesDir}/*; do
-            dest="/etc/skel/.config/"
-            echo "Checking $file..."
-            echo "Copying $file to $dest..."
-            mkdir -p /etc/skel/.config/
-            ${pkgs.rsync}/bin/rsync -a $file $dest
-          done
-        '
-      '';
+    # system.activationScripts.copyDotfiles = lib.stringAfter
+    #   [ "users" ]
+    #   ''
+    #     echo "Copying dotfiles to ${cfg.name}'s home directory..."
+    #     ${pkgs.bash}/bin/bash -c '
+    #       echo "Dotfiles directory: ${builtins.toString dotfilesDir}"
+    #       for file in ${builtins.toString dotfilesDir}/*; do
+    #         dest="/home/${builtins.toString cfg.name}/.config/"
+    #         echo "Checking $file..."
+    #         echo "Copying $file to $dest..."
+    #         mkdir -p /home/${builtins.toString cfg.name}/.config
+    #         chown -R ${builtins.toString cfg.name}:users $dest
+    #         chmod -R 755 /home/${builtins.toString cfg.name}/.config
+    #         ${pkgs.rsync}/bin/rsync -a $file $dest
+    #         chown -R ${builtins.toString cfg.name}:ldap_user $dest
+    #       done
+    #     '
+    #   '';
+    # # TODO: Make what gets copied here more generic for all users
+    # # TODO: This needs a zshrc or does it? home-manager needs to be accessible
+    # system.activationScripts.copySkelDotfiles = lib.stringAfter
+    #   [ "users" ]
+    #   ''
+    #     echo "Copying dotfiles to /etc/skel home directory..."
+    #     ${pkgs.bash}/bin/bash -c '
+    #       rm -rf /etc/skel/*
+    #       echo "Dotfiles directory: ${builtins.toString dotfilesDir}"
+    #       for file in ${builtins.toString dotfilesDir}/*; do
+    #         dest="/etc/skel/.config/"
+    #         echo "Checking $file..."
+    #         echo "Copying $file to $dest..."
+    #         mkdir -p /etc/skel/.config/
+    #         ${pkgs.rsync}/bin/rsync -a $file $dest
+    #       done
+    #     '
+    #   '';
   };
 }
 
