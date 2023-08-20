46e118c81abae43eed563b298888fa5944cda0be
Matt Camp <matt@aicampground.com>
2023-08-20 10:11:23 -0500

    fixing files

--- modules/home/cli/ranger/default.nix
+++ modules/home/cli/ranger/default.nix
@@ -11,11 +11,17 @@ in
   };
 
   config = mkIf cfg.enable {
-    programs.ranger = {
-      enable = true;
-    };
-    home.file = { 
-      ".config/ranger".source = ./configs;
+    
+    home.packages = with pkgs; [
+      ranger
+    ];
+    home.file = {
+      ".config/ranger/rc.conf" = { source = ./configs/rc.conf; };
+      ".config/ranger/rifle.conf" = { source = ./configs/rifle.conf; };
+      ".config/ranger/devicons.py" = { source = ./configs/devicons.py; };
+      ".config/ranger/scope.sh" = { source = ./configs/scope.sh; };
+      ".config/ranger/plugins/__init__.py" = { source = ./configs/plugins/__init__.py; };
+      ".config/ranger/plugins/devicons_linemode.py" = { source = ./configs/plugins/devicons_linemode.py; };
     };
   };
 }
