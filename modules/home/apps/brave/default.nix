419c7b7f0153a73dd64636659497b09808c5bcaa
Matt Camp <mcamp@ata-llc.com>
2023-08-21 10:32:21 -0500

    add todo

--- modules/home/apps/brave/default.nix
+++ modules/home/apps/brave/default.nix
@@ -63,5 +63,29 @@ in
     # };
 
   };
+# TODO: Add this shell script to set searx as default search
+# #!/bin/bash
+# 
+# # Define the search engine JSON entry with favicon_url
+# search_engine='{
+#   "default": false,
+#   "name": "Searx",
+#   "keyword": "searx",
+#   "search_url": "https://searx.aicampground.com/search?q={searchTerms}",
+#   "suggestions_url": "",
+#   "favicon_url": "https://searx.aicampground.com/static/themes/simple/img/favicon.svg"
+# }'
+# 
+# # Escape special characters for sed
+# escaped_search_engine=$(echo "$search_engine" | sed 's/[\/&]/\\&/g')
+# 
+# # Path to the Preferences file
+# preferences_file="$HOME/.config/BraveSoftware/Brave-Browser/Default/Preferences"
+# 
+# # Add the new search engine entry to Preferences
+# sed -i "/\"search_engines\": \[/a $escaped_search_engine," "$preferences_file"
+# 
+# echo "New search engine 'Searx' with favicon added to Preferences."
+
 }
 
