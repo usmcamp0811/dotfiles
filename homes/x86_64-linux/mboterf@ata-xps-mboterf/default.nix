b372f601812a9c9f745ef5bef8f84da2c81df6e5
Matt Camp <matt@aicampground.com>
2023-08-20 21:34:38 -0500

    bruce gets a profile to start with

---
+++ homes/x86_64-linux/mboterf@ata-xps-mboterf/default.nix
@@ -0,0 +1,53 @@
+{ lib, pkgs, config, osConfig ? { }, format ? "unknown", ... }:
+
+with lib.campground;
+{
+  campground = {
+    user = {
+      enable = true;
+      name = config.snowfallorg.user.name;
+    };
+    desktop = {
+      wallpapers = enabled;
+      qtile = {
+        enable = true;
+        wallpaper = "hsv-saturnV.png";
+      };
+    };
+
+    cli = {
+      zsh = enabled;
+      bash = enabled;
+      env = enabled;
+      home-manager = enabled;
+      k9s = enabled;
+      broot = enabled;
+      ranger = enabled;
+      # neovim = enabled;
+      #TODO: Add my Nvim config 
+    };
+    services = {
+      # picom = enabled;
+    };
+
+    apps = {
+      firefox = enabled;
+      brave = enabled;
+      libreoffice = enabled;
+      alacritty = enabled;
+      kitty = enabled;
+      rofi = enabled;
+      mpv = enabled;
+      #TODO: Add Qutebrowser
+    };
+    tools = {
+      git = enabled;
+      direnv = enabled;
+      virtmanager = enabled; # don't forget to add to libvirtd group
+      julia = enabled;
+      python = enabled;
+    };
+  };
+
+  home.stateVersion = "23.05";
+}
