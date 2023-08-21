0b7b79ecbd88ec91b84865faf0036db9501e0bff
Matt Camp <matt@aicampground.com>
2023-08-21 07:44:22 -0500

    this got fucked up

--- modules/nixos/hardware/nvidia/default.nix
+++ modules/nixos/hardware/nvidia/default.nix
@@ -10,122 +10,122 @@ in
   };
 
   config = mkIf cfg.enable {
-    boot.kernelParams = [ "module_blacklist=i915" ]; # blacklist integrated gpu
-#     boot.extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
-# boot = {
-#         blacklistedKernelModules = ["nouveau" "nvidiafb"];
-#         services.udev.extraRules =
-#         ''
-#           # Create /dev/nvidia-uvm when the nvidia-uvm module is loaded.
-#           KERNEL=="nvidia", RUN+="${pkgs.runtimeShell} -c 'mknod -m 666 /dev/nvidiactl c $$(grep nvidia-frontend /proc/devices | cut -d \  -f 1) 255'"
-#           KERNEL=="nvidia", RUN+="${pkgs.runtimeShell} -c 'for i in $$(cat /proc/driver/nvidia/gpus/*/information | grep Minor | cut -d \  -f 4); do mknod -m 666 /dev/nvidia$${i} c $$(grep nvidia-frontend /proc/devices | cut -d \  -f 1) $${i}; done'"
-#           KERNEL=="nvidia_modeset", RUN+="${pkgs.runtimeShell} -c 'mknod -m 666 /dev/nvidia-modeset c $$(grep nvidia-frontend /proc/devices | cut -d \  -f 1) 254'"
-#           KERNEL=="nvidia_uvm", RUN+="${pkgs.runtimeShell} -c 'mknod -m 666 /dev/nvidia-uvm c $$(grep nvidia-uvm /proc/devices | cut -d \  -f 1) 0'"
-#           KERNEL=="nvidia_uvm", RUN+="${pkgs.runtimeShell} -c 'mknod -m 666 /dev/nvidia-uvm-tools c $$(grep nvidia-uvm /proc/devices | cut -d \  -f 1) 1'"
-#         ''
-#         + lib.optionalString cfg.powerManagement.finegrained (
-#           lib.optionalString (lib.versionOlder config.boot.kernelPackages.kernel.version "5.5") ''
-#             # Remove NVIDIA USB xHCI Host Controller devices, if present
-#             ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{remove}="1"
+#     boot.kernelParams = [ "module_blacklist=i915" ]; # blacklist integrated gpu
+# #     boot.extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
+# # boot = {
+# #         blacklistedKernelModules = ["nouveau" "nvidiafb"];
+# #         services.udev.extraRules =
+# #         ''
+# #           # Create /dev/nvidia-uvm when the nvidia-uvm module is loaded.
+# #           KERNEL=="nvidia", RUN+="${pkgs.runtimeShell} -c 'mknod -m 666 /dev/nvidiactl c $$(grep nvidia-frontend /proc/devices | cut -d \  -f 1) 255'"
+# #           KERNEL=="nvidia", RUN+="${pkgs.runtimeShell} -c 'for i in $$(cat /proc/driver/nvidia/gpus/*/information | grep Minor | cut -d \  -f 4); do mknod -m 666 /dev/nvidia$${i} c $$(grep nvidia-frontend /proc/devices | cut -d \  -f 1) $${i}; done'"
+# #           KERNEL=="nvidia_modeset", RUN+="${pkgs.runtimeShell} -c 'mknod -m 666 /dev/nvidia-modeset c $$(grep nvidia-frontend /proc/devices | cut -d \  -f 1) 254'"
+# #           KERNEL=="nvidia_uvm", RUN+="${pkgs.runtimeShell} -c 'mknod -m 666 /dev/nvidia-uvm c $$(grep nvidia-uvm /proc/devices | cut -d \  -f 1) 0'"
+# #           KERNEL=="nvidia_uvm", RUN+="${pkgs.runtimeShell} -c 'mknod -m 666 /dev/nvidia-uvm-tools c $$(grep nvidia-uvm /proc/devices | cut -d \  -f 1) 1'"
+# #         ''
+# #         + lib.optionalString cfg.powerManagement.finegrained (
+# #           lib.optionalString (lib.versionOlder config.boot.kernelPackages.kernel.version "5.5") ''
+# #             # Remove NVIDIA USB xHCI Host Controller devices, if present
+# #             ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{remove}="1"
+# #
+# #             # Remove NVIDIA USB Type-C UCSI devices, if present
+# #             ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{remove}="1"
+# #
+# #             # Remove NVIDIA Audio devices, if present
+# # #             ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{remove}="1"
+# # #           ''
+# # #           + ''
+# # #             # Enable runtime PM for NVIDIA VGA/3D controller devices on driver bind
+# # #             ACTION=="bind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030000", TEST=="power/control", ATTR{power/control}="auto"
+# # #             ACTION=="bind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030200", TEST=="power/control", ATTR{power/control}="auto"
+# # #
+# # #             # Disable runtime PM for NVIDIA VGA/3D controller devices on driver unbind
+# # #             ACTION=="unbind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030000", TEST=="power/control", ATTR{power/control}="on"
+# # #             ACTION=="unbind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030200", TEST=="power/control", ATTR{power/control}="on"
+# # #           ''
+# # #         );
+# # #     };
+# #     services.xserver = {
+# #       videoDrivers = ["nvidia" "modesetting" ];
+# #       exportConfiguration = true;
+# #       modules = [pkgs.xorg.xf86videointel];
+# #     };
+# #     hardware = {
+# #       opengl = {
+# #         enable = true;
+# #         driSupport = true;
+# #         driSupport32Bit = true; # Required for steam
+# #       };
+# #       nvidia = {
+# #         modesetting.enable = true; # required for native resolution in TTY
+# #         powerManagement.enable = true;
+# #         open = false;
+# #         nvidiaSettings = true;
+# #         package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta; # stable, production, latest, beta, or vulkan_beta
+# #         #Fixes a glitch
+# #         nvidiaPersistenced = true;
+# #         prime = {
+# #             offload.enable = true;
+# #             #sync.enable = true;
+# #             intelBusId = "PCI:0:2:0";
+# #             nvidiaBusId = "PCI:1:0:0";
+# #         };
+# #       };
+# #     };
+# #     systemd.user.services.xrandr-outputsource = {
+# #       script = ''
+# #         ${pkgs.xorg.xrandr}/bin/xrandr --setprovideroutputsource NVIDIA-0 modesetting && ${pkgs.xorg.xrandr}/bin/xrandr --auto
+# #       '';
+# #       wantedBy = [ "graphical-session.target" ];
+# #       partOf = [ "graphical-session.target" ];
+# #       enable = true;
+# #       exportConfiguration = true;
+# #       videoDrivers = [ "nvidia" "displaylink" ];
+# #     };
+# #
+# #   };
+#   # boot.extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
 #
-#             # Remove NVIDIA USB Type-C UCSI devices, if present
-#             ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{remove}="1"
+#   # set output source to Nvidia for HDMI port
+#   systemd.user.services.xrandr-outputsource = {
+#     script = ''
+#       ${pkgs.xorg.xrandr}/bin/xrandr --setprovideroutputsource NVIDIA-G0 modesetting && ${pkgs.xorg.xrandr}/bin/xrandr --auto
+#     '';
+#     wantedBy = [ "graphical-session.target" ];
+#     partOf = [ "graphical-session.target" ];
+#     enable = true;
+#   };
+#   # services.xserver.displayManager.setupCommands = ''
+#   #   ${pkgs.lib.getBin pkgs.xorg.xrandr}/bin/xrandr --setprovideroutsource modesetting NVIDIA-0
+#   #   ${pkgs.lib.getBin pkgs.xorg.xrandr}/bin/xrandr --auto
+#   # '';
+#   boot.kernelPackages = pkgs.linuxPackages_latest;
 #
-#             # Remove NVIDIA Audio devices, if present
-#             ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{remove}="1"
-#           ''
-#           + ''
-#             # Enable runtime PM for NVIDIA VGA/3D controller devices on driver bind
-#             ACTION=="bind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030000", TEST=="power/control", ATTR{power/control}="auto"
-#             ACTION=="bind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030200", TEST=="power/control", ATTR{power/control}="auto"
-#
-#             # Disable runtime PM for NVIDIA VGA/3D controller devices on driver unbind
-#             ACTION=="unbind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030000", TEST=="power/control", ATTR{power/control}="on"
-#             ACTION=="unbind", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x030200", TEST=="power/control", ATTR{power/control}="on"
-#           ''
-#         );
+#   hardware = {
+#     nvidia = {
+#       modesetting.enable = true;
+#       powerManagement.enable = true;
+#       powerManagement.finegrained = false;
+#       prime = {
+#         offload.enable = true;
+#         intelBusId = "PCI:0:2:0";
+#         nvidiaBusId = "PCI:1:0:0";
+#       };
+#       package = config.boot.kernelPackages.nvidiaPackages.stable;
 #     };
-    services.xserver = {
-      videoDrivers = ["nvidia" "modesetting" ];
-      exportConfiguration = true;
-      modules = [pkgs.xorg.xf86videointel];
-    };
-    hardware = {
-      opengl = {
-        enable = true;
-        driSupport = true;
-        driSupport32Bit = true; # Required for steam
-      };
-      nvidia = {
-        modesetting.enable = true; # required for native resolution in TTY
-        powerManagement.enable = true;
-        open = false;
-        nvidiaSettings = true;
-        package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta; # stable, production, latest, beta, or vulkan_beta
-        #Fixes a glitch
-        nvidiaPersistenced = true;
-        prime = {
-            offload.enable = true;
-            #sync.enable = true;
-            intelBusId = "PCI:0:2:0";
-            nvidiaBusId = "PCI:1:0:0";
-        };
-      };
-    };
-    systemd.user.services.xrandr-outputsource = {
-      script = ''
-        ${pkgs.xorg.xrandr}/bin/xrandr --setprovideroutputsource NVIDIA-0 modesetting && ${pkgs.xorg.xrandr}/bin/xrandr --auto
-      '';
-      wantedBy = [ "graphical-session.target" ];
-      partOf = [ "graphical-session.target" ];
-      enable = true;
-      exportConfiguration = true;
-      videoDrivers = [ "nvidia" "displaylink" ];
-    };
-
-  };
-  # boot.extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
-
-  # set output source to Nvidia for HDMI port
-  systemd.user.services.xrandr-outputsource = {
-    script = ''
-      ${pkgs.xorg.xrandr}/bin/xrandr --setprovideroutputsource NVIDIA-G0 modesetting && ${pkgs.xorg.xrandr}/bin/xrandr --auto
-    '';
-    wantedBy = [ "graphical-session.target" ];
-    partOf = [ "graphical-session.target" ];
-    enable = true;
-  };
-  # services.xserver.displayManager.setupCommands = ''
-  #   ${pkgs.lib.getBin pkgs.xorg.xrandr}/bin/xrandr --setprovideroutsource modesetting NVIDIA-0
-  #   ${pkgs.lib.getBin pkgs.xorg.xrandr}/bin/xrandr --auto
-  # '';
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-
-  hardware = {
-    nvidia = {
-      modesetting.enable = true;
-      powerManagement.enable = true;
-      powerManagement.finegrained = false;
-      prime = {
-        offload.enable = true;
-        intelBusId = "PCI:0:2:0";
-        nvidiaBusId = "PCI:1:0:0";
-      };
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
-    };
-
-    opengl = {
-      enable = true;
-      driSupport = true;
-      driSupport32Bit = true;
-    };
-
-  };
-   environment.systemPackages = with pkgs; [
-     wget
-     pciutils
-     glxinfo
-   ];
+#
+#     opengl = {
+#       enable = true;
+#       driSupport = true;
+#       driSupport32Bit = true;
+#     };
+#
+#   };
+#    environment.systemPackages = with pkgs; [
+#      wget
+#      pciutils
+#      glxinfo
+#    ];
 
 #     services.xserver.enable = true;
 #     # services.xserver.videoDrivers = [ "displaylink" "nvidia" ];
