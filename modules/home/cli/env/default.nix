ed37818e7e032c32d9cb82cf42c1dd8c08209475
Matt Camp <matt@aicampground.com>
2023-08-29 19:56:43 -0500

    update and fix the sshkeys

--- modules/home/cli/env/default.nix
+++ modules/home/cli/env/default.nix
@@ -74,8 +74,8 @@ in
     '';
 
     home.activation.sshKeys = inputs.home-manager.lib.hm.dag.entryAfter ["writeBoundary"] ''
-      if [ -e "/var/lib/vault/users/${cfg.user-name}/id_" ]; then
-        cp /var/lib/vault/users/${cfg.user-name}/id_* /home/${cfg.user-name}/.ssh/
+      if [ -e "/var/lib/vault/users/${cfg-user.name}/id_" ]; then
+        cp /var/lib/vault/users/${cfg-user.name}/id_* /home/${cfg-user.name}/.ssh/
       fi
     '';
 
