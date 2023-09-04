0ba630d14a6b891f5e506f2d887688b38355e5b8
Matt Camp <matt@aicampground.com>
2023-09-03 22:56:22 -0500

    wip

--- modules/nixos/system/clevis/default.nix
+++ modules/nixos/system/clevis/default.nix
@@ -26,6 +26,8 @@ in
         port = 22;
         shell = "/bin/cryptsetup-askpass";
         authorizedKeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINLbrIDbLSEpfOc4onBP8y6aKCNEN5rEe0J3h7klfKzG mcamp@butler" ];
+        hostKeys = [ "/etc/ssh/ssh_host_rsa_key" "/etc/ssh/ssh_host_ed25519_key" ];
+
       };
     };
     # TODO: This should probably be parameterized and or not here because it could vary per system
