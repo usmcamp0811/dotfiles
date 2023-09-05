19c770bee889af6c726bb0cbe16faa30e32ae008
Matt Camp <mcamp@ata-llc.com>
2023-09-05 08:31:01 -0500

    lets see if this works

--- modules/nixos/system/zfs/default.nix
+++ modules/nixos/system/zfs/default.nix
@@ -43,8 +43,8 @@ in
     # use this lspci -v | grep -iA8 'network\|ethernet' to then ask Chad what modules to use here
     boot.initrd.availableKernelModules = [ "iwlwifi" "igc" "nfsv4" "cdc_ether" ];
     boot.kernelParams = [ "ip=dhcp" ];
-    boot.kernelModules = [ "r8169" ];
-    boot.initrd.kernelModules = [ "r8169" ];
+    boot.kernelModules = [ "r8169" "cdc_ether" ];
+    boot.initrd.kernelModules = [ "r8169" "cdc_ether" ];
     # boot.kernelModules = [ "iwlwifi" "cdc_ether" ];
     # boot.initrd.kernelModules = [ "iwlwifi" "cdc_ether" ];
   };
