0f0a7f1d4c84f048b2ab1434b9cb2d9bf7a11e58
Matt Camp <matt@aicampground.com>
2023-09-10 11:06:14 -0500

    cleaning

--- modules/nixos/services/k0s/default.nix
+++ modules/nixos/services/k0s/default.nix
@@ -4,26 +4,6 @@ with lib.campground;
 let
   cfg = config.campground.services.k0s;
   inherit (pkgs.campground) k0s;
-  k0sConfig = pkgs.writeText "k0s.yaml" ''
-  # generated-by-k0sctl 2023-04-24T20:51:01-05:00
-  apiVersion: k0s.k0sproject.io/v1beta1
-  kind: ClusterConfig
-  spec:
-    api:
-      address: 10.8.0.135
-      sans:
-      - 10.8.0.135
-      - 10.8.0.161
-    network:
-      nodeLocalLoadBalancing:
-        enabled: true
-        type: EnvoyProxy
-      podCIDR: 10.244.0.0/16
-      provider: kuberouter
-      serviceCIDR: 10.96.0.0/12
-    telemetry:
-      enabled: false
-'';
 in
 {
   options.campground.services.k0s = with types; {
@@ -40,7 +20,6 @@ in
       calico-cni-plugin
     ];
 
-    # systemd.services.iscsi = enabled;
     security.apparmor.enable = true;
 
     environment.etc."cni/net.d/10-flannel.conflist".text = ''
@@ -64,7 +43,6 @@ in
         ]
       }
     '';
-    # services.kubernetes.kubelet.cni.packages = [pkgs.cni-plugins pkgs.cni-plugin-flannel];
 
     environment.etc."cni/net.d/10-kuberouter.conflist".text = ''
       {"cniVersion":"0.3.0","name":"mynet","plugins":[{"auto-mtu":true,"bridge":"kube-bridge","hairpinMode":true,"ipMasq":false,"ipam":{"subnet":"10.244.2.0/24","type":"host-local"},"isDefaultGateway":true,"mtu":1500,"name":"kubernetes","type":"bridge"},{"capabilities":{"portMappings":true,"snat":true},"mtu":1500,"type":"portmap"}]}
@@ -82,53 +60,7 @@ in
       };
       wantedBy = [ "multi-user.target" ];
     };
-    # services.kubernetes = {
-    #   roles = ["master" "node"];
-    #   masterAddress = "10.8.0.161";
-    #   flannel = {
-    #     enable = true;
-    #   };
-    # };
     services.openiscsi.enable = true;
     services.openiscsi.name = "daly";
-    # systemd.services.iscsid = {
-    #   description = "Open-iSCSI";
-    #   documentation = [ "man:iscsid(8)" "man:iscsiuio(8)" "man:iscsiadm(8)" ];
-    #   wantedBy = [ "multi-user.target" ];
-    #   requires = [ "iscsi-init.service" ];
-    #   after = [ "network-online.target" "iscsiuio.service" "iscsi-init.service" ];
-    #   before = [ "remote-fs-pre.target" ];
-    #   wants = [ "remote-fs-pre.target" ];
-    #   serviceConfig = {
-    #     Type = "notify";
-    #     NotifyAccess = "main";
-    #     ExecStart = "${pkgs.openiscsi}/bin/iscsid -f";
-    #     KillMode = "mixed";
-    #     Restart = "on-failure";
-    #     # Add these lines to specify the correct paths for the configuration files
-    #     Environment = [
-    #       "ISCSI_CONFIG_DIR=/etc/iscsi"
-    #     ];
-    #   };
-    # };
-    #
-    # systemd.sockets.iscsid = {
-    #   wantedBy = [ "sockets.target" ];
-    # };
-    #
-
-    # systemd.services.k0scontroller = {
-    #   description = "k0s - The Zero Friction Kubernetes";
-    #   documentation = [ "https://docs.k0sproject.io" ];
-    #   path = with pkgs; [
-    #     util-linux # required by kubelet: https://github.com/k0sproject/k0s/issues/3386
-    #   ];
-    #   after = [ "network-online.target" ];
-    #   wants = [ "network-online.target" ];
-    #   wantedBy = [ "multi-user.target" ];
-    #   serviceConfig = {
-    #     ExecStart = "${k0s}/bin/k0s controller --config=${k0sConfig} --data-dir=/var/lib/k0s --single=true";
-    #   };
-    # };
   };
 }
