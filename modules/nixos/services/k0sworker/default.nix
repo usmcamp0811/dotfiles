c9fb0cb1b6decf4e4a042025d4c3882be7810820
Matt Camp <matt@aicampground.com>
2023-09-10 13:30:37 -0500

    WIPip

--- modules/nixos/services/k0sworker/default.nix
+++ modules/nixos/services/k0sworker/default.nix
@@ -87,7 +87,7 @@ in
     };
     services.openiscsi = {
       enable = true;
-      name = config.system.name;
+      name = "iqn.2016-04.com.open-iscsi:${config.system.name}";
     };
 
     campground.services.vault-agent.services.k0sworker = {
