7c007d99a7c608587d9aac46846b82c5abdd64ae
Matt Camp <matt@aicampground.com>
2023-09-12 08:40:22 -0500

    adding barrier

---
+++ modules/nixos/apps/barrier/default.nix
@@ -0,0 +1,17 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.campground;
+let cfg = config.campground.apps.barrier;
+in
+{
+  options.campground.apps.barrier = with types; {
+    enable = mkBoolOpt false "Whether or not to enable barrier.";
+  };
+
+  config = mkIf cfg.enable { 
+    environment.systemPackages = with pkgs; [
+      barrier
+    ];
+  };
+}
