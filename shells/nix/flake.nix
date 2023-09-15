eccbe88bed9bb4704245c783707774006bce18a0
Matt Camp <matt@aicampground.com>
2023-09-14 20:52:15 -0500

    maybe this

--- shells/nix/flake.nix
+++ shells/nix/flake.nix
@@ -1,29 +1,21 @@
 {
-  description = "A Nix-flake-based Nix development environment";
+  description = "virtual environments";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.devshell.url = "github:numtide/devshell";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
-    in
-    {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            cachix
-            lorri
-            niv
-            nixfmt
-            statix
-            vulnix
-            haskellPackages.dhall-nix
-            rnix-lsp
-          ];
+  outputs = { self, flake-utils, devshell, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      devShell =
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+
+            overlays = [ devshell.overlays.default ];
+          };
+        in
+        pkgs.devshell.mkShell {
+          imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
         };
-      });
-    };
+    });
 }
