efe450cb89c7a001ee6d48f5ccfcb802870fc818
Matt Camp <mcamp@ata-llc.com>
2023-09-15 13:12:22 -0500

    please work

---
+++ systems/x86_64-docker/test/default.nix
@@ -0,0 +1,53 @@
+{ pkgs, inputs, lib, nixos-hardware, nixosModules, ... }:
+
+with lib;
+with lib.campground;
+let
+  newUser = name: {
+    isNormalUser = true;
+    createHome = true;
+    home = "/home/${name}";
+    shell = pkgs.zsh;
+  };
+
+in
+{
+  home-manager.users.nixos.snowfallorg.user.name = "nixos";
+  # boot.loader.grub = enabled;
+
+  campground = {
+    nix = enabled;
+    cli-apps = {
+      flake = enabled;
+    };
+    tools = {
+      git = enabled;
+      misc = enabled;
+    };
+    services = {
+      openssh = enabled;
+    };
+    system = {
+      fonts = enabled;
+      locale = enabled;
+      time = enabled;
+      xkb = enabled;
+    };
+    user = {
+      name = "nixos";
+      fullName = "Matt";
+      email = "mcamp@ata-llc.com";
+      extraGroups = ["wheel"];
+    };
+  };
+
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+}
+
