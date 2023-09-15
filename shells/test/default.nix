be1094b4ba38bd8d5b7b601c435426b20f4acf4d
Matt Camp <matt@aicampground.com>
2023-09-14 20:57:09 -0500

    test

---
+++ shells/test/default.nix
@@ -0,0 +1,26 @@
+{ mkShell
+, pkgs
+, ...
+}:
+mkShell {
+  buildInputs = with pkgs; [
+    deadnix
+    hydra-check
+    nix-diff
+    nix-index
+    nix-prefetch-git
+    nixpkgs-fmt
+    nixpkgs-hammering
+    nixpkgs-lint
+    snowfallorg.flake
+    statix
+  ];
+
+  shellHook = ''
+
+    echo 🔨 Welcome to the Test Shell
+
+
+  '';
+
+}
