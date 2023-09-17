f5836c20a7d34d3278fb5f22fb8deffb2a8e0e4a
Matt Camp <matt@aicampground.com>
2023-09-17 14:40:05 -0500

    seems to work

--- packages/test-container/default.nix
+++ packages/test-container/default.nix
@@ -8,6 +8,15 @@
 , ...
 }:
 let
+  inherit (lib) mapAttrsToList concatStringsSep;
+  inherit (lib.campground) override-meta;
+
+  new-meta = with lib; {
+    description = "k0s - The Zero Friction Kubernetes";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ jakehamilton ];
+  };
+
   image = pkgs.dockerTools.buildImage {
     name = "hello-docker";
     config = {
