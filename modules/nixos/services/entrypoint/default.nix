9c7e35aedaa184eda49a0e35d5b9044caefcb246
Matt Camp <matt@aicampground.com>
2023-09-16 23:17:03 -0500

    this seems like a good idea

---
+++ modules/nixos/services/entrypoint/default.nix
@@ -0,0 +1,38 @@
+{ lib, config, pkgs, ... }:
+with lib;
+with lib.campground;
+let
+  cfg = config.campground.services.docker;
+in
+{
+  options.campground.services.docker = with types; {
+    enable = mkBoolOpt false "Enable Docker;";
+    user = mkOpt str "root" "User to run the container";
+    group = mkOpt str "root" "Group of the user running the container";
+    script = mkOpt str "/path/to/script" "Path to the entrypoint script";
+    cmd = mkOpt str "" "The CMD to run in the Docker container";
+  };
+
+  config = mkIf cfg.enable {
+    users.users."${cfg.user}" = {
+      isSystemUser = true;
+      group = "${cfg.group}";
+    };
+
+    users.groups."${cfg.group}" = {};
+    systemd.user.services.docker-entrypoint = {
+      description = "Entrypoint Systemd Service for use in Docker Images";
+      serviceConfig = {
+        Type = "oneshot";
+        User = cfg.user;
+        Group = cfg.group;
+        ExecStart = "${pkgs.writeScript "entrypoint" ''
+          #!/bin/sh
+          echo "Welcome to the Campground Container."
+          /bin/sh ${cfg.script} ${cfg.cmd}
+        ''}/bin/entrypoint";
+      };
+      wantedBy = [ "default.target" ];
+    };
+  };
+}
