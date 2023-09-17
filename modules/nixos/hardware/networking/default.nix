8d8af752eae6cdd7effc0303dd6ad5819bab374f
Matt Camp <matt@aicampground.com>
2023-09-17 10:52:26 -0500

    this works

--- modules/nixos/hardware/networking/default.nix
+++ modules/nixos/hardware/networking/default.nix
@@ -25,6 +25,10 @@ in
       };
     };
 
+    # TODO: Figure this out cause if its on jupyter is not accessible
+    networking.firewall = {
+      enable = false;
+    };
     # Fixes an issue that normally causes nixos-rebuild to fail.
     # https://github.com/NixOS/nixpkgs/issues/180175
     systemd.services.NetworkManager-wait-online.enable = false;
