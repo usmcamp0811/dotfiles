832eec65f1f22df428c30c4e63e9445bde600bb3
Matt Camp <matt@aicampground.com>
2023-09-16 21:53:51 -0500

    err

--- modules/nixos/services/jupyterlab/default.nix
+++ modules/nixos/services/jupyterlab/default.nix
@@ -10,12 +10,12 @@ in
   };
 
   config = mkIf cfg.enable {
-    users.users.jupyter = {
-      isSystemUser = true;
-      group = "jupyter";
-    };
-
-    users.groups.jupyter = {};
+    # users.users.jupyter = {
+    #   isSystemUser = true;
+    #   group = "jupyter";
+    # };
+    #
+    # users.groups.jupyter = {};
 
     environment.systemPackages = with pkgs; [
       jupyter-lab
@@ -24,8 +24,15 @@ in
     # systemd.services.jupyterlab = {
     #   description = "Jupyter Lab";
     #   wantedBy = [ "multi-user.target" ];
+    #   after = [ "network.target" ];
     #   serviceConfig = {
-    #     ExecStart = "${jupyter-lab}";
+    #     Type = "oneshot";
+    #     User = "mcamp";
+    #     Group = "ldap_user";
+    #     Environment = "JUPYTER_CONFIG_DIR=/home/mcamp/.config/jupyter";
+    #     WorkingDirectory = "/home/mcamp/code";
+    #     ExecStart = "/bin/sh -c 'whoami > /tmp/whoami_jupyterlab.txt && ${pkgs.jupyter-lab}/bin/jupyter-lab --ip 0.0.0.0 --config /home/mcamp/.config/jupyter'";
+    #     # Restart = "always";
     #   };
     # };
   };
