f205c388c904fa67c233a634918e010e336dc83d
Matt Camp <mcamp@ata-llc.com>
2023-09-18 13:57:24 -0500

    add kexec

---
+++ systems/x86_64-kexec/base/default.nix
@@ -0,0 +1,54 @@
+{ pkgs, inputs, lib, nixos-hardware, nixosModules, ... }:
+
+with lib;
+with lib.campground;
+let
+  newUser = name: {
+    isNormalUser = true;
+    createHome = true;
+    home = "/home/${name}";
+    shell = pkgs.zsh;
+  };
+
+in
+{
+  home-manager.users.ec2-user.snowfallorg.user.name = "ec2-user";
+  boot.loader.grub = enabled;
+  virtualisation.amazon-init = enabled;
+  campground = {
+    nix = enabled;
+    cli-apps = {
+      flake = enabled;
+    };
+    tools = {
+      git = enabled;
+      misc = enabled;
+    };
+    services = {
+      openssh = enabled;
+    };
+    system = {
+      # boot = enabled;
+      fonts = enabled;
+      locale = enabled;
+      time = enabled;
+      xkb = enabled;
+    };
+    user = {
+      name = "ec2-user";
+      fullName = "Matt";
+      email = "mcamp@ata-llc.com";
+      extraGroups = ["wheel"];
+    };
+  };
+
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+}
+
