e6e0b1e8bffc4aebeec91e7f923341efce21888f
Matt Camp <matt@aicampground.com>
2023-09-18 22:45:38 -0500

    WIP

--- packages/container-example-flask-app/default.nix
+++ packages/container-example-flask-app/default.nix
@@ -13,6 +13,8 @@ with lib.campground;
 let
   inherit (lib) mapAttrsToList concatStringsSep;
   inherit (lib.campground) override-meta;
+  # allows us to just use the app/package
+  inherit (pkgs.campground) example-flask-app;
   pname = "simple-flask-app";
 
   description = "A Simple Flask App";
@@ -25,25 +27,47 @@ let
     maintainers = with maintainers; [ mattcamp ];
   };
 
-  example-flask-image = pkgs.dockerTools.buildImage{
+  example-flask-image = let
+    nonRootShadowSetup = { user, uid, gid ? uid }: with pkgs; [
+      (
+      writeTextDir "etc/shadow" ''
+        root:!x:::::::
+        ${user}:!:::::::
+      ''
+      )
+      (
+      writeTextDir "etc/passwd" ''
+        root:x:0:0::/root:${runtimeShell}
+        ${user}:x:${toString uid}:${toString gid}::/home/${user}:
+      ''
+      )
+      (
+      writeTextDir "etc/group" ''
+        root:x:0:
+        ${user}:x:${toString gid}:
+      ''
+      )
+      (
+      writeTextDir "etc/gshadow" ''
+        root:x::
+        ${user}:x::
+      ''
+      )
+    ];
+  in
+  pkgs.dockerTools.buildLayeredImage{
     name = "example-flask-app" ;
     tag = "latest";
-    copyToRoot = pkgs.buildEnv{
-      name = "image-root";
-      pathsToLink = [ "/bin" ];
-      paths = [ campground.example-flask-app ];
-    };
-    runAsRoot = ''
-      # Create a new user for running uWSGI
-      adduser -D uwsgi_user
+    contents = [ example-flask-app pkgs.bash pkgs.coreutils ] ++ nonRootShadowSetup { uid = 999; user = "uwsgi_user"; };
+    extraCommands = ''
+      mkdir -p usr/bin
+      cat ${example-flask-app}/bin/run-flask-app > /usr/bin/run-flask-app
+      chmod +x /usr/bin/run-flask-app
     '';
     config = {
       WorkingDir = "/www/data";
       Cmd = [
-        "/bin/su"
-        "uwsgi_user"
-        "-c"
-        "${campground.example-flask-app}/bin/run-flask-app"
+        "/bin/sh ${example-flask-app}/bin/run-flask-app"
       ];
       ExposedPorts = {
         "8081/tcp" = {};
