bb44b4d80abffb6992f7efd24cf62e4df8304b99
Matt Camp <matt@aicampground.com>
2023-09-19 07:31:14 -0500

    where i be

--- packages/container-example-flask-app/default.nix
+++ packages/container-example-flask-app/default.nix
@@ -27,47 +27,19 @@ let
     maintainers = with maintainers; [ mattcamp ];
   };
 
-  example-flask-image = let
-    nonRootShadowSetup = { user, uid, gid ? uid }: with pkgs; [
-      (
-      writeTextDir "etc/shadow" ''
-        root:!x:::::::
-        ${user}:!:::::::
-      ''
-      )
-      (
-      writeTextDir "etc/passwd" ''
-        root:x:0:0::/root:${runtimeShell}
-        ${user}:x:${toString uid}:${toString gid}::/home/${user}:
-      ''
-      )
-      (
-      writeTextDir "etc/group" ''
-        root:x:0:
-        ${user}:x:${toString gid}:
-      ''
-      )
-      (
-      writeTextDir "etc/gshadow" ''
-        root:x::
-        ${user}:x::
-      ''
-      )
-    ];
-  in
-  pkgs.dockerTools.buildLayeredImage{
+  example-flask-image = pkgs.dockerTools.buildLayeredImage{
     name = "example-flask-app" ;
     tag = "latest";
-    contents = [ example-flask-app pkgs.bash pkgs.coreutils ] ++ nonRootShadowSetup { uid = 999; user = "uwsgi_user"; };
+    contents = [ pkgs.campground.example-flask-app pkgs.bash pkgs.coreutils ];
     extraCommands = ''
       mkdir -p usr/bin
-      cat ${example-flask-app}/bin/run-flask-app > /usr/bin/run-flask-app
+      cat ${pkgs.campground.example-flask-app}/bin/run-flask-app > /usr/bin/run-flask-app
       chmod +x /usr/bin/run-flask-app
     '';
     config = {
       WorkingDir = "/www/data";
       Cmd = [
-        "/bin/sh ${example-flask-app}/bin/run-flask-app"
+        "/bin/sh ${pkgs.campground.example-flask-app}/bin/run-flask-app"
       ];
       ExposedPorts = {
         "8081/tcp" = {};
