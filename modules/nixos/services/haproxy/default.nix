f39cb7aa59de7429869e14dcf0e83a38473c4711
Matt Camp <matt@aicampground.com>
2023-09-23 12:42:43 -0500

    adding haproxy

---
+++ modules/nixos/services/haproxy/default.nix
@@ -0,0 +1,62 @@
+{ lib, config, pkgs, ... }:
+with lib;
+with lib.campground;
+let
+  cfg = config.campground.services.haproxy;
+in
+{
+  options.campground.services.haproxy = with types; {
+    enable = mkBoolOpt false "Enable HAProxy;";
+    defaults = mkOption {
+      type = types.attrsOf types.str;
+      default = {
+        mode = "tcp";
+        "timeout connect" = "5s";
+        "timeout client" = "50s";
+        "timeout server" = "50s";
+      };
+      description = "Default settings for HAProxy.";
+    };
+    frontend-ip = mkOpt str "*" "IP to access HAProxy on";
+    frontend-port = mkOpt str "80" "Port to access HAProxy on";
+    backendServers = mkOption {
+      type = types.attrsOf (types.submodule {
+        options = {
+          port = mkOption {
+            type = types.int;
+            default = 8080;
+            description = "Port for the backend server.";
+          };
+        };
+      });
+      default = {};
+      description = "Backend servers for HAProxy.";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    services.haproxy = {
+      enable = true;
+      config = ''
+        global
+          log /dev/log local0
+          chroot /var/lib/haproxy
+          user haproxy
+          group haproxy
+
+        defaults
+          ${lib.concatStringsSep "\n" (lib.mapAttrsToList (name: value: "${name} ${value}") cfg.defaults)}
+
+        frontend http-in
+          bind ${cfg.frontend-ip}:${cfg.frontend-port}
+          default_backend servers
+
+        backend servers
+          balance roundrobin
+          ${lib.concatStringsSep "\n" (lib.mapAttrsToList (name: value: "server ${name} ${name}:${value.port} check") cfg.backendServers)}
+      '';
+    };
+
+  };
+}
+
