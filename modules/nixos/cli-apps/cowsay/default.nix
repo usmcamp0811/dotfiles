2eca70b2ac6ccf3f10b40a9558cc70dbf25acd18
Matt Camp <matt@aicampground.com>
2023-09-26 09:41:52 -0500

    cowsay what

---
+++ modules/nixos/cli-apps/cowsay/default.nix
@@ -0,0 +1,20 @@
+inputs@{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.campground;
+let
+  cfg = config.campground.cli-apps.cowsay;
+in
+{
+  options.campground.cli-apps.cowsay = with types; {
+    enable = mkBoolOpt false "Whether or not to enable cowsay.";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      snowfallorg.cowsay
+    ];
+  };
+}
+
+
