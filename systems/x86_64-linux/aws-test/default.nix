169ebe834321411506647e2d8e901c5b4b9f5873
Matt Camp <matt@aicampground.com>
2023-09-25 19:06:54 -0500

    i dont know

--- systems/x86_64-linux/aws-test/default.nix
+++ systems/x86_64-linux/aws-test/default.nix
@@ -13,7 +13,7 @@ let
 in
 {
   imports = [ 
-    ./hardware.nix
+    # ./hardware.nix
   ];
   boot.loader.grub = enabled;
   home-manager.users.ec2-user.snowfallorg.user.name = "ec2-user";
@@ -38,7 +38,7 @@ in
       };
     };
     system = {
-      # boot = enabled;
+      boot = enabled;
       fonts = enabled;
       locale = enabled;
       time = enabled;
