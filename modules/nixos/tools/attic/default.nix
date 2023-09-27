89fee1cbece86750288a2d1eb0eb0f050fd0f1fb
Matt Camp <matt@aicampground.com>
2023-09-26 20:14:24 -0500

    add attic

---
+++ modules/nixos/tools/attic/default.nix
@@ -0,0 +1,18 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+with lib.campground;
+let
+  cfg = config.campground.tools.attic;
+in
+{
+  options.campground.tools.attic = {
+    enable = mkEnableOption "Attic";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      attic
+    ];
+  };
+}
