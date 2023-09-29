af88853da7e5f039b09cb4c186857df30a963ad7
Matt Camp <matt@aicampground.com>
2023-09-29 10:41:15 -0500

    add policies for shit dont be stupid!

--- systems/x86_64-linux/daly/default.nix
+++ systems/x86_64-linux/daly/default.nix
@@ -72,6 +72,10 @@ in
   campground.services = {
     ldap-server = enabled;
 
+    openvpn = {
+      enable = true;
+
+    };
     openssh = {
       authorizedKeys = [ "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAGs9njLHA3yyrX6BTf5Z3Xj8jzOh9zVYfJoeai6WhmBtjr34KV0F79YKafvJPS4gasOTFpnKXObvBo0jG3/AIN+dwBohHtFtXSYBgZecFg847XoeN+7cIveqgI2Q1Jn2sFoUTzGiwKxqLRM7ZuTtRJGfoizOxlYHdyovus67jfDxewP5A== mcamp@Butler" ];
     };
--- systems/x86_64-linux/daly/vault/policies/campground.hcl
+++ systems/x86_64-linux/daly/vault/policies/campground.hcl
@@ -8,3 +8,17 @@ path "secret/campground/data/*" {
   capabilities = ["read", "list"]
 }
 
+# Allow reading from the PKI secrets engine to issue certificates
+path "pki/issue/campground-vpn-server-role" {
+  capabilities = ["create", "read", "update"]
+}
+
+# Allow reading the CA certificate
+path "pki/ca" {
+  capabilities = ["read"]
+}
+
+# Allow reading the CRL configuration
+path "pki/crl" {
+  capabilities = ["read"]
+}
