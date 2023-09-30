e068e16d314a3abc73aeab338bb79a4e7dedb903
Matt Camp <matt@aicampground.com>
2023-09-29 20:54:59 -0500

    probably good but need to document

--- modules/nixos/services/openvpn/default.nix
+++ modules/nixos/services/openvpn/default.nix
@@ -7,6 +7,11 @@ let
   gen-clients = pkgs.writeShellScriptBin "generate-client-ovpn" ''
     #!/usr/bin/env bash
 
+    CLIENT_NAME=$1
+    COMMON_NAME="''${CLIENT_NAME}.client.aicampground.com"
+
+    export VAULT_ADDR=${cfg.vault-address}
+
     if [ -z "$1" ]; then
       echo "Usage: $0 <client_name>"
       exit 1
@@ -35,8 +40,8 @@ let
     echo "Getting token..."
 
     token=$(${pkgs.vault}/bin/vault write -field=token auth/approle/login \
-      role_id="${cfg.role-id}" \
-      secret_id="${cfg.secret-id}" \
+      role_id="$(cat ${cfg.role-id})" \
+      secret_id="$(cat ${cfg.secret-id})" \
     )
 
     echo "Logging in..."
@@ -46,19 +51,19 @@ let
     # Check if the client role exists
     ROLE_EXISTS=$(${pkgs.vault}/bin/vault read -format=json ${cfg.vault-client-path} | ${pkgs.jq}/bin/jq -e .data > /dev/null 2>&1)
 
-    # If the role doesn't exist, create it
-    if [ $? -ne 0 ]; then
+    if [ -z "$ROLE_EXISTS" ]; then
+      echo "ROLE_EXISTS is empty, creating the client role."
       ${pkgs.vault}/bin/vault write ${cfg.vault-client-path} \
         allowed_domains="$COMMON_NAME" \
         allow_subdomains="true" \
         max_ttl="72h"
       echo "Client role campground-vpn-client-role has been created."
-    else
+    elif [ "$ROLE_EXISTS" -ne 0 ]; then
       echo "Client role campground-vpn-client-role already exists."
+    else
+      echo "An unexpected condition occurred."
     fi
 
-    CLIENT_NAME=$1
-    COMMON_NAME="''${CLIENT_NAME}.client.aicampground.com"
     
     echo "Writing certificates..."
 
