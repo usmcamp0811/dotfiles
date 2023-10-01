e3301d04613a18a817d94baf1e07206a51c1f8e7
Matt Camp <matt@aicampground.com>
2023-10-01 10:24:35 -0500

    updating default vault paths

--- modules/nixos/services/openvpn/default.nix
+++ modules/nixos/services/openvpn/default.nix
@@ -10,9 +10,9 @@ in
     enable = mkBoolOpt false "Enable OpenVPN Server;";
     role-id = mkOpt str config.campground.services.vault-agent.settings.vault.role-id "Absolute path to the Vault role-id";
     secret-id = mkOpt str config.campground.services.vault-agent.settings.vault.secret-id "Absolute path to the Vault secret-id";
-    vault-path = mkOpt str "pki/issue/campground-vpn-server-role" "The Vault path to the Server Cert in Vault";
-    vault-client-path = mkOpt str "pki/issue/campground-vpn-client-role" "The Vault path to the Client Cert in Vault"; 
-    vault-ca-path = mkOpt str "pki/cert/ca" "The Vault path to the CA Cert in Vault"; 
+    vault-path = mkOpt str "campgorund-vpn/issue/campground-vpn-server-role" "The Vault path to the Server Cert in Vault";
+    vault-client-path = mkOpt str "campgorund-vpn/issue/client-role" "The Vault path to the Client Cert in Vault"; 
+    vault-ca-path = mkOpt str "campgorund-vpn/cert/ca" "The Vault path to the CA Cert in Vault"; 
     vault-address = mkOption {
       type = str;
       default = config.campground.services.vault-agent.settings.vault.address;
