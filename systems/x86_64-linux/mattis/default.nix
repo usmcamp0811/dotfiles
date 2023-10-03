97769892edabf4567054b751ac83725b24d51d8b
Matt Camp <mcamp@ata-llc.com>
2023-10-02 20:29:26 -0500

    temp

--- systems/x86_64-linux/mattis/default.nix
+++ systems/x86_64-linux/mattis/default.nix
@@ -39,18 +39,18 @@ in
       };
       # manage local passwd in vault
       passwds = enabled;
-      wifi = {
-      # TODO: is there anything I can do to clean this up a little.. seems a little verbose
-        enable = false;
-        networks = {
-          SkyNet = {
-            ssid = "SkyNet";
-          };
-          SkyNet5 = {
-            ssid = "SkyNet5";
-          };
-        };
-      };
+      # wifi = {
+      # # TODO: is there anything I can do to clean this up a little.. seems a little verbose
+      #   enable = false;
+      #   networks = {
+      #     SkyNet = {
+      #       ssid = "SkyNet";
+      #     };
+      #     SkyNet5 = {
+      #       ssid = "SkyNet5";
+      #     };
+      #   };
+      # };
       # vpn = {
       #   enable = false;
       #   networks = {
@@ -77,14 +77,14 @@ in
     ldap-client = enabled;
     # jupyter = enabled;
     k0sworker = enabled;
-    zfs-key-server = {
-      enable = true;
-      tang-servers = [
-       "http://webb:1234" 
-       "http://lucas:1234" 
-       "http://ermy:1234" 
-      ];
-    };
+    # zfs-key-server = {
+    #   enable = true;
+    #   tang-servers = [
+    #    "http://webb:1234" 
+    #    "http://lucas:1234" 
+    #    "http://ermy:1234" 
+    #   ];
+    # };
     user-secrets = {
       enable = true;
       users = {
@@ -96,16 +96,16 @@ in
         };
       };
     };
-    vault-agent = {
-      enable = true;
-      settings = {
-        vault = {
-          address = "https://vault.lan.aicampground.com";
-          role-id = "/var/lib/vault/mattis/role-id";
-          secret-id = "/var/lib/vault/mattis/secret-id";
-        };
-      };
-    };
+    # vault-agent = {
+    #   enable = true;
+    #   settings = {
+    #     vault = {
+    #       address = "https://vault.lan.aicampground.com";
+    #       role-id = "/var/lib/vault/mattis/role-id";
+    #       secret-id = "/var/lib/vault/mattis/secret-id";
+    #     };
+    #   };
+    # };
   };
 
 
