495228dc53c2847eb5645aebbb56ec3fc2954917
Matt Camp <matt@aicampground.com>
2023-10-03 21:54:59 -0500

    this works elsewhere why not here

--- modules/nixos/services/ldap-server/default.nix
+++ modules/nixos/services/ldap-server/default.nix
@@ -91,7 +91,7 @@ in
     suffix = mkOpt str "dc=${cfg.domain-name},dc=com" "The suffix";
     role-id = mkOpt str config.campground.services.vault-agent.settings.vault.role-id "Absolute path to the Vault role-id";
     secret-id = mkOpt str config.campground.services.vault-agent.settings.vault.secret-id "Absolute path to the Vault secret-id";
-    vault-path = mkOpt str "campground-pki/issue/ldap-server-role" "The Vault path to the Server Cert in Vault";
+    vault-path = mkOpt str "campground-pki/issue/vpn-server-role" "The Vault path to the Server Cert in Vault";
     common-name = mkOpt str "server.${cfg.domain-name}" "Common Name for Server Certs";
     vault-address = mkOption {
       type = str;
