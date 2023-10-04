3e282f93e0b5067acb944696db1f0aab5ab392f1
Matt Camp <matt@aicampground.com>
2023-10-04 10:02:52 -0500

    cool

--- modules/nixos/services/ldap-server/default.nix
+++ modules/nixos/services/ldap-server/default.nix
@@ -92,7 +92,7 @@ in
     role-id = mkOpt str config.campground.services.vault-agent.settings.vault.role-id "Absolute path to the Vault role-id";
     secret-id = mkOpt str config.campground.services.vault-agent.settings.vault.secret-id "Absolute path to the Vault secret-id";
     vault-path = mkOpt str "campground-pki/issue/ldap-server-role" "The Vault path to the Server Cert in Vault";
-    common-name = mkOpt str "ldap.${cfg.domain-name}" "Common Name for Server Certs";
+    common-name = mkOpt str "ldap.server.aicampground.com" "Common Name for Server Certs";
     vault-address = mkOption {
       type = str;
       default = config.campground.services.vault-agent.settings.vault.address;
