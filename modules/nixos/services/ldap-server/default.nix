f61ed660fb1b21250d073d1443c119a3ec1131c5
Matt Camp <matt@aicampground.com>
2023-10-04 18:20:36 -0500

    ip

--- modules/nixos/services/ldap-server/default.nix
+++ modules/nixos/services/ldap-server/default.nix
@@ -61,14 +61,6 @@ in
         };
 
         children = {
-          "ou=People,dc=aicampground,dc=com" = {
-            objectClass = [ "top" "organizationalUnit" ];
-            ou = "People";
-          };
-          "ou=Group,dc=aicampground,dc=com" = {
-            objectClass = [ "top" "organizationalUnit" ];
-            ou = "Group";
-          };
           "cn=schema".includes = [
             "${pkgs.openldap}/etc/schema/core.ldif"
             "${pkgs.openldap}/etc/schema/cosine.ldif"
