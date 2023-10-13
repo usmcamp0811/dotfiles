14ffa30896de2b9f63a25d49af35af4448935318
Matt Camp <matt@aicampground.com>
2023-10-12 21:40:35 -0500

    add syncthing

---
+++ modules/home/services/syncthing/default.nix
@@ -0,0 +1,20 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.campground;
+let 
+  cfg = config.campground.services.syncthing;
+in
+{
+  options.campground.services.syncthing = with types; {
+    enable = mkBoolOpt false "Whether or not to enable syncthing.";
+  };
+
+  config = mkIf cfg.enable {
+    services.syncthing = {
+      enable = true;
+      tray = true;
+    };
+  };
+}
+
