e3ef79b1b6980f29cb1f7f28746e6f305e5b09ca
Matt Camp <matt@aicampground.com>
2023-10-14 13:56:01 -0500

    builds wip

--- modules/nixos/services/cert-manager/default.nix
+++ modules/nixos/services/cert-manager/default.nix
@@ -1,12 +1,13 @@
 { lib, config, pkgs, ... }:
 with lib;
 with lib.campground;
+
 let
   cfg = config.campground.services.cert-manager;
 in
 {
   options.campground.services.cert-manager = with types; {
-    enable = mkBoolOpt false "Whether to enable the fetch-cert-manager-certs service.";
+    enable = mkEnableOption "Whether to enable the fetch-cert-manager-certs service.";
 
     certs = lib.mkOption {
       type = lib.types.listOf (lib.types.submodule {
@@ -24,23 +25,23 @@ in
       default = [];
       description = "List of certs to fetch.";
     };
+    # Assuming that mkOpt is defined elsewhere
     role-id = mkOpt str config.campground.services.vault-agent.settings.vault.role-id "Absolute path to the Vault role-id";
     secret-id = mkOpt str config.campground.services.vault-agent.settings.vault.secret-id "Absolute path to the Vault secret-id";
     vault-path = mkOpt str "secret/campground/k8s" "The Vault path to the KV containing the Kubeconfig.";
     vault-address = mkOption {
-      type = str;
+      type = types.str;
       default = config.campground.services.vault-agent.settings.vault.address;
       description = "The address of your Vault";
     };
     kvVersion = mkOption {
-      type = enum ["v1" "v2"];
+      type = types.enum ["v1" "v2"];
       default = "v2";
       description = "KV store version";
     };
   };
 
   config = lib.mkIf cfg.enable {
-    
     campground.apps.k9s = {
       enable = true;
       role-id = cfg.role-id;
@@ -50,8 +51,8 @@ in
       vault-address = cfg.vault-address;
     };
 
-    systemd.services = lib.listToAttrs (lib.mapAttrsToList (name: cert: {
-      name = "fetchCertManagerCert-${name}";
+    systemd.services = lib.listToAttrs (builtins.map (cert: {
+      name = "fetchCertManagerCert-${cert.namespace}";
       value = {
         description = "Fetch certificates from cert-manager and store them";
         serviceConfig = {
@@ -63,16 +64,16 @@ in
           ${pkgs.kubectl}/bin/kubectl get secret ${cert.tlsSecret} -n ${cert.namespace} -o jsonpath="{.data.tls\.key}" | base64 --decode > /var/lib/vault/certs/${cert.namespace}-${cert.tlsSecret}-tls.key
         '';
       };
-    }) (lib.flip lib.listToAttrs (builtins.attrNames cfg.certs)) cfg.certs);
+    }) cfg.certs);
 
-    systemd.timers = lib.listToAttrs (lib.mapAttrsToList (name: cert: {
-      name = "fetchCertManagerCert-${name}.timer";
+    systemd.timers = lib.listToAttrs (builtins.map (cert: {
+      name = "fetchCertManagerCert-${cert.namespace}.timer";
       value = {
-        description = "Timer to run fetchCertManagerCert-${name} service";
-        partOf = [ "fetchCertManagerCert-${name}.service" ];
-        timerConfig.OnCalendar = "daily";  # Run daily
-        timerConfig.Persistent = true;  # Run at the next boot if the timer elapses while the system is off
+        description = "Timer to run fetchCertManagerCert-${cert.namespace} service";
+        partOf = [ "fetchCertManagerCert-${cert.namespace}.service" ];
+        timerConfig.OnCalendar = "daily";
+        timerConfig.Persistent = true;
       };
-    }) (lib.flip lib.listToAttrs (builtins.attrNames cfg.certs)) cfg.certs);
+    }) cfg.certs);
   };
 }
