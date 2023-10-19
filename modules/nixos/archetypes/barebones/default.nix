1f0b6f489f30aef23afc1d6aafd58f69c08f48c9
Matt Camp <matt@aicampground.com>
2023-10-19 18:33:44 -0500

    wip

---
+++ modules/nixos/archetypes/barebones/default.nix
@@ -0,0 +1,19 @@
+{ options, config, lib, pkgs, ... }:
+with lib;
+with lib.campground;
+let cfg = config.campground.archetypes.barebones;
+in
+{
+  options.campground.archetypes.barebones = with types; {
+    enable =
+      mkBoolOpt false "Whether or not to enable the barebones archetype.";
+  };
+
+  config = mkIf cfg.enable {
+    campground = {
+      suites = {
+        common = enabled;
+      };
+    };
+  };
+}
