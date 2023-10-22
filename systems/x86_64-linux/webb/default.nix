b8e90a11ae448bb7cd67955f32eee8b7e0d70356
Matt Camp <matt@aicampground.com>
2023-10-22 12:43:17 -0500

    clean up attempt of webb

--- systems/x86_64-linux/webb/default.nix
+++ systems/x86_64-linux/webb/default.nix
@@ -1,7 +1,8 @@
-{ pkgs, config, lib, nixos-hardware, nixosModules, agenix, ... }:
+{ pkgs, config, lib, nixos-hardware, nixosModules, ... }:
 
 with lib;
 with lib.campground;
+
 let
   newUser = name: {
     isNormalUser = true;
@@ -11,209 +12,98 @@ let
   };
 in
 {
-  imports = [ 
-    ./hardware.nix
-  ];
-  # boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+  imports = [ ./hardware.nix ];
+
   campground = {
-    archetypes = {
-      workstation = enabled;
-    };
-
-    apps = {
-    };
-
-    tools = {
-      icehouse = enabled;
-
-    };
-
-    # security = {
-    #   acme = enabled;
-    # };
-
-    nfs = {
-      campfs = enabled;
-    };
+    archetypes.workstation = enabled;
+    tools.icehouse = enabled;
+    nfs.campfs = enabled;
 
     system = {
       boot = enabled;
-      nic-teaming = {
-        enable = false;
-        ip = "10.8.0.127";
-        bondNICs = [
-          "eno1"
-          "eno2"
-          "eno3"
-          "eno4"
-        ];
-      };
-      # vpn = enabled;
       zfs = {
         enable = true;
         hostId = "119db424";
         keyfile-url = "http://10.8.0.1:1234/zfs-keyfile";
       };
-      # manage local passwd in vault
       passwds = enabled;
     };
 
-    hardware.audio = {
+    user = {
+      name = "abe";
+      fullName = "Matt Camp";
+      email = "matt@aicampground.com";
+      extraGroups = ["wheel"];
     };
 
-  };
+    services = {
+      postgresql = {
+        enable = true;
+        enableTCPIP = true;
+        backupEnable = true;
+        backupLocation = "/persist/postgresqlBackups/";
+        authentication = ''
+          local all root trust
+          local all postgres peer
+          local all vaultwarden trust
+          host  all  all  0.0.0.0/0  reject
+          host  all  all  ::0/0  reject
+        '';
+        databases = [ 
+          { 
+            name = "vaultwarden"; 
+            user = "vaultwarden"; 
+          } 
+        ];
+      };
 
-  campground.user = {
-    name = "abe";
-    fullName = "Matt Camp";
-    email = "matt@aicampground.com";
-    extraGroups = ["wheel"];
-  };
+      vaultwarden = { 
+        enable = true; 
+      };
+      openssh = { 
+        authorizedKeys = [ 
+          "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAGs9njLHA3yyrX6BTf5Z3Xj8jzOh9zVYfJoeai6WhmBtjr34KV0F79YKafvJPS4gasOTFpnKXObvBo0jG3/AIN+dwBohHtFtXSYBgZecFg847XoeN+7cIveqgI2Q1Jn2sFoUTzGiwKxqLRM7ZuTtRJGfoizOxlYHdyovus67jfDxewP5A== mcamp@Butler"
+        ];
+      };
+      ldap-client = enabled;
+      secret-service = enabled;
+      tang = enabled;
+      k0sworker = enabled;
+      ntp = enabled;
 
-  campground.services = {
+      zfs-key-server = {
+        enable = true;
+        port = 8123;
+        tang-servers = [ 
+          "http://daly:1234" 
+          "http://mattis:1234" 
+          "http://lucas:1234" 
+          "http://ermy:1234" 
+        ];
+      };
 
-    postgresql = {
-      enable = true;
-      enableTCPIP = true;
-      backupEnable = true;
-      backupLocation = "/persist/postgresqlBackups/";
-      authentication = ''
-        # Allow only local connections for the root user
-        local all root trust 
-        local all postgres peer
-        local all vaultwarden trust
-        # Deny other remote connections
-        host  all  all  0.0.0.0/0  reject
-        host  all  all  ::0/0  reject
-      '';
-      databases = [
-        { 
-          name = "vaultwarden"; 
-          user = "vaultwarden";
-        }
-      ];
-    };
-    vaultwarden = {
-      enable = true;
-    };
-    openssh = {
-      authorizedKeys = [ "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAGs9njLHA3yyrX6BTf5Z3Xj8jzOh9zVYfJoeai6WhmBtjr34KV0F79YKafvJPS4gasOTFpnKXObvBo0jG3/AIN+dwBohHtFtXSYBgZecFg847XoeN+7cIveqgI2Q1Jn2sFoUTzGiwKxqLRM7ZuTtRJGfoizOxlYHdyovus67jfDxewP5A== mcamp@Butler" ];
-    };
-    ldap-client = enabled;
-    secret-service = enabled;
-    tang = enabled;
-    k0sworker = enabled;
-    # k0scontroller = enabled;
-    ntp = enabled;
-    zfs-key-server = {
-      enable = true;
-      tang-servers = [
-       "http://daly:1234" 
-       "http://mattis:1234" 
-       "http://lucas:1234" 
-       "http://ermy:1234" 
-      ];
-      port = 8123;
-    };
-    user-secrets = {
-      enable = true;
-      users = {
-        mcamp =  {
-          files = [
-            "id_ed25519"
-            "passwords"
-          ];
+      user-secrets = {
+        enable = true;
+        users.mcamp = { 
+          files = [ 
+            "id_ed25519" 
+            "passwords" 
+          ]; 
         };
       };
-    };
-    # homer = {
-    #   enable = false;
-    #   host = "webb";
-    #
-    #   package = pkgs.campground.homer-catppuccin.override { favicon = "light"; };
-    #
-    #   settings = {
-    #     title = "Dashboard";
-    #     subtitle = "Campground Home";
-    #
-    #     logo = pkgs.campground.homer-catppuccin.logos.light;
-    #
-    #     stylesheet = [
-    #       pkgs.campground.homer-catppuccin.stylesheets.latte
-    #       pkgs.campground.homer-catppuccin.stylesheets.frappe
-    #     ];
-    #
-    #     footer = "";
-    #
-    #     connectivityCheck = true;
-    #
-    #     columns = "auto";
-    #
-    #     defaults = {
-    #       layout = "list";
-    #       colorTheme = "auto";
-    #     };
-    #
-    #     services = [
-    #       {
-    #         name = "Administration";
-    #         icon = "fas fa-shield-halved";
-    #         items = [
-    #           {
-    #             name = "Vault";
-    #             icon = "fas fa-lock";
-    #             url = "http://vault.lan";
-    #             target = "_blank";
-    #           }
-    #         ];
-    #       }
-    #     ];
-    #   };
-    #
-    #   # settings-path = "/var/lib/homer/config.yml";
-    # };
 
-    vault-agent = {
-      enable = true;
-      settings = {
-        vault = {
-          address = "https://vault.lan.aicampground.com";
-          role-id = "/var/lib/vault/webb/role-id";
-          secret-id = "/var/lib/vault/webb/secret-id";
+      vault-agent = {
+        enable = true;
+        settings = { 
+          vault = { 
+            address = "https://vault.lan.aicampground.com"; 
+            role-id = "/var/lib/vault/webb/role-id"; 
+            secret-id = "/var/lib/vault/webb/secret-id"; 
+          }; 
         };
       };
     };
   };
-  # services.nginx = {
-  #   enable = true;
-  #   recommendedProxySettings = true;
-  #
-  #   virtualHosts =
-  #     # TODO: get certs from certManager 
-  #     # let
-  #     #   shared-config = {
-  #     #     extra-config = {
-  #     #       forceSSL = true;
-  #     #
-  #     #       sslCertificate = "${config.security.acme.certs."daly.campground.lan".directory}/fullchain.pem";
-  #     #       sslCertificateKey = "${config.security.acme.certs."daly.campground.lan".directory}/key.pem";
-  #     #     };
-  #     #   };
-  #     # in
-  #     {
-  #       "vault.lan" = network.create-proxy
-  #         ((network.get-address-parts config.services.vault.address));
-  #           # // shared-config);
-  #     };
-  # };
 
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
+  system.stateVersion = "23.05";
 }
-
