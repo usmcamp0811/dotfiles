2fb32ee31e459b38c5eb624a33c260674cdaed73
Matt Camp <matt@aicampground.com>
2023-10-22 14:21:04 -0500

    wip mattermost

---
+++ modules/nixos/services/mattermost/default.nix
@@ -0,0 +1,70 @@
+{ lib, config, pkgs, ... }:
+with lib;
+with lib.campground;
+let
+  cfg = config.campground.services.mattermost;
+in
+{
+  options.campground.services.mattermost = with types; {
+    enable = mkBoolOpt false "Enable Mattermost;";
+  };
+
+  config = mkIf cfg.enable {
+    services.mattermost = {
+      enable = true;
+
+      siteUrl = "https://mattermost.aicampground.com";
+      listenAddress = "127.0.0.1:8065";
+
+      # TODO reevaluate option on fresh install
+      # Database was created before this option existed. Also using this
+      # requires to put add the password to the nix store.
+      localDatabaseCreate = false;
+
+      extraConfig = {
+        ServiceSettings = {
+          EnableEmailInvitations = true;
+          EnableOAuthServiceProvider = true;
+          TrustedProxyIPHeader = [ "X-Forwarded-For" "X-Real-IP" ];
+          AllowCorsFrom = "*";
+        };
+
+        FileSettings.Directory = "/var/lib/mattermost/files";
+      };
+    };
+
+
+    lollypops.secrets.files."mattermost/envfile" = { };
+
+    systemd.services.mattermost = {
+
+      serviceConfig = {
+
+        EnvironmentFile = config.lollypops.secrets.files."mattermost/envfile".path;
+
+        Environment = [
+
+          # TODO Check syntax for header
+
+          # Secret envfile contains:
+          # MM_EMAILSETTINGS_CONNECTIONSECURITY=
+          # MM_EMAILSETTINGS_ENABLEPREVIEWMODEBANNER=
+          # MM_EMAILSETTINGS_ENABLESMTPAUTH=
+          # MM_EMAILSETTINGS_FEEDBACKEMAIL=
+          # MM_EMAILSETTINGS_PUSHNOTIFICATIONCONTENTS=
+          # MM_EMAILSETTINGS_REPLYTOADDRESS=
+          # MM_EMAILSETTINGS_SENDEMAILNOTIFICATIONS=
+          # MM_EMAILSETTINGS_SMTPPASSWORD=
+          # MM_EMAILSETTINGS_SMTPPORT=
+          # MM_EMAILSETTINGS_SMTPSERVER=
+          # MM_EMAILSETTINGS_SMTPUSERNAME=
+          # MM_FILESETTINGS_PUBLICLINKSALT=
+          # MM_SQLSETTINGS_ATRESTENCRYPTKEY=
+          # MM_SQLSETTINGS_DATASOURCE=
+          # MM_EXTRA_SQLSETTINGS_DB_PASSWORD=
+        ];
+      };
+    };
+
+  };
+}
