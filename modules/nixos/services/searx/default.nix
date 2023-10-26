2eda518a142ce73d339546094898936ffeb47bb2
Matt Camp <matt@aicampground.com>
2023-10-25 20:09:29 -0500

    did up searx some

--- modules/nixos/services/searx/default.nix
+++ modules/nixos/services/searx/default.nix
@@ -8,6 +8,19 @@ in
   options.campground.services.searx = with types; {
     enable = mkBoolOpt false "Enable an Searx;";
     port = mkOpt int 8080 "Port to Host the searx server on.";
+    role-id = mkOpt str config.campground.services.vault-agent.settings.vault.role-id "Absolute path to the Vault role-id";
+    secret-id = mkOpt str config.campground.services.vault-agent.settings.vault.secret-id "Absolute path to the Vault secret-id";
+    vault-path = mkOpt str "secret/campground/searx" "The Vault path to the KV containing the Searx Secrets.";
+    kvVersion = mkOption {
+      type = enum ["v1" "v2"];
+      default = "v2";
+      description = "KV store version";
+    };
+    vault-address = mkOption {
+      type = str;
+      default = config.campground.services.vault-agent.settings.vault.address;
+      description = "The address of your Vault";
+    };
   };
 
   config = mkIf cfg.enable {
@@ -16,17 +29,41 @@ in
     services = {
       searx = {
         enable = true;
+        environmentFile = "/tmp/detsys-vault/searx.env";
         settings = {
+          runInUwsgi = true;
           server.port = cfg.port;
           server.bind_address = "0.0.0.0";
           server.secret_key = "@SEARX_SECRET_KEY@";
-        #   engines = nixpkgs.lib.singleton
-        #     {
-        #       name = "wolframalpha";
-        #       shortcut = "wa";
-        #       api_key = "@WOLFRAM_API_KEY@";
-        #       engine = "wolframalpha_api";
-        #     };
+        };
+      };
+    };
+
+    campground.services.vault-agent.services.searx = {
+      settings = {
+        vault.address = cfg.vault-address;
+        auto_auth = {
+          method = [{
+            type = "approle";
+            config = {
+              role_id_file_path = cfg.role-id;
+              secret_id_file_path = cfg.secret-id;
+              remove_secret_id_file_after_reading = false;
+            };
+          }];
+        };
+      };
+      secrets = {
+        file = {
+          files = {
+            "searx.env" = {
+              text = ''
+                SEARX_SECRET_KEY={{ with secret "${cfg.vault-path}" }}{{ if eq "${cfg.kvVersion}" "v1" }}{{ .Data.SEARX_SECRET_KEY }}{{ else }}{{ .Data.data.SEARX_SECRET_KEY }}{{ end }}{{ end }}
+              '';
+              permissions = "0600";  # Make the script executable
+              change-action = "restart";
+            };
+          };
         };
       };
     };
