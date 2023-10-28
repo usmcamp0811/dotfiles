4f2704d748fe9526c1ad502062183fd08ecbe70b
Matt Camp <matt@aicampground.com>
2023-10-28 13:20:17 -0500

    house keeping

--- modules/nixos/system/zfs/default.nix
+++ modules/nixos/system/zfs/default.nix
@@ -9,7 +9,6 @@ in
     enable = mkBoolOpt false "Whether or not to configure zfs.";
     hostId = mkOpt str "12345678" "The output of head -c 8 /etc/machine-id";
     keyfile-url = mkOpt str "http://key-server:8080/zfs-keyfile" "The URL for the Clevis encrypted Keyfile";
-    snapshot_datasets = mkOpt (lib.types.listOf lib.types.str) [] "List of ZFS datasets to snapshot.";
     public_keys = mkOpt (lib.types.listOf lib.types.str) [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINLbrIDbLSEpfOc4onBP8y6aKCNEN5rEe0J3h7klfKzG mcamp@butler" ] "List of public ssh keys to access the Phase 1 Boot for remote unlocking of ZFS";
   };
 
@@ -39,6 +38,7 @@ in
         enable = true;
         port = 22;
         authorizedKeys = cfg.public_keys;
+        # TODO: Do somehting to make sure these keys exist. Currently wont exist until you ssh somewhere for the first time.
         hostKeys = [ "/etc/ssh/ssh_host_rsa_key" "/etc/ssh/ssh_host_ed25519_key" ];
       };
     };
@@ -54,35 +54,6 @@ in
     services.zfs.autoSnapshot = {
       enable = true;
     };
-
-    # systemd.services.zfs-auto-snapshot = {
-    #   description = "ZFS auto snapshot service";
-    #   script = ''
-    #     #!/bin/sh
-    #     for pattern in ${toString cfg.snapshot_datasets}; do
-    #       # Check if the pattern ends with /*
-    #       if echo "$pattern" | grep -qE "/\*$"; then
-    #         # Remove the /* from the end
-    #         base_ds=$(echo "$pattern" | sed 's/\/\*$//')
-    #         # List datasets that match the pattern
-    #         datasets=$(${pkgs.zfs}/bin/zfs list -o name -H | grep "^$base_ds/")
-    #       else
-    #         datasets=$pattern
-    #       fi
-    #       for ds in $datasets; do
-    #         ${pkgs.zfs}/bin/zfs snapshot "$ds@$(date '+%Y%m%d%H%M%S')"
-    #       done
-    #     done
-    #   '';
-    #   serviceConfig.Type = "oneshot";
-    # };
-
-    # systemd.timers.zfs-auto-snapshot = {
-    #   description = "Run ZFS snapshots nightly";
-    #   wantedBy = [ "timers.target" ];
-    #   timerConfig.OnCalendar = "daily";
-    #   timerConfig.Persistent = true;
-    # };
   };
 
 }
