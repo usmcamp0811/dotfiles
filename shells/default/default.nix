389150e3b3f8ebf6b6cdab05aa016624c29660a1
Matt Camp <matt@aicampground.com>
2023-10-29 09:42:01 -0500

    wip

--- shells/default/default.nix
+++ shells/default/default.nix
@@ -8,6 +8,53 @@
 with lib;
 with lib.campground;
 let 
+  checkVaultPath = pkgs.writeShellScriptBin "check-vault-path" ''
+    full_path="$1"
+
+    # Check for PKI engines
+    if [[ "$full_path" == *"/issue/"* ]]; then
+      engine_path=$(echo "$full_path" | awk -F '/issue/' '{print $1}')
+      role=$(echo "$full_path" | awk -F '/issue/' '{print $2}')
+
+      if vault read -format=json "$engine_path/roles/$role" > /dev/null 2>&1; then
+        exit 0
+      fi
+    else
+      # Try listing the parent path
+      parent_path=$(dirname "$full_path")
+      if [ "$parent_path" == "." ]; then
+        parent_path=""
+      fi
+
+      output=$(vault list -format=json "$parent_path" 2>&1)
+      if [[ "$output" == *"$full_path"* || "$output" == *"listing is not allowed"* ]]; then
+        exit 0
+      elif vault kv get $1 > /dev/null 2>&1; then
+        exit 0
+      fi
+    fi
+
+    exit 1
+  '';
+  # checkVaultPath = pkgs.writeShellScriptBin "check-vault-path" ''
+  #   full_path="$1"
+  #
+  #   # Check for PKI engines
+  #   if [[ "$full_path" == *"/issue/"* ]]; then
+  #     engine_path=$(echo "$full_path" | awk -F '/issue/' '{print $1}')
+  #     role=$(echo "$full_path" | awk -F '/issue/' '{print $2}')
+  #     
+  #     if vault read -format=json "$engine_path/roles/$role" > /dev/null 2>&1; then
+  #       exit 0
+  #     fi
+  #   else
+  #     if vault kv get $1 > /dev/null 2>&1; then
+  #       exit 0
+  #     fi
+  #   fi
+  #
+  #   exit 1
+  # '';
   # test = builtins.toJSON (findVaultPaths 8 config.campground);
   getVaultPaths = pkgs.writeShellScriptBin "get-vault-paths" ''
     # Create empty JSON object
@@ -53,20 +100,6 @@ let
     done
 
     echo "$outputJson"
-  '';
-
-  checkVaultPath = pkgs.writeShellScriptBin "check-vault-path" ''
-
-    full_path="$1"
-
-    if vault kv get $1 > /dev/null 2>&1; then
-      # echo "KV1 exists"
-      exit 0
-    fi
-
-    # echo "Path does not exist."
-    exit 1
-
   '';
 
 in
