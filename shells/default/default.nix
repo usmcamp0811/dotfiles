22bf898541b6c6cfc7c35cfe2056595e24b9b991
Matt Camp <matt@aicampground.com>
2023-10-29 11:43:37 -0500

    wip

--- shells/default/default.nix
+++ shells/default/default.nix
@@ -101,7 +101,22 @@ let
 
     echo "$outputJson"
   '';
-
+  pypkgs-build-requirements = {
+    pandas = [ "versioneer" ];
+  };
+  p2n-overrides = pkgs.poetry2nix.defaultPoetryOverrides.extend (self: super:
+    builtins.mapAttrs (package: build-requirements:
+      (builtins.getAttr package super).overridePythonAttrs (old: {
+        buildInputs = (old.buildInputs or [ ]) ++ (builtins.map (pkg: if builtins.isString pkg then builtins.getAttr pkg super else pkg) build-requirements);
+      })
+    ) pypkgs-build-requirements
+  );
+  devshell-python = pkgs.poetry2nix.mkPoetryEnv  {
+    projectDir = ./.;
+    python = pkgs.python3;
+    overrides = p2n-overrides;
+    preferWheels = true;
+  };
 in
 mkShell {
   buildInputs = [
@@ -117,6 +132,7 @@ mkShell {
     pkgs.statix
     getVaultPaths
     checkVaultPath
+    devshell-python
   ];
 
   shellHook = ''
