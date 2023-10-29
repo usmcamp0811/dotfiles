3c65f6fdc8c90505166aadc0187a611a57ef5221
Matt Camp <matt@aicampground.com>
2023-10-29 08:41:31 -0500

    getting there

--- shells/default/default.nix
+++ shells/default/default.nix
@@ -9,7 +9,10 @@ with lib;
 with lib.campground;
 let 
   # test = builtins.toJSON (findVaultPaths 8 config.campground);
-  getVaultPaths = pkgs.writeShellScriptBin  "get-vault-paths" ''
+  getVaultPaths = pkgs.writeShellScriptBin "get-vault-paths" ''
+    # Create empty JSON object
+    outputJson="{}"
+
     # Fetch list of systems
     systems=$(nix repl 2>/dev/null <<EOF
     :lf .
@@ -17,33 +20,39 @@ let
     EOF
     )
 
-    # Strip ANSI escape codes and remove unwanted characters
+    # Clean the list
     systems=$(echo "$systems" | sed 's/\x1b\[[0-9;]*m//g' | tr -d '[],"' | tr '\n' ' ')
-
-    echo "SYS: $systems"
-
-    # Initialize empty array to collect all paths
-    allPaths=()
 
     # Loop through each system and fetch paths
     for system in $systems; do
-        result=$(nix repl 2>/dev/null <<EOF
+      result=$(nix repl 2>/dev/null <<EOF
     :lf .
     lib.findVaultPaths 3 outputs.nixosConfigurations.$system.config.campground
     EOF
-        )
-        
-        # Strip ANSI escape codes and remove unwanted characters
-        result=$(echo "$result" | sed 's/\x1b\[[0-9;]*m//g' | tr -d '[],"' | tr '\n' ' ')
-        
-        # Add to allPaths
-        allPaths+=($result)
+      )
+
+      # Clean the list
+      result=$(echo "$result" | sed 's/\x1b\[[0-9;]*m//g' | tr -d '[],"' | tr '\n' ' ')
+
+      # Initialize an empty array for path checks
+      pathChecks=[]
+
+      # Check each path with check-vault-path
+      for path in $result; do
+        checkResult=$(${checkVaultPath}/bin/check-vault-path "$path"; echo $?)
+        pathExistObj="{\"path\": \"$path\", \"exists\": $checkResult}"
+        pathChecks+=("$pathExistObj")
+      done
+
+      # Convert pathChecks array to JSON array string
+      pathChecksJson=$(printf "%s\n" "''${pathChecks[@]}" | ${pkgs.jq}/bin/jq -c -s '.')
+
+      # Add to output JSON
+      outputJson=$(echo "$outputJson" | ${pkgs.jq}/bin/jq --arg system "$system" --argjson paths "$pathChecksJson" '. + {($system): $paths}')
+
     done
 
-    # Dedupe the list and save it as a variable
-    uniquePaths=$(echo "''${allPaths[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' ')
-
-    echo "$uniquePaths"
+    echo "$outputJson"
   '';
 
   checkVaultPath = pkgs.writeShellScriptBin "check-vault-path" ''
@@ -51,11 +60,11 @@ let
     full_path="$1"
 
     if vault kv get $1 > /dev/null 2>&1; then
-      echo "KV1 exists"
+      # echo "KV1 exists"
       exit 0
     fi
 
-    echo "Path does not exist."
+    # echo "Path does not exist."
     exit 1
 
   '';
