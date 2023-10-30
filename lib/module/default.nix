f4938b7bfa8c1803ec4875355fd5091fa657a548
Matt Camp <matt@aicampground.com>
2023-10-29 19:34:44 -0500

    need to remove user-secrets base

--- lib/module/default.nix
+++ lib/module/default.nix
@@ -60,26 +60,36 @@ with lib; rec {
     enable = false;
   };
 
-  findVaultPaths = depth: cfg:
-    if depth <= 0 then []
-    else
-      let
-        isAttrs = x: builtins.isAttrs x && !builtins.isFunction x;
-        tryRecurse = x:
-          let res = builtins.tryEval (findVaultPaths (depth - 1) x);
-          in if res.success then res.value else [];
-      in
-        if isAttrs cfg then
-          builtins.foldl' (acc: key:
-            let
-              value = cfg.${key};
-              res = builtins.tryEval value;
-            in
-              if res.success then
-                if isAttrs res.value then acc ++ (tryRecurse res.value)
-                else if key == "vault-path" && cfg.enable or false then acc ++ [res.value]
-                else acc
-              else acc
-          ) [] (builtins.attrNames cfg)
+findVaultPaths = depth: cfg:
+  if depth <= 0 then []
+  else
+    let
+      isAttrs = x: builtins.isAttrs x && !builtins.isFunction x;
+      tryRecurse = x:
+        let res = builtins.tryEval (findVaultPaths (depth - 1) x);
+        in if res.success then res.value else [];
+      getSecretPaths = attr:
+        if builtins.hasAttr "user-secrets" attr && attr.user-secrets.enable then
+          let
+            baseVaultPath = attr.user-secrets.vault-path or "";
+            userNames = builtins.attrNames attr.user-secrets.users or [];
+          in
+            builtins.map (username: "${baseVaultPath}/${username}") userNames
         else [];
+    in
+      if isAttrs cfg then
+        builtins.foldl' (acc: key:
+          let
+            value = cfg.${key};
+            res = builtins.tryEval value;
+          in
+            if res.success then
+              if isAttrs res.value then 
+                acc ++ (tryRecurse res.value)
+              else if key == "vault-path" && cfg.enable or false then 
+                acc ++ [res.value]
+              else acc
+            else acc
+        ) (getSecretPaths cfg) (builtins.attrNames cfg)
+      else [];
 }
