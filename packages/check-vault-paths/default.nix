bd495ac9be583c58402cb4a143b9525dc0fb1d13
Matt Camp <matt@aicampground.com>
2023-10-30 19:43:20 -0500

    wip

--- packages/check-vault-paths/default.nix
+++ packages/check-vault-paths/default.nix
@@ -17,37 +17,41 @@ let
   description = "A thing to check Vault to see if all the paths in the Flake are good";
 
   version = "1.0.0";
+  checkVaultPath = import ./checkVaultPath.nix { inherit pkgs; };
+  # getVaultPaths  = import ./getVaultPaths.nix  { inherit pkgs checkVaultPath; };
+  #
+  # devshell-python = import ./python-env.nix  { inherit pkgs; };
 
-  checkVaultPath = pkgs.writeShellScriptBin "check-vault-path" ''
-    full_path="$1"
-
-    # Check for PKI engines
-    if [[ "$full_path" == *"/issue/"* ]]; then
-      engine_path=$(echo "$full_path" | awk -F '/issue/' '{print $1}')
-      role=$(echo "$full_path" | awk -F '/issue/' '{print $2}')
-
-      if vault read -format=json "$engine_path/roles/$role" > /dev/null 2>&1; then
-        exit 0
-      fi
-    else
-      # Try listing the parent path
-      parent_path=$(dirname "$full_path")
-      if [ "$parent_path" == "." ]; then
-        parent_path=""
-      fi
-
-      output=$(vault list -format=json "$parent_path" 2>&1)
-      if [[ "$output" == *"$full_path"* || "$output" == *"listing is not allowed"* ]]; then
-        exit 0
-      elif vault kv get $1 > /dev/null 2>&1; then
-        exit 0
-      elif vault kv list $1/ > /dev/null 2>&1; then
-        exit 0
-      fi
-    fi
-
-    exit 1
-  '';
+  # checkVaultPath = pkgs.writeShellScriptBin "check-vault-path" ''
+  #   full_path="$1"
+  #
+  #   # Check for PKI engines
+  #   if [[ "$full_path" == *"/issue/"* ]]; then
+  #     engine_path=$(echo "$full_path" | awk -F '/issue/' '{print $1}')
+  #     role=$(echo "$full_path" | awk -F '/issue/' '{print $2}')
+  #
+  #     if vault read -format=json "$engine_path/roles/$role" > /dev/null 2>&1; then
+  #       exit 0
+  #     fi
+  #   else
+  #     # Try listing the parent path
+  #     parent_path=$(dirname "$full_path")
+  #     if [ "$parent_path" == "." ]; then
+  #       parent_path=""
+  #     fi
+  #
+  #     output=$(vault list -format=json "$parent_path" 2>&1)
+  #     if [[ "$output" == *"$full_path"* || "$output" == *"listing is not allowed"* ]]; then
+  #       exit 0
+  #     elif vault kv get $1 > /dev/null 2>&1; then
+  #       exit 0
+  #     elif vault kv list $1/ > /dev/null 2>&1; then
+  #       exit 0
+  #     fi
+  #   fi
+  #
+  #   exit 1
+  # '';
 getVaultPaths = pkgs.writeShellScriptBin "get-vault-paths" ''
     # Create empty JSON object
     outputJson="{}"
---
+++ packages/check-vault-paths/getVaultPaths.nix
@@ -0,0 +1,46 @@
+{ pkgs, checkVaultPath }:
+pkgs.writeShellScriptBin "get-vault-paths" ''
+    # Create empty JSON object
+    outputJson="{}"
+
+    # Fetch list of systems
+    systems=$(nix repl 2>/dev/null <<EOF
+    :lf .
+    builtins.attrNames outputs.nixosConfigurations
+    EOF
+    )
+
+    # Clean the list
+    systems=$(echo "$systems" | sed 's/\x1b\[[0-9;]*m//g' | tr -d '[],"' | tr '\n' ' ')
+
+    # Loop through each system and fetch paths
+    for system in $systems; do
+      # Reset the pathChecks for each system
+      pathChecks=()
+
+      result=$(nix repl 2>/dev/null <<EOF
+    :lf .
+    lib.findVaultPaths 3 outputs.nixosConfigurations.$system.config.campground
+    EOF
+      )
+
+      # Clean the list
+      result=$(echo "$result" | sed 's/\x1b\[[0-9;]*m//g' | tr -d '[],"' | tr '\n' ' ')
+
+      # Check each path with check-vault-path
+      for path in $result; do
+        checkResult=$(${checkVaultPath}/bin/check-vault-path "$path"; echo $?)
+        pathExistObj="{\"path\": \"$path\", \"exists\": $checkResult}"
+        pathChecks+=("$pathExistObj")
+      done
+
+      # Convert pathChecks array to JSON array string
+      pathChecksJson=$(printf "%s\n" "''${pathChecks[@]}" | ${pkgs.jq}/bin/jq -c -s '.')
+
+      # Add to output JSON
+      outputJson=$(echo "$outputJson" | ${pkgs.jq}/bin/jq --arg system "$system" --argjson paths "$pathChecksJson" '. + {($system): $paths}')
+
+    done
+
+    echo "$outputJson"
+  ''
