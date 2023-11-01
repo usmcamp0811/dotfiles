d77a462df096464a8e170d4072bd5ea8cd3cd14a
Matt Camp <matt@aicampground.com>
2023-10-31 20:54:25 -0500

    adding peperless wip

---
+++ modules/nixos/services/paperless/default.nix
@@ -0,0 +1,126 @@
+{ lib, config, pkgs, ... }:
+with lib;
+with lib.campground;
+let
+  cfg = config.campground.services.paperless;
+in
+{
+  options.campground.services.paperless = with types; {
+    enable = mkBoolOpt false "Enable Mattermost;";
+    dataDir = mkOpt str "/var/lib/paperless" "Location to store data";
+    mediaDir = mkOpt str "var/lib/paperless/media" "Location to store media";
+    consumptionDir = mkOpt str "/var/lib/paperless/consume" "Place to import files from";
+    address = mkOpt str "localhost" "Host address";
+    gotenbergPort = mkOpt str "3000" "Gotenberg Port";
+    tikaPort = mkOpt str "9998" "Tika Port";
+    domainName = mkOpt str "https://docs.lan.aicampground.com" "domain to use";
+    role-id = mkOpt str config.campground.services.vault-agent.settings.vault.role-id "Absolute path to the Vault role-id";
+    secret-id = mkOpt str config.campground.services.vault-agent.settings.vault.secret-id "Absolute path to the Vault secret-id";
+    vault-path = mkOpt str "secret/campground/paperless" "The Vault path to the KV containing the KVs that are for each database";
+    kvVersion = mkOption {
+      type = enum ["v1" "v2"];
+      default = "v2";
+      description = "KV store version";
+    };
+    vault-address = mkOption {
+      type = str;
+      default = config.campground.services.vault-agent.settings.vault.address;
+      description = "The address of your Vault";
+    };
+  };
+
+  config = mkIf cfg.enable {
+
+    campground.services.postgresql = {
+      enable = true;
+      # TODO: configure authentication in a way that its set here and doesn't break other places
+      # authentication = ''
+      #   local all root trust
+      #   local all postgres peer
+      #   local vaultwarden vaultwarden trust
+      #   local paperless paperless trust
+      #   host  all  all  0.0.0.0/0  reject
+      #   host  all  all  ::0/0  reject
+      # '';
+      databases = [ 
+        { 
+          name = "paperless"; 
+          user = "paperless"; 
+        } 
+      ];
+    };
+
+    services.paperless = {
+      enable = true;
+      dataDir = cfg.dataDir;
+      mediaDir = cfg.mediaDir;
+      consumptionDir = cfg.consumptionDir;
+      passwordFile = "/tmp/detsys-vault/paperless.env"; 
+      address = "0.0.0.0";
+      port = 28981;
+      user = "paperless";
+      group = "paperless";
+      package = pkgs.paperless-ngx;
+      extraConfig = {
+        PAPERLESS_URL = cfg.domainName;
+
+        PAPERLESS_OCR_LANGUAGE = "eng";
+        PAPERLESS_TASK_WORKERS = 4;
+        PAPERLESS_THREADS_PER_WORKER = 8;
+
+        PAPERLESS_TIKA_ENABLED = true;
+        PAPERLESS_TIKA_ENDPOINT = "http://127.0.0.1:${cfg.tikaPort}";
+        PAPERLESS_TIKA_GOTENBERG_ENDPOINT = "http://127.0.0.1:${cfg.gotenbergPort}";
+
+        PAPERLESS_EMAIL_TASK_CRON = "*/5 * * * *";
+      };
+    };
+
+    virtualisation.oci-containers.containers.gotenberg = {
+      user = "gotenberg:gotenberg";
+      image = "gotenberg/gotenberg:7.8.1";
+
+      cmd = ["gotenberg" "--chromium-disable-javascript=true" "--chromium-allow-list=file:///tmp/.*"];
+
+      ports = [
+        "127.0.0.1:${gotenbergPort}:3000"
+      ];
+    };
+
+    virtualisation.oci-containers.containers.tika = {
+      image = "apache/tika:2.4.0";
+
+      ports = [
+        "127.0.0.1:${tikaPort}:9998"
+      ];
+    };
+
+    campground.services.vault-agent.services.paperless = {
+      settings = {
+        vault.address = cfg.vault-address;
+        auto_auth = {
+          method = [{
+            type = "approle";
+            config = {
+              role_id_file_path = cfg.role-id;
+              secret_id_file_path = cfg.secret-id;
+              remove_secret_id_file_after_reading = false;
+            };
+          }];
+        };
+      };
+      secrets = {
+        file = {
+          files = {
+            "paperless.env" = {
+              text = ''{{ with secret "${cfg.vault-path}" }}
+                {{ if eq "${cfg.kvVersion}" "v1" }}{{ .Data.password }}{{ else }}{{ .Data.data.password }}{{ end }}{{ end }}'';
+              permissions = "0600";
+              change-action = "restart";
+            };
+          };
+        };
+      };
+    };
+  };
+}
