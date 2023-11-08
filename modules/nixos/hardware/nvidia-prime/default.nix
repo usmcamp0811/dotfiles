f5d25ab793f765fe8cbda3a5782d45622c223a1d
Matt Camp <matt@aicampground.com>
2023-11-08 09:45:40 -0600

    add nvidia-prime

---
+++ modules/nixos/hardware/nvidia-prime/default.nix
@@ -0,0 +1,93 @@
+{ options, config, inputs, pkgs, lib, ... }:
+
+with lib;
+let
+  cfg = config.campground.hardware.nvidia-prime;
+  displaySetupScript = pkgs.writeShellScript "display_setup.sh" ''
+    #!/bin/sh
+    ${pkgs.xorg.xrandr}/bin/xrandr --setprovideroutputsource NVIDIA-G0
+    ${pkgs.xorg.xrandr}/bin/xrandr --auto
+  '';
+  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" '' 
+     export __NV_PRIME_RENDER_OFFLOAD=1 
+     export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0 
+     export __GLX_VENDOR_LIBRARY_NAME=nvidia 
+     export __VK_LAYER_NV_optimus=NVIDIA_only 
+     exec "$@" 
+   '';
+in
+{
+  options.campground.hardware.nvidia = with types; {
+    enable = mkEnableOption "Nvidia support";
+  };
+
+  config = mkIf cfg.enable {
+
+      services.udev.extraRules = ''
+        KERNEL=="nvidia*", MODE="0666"
+      '';
+
+
+      services.xserver.extraConfig = ''
+        Section "Screen"
+          Identifier "Screen-nvidia[0]"
+          Device "Device-nvidia[0]"
+        EndSection
+      '';
+
+    boot = {
+      extraModprobeConfig = ''
+        option nvidia-drm.modeset=1
+      '';
+      blacklistedKernelModules = [
+        "nouveau"
+        "rivafb"
+        "nvidiafb"
+        "rivatv"
+        "nv"
+        "uvcvideo"
+      ];
+      extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
+      kernelModules = [ "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
+      kernelParams = [
+        "nouveau.modeset=0"
+        "nohibernate"
+        "nvidia-drm.modeset=1"
+      ];
+    };
+
+    services.xserver = {
+      videoDrivers = ["nvidia"];
+      exportConfiguration = true;
+    };
+
+    hardware = {
+      nvidia = {
+        modesetting.enable = true;
+        prime = {
+          offload = {
+            enable = true;
+            enableOffloadCmd = true;
+          };
+          # sync.enable = true;
+          intelBusId = "PCI:0:2:0";
+          nvidiaBusId = "PCI:1:0:0";
+        };
+        package = config.boot.kernelPackages.nvidiaPackages.latest;
+        nvidiaPersistenced = true;
+      };
+
+      opengl = {
+        enable = true;
+        driSupport = true;
+        extraPackages = with pkgs; [
+          intel-media-driver
+          vaapiIntel
+          vaapiVdpau
+          libvdpau-va-gl
+          nvidia-vaapi-driver
+        ];
+      };
+    };
+  };
+}
