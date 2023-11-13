b54c8e0352ac70e200ea4ed74d7e70e41df94a24
Matt Camp <matt@aicampground.com>
2023-11-13 10:36:13 -0600

    add dvc

---
+++ modules/home/tools/dvc/default.nix
@@ -0,0 +1,24 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.campground;
+let cfg = config.campground.tools.dvc;
+in
+{
+  options.campground.tools.dvc = with types; {
+    enable =
+      mkBoolOpt false "Whether or not to enable common DVC.";
+  };
+
+  config = mkIf cfg.enable {
+
+    home.packages = with pkgs; [
+      dvc
+    ];
+
+    # home.sessionVariables = {
+    #   PYTHON_KEYRING_BACKEND="keyring.backends.null.Keyring";
+    #   LD_LIBRARY_PATH = "${pkgs.gcc.cc.lib}/lib:${pkgs.zlib}/lib";
+    # };
+  };
+}
