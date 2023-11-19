228f0f5a3c6aff1e323721df1a41efad2a847484
Matt Camp <matt@aicampground.com>
2023-11-19 13:42:09 -0600

    wip

--- modules/nixos/services/openvpn/default.nix
+++ modules/nixos/services/openvpn/default.nix
@@ -7,7 +7,7 @@ let
     set -e
 
     SERVER_NAME=$1
-    COMMON_NAME="''${SERVER_NAME}.client.${cfg.domain-name}"
+    COMMON_NAME="server.vpn.${cfg.domain-name}"
 
     export VAULT_ADDR=${cfg.vault-address}
 
@@ -31,7 +31,7 @@ let
     echo "Seal Status: $seal_status"
 
     if [ $seal_status = "true" ]; then
-      echo "Vault is currently sealed, cannot generate client certificats."
+      echo "Vault is currently sealed, cannot generate server certificats."
       exit 1
     fi
 
@@ -66,12 +66,12 @@ let
     
     mkdir -p /var/lib/vault/ovpn/server/
 
-    cat $CA_CERT > /var/lib/vault/ovpn/ca.crt
-    cat $SERVER_CERT > /var/lib/vault/ovpn/server.crt
-    cat $SERVER_KEY > /var/lib/vault/ovpn/server.key
-# Fix permissions
-    chown -R ovpn:ovpn /var/lib/vault/ovpn/*
-    chmod -R 0600 /var/lib/vault/ovpn
+    # generate diffie-hellman parameters
+    ${pkgs.openssl}/bin/openssl dhparam -out /var/lib/vault/ovpn/dh.pem 2048
+
+    echo "$CA_CERT" > /var/lib/vault/ovpn/ca.crt
+    echo "$SERVER_CERT" > /var/lib/vault/ovpn/server.crt
+    echo "$SERVER_KEY" > /var/lib/vault/ovpn/server.key
   '';
 
 in
@@ -105,153 +105,153 @@ in
       gen-server
       openvpn
     ];
-    # users.users.ovpn = {
-    #   isSystemUser = true;
-    #   group = "ovpn";
-    #   description = "OpenVPN service user";
-    # };
-    #
-    # users.groups.ovpn = {};
+    users.users.ovpn = {
+      isSystemUser = true;
+      group = "ovpn";
+      description = "OpenVPN service user";
+    };
 
-    # boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
+    users.groups.ovpn = {};
 
-    # networking.nat = {
-    #   enable = true;
-    #   externalInterface = "eth0";
-    #   internalInterfaces = [ "tun0" ];
-    # };
+    boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
 
-    # networking.firewall.allowedUDPPorts = [ 1194 ];
+    networking.nat = {
+      enable = true;
+      externalInterface = "eth0";
+      internalInterfaces = [ "tun0" ];
+    };
 
-    # services.openvpn.servers = {
-    #   campground = {
-    #     config = ''
-    #       port 1194
-    #       proto udp
-    #       dev tun
-    #       ca /var/lib/vault/ovpn/ca.crt
-    #       cert /var/lib/vault/ovpn/server.crt
-    #       key /var/lib/vault/ovpn/server.key
-    #       dh /var/lib/vault/ovpn/dh.pem
-    #       ifconfig-pool-persist ipp.txt
-    #       keepalive 10 120
-    #       cipher AES-256-GCM
-    #       ncp-ciphers AES-256-GCM:AES-256-CBC
-    #       topology subnet
-    #       auth SHA512
-    #       persist-key
-    #       persist-tun
-    #       status openvpn-status.log
-    #       verb 1
-    #       tls-server
-    #       tls-version-min 1.2
-    #       tls-auth /var/lib/vault/ovpn/ta.key 0
-    #       server 10.8.1.0 255.255.255.0
-    #       push "redirect-gateway def1 bypass-dhcp"
-    #       push "route 10.8.1.0 255.255.255.0"
-    #       push "dhcp-option DNS 8.8.8.8"
-    #       push "dhcp-option DNS 8.8.4.4"
-    #     '';
-    #   };
-    # };
+    networking.firewall.allowedUDPPorts = [ 1194 ];
+
+    services.openvpn.servers = {
+      campground = {
+        config = ''
+          port 1194
+          proto udp
+          dev tun
+          ca /var/lib/vault/ovpn/ca.crt
+          cert /var/lib/vault/ovpn/server.crt
+          key /var/lib/vault/ovpn/server.key
+          dh /var/lib/vault/ovpn/dh.pem
+          ifconfig-pool-persist ipp.txt
+          keepalive 10 120
+          cipher AES-256-GCM
+          ncp-ciphers AES-256-GCM:AES-256-CBC
+          topology subnet
+          auth SHA512
+          persist-key
+          persist-tun
+          status openvpn-status.log
+          verb 1
+          tls-server
+          tls-version-min 1.2
+          tls-auth /var/lib/vault/ovpn/ta.key 0
+          server 10.8.1.0 255.255.255.0
+          push "redirect-gateway def1 bypass-dhcp"
+          push "route 10.8.1.0 255.255.255.0"
+          push "dhcp-option DNS 8.8.8.8"
+          push "dhcp-option DNS 8.8.4.4"
+        '';
+      };
+    };
 
 # TODO: Refactor so that this just renews the server cert 
 # TODO: Refactor to make the `copyVPNcerts.sh` script is installed and can be run independent of the systmed service
 # TODO: Clean up or otherwise just make things look better and more uniform
 # TODO: Add OpenVPN Admin: https://github.com/flant/ovpn-admin
 # Probably just make it a package.. looks simple enough
-    # systemd.timers.genVPNserver-cert = {
-    #   description = "Timer for Generate VPN Client Certs";
-    #   wantedBy = [ "timers.target" ];
-    #   timerConfig = {
-    #     OnCalendar = "daily"; # Runs every day at midnight
-    #   };
-    #   unitConfig = {
-    #     PartOf = [ "genVPNserver-cert.service" ];
-    #   };
-    # };
-    #
-    # systemd.services.genVPNserver-cert = {
-    #   description = "Get VPN Server Certs from Vault";
-    #   serviceConfig = {
-    #     Type = "oneshot";
-    #     User = "root";
-    #     ExecStart = "${pkgs.bash}/bin/bash /tmp/detsys-vault/copyVPNcerts.sh";
-    #     after = [ "vault-agent.service" ];
-    #     before = [ "openvpn-campground.service" ];
-    #   };
-    #   wantedBy = [ "multi-user.target" ];
-    # };
+    systemd.timers.genVPNserver-cert = {
+      description = "Timer for Generate VPN Client Certs";
+      wantedBy = [ "timers.target" ];
+      timerConfig = {
+        OnCalendar = "daily"; # Runs every day at midnight
+      };
+      unitConfig = {
+        PartOf = [ "genVPNserver-cert.service" ];
+      };
+    };
+
+    systemd.services.genVPNserver-cert = {
+      description = "Get VPN Server Certs from Vault";
+      serviceConfig = {
+        Type = "oneshot";
+        User = "root";
+        ExecStart = "${pkgs.bash}/bin/bash /tmp/detsys-vault/copyVPNcerts.sh";
+        after = [ "vault-agent.service" ];
+        before = [ "openvpn-campground.service" ];
+      };
+      wantedBy = [ "multi-user.target" ];
+    };
 # TODO: Refactor this so that we rotate server certs in a similar manner as the client certs
-    # campground.services.vault-agent.services.genVPNserver-cert = {
-    #   settings = {
-    #     vault.address = cfg.vault-address;
-    #     auto_auth = {
-    #       method = [{
-    #         type = "approle";
-    #         config = {
-    #           role_id_file_path = cfg.role-id;
-    #           secret_id_file_path = cfg.secret-id;
-    #           remove_secret_id_file_after_reading = false;
-    #         };
-    #       }];
-    #     };
-    #   };
-    #   secrets = {
-    #     file = {
-    #       files = {
-    #         "copyVPNcerts.sh" = {
-    #           text = ''
-    #             #!/bin/sh
-    #             set -e  # exit immediately on error
-    #
-    #             # Create directory for VPN certificates
-    #             mkdir -p /var/lib/vault/ovpn/
-    #
-    #             # Create directory for client certificates
-    #             mkdir -p /var/lib/vault/ovpn/clients/
-    #
-    #             # Generate server.crt
-    #             cat <<EOL > /var/lib/vault/ovpn/server.crt
-    #             {{ with secret "${cfg.vault-path}" "common_name=${cfg.common-name}" }}
-    #             {{ .Data.certificate }}
-    #             {{ end }}
-    #             EOL
-    #
-    #             # Generate server.key
-    #             cat <<EOL > /var/lib/vault/ovpn/server.key
-    #             {{ with secret "${cfg.vault-path}" "common_name=${cfg.common-name}" }}
-    #             {{ .Data.private_key }}
-    #             {{ end }}
-    #             EOL
-    #
-    #             # Generate ca.crt
-    #             cat <<EOL > /var/lib/vault/ovpn/ca.crt
-    #             {{ with secret "${cfg.vault-path}" "common_name=${cfg.common-name}" }}
-    #             {{ .Data.issuing_ca }}
-    #             {{ end }}
-    #             EOL
-    #
-    #             # # Generate Diffie-Hellman parameters
-    #             # ${pkgs.openssl}/bin/openssl dhparam -out /var/lib/vault/ovpn/dh.pem 2048
-    #
-    #             # Generate tls-auth key
-    #             # generate it like this ->  openvpn --genkey secret /var/lib/vault/ovpn/ta.key and put in the vault
-    #
-    #             cat <<EOL > /var/lib/vault/ovpn/ta.key
-    #             {{ with secret "${cfg.vault-tls-path}" }}{{ if eq "${cfg.kvVersion}" "v1" }}{{ .Data.tls }}{{ else }}{{ .Data.data.tls }}{{ end }}{{ end }}
-    #             EOL
-    #
-    #             # Fix permissions
-    #             chown -R ovpn:ovpn /var/lib/vault/ovpn/*
-    #             chmod -R 0600 /var/lib/vault/ovpn
-    #           '';
-    #           permissions = "0400";
-    #           change-action = "restart";
-    #         };
-    #       };
-    #     };
-    #   };
-    /* # }; */
+    campground.services.vault-agent.services.genVPNserver-cert = {
+      settings = {
+        vault.address = cfg.vault-address;
+        auto_auth = {
+          method = [{
+            type = "approle";
+            config = {
+              role_id_file_path = cfg.role-id;
+              secret_id_file_path = cfg.secret-id;
+              remove_secret_id_file_after_reading = false;
+            };
+          }];
+        };
+      };
+      secrets = {
+        file = {
+          files = {
+            "copyVPNcerts.sh" = {
+              text = ''
+                #!/bin/sh
+                set -e  # exit immediately on error
+
+                # Create directory for VPN certificates
+                mkdir -p /var/lib/vault/ovpn/
+
+                # Create directory for client certificates
+                mkdir -p /var/lib/vault/ovpn/clients/
+
+                # Generate server.crt
+                cat <<EOL > /var/lib/vault/ovpn/server.crt
+                {{ with secret "${cfg.vault-path}" "common_name=${cfg.common-name}" }}
+                {{ .Data.certificate }}
+                {{ end }}
+                EOL
+
+                # Generate server.key
+                cat <<EOL > /var/lib/vault/ovpn/server.key
+                {{ with secret "${cfg.vault-path}" "common_name=${cfg.common-name}" }}
+                {{ .Data.private_key }}
+                {{ end }}
+                EOL
+
+                # Generate ca.crt
+                cat <<EOL > /var/lib/vault/ovpn/ca.crt
+                {{ with secret "${cfg.vault-path}" "common_name=${cfg.common-name}" }}
+                {{ .Data.issuing_ca }}
+                {{ end }}
+                EOL
+
+                # # Generate Diffie-Hellman parameters
+                # ${pkgs.openssl}/bin/openssl dhparam -out /var/lib/vault/ovpn/dh.pem 2048
+
+                # Generate tls-auth key
+                # generate it like this ->  openvpn --genkey secret /var/lib/vault/ovpn/ta.key and put in the vault
+
+                cat <<EOL > /var/lib/vault/ovpn/ta.key
+                {{ with secret "${cfg.vault-tls-path}" }}{{ if eq "${cfg.kvVersion}" "v1" }}{{ .Data.tls }}{{ else }}{{ .Data.data.tls }}{{ end }}{{ end }}
+                EOL
+
+                # Fix permissions
+                chown -R ovpn:ovpn /var/lib/vault/ovpn/*
+                chmod -R 0600 /var/lib/vault/ovpn
+              '';
+              permissions = "0400";
+              change-action = "restart";
+            };
+          };
+        };
+      };
+    };
   };
 }
