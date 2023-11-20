f25755ac89415bdb58c17a9fb0316c2c3e420de3
Matt Camp <matt@aicampground.com>
2023-11-19 19:44:12 -0600

    wip

--- modules/nixos/services/wireguard/default.nix
+++ modules/nixos/services/wireguard/default.nix
@@ -2,17 +2,22 @@
 with lib;
 with lib.campground;
 let
-  cfg = config.campground.services.openvpn;
+  cfg = config.campground.services.wireguard;
 in
 {
-  options.campground.services.openvpn = with types; {
+  options.campground.services.wireguard = with types; {
     enable = mkBoolOpt false "Enable OpenVPN Server;";
+    server = mkBoolOpt true "Is a Wireguard Server";
+    pulicKey = mkOpt str "123456789" "The server's public key";
+    endpoint = mkOpt str "vpn.aicampground.com" "VPN Domain Name / IP address.";
+    port = mkOpt int "1149" "Port to use for the VPN";
+    ips = mkOpt listOf str [ "10.100.0.2/24" "fc10:100:0::1/64" ] "List of IPs of the server end of the tunner interface.";
+    allowedIPs = mkOpt listOf str [ "10.100.0.5/32" "fc10:100:0::5/128" ] "List of IPs of the client IPs supported.";
+    postRoutCIDR = mkOpt str "10.8.0.0/24" "CIDR to route traffic to..";
+
     role-id = mkOpt str config.campground.services.vault-agent.settings.vault.role-id "Absolute path to the Vault role-id";
     secret-id = mkOpt str config.campground.services.vault-agent.settings.vault.secret-id "Absolute path to the Vault secret-id";
-    vault-path = mkOpt str "campground-pki/issue/vpn-server-role" "The Vault path to the Server Cert in Vault";
-    vault-client-path = mkOpt str "campground-pki/issue/vpn-client-role" "The Vault path to the Client Cert in Vault"; 
-    vault-ca-path = mkOpt str "campground-pki/cert/ca" "The Vault path to the CA Cert in Vault"; 
-    vault-tls-path = mkOpt str "secret/campground/vpn" "The Vault path to the TLS Key in Vault"; 
+    vault-path = mkOpt str "secret/campground/wireguard" "The Vault path to the Server Cert in Vault";
     kvVersion = mkOption {
       type = enum ["v1" "v2"];
       default = "v2";
@@ -23,92 +28,76 @@ in
       default = config.campground.services.vault-agent.settings.vault.address;
       description = "The address of your Vault";
     };
-    common-name = mkOpt str "server.vpn.${cfg.domain-name}" "Common Name for Server Certs";
-    domain-name = mkOpt str "aicampground.com" "Domain Name for Certs";
-    vpn-cert-csv = mkOpt str "/var/lib/vault/ovpn/vpn-certs.csv" "CSV with Cert Serial Numbers";
   };
 
   config = mkIf cfg.enable {
+
+    networking.firewall.allowedUDPPorts = [ cfg.port ];
+
     networking.wireguard.enable = true;
+    # TODO: Support multiple vpns
     networking.wireguard.interfaces."wg0" = {
-     privateKeyFile = "/var/lib/wireguard/wg0-priv-key";
-     ips = [ "10.10.10.1/24" "fc10:10:10::1/64" ];
-     listenPort = 1194;
-     peers = [                                                                                                                                                                                                                        
-       { publicKey = "XnBofROntu+DYUaGC8VK6t8SRpKoYuQ6GjDSZNmFME4=";
-         allowedIPs = [ "10.10.10.2/32" "fc10:10:10::2/128" ]; }
+     privateKeyFile = "/var/lib/wireguard/wg0-private-key";
+     # The port that WireGuard listens to. Must be accessible by the client.
+     listenPort = cfg.port;
+
+     # Determines the IP address and subnet of the server's end of the tunnel interface.
+     ips = cfg.ips;
+
+     # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
+     # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
+     postSetup = ''
+       ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s ${cfg.postRoutCIDR} -o eth0 -j MASQUERADE;
+     '';
+
+     # This undoes the above command
+     postShutdown = ''
+       ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s ${cfg.postRoutCIDR} -o eth0 -j MASQUERADE
+     '';
+     peers = [
+       { publicKey = cfg.publicKey;
+         allowedIPs = cfg.allowedIPs;
+       }
      ];
     };
-# TODO: Refactor this so that we rotate server certs in a similar manner as the client certs
-    # campground.services.vault-agent.services.genVPNserver-cert = {
-    #   settings = {
-    #     vault.address = cfg.vault-address;
-    #     auto_auth = {
-    #       method = [{
-    #         type = "approle";
-    #         config = {
-    #           role_id_file_path = cfg.role-id;
-    #           secret_id_file_path = cfg.secret-id;
-    #           remove_secret_id_file_after_reading = false;
-    #         };
-    #       }];
-    #     };
-    #   };
-    #   secrets = {
-    #     file = {
-    #       files = {
-    #         "copyVPNcerts.sh" = {
-    #           text = ''
-    #             #!/bin/sh
-    #             set -e  # exit immediately on error
-    #
-    #             # Create directory for VPN certificates
-    #             mkdir -p /var/lib/vault/ovpn/
-    #
-    #             # Create directory for client certificates
-    #             mkdir -p /var/lib/vault/ovpn/clients/
-    #
-    #             # Generate server.crt
-    #             cat <<EOL > /var/lib/vault/ovpn/server.crt
-    #             {{ with secret "${cfg.vault-path}" "common_name=${cfg.common-name}" }}
-    #             {{ .Data.certificate }}
-    #             {{ end }}
-    #             EOL
-    #
-    #             # Generate server.key
-    #             cat <<EOL > /var/lib/vault/ovpn/server.key
-    #             {{ with secret "${cfg.vault-path}" "common_name=${cfg.common-name}" }}
-    #             {{ .Data.private_key }}
-    #             {{ end }}
-    #             EOL
-    #
-    #             # Generate ca.crt
-    #             cat <<EOL > /var/lib/vault/ovpn/ca.crt
-    #             {{ with secret "${cfg.vault-path}" "common_name=${cfg.common-name}" }}
-    #             {{ .Data.issuing_ca }}
-    #             {{ end }}
-    #             EOL
-    #
-    #             # # Generate Diffie-Hellman parameters
-    #             # ${pkgs.openssl}/bin/openssl dhparam -out /var/lib/vault/ovpn/dh.pem 2048
-    #
-    #             # Generate tls-auth key
-    #             # generate it like this ->  openvpn --genkey secret /var/lib/vault/ovpn/ta.key and put in the vault
-    #
-    #             cat <<EOL > /var/lib/vault/ovpn/ta.key
-    #             {{ with secret "${cfg.vault-tls-path}" }}{{ if eq "${cfg.kvVersion}" "v1" }}{{ .Data.tls }}{{ else }}{{ .Data.data.tls }}{{ end }}{{ end }}
-    #             EOL
-    #
-    #             # Fix permissions
-    #             chown -R ovpn:ovpn /var/lib/vault/ovpn/*
-    #             chmod -R 0600 /var/lib/vault/ovpn
-    #           '';
-    #           permissions = "0400";
-    #           change-action = "restart";
-    #         };
-    #       };
-    #     };
-    #   };
-    # };
+    campground.services.vault-agent.services.genVPNserver-cert = {
+      settings = {
+        vault.address = cfg.vault-address;
+        auto_auth = {
+          method = [{
+            type = "approle";
+            config = {
+              role_id_file_path = cfg.role-id;
+              secret_id_file_path = cfg.secret-id;
+              remove_secret_id_file_after_reading = false;
+            };
+          }];
+        };
+      };
+      secrets = {
+        file = {
+          files = {
+            "getWireguardKeys.sh" = {
+              text = ''
+                #!/bin/sh
+                set -e  # exit immediately on error
+
+                # Create directory for VPN certificates
+                mkdir -p /var/lib/wireguard
+
+                cat <<EOL > /var/lib/wireguard/wg0-private-key
+                {{ with secret "${cfg.vault-path}" }}{{ if eq "${cfg.kvVersion}" "v1" }}{{ .Data.privateKey }}{{ else }}{{ .Data.data.privateKey }}{{ end }}{{ end }}
+                EOL
+
+                # Fix permissions
+                chmod -R 0600 /var/lib/wireguard
+              '';
+              permissions = "0400";
+              change-action = "restart";
+            };
+          };
+        };
+      };
+    };
   };
 }
