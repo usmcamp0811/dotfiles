978478f569809f7dd03c7f7c5ea1e133bb5a175c
Matt Camp <matt@aicampground.com>
2023-12-04 22:25:00 -0600

    wip

--- overlays/poetry2nix/default.nix
+++ overlays/poetry2nix/default.nix
@@ -1,8 +1,17 @@
 { poetry2nix, ... }:
 
-final: prev:
+self: super: {
 
-{
-  poetry2nix = poetry2nix.packages.${prev.system}.default;
+  # p2self & p2super refers to poetry2nix
+  poetry2nix = super.poetry2nix.overrideScope' (p2nixself: p2nixsuper: {
+
+    # pyself & pysuper refers to python packages
+    defaultPoetryOverrides = p2nixsuper.defaultPoetryOverrides.extend (pyself: pysuper: {
+
+      my-custom-pkg = super.my-custom-pkg.overridePythonAttrs (oldAttrs: { });
+
+    });
+
+  });
 }
 
