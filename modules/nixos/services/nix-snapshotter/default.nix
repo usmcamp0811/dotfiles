8bd0b45b125ebe6a0b47e65b3ed39a9349d00c2c
Matt Camp <matt@aicampground.com>
2023-12-23 19:02:16 -0600

    builds

--- modules/nixos/services/nix-snapshotter/default.nix
+++ modules/nixos/services/nix-snapshotter/default.nix
@@ -1,18 +1,26 @@
-{ lib, config, pkgs, ... }:
+{ inputs, lib, config, pkgs, ... }:
 with lib;
 with lib.campground;
 let
   cfg = config.campground.services.nix-snapshotter;
 in
 {
+  imports = [
+    inputs.nix-snapshotter.nixosModules.default
+  ];
+
   options.campground.services.nix-snapshotter = with types; {
     enable = mkBoolOpt false "Enable Nix Snapshotter;";
   };
 
   config = mkIf cfg.enable {
+
     services.nix-snapshotter = {
       enable = true;
       setContainerdSnapshotter = true;
     };
+
+    environment.systemPackages = [ pkgs.nerdctl ];
+
   };
 }
--- overlays/nix-snapshotter/default.nix
+++ overlays/nix-snapshotter/default.nix
@@ -3,6 +3,7 @@
 final: prev:
 
 {
+  # nix-snapshotter = nix-snapshotter.overlays.default;
   nix-snapshotter = nix-snapshotter.packages.${prev.system}.nix-snapshotter;
 }
 
