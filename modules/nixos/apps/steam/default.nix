42f6e9f39e5697486333dbda27a769226cb3f4fb
Matt Camp <matt@aicampground.com>
2023-12-27 19:03:32 -0600

    add steam

---
+++ modules/nixos/apps/steam/default.nix
@@ -0,0 +1,30 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.campground;
+let
+  cfg = config.campground.apps.steam;
+in
+{
+  options.campground.apps.steam = with types; {
+    enable = mkBoolOpt false "Whether or not to enable support for Steam.";
+  };
+
+  config = mkIf cfg.enable {
+    programs.steam.enable = true;
+    programs.steam.remotePlay.openFirewall = true;
+
+    hardware.steam-hardware.enable = true;
+
+    # Enable GameCube controller support.
+    services.udev.packages = [ pkgs.dolphinEmu ];
+
+    environment.systemPackages = with pkgs.campground; [
+      steam
+    ];
+
+    environment.sessionVariables = {
+      STEAM_EXTRA_COMPAT_TOOLS_PATHS = "$HOME/.steam/root/compatibilitytools.d";
+    };
+  };
+}
