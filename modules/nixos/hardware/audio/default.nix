ac549c43de81fe36a33bb9c651b7eb881e94d1cc
Matt Camp <matt@aicampground.com>
2023-12-27 20:23:08 -0600

    go back to pipewire audio

--- modules/nixos/hardware/audio/default.nix
+++ modules/nixos/hardware/audio/default.nix
@@ -123,39 +123,39 @@ in
     security.rtkit.enable = true;
 
     services.pipewire = {
-      enable = false;
-      # alsa.enable = true;
-      # pulse.enable = true;
-      # jack.enable = true;
+      enable = true;
+      alsa.enable = true;
+      pulse.enable = true;
+      jack.enable = true;
 
-      # wireplumber.enable = true;
+      wireplumber.enable = true;
     };
 
-    environment.etc = {
-      "pipewire/pipewire.conf.d/10-pipewire.conf".source =
-        pkgs.writeText "pipewire.conf" (builtins.toJSON pipewire-config);
-      # "pipewire/pipewire.conf.d/21-alsa.conf".source =
-      #   pkgs.writeText "pipewire.conf" (builtins.toJSON alsa-config);
-      #
-      # "wireplumber/wireplumber.conf".source =
-      #   pkgs.writeText "pipewire.conf" (builtins.toJSON pipewire-config);
-      #
-      # "wireplumber/scripts/config.lua.d/alsa.lua".text = ''
-      #   local input = ${lua-format.generate "sample.lua" cfg.alsa-monitor}
-      #
-      #   if input.rules == nil then
-      #    input.rules = {}
-      #   end
-      #
-      #   local rules = input.rules
-      #
-      #   for _, rule in ipairs(input.rules) do
-      #     table.insert(alsa_monitor.rules, rule)
-      #   end
-      # '';
-    };
+    # environment.etc = {
+    #   "pipewire/pipewire.conf.d/10-pipewire.conf".source =
+    #     pkgs.writeText "pipewire.conf" (builtins.toJSON pipewire-config);
+    #   # "pipewire/pipewire.conf.d/21-alsa.conf".source =
+    #   #   pkgs.writeText "pipewire.conf" (builtins.toJSON alsa-config);
+    #   #
+    #   # "wireplumber/wireplumber.conf".source =
+    #   #   pkgs.writeText "pipewire.conf" (builtins.toJSON pipewire-config);
+    #   #
+    #   # "wireplumber/scripts/config.lua.d/alsa.lua".text = ''
+    #   #   local input = ${lua-format.generate "sample.lua" cfg.alsa-monitor}
+    #   #
+    #   #   if input.rules == nil then
+    #   #    input.rules = {}
+    #   #   end
+    #   #
+    #   #   local rules = input.rules
+    #   #
+    #   #   for _, rule in ipairs(input.rules) do
+    #   #     table.insert(alsa_monitor.rules, rule)
+    #   #   end
+    #   # '';
+    # };
 
-    hardware.pulseaudio.enable = mkForce true;
+    hardware.pulseaudio.enable = mkForce false;
 
     environment.systemPackages = with pkgs; [
       pulsemixer
