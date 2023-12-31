c570d06896fba797cf6e75d90c032db1b900954f
Matt Camp <matt@aicampground.com>
2023-12-30 20:40:12 -0600

    forgot to add abe

---
+++ homes/x86_64-linux/abe@reckless/default.nix
@@ -0,0 +1,24 @@
+{ lib, pkgs, config, osConfig ? { }, format ? "unknown", ... }:
+
+with lib.campground;
+{
+  campground = {
+    user = {
+      enable = true;
+      name = config.snowfallorg.user.name;
+      # name = "abe";
+    };
+
+    cli = {
+      zsh = enabled;
+    #   neovim = enabled;
+      home-manager = enabled;
+    };
+    #
+    # tools = {
+    #   git = enabled;
+    #   direnv = enabled;
+    # };
+  };
+  home.stateVersion = "23.05";
+}
