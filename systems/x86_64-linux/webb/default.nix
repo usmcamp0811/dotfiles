5c8852d0b72a32afcb1a2634e08f47aacb1b531b
Matt Camp <matt@aicampground.com>
2024-01-01 21:29:13 -0600

    lucas is being dumb.. and adidng chesty

--- systems/x86_64-linux/webb/default.nix
+++ systems/x86_64-linux/webb/default.nix
@@ -143,8 +143,9 @@ in
         tang-servers = [ 
           "http://daly:1234" 
           # "http://mattis:1234" 
-          "http://lucas:1234" 
+          # "http://lucas:1234" 
           "http://reckless:1234"
+          "http://chesty:1234"
           "http://ermy:1234" 
         ];
       };
