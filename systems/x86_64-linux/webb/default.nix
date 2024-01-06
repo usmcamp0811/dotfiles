dbf69147ee22e869aa27ba2f47e76e3c8d7c6078
Matt Camp <matt@aicampground.com>
2024-01-05 21:51:00 -0600

    wip

--- systems/x86_64-linux/webb/default.nix
+++ systems/x86_64-linux/webb/default.nix
@@ -17,6 +17,7 @@ in
   campground = {
     archetypes.workstation = enabled;
     tools.icehouse = enabled;
+    nfs.client = enabled;
     # nfs.server = enabled;
     # nfs.campfs = enabled;
     # nfs.chestyfs = enabled;
