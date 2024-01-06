c00856f71278090206001a0d1c0cbe140fe37a5b
Matt Camp <matt@aicampground.com>
2024-01-06 13:50:34 -0600

    nfs wip

--- systems/x86_64-linux/reckless/hardware.nix
+++ systems/x86_64-linux/reckless/hardware.nix
@@ -134,10 +134,10 @@
   networking.firewall.allowedUDPPorts = [ 2049 20048 ];
   services.nfs.server.enable = true;
   services.nfs.server.exports = ''
-    /export/media 10.8.0.1/24(rw,nohide,insecure,no_subtree_check) /export/media 172.16.10.190(rw,nohide,insecure,no_subtree_check)
-    /export/media/movies 10.8.0.1/24(rw,nohide,insecure,no_subtree_check) /export/media/movies 172.16.10.190(rw,nohide,insecure,no_subtree_check)
-    /export/media/tv-shows 10.8.0.1/24(rw,nohide,insecure,no_subtree_check) /export/media/tv-shows 172.16.10.190(rw,nohide,insecure,no_subtree_check)
-    /export/media/audiobooks 10.8.0.1/24(rw,nohide,insecure,no_subtree_check) /export/media/audiobooks 172.16.10.190(rw,nohide,insecure,no_subtree_check)
-    /export/media/music 10.8.0.1/24(rw,nohide,insecure,no_subtree_check) /export/media/music 172.16.10.190(rw,nohide,insecure,no_subtree_check)
+    /export/media 10.8.0.1/24(rw,nohide,insecure,no_subtree_check) 172.16.10.190(rw,nohide,insecure,no_subtree_check)
+    /export/media/movies 10.8.0.1/24(rw,nohide,insecure,no_subtree_check) 172.16.10.190(rw,nohide,insecure,no_subtree_check)
+    /export/media/tv-shows 10.8.0.1/24(rw,nohide,insecure,no_subtree_check) 172.16.10.190(rw,nohide,insecure,no_subtree_check)
+    /export/media/audiobooks 10.8.0.1/24(rw,nohide,insecure,no_subtree_check) 172.16.10.190(rw,nohide,insecure,no_subtree_check)
+    /export/media/music 10.8.0.1/24(rw,nohide,insecure,no_subtree_check) 172.16.10.190(rw,nohide,insecure,no_subtree_check)
   '';
 }
