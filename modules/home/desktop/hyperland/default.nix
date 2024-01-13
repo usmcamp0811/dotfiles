f95067de9c4dc1075c9ec2f4c10d0dc5adfbadd0
Matt Camp <matt@aicampground.com>
2024-01-13 17:12:27 -0600

    wip

--- modules/home/desktop/hyperland/default.nix
+++ modules/home/desktop/hyperland/default.nix
@@ -12,18 +12,37 @@ in
 
   config = mkIf cfg.enable {
     wayland.windowManager.hyprland = {
-      # Whether to enable Hyprland wayland compositor
       enable = true;
-      # The hyprland package to use
-      package = pkgs.hyprland;
-      # Whether to enable XWayland
+      extraConfig = /* bash */ ''
+        ${cfg.prependConfig}
+        env = XDG_DATA_DIRS,'${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}':$XDG_DATA_DIRS
+        env = HYPRLAND_TRACE,1
+        ${cfg.appendConfig}
+      '';
+      package = hyprland.packages.${system}.hyprland;
+      settings = {
+        exec = [
+          "${getExe pkgs.libnotify} --icon ~/.face -u normal \"Hello $(whoami)\""
+        ];
+      };
+      systemd = {
+        enable = true;
+      };
       xwayland.enable = true;
-
-      # Optional
-      # Whether to enable hyprland-session.target on hyprland startup
-      systemd.enable = true;
-      # Whether to enable patching wlroots for better Nvidia support
-      enableNvidiaPatches = true;
     };
+    # wayland.windowManager.hyprland = {
+    #   # Whether to enable Hyprland wayland compositor
+    #   enable = true;
+    #   # The hyprland package to use
+    #   package = pkgs.hyprland;
+    #   # Whether to enable XWayland
+    #   xwayland.enable = true;
+    #
+    #   # Optional
+    #   # Whether to enable hyprland-session.target on hyprland startup
+    #   systemd.enable = true;
+    #   # Whether to enable patching wlroots for better Nvidia support
+    #   enableNvidiaPatches = true;
+    # };
   };
 }
