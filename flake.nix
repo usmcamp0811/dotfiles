afec5331d64094d335c5d4069d5056ba3cdd6c66
Matt Camp <matt@aicampground.com>
2024-01-13 15:26:27 -0600

    wip

--- flake.nix
+++ flake.nix
@@ -23,20 +23,20 @@
     # Hyprland
     hyprland = {
       url = "github:hyprwm/Hyprland";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "unstable";
     };
 
     # Hyprland user contributions flake
     hyprland-contrib = {
       url = "github:hyprwm/contrib";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "unstable";
     };
 
 
     # NixPkgs-Wayland
     nixpkgs-wayland = {
       url = "github:nix-community/nixpkgs-wayland";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "unstable";
     };
     campground-nvim.url = "gitlab:usmcamp0811/campground-nvim";
     # campground-nvim.url = "path:/home/mcamp/code/campground-nvim";
--- modules/nixos/desktop/hyperland/default.nix
+++ modules/nixos/desktop/hyperland/default.nix
@@ -10,6 +10,8 @@ with lib;
 with lib.campground;
 let
   inherit (inputs) hyprland;
+  inherit (inputs) nixpkgs-wayland;
+
 
   cfg = config.campground.desktop.hyprland;
   programs = lib.makeBinPath [ config.programs.hyprland.package ];
@@ -46,8 +48,18 @@ in
 
         environment.systemPackages = with pkgs; [
           hyprpaper
+          cliphist
+          swayimg
+          nixpkgs-wayland.packages.${system}.wdisplays
+          wl-screenrec
+          nixpkgs-wayland.packages.${system}.wl-clipboard
+          wlr-randr
+          # Not really wayland specific, but I don't want to make a new module for it
+          brightnessctl
+          glib # for gsettings
+          gtk3.out # for gtk-launch
+          playerctl
         ];
-
         # campground = {
         #   # apps = {
         #   #   partitionmanager = enabled;
