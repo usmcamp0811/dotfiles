1ad1572bb39a8ef943b533ab1637718e52203df5
Matt Camp <matt@aicampground.com>
2024-01-14 13:22:16 -0600

    addinggame mode

---
+++ modules/nixos/apps/gamemode/default.nix
@@ -0,0 +1,49 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.campground;
+let
+  cfg = config.campground.apps.gamemode;
+
+  defaultStartScript = ''
+    ${getExe' pkgs.libnotify "notify-send"} 'GameMode started'
+  '';
+
+  defaultEndScript = ''
+    ${getExe' pkgs.libnotify "notify-send"} 'GameMode ended'
+  '';
+in
+{
+  options.campground.apps.gamemode = with types; {
+    enable = mkBoolOpt false "Whether or not to enable gamemode.";
+    endscript = mkOpt (nullOr str) null "The script to run when disabling gamemode.";
+    startscript = mkOpt (nullOr str) null "The script to run when enabling gamemode.";
+  };
+
+  config =
+  let
+    startScript =
+      if (cfg.startscript == null)
+      then pkgs.writeShellScript "gamemode-start" defaultStartScript
+      else pkgs.writeShellScript "gamemode-start" cfg.startscript;
+    endScript =
+      if (cfg.endscript == null)
+      then pkgs.writeShellScript "gamemode-end" defaultEndScript
+      else pkgs.writeShellScript "gamemode-end" cfg.endscript;
+  in mkIf cfg.enable {
+    programs.gamemode = {
+      enable = true;
+      settings = {
+        general = {
+          softrealtime = "auto";
+          renice = 15;
+        };
+        custom = {
+          start = startScript.outPath;
+          end = endScript.outPath;
+        };
+      };
+    };
+  };
+}
+
