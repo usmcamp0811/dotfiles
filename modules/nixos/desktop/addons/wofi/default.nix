ecd53aad15fc3e357d5473c4a57e03efa6a1ec65
Matt Camp <matt@aicampground.com>
2024-01-14 19:13:56 -0600

    add wofi

---
+++ modules/nixos/desktop/addons/wofi/default.nix
@@ -0,0 +1,24 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.campground;
+let cfg = config.campground.desktop.addons.wofi;
+in
+{
+  options.campground.desktop.addons.wofi = with types; {
+    enable = mkBoolOpt false "Whether to enable the Wofi in the desktop environment.";
+  };
+
+  config = mkIf cfg.enable { 
+    environment.systemPackages = with pkgs; [
+      wofi
+      wofi-emoji
+    ];
+
+    khanelinix.home.configFile = {
+      "wofi/config".source = ./config;
+      "wofi/style.css".source = ./style.css;
+    };
+
+  };
+}
