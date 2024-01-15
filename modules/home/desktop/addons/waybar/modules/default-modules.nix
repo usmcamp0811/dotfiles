ebd19001faf3bf474065b2dbf3c44e4cd778bf68
Matt Camp <matt@aicampground.com>
2024-01-14 22:38:52 -0600

    auto get weather

--- modules/home/desktop/addons/waybar/modules/default-modules.nix
+++ modules/home/desktop/addons/waybar/modules/default-modules.nix
@@ -8,7 +8,7 @@ in
 {
   "clock" = {
     "tooltip-format" = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
-    "format" = "{:%a %d; %b \n %I:%M %p }";
+    "format" = "{:%a %b %d \n %H:%M }";
     "format-alt" = "{:%Y-%m-%d}";
   };
 
