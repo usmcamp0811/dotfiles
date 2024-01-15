ccea97e9bb65228d7fe0b9f1826e0e09d09b0bd4
Matt Camp <matt@aicampground.com>
2024-01-15 11:58:48 -0600

    webb cleanup

--- systems/x86_64-linux/webb/default.nix
+++ systems/x86_64-linux/webb/default.nix
@@ -31,7 +31,6 @@ in
     };
 
     services = {
-
       nixery = enabled;
       docker = enabled;
       minio = enabled;
@@ -102,13 +101,6 @@ in
           host  all  all  ::0/0  reject
         '';
       };
-
-      openssh = { 
-        authorizedKeys = [ 
-          "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAGs9njLHA3yyrX6BTf5Z3Xj8jzOh9zVYfJoeai6WhmBtjr34KV0F79YKafvJPS4gasOTFpnKXObvBo0jG3/AIN+dwBohHtFtXSYBgZecFg847XoeN+7cIveqgI2Q1Jn2sFoUTzGiwKxqLRM7ZuTtRJGfoizOxlYHdyovus67jfDxewP5A== mcamp@Butler"
-        ];
-      };
-
       wireguard = {
         enable = true;
         port = 1149;
@@ -126,20 +118,17 @@ in
           }
         ];
       };
-
       zfs-key-server = {
         enable = true;
         port = 8123;
         tang-servers = [ 
           "http://daly:1234" 
-          # "http://mattis:1234" 
-          # "http://lucas:1234" 
+          "http://lucas:1234" 
           "http://reckless:1234"
           "http://chesty:1234"
           "http://ermy:1234" 
         ];
       };
-
       user-secrets = {
         enable = true;
         users.mcamp = { 
