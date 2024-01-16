9bf02086bb099a65f1c0a4f9c99ff757db3bb6ec
Matt Camp <matt@aicampground.com>
2024-01-15 19:55:33 -0600

    adding swaynotiifcationcenter

---
+++ modules/nixos/desktop/addons/swaynotificationcenter/default.nix
@@ -0,0 +1,28 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.campground;
+let cfg = config.campground.desktop.addons.swaynotificationcenter;
+in
+{
+  options.campground.desktop.addons.swaynotificationcenter = with types; {
+    enable = mkBoolOpt false "Whether to enable swaynotificationcenter in the desktop environment.";
+  };
+
+  config = mkIf cfg.enable { 
+    environment.systemPackages = with pkgs; [
+      swaynotificationcenter
+      libnotify
+    ];
+
+    khanelinix.home = {
+      configFile."swaync/" = {
+        source = lib.cleanSourceWith {
+          src = lib.cleanSource ./config/.;
+        };
+
+        recursive = true;
+      };
+    };
+  };
+}
