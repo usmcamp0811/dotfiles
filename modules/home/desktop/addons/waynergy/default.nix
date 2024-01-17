fae80b7f03fc25ba1e0c69c6f6d594b052dd5692
Matt Camp <matt@aicampground.com>
2024-01-16 21:23:39 -0600

    gonna try this tomorrow

---
+++ modules/home/desktop/addons/waynergy/default.nix
@@ -0,0 +1,19 @@
+{ inputs, options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.campground;
+let 
+  cfg = config.campground.desktop.addons.waynergy;
+in
+{
+  options.campground.desktop.addons.waynergy = with types; {
+    enable =
+      mkBoolOpt false "Whether to enable waynergy in the desktop environment.";
+  };
+  config = mkIf cfg.enable {
+    home.packages = [
+      pkgs.waynergy
+    ];
+  };
+}
+
