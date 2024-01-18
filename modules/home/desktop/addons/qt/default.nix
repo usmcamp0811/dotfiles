8d60e77876252bcbc2e2989228d18c54c257c1f1
Matt Camp <matt@aicampground.com>
2024-01-17 19:28:49 -0600

    bunch of new things

---
+++ modules/home/desktop/addons/qt/default.nix
@@ -0,0 +1,81 @@
+{ inputs, options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.campground;
+let 
+  cfg = config.campground.desktop.addons.qt;
+  settings = {
+    Appearance = {
+      color_scheme_path = "";
+      custom_palette = true;
+      icon_theme = config.campground.desktop.addons.gtk.icon.name;
+      standard_dialogs = "gtk3";
+      style = "kvantum";
+    };
+
+    Fonts = {
+      fixed = "MonaspiceKr Nerd Font 10";
+      general = "MonaspiceNe Nerd Font 10";
+    };
+
+    Interface = {
+      activate_item_on_single_click = 1;
+      buttonbox_layout = 0;
+      cursor_flash_time = 1000;
+      dialog_buttons_have_icons = 1;
+      double_click_interval = 400;
+      gui_effects = null; # You might need to adjust this depending on Nix version
+      keyboard_scheme = 2;
+      menus_have_icons = true;
+      show_shortcuts_in_context_menus = true;
+      stylesheets = null; # You might need to adjust this depending on Nix version
+      toolbutton_style = "kvantum";
+      underline_shortcut = 1;
+      wheel_scroll_lines = 3;
+    };
+
+    Troubleshooting = {
+      force_raster_widgets = 1;
+      ignored_applications = null; # You might need to adjust this depending on Nix version
+    };
+  };
+
+  colorSchemePath = "${pkgs.catppuccin}/qt5ct/Catppuccin-Macchiato.conf";
+in
+{
+  options.campground.desktop.addons.qt = with types; {
+    enable = mkBoolOpt false "Whether to customize qt and apply themes.";
+    theme = {
+      name =
+        mkOpt str "Catppuccin-Macchiato-Blue"
+          "The name of the kvantum theme to apply.";
+      pkg = mkOpt package pkgs.catppuccin-kvantum "The package to use for the theme.";
+    };
+  };
+  config = mkIf cfg.enable {
+    xdg.configFile = {
+      "Kvantum".source = ./Kvantum;
+      "qt5ct/qt5ct.conf".text = lib.generators.toINI { }
+        (settings // {
+          Appearance = mergeAttrs settings.Appearance { color_scheme_path = colorSchemePath; };
+        });
+      "qt6ct/qt6ct.conf".text = lib.generators.toINI { }
+        (settings // {
+          Appearance = mergeAttrs settings.Appearance { color_scheme_path = colorSchemePath; };
+        });
+    };
+
+    qt = {
+      enable = true;
+
+      platformTheme = "qtct";
+      style = {
+        inherit (cfg.theme) name;
+        package = cfg.theme.pkg.override {
+          accent = "Blue";
+          variant = "Macchiato";
+        };
+      };
+    };
+  };
+}
---
+++ modules/home/desktop/addons/swayidle/default.nix
@@ -0,0 +1,48 @@
+{ inputs, options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.campground;
+let 
+  cfg = config.campground.desktop.addons.swaylock;
+  inherit (inputs) nixpkgs-wayland;
+in
+{
+  options.campground.desktop.addons.swaylock = with types; {
+    enable =
+      mkBoolOpt false "Whether to enable swaylock in the desktop environment.";
+  };
+  config = mkIf cfg.enable {
+    services.swayidle = {
+      enable = true;
+      package = nixpkgs-wayland.packages.${system}.swayidle;
+
+      events = [
+        {
+          event = "before-sleep";
+          command = "${getExe config.programs.swaylock.package} -defF";
+        }
+        {
+          # TODO: Make dynamic for window manager
+          event = "after-resume";
+          command = "${getExe' config.wayland.windowManager.hyprland.package "hyprctl"} dispatch dpms on";
+        }
+        {
+          event = "lock";
+          command = "${getExe config.programs.swaylock.package} -defF";
+        }
+      ];
+      timeouts = [
+        {
+          timeout = 900;
+          command = "${getExe config.programs.swaylock.package} -defF";
+        }
+        {
+          # TODO: Make dynamic for window manager
+          timeout = 1200;
+          command = "${getExe' config.wayland.windowManager.hyprland.package "hyprctl"} dispatch dpms off";
+        }
+      ];
+    };
+  };
+}
+
