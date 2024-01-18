48138b5cf17e5fd4835c0576d053692170320832
Matt Camp <matt@aicampground.com>
2024-01-17 22:05:06 -0600

    wip

--- modules/home/desktop/addons/swaynotificationcenter/default.nix
+++ modules/home/desktop/addons/swaynotificationcenter/default.nix
@@ -4,6 +4,11 @@ with lib;
 with lib.campground;
 let 
   cfg = config.campground.desktop.addons.swaynotificationcenter;
+  swayncConfig = import ./config.nix { inherit pkgs; };
+  swayncConfigFile = pkgs.writeTextFile {
+    name = "swaync-config.json";
+    text = builtins.toJSON swayncConfig;
+  };
 in
 {
   options.campground.desktop.addons.swaynotificationcenter = {
@@ -17,6 +22,7 @@ in
       libnotify
     ];
 # TODO: Nixifiy the config.json so we can get the correct paths to things
+    home.file.".config/swaync/config.json".source = swayncConfigFile;
     xdg.configFile = {
       "swaync" = {
         source = lib.cleanSourceWith {
