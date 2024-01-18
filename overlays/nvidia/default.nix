896445c62b7822ebcf21657e9634bf0713cc71db
Matt Camp <matt@aicampground.com>
2024-01-18 09:52:58 -0600

    nvidia wip

--- overlays/nvidia/default.nix
+++ overlays/nvidia/default.nix
@@ -4,5 +4,8 @@ final: prev:
 
 {
 
+  inherit (channels.unstable) linuxPackages_latest;
+  inherit (channels.unstable) linuxKernel;
+  # nvidia_x11 = unstable.legacyPackages.${prev.system}.nvidia_x11;
   # nvidia_x11 = unstable.legacyPackages.${prev.system}.nvidia_x11;
 }
