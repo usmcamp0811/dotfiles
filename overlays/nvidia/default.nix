7d19b8f36eeda1e4057cdb9cb943b1aa038d9780
Matt Camp <matt@aicampground.com>
2024-01-18 20:41:37 -0600

    flake update

--- overlays/nvidia/default.nix
+++ overlays/nvidia/default.nix
@@ -4,8 +4,6 @@ final: prev:
 
 {
 
-  inherit (channels.unstable) linuxPackages_latest;
-  inherit (channels.unstable) linuxKernel;
   # nvidia_x11 = unstable.legacyPackages.${prev.system}.nvidia_x11;
   # nvidia_x11 = unstable.legacyPackages.${prev.system}.nvidia_x11;
 }
