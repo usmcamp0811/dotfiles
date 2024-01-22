b3b68e1f0bd627bb56421ad50dd609f789386418
Matt Camp <matt@aicampground.com>
2024-01-21 18:52:15 -0600

    wip

--- overlays/linux/default.nix
+++ overlays/linux/default.nix
@@ -10,8 +10,8 @@ final: prev:
   # https://github.com/NixOS/nixpkgs/issues/154163
   # linuxPackages_latest = unstable.legacyPackages.${prev.system}.linuxPackages_latest;
   # nvidia_x11 = unstable.legacyPackages.${prev.system}.linuxPackages_latest.nvidia_x11;
-      linux = unstable.linux_latest;
-      nvidia_x11 = unstable.nvidia_x11_latest;
+      # linux = unstable.linux_latest;
+      # nvidia_x11 = unstable.nvidia_x11_latest;
   # makeModulesClosure = x: prev.makeModulesClosure (x // {
   #   allowMissing = true;
   # });
