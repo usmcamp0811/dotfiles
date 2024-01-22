a5fa6ac4627b2716e5c7449b6b8f336ba5de59f6
Matt Camp <matt@aicampground.com>
2024-01-21 18:08:30 -0600

    hydra wip

--- flake.nix
+++ flake.nix
@@ -241,13 +241,13 @@
       # Fixed bug in Amazon image builder: https://github.com/nix-community/nixos-generators/issues/150
       systems.hosts.base.modules = [({...}: { amazonImage.sizeMB = 16 * 1024; })];
 
-      deploy = lib.mkDeploy { inherit (inputs) self; };
+      # deploy = lib.mkDeploy { inherit (inputs) self; };
 
-      checks =
-        builtins.mapAttrs
-          (system: deploy-lib:
-            deploy-lib.deployChecks inputs.self.deploy)
-          inputs.deploy-rs.lib;
+      # checks =
+      #   builtins.mapAttrs
+      #     (system: deploy-lib:
+      #       deploy-lib.deployChecks inputs.self.deploy)
+      #     inputs.deploy-rs.lib;
     };
 
 }
