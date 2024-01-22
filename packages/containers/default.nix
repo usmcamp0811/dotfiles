76ecaac5972f1d562507a2174b5b67845734e086
Matt Camp <matt@aicampground.com>
2024-01-21 21:06:13 -0600

    saving this here

--- packages/containers/default.nix
+++ packages/containers/default.nix
@@ -7,60 +7,60 @@
 , hosts ? { }
 , ...
 }:{}
-# let
-#   inherit (lib) mapAttrsToList concatStringsSep;
-#   inherit (lib.campground) override-meta;
-#   inherit (pkgs.nix-snapshotter) buildImage;
-#
-#       examples = rec {
-#         hello = buildImage {
-#           name = "ghcr.io/aicampground/my-hello2";
-#           tag = "latest";
-#           config = {
-#             entrypoint = ["${pkgs.hello}/bin/hello"];
-#           };
-#         };
-#
-#         redis = buildImage {
-#           name = "ghcr.io/pdtpartners/my-redis";
-#           tag = "latest";
-#           config = {
-#             entrypoint = [ "${pkgs.redis}/bin/redis-server" ];
-#           };
-#         };
-#
-#         mlflow = buildImage {
-#           name = "aicampground.com/testing/mlflow";
-#           tag = "latest";
-#           config = {
-#             entrypoint = [ "${pkgs.campground.mlflow}/bin/mlflow-server" ];
-#           };
-#         };
-#
-#         redisWithShell = buildImage {
-#           name = "ghcr.io/pdtpartners/redis-shell";
-#           tag = "latest";
-#           fromImage = redis;
-#           config = {
-#             entrypoint = [ "/bin/sh" ];
-#           };
-#           copyToRoot = pkgs.buildEnv {
-#             name = "system-path";
-#             pathsToLink = [ "/bin" ];
-#             paths = [
-#               pkgs.bashInteractive
-#               pkgs.coreutils
-#               pkgs.redis
-#             ];
-#           };
-#         };
-#       };
-#
-#   new-meta = with lib; {
-#     description = "nix-snapshotter container";
-#     license = licenses.asl20;
-#     maintainers = with maintainers; [ mattcamp ];
-#   };
-# in 
-# override-meta new-meta examples.redisWithShell
+let
+  inherit (lib) mapAttrsToList concatStringsSep;
+  inherit (lib.campground) override-meta;
+  inherit (pkgs.nix-snapshotter) buildImage;
+
+      examples = rec {
+        hello = buildImage {
+          name = "ghcr.io/aicampground/my-hello2";
+          tag = "latest";
+          config = {
+            entrypoint = ["${pkgs.hello}/bin/hello"];
+          };
+        };
+
+        redis = buildImage {
+          name = "ghcr.io/pdtpartners/my-redis";
+          tag = "latest";
+          config = {
+            entrypoint = [ "${pkgs.redis}/bin/redis-server" ];
+          };
+        };
+
+        mlflow = buildImage {
+          name = "aicampground.com/testing/mlflow";
+          tag = "latest";
+          config = {
+            entrypoint = [ "${pkgs.campground.mlflow}/bin/mlflow-server" ];
+          };
+        };
+
+        redisWithShell = buildImage {
+          name = "ghcr.io/pdtpartners/redis-shell";
+          tag = "latest";
+          fromImage = redis;
+          config = {
+            entrypoint = [ "/bin/sh" ];
+          };
+          copyToRoot = pkgs.buildEnv {
+            name = "system-path";
+            pathsToLink = [ "/bin" ];
+            paths = [
+              pkgs.bashInteractive
+              pkgs.coreutils
+              pkgs.redis
+            ];
+          };
+        };
+      };
+
+  new-meta = with lib; {
+    description = "nix-snapshotter container";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ mattcamp ];
+  };
+in 
+override-meta new-meta examples.redisWithShell
 
