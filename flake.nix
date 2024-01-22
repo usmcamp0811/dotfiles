cee32fb09a6e7cc4d2fffedea0c3fa11ec1817a8
Matt Camp <matt@aicampground.com>
2024-01-21 18:10:58 -0600

    hydra wip

--- flake.nix
+++ flake.nix
@@ -243,11 +243,11 @@
 
       # deploy = lib.mkDeploy { inherit (inputs) self; };
 
-      # checks =
-      #   builtins.mapAttrs
-      #     (system: deploy-lib:
-      #       deploy-lib.deployChecks inputs.self.deploy)
-      #     inputs.deploy-rs.lib;
+      checks =
+        builtins.mapAttrs
+          (system: deploy-lib:
+            deploy-lib.deployChecks inputs.self.deploy)
+          inputs.deploy-rs.lib;
     };
 
 }
