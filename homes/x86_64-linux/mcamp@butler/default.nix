471df3d1f2c8225fdc34b0bf452e06e8cbaa794d
Matt Camp <matt@aicampground.com>
2024-01-28 15:18:18 -0600

    swaylock is good to go

--- homes/x86_64-linux/mcamp@butler/default.nix
+++ homes/x86_64-linux/mcamp@butler/default.nix
@@ -17,7 +17,7 @@ with lib.campground;
         rofi = enabled;
         swaynotificationcenter = enabled;
         networkmanagerapplet = enabled;
-        # swayidle = enabled;
+        swayidle = enabled;
         swaylock = enabled;
         input-leap = enabled;
         qt = enabled;
