c3b859cad0bdde65986f06057e0baff21b248c53
Matt Camp <matt@aicampground.com>
2024-02-11 20:44:43 -0600

    err

--- overlays/mlflow/default.nix
+++ overlays/mlflow/default.nix
@@ -2,13 +2,13 @@
 
 final: prev:
 {
-  inherit (channels.unstable) python311Packages;
-  # python311 = unstable.legacyPackages.${prev.system}.python311;
-  # mlflow-unstable = unstable.legacyPackages.${prev.system}.python311Packages.mlflow;
-  # boto3-unstable = unstable.legacyPackages.${prev.system}.python311Packages.boto3;
-  # psycopg2-unstable = unstable.legacyPackages.${prev.system}.python311Packages.psycopg2;
-  # mysqlclient-unstable = unstable.legacyPackages.${prev.system}.python311Packages.mysqlclient;
-  # gunicorn-unstable = unstable.legacyPackages.${prev.system}.python311Packages.gunicorn;
-  # poetry = nixpkgs.legacyPackages.${prev.system}.poetry;
+  # inherit (channels.unstable) python311Packages;
+  python3-11 = unstable.legacyPackages.${prev.system}.python311;
+  mlflow-unstable = unstable.legacyPackages.${prev.system}.python311Packages.mlflow;
+  boto3-unstable = unstable.legacyPackages.${prev.system}.python311Packages.boto3;
+  psycopg2-unstable = unstable.legacyPackages.${prev.system}.python311Packages.psycopg2;
+  mysqlclient-unstable = unstable.legacyPackages.${prev.system}.python311Packages.mysqlclient;
+  gunicorn-unstable = unstable.legacyPackages.${prev.system}.python311Packages.gunicorn;
+  poetry = nixpkgs.legacyPackages.${prev.system}.poetry;
 }                                                                                   
                                                                                    
