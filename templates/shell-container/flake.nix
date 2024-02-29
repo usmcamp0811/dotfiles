a6e9666038af18921c73039979d5f5120e842b0b
Matt Camp <matt@aicampground.com>
2024-02-28 19:46:42 -0600

    kicked it up a notch

--- templates/shell-container/flake.nix
+++ templates/shell-container/flake.nix
@@ -30,7 +30,21 @@
             ];
         };
         pluto = pkgs.writeShellScriptBin "pluto" ''
-          ${julia-env}/bin/julia -e "using Pluto; Pluto.run()"
+          #!/usr/bin/env bash
+          HOST="0.0.0.0" # Default host
+          PORT=1234      # Default port
+
+          # Parse command-line arguments for --host and --port
+          while [[ "$#" -gt 0 ]]; do
+              case $1 in
+                  --host) HOST="$2"; shift ;;
+                  --port) PORT="$2"; shift ;;
+                  *) echo "Unknown parameter passed: $1"; exit 1 ;;
+              esac
+              shift
+          done
+
+          ${julia-env}/bin/julia -e "using Pluto; Pluto.run(host=\"$HOST\", port=$PORT)"
         '';
         shell-img = pkgs.dockerTools.buildNixShellImage {
           name = "shell-container" ;
