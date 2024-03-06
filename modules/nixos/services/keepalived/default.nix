7f7ba72c87412c8a5bee33f7da65a975f226b402
Matt Camp <matt@aicampground.com>
2024-03-05 22:30:34 -0600

    adding keepalived wip

---
+++ modules/nixos/services/keepalived/default.nix
@@ -0,0 +1,28 @@
+{ lib, config, pkgs, ... }:
+with lib;
+with lib.campground;
+let
+  cfg = config.campground.services.keepalived;
+in
+{
+  options.campground.services.keepalived = with types; {
+    enable = mkBoolOpt false "Enable KeepAliveD;";
+    instanceName = mkOpt str "campground" "The instance Name";
+    interface = mkOpt str "eth1" "The interface name";
+    ip = mkOpt str "10.0.0.1" "The IP to bind to";
+    workDir = mkOpt str "/code" "Working dir to start Jupyter in.";
+  };
+
+  config = mkIf cfg.enable {
+    networking.firewall.extraCommands = "iptables -A INPUT -p vrrp -j ACCEPT";
+    services.keepalived.enable = true;
+    services.keepalived.vrrpInstances.${cfg.instanceName} = {
+      interface = "eth1";
+      state = "MASTER";
+      priority = 50;
+      virtualIps = [{ addr = cfg.ip; }];
+      virtualRouterId = 1;
+    };
+    environment.systemPackages = [ pkgs.tcpdump ];
+  };
+}
