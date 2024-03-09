9c20c360b1efef25f4551d8977fe0ac3eaa024d7
Matt Camp <matt@aicampground.com>
2024-03-09 13:23:14 -0600

    wip

--- modules/nixos/suites/hosting/default.nix
+++ modules/nixos/suites/hosting/default.nix
@@ -11,24 +11,77 @@ in
     interface = mkOpt str "eno1" "Interface to use for the LAN Instance";
     lan-interface = mkOpt str cfg.interface "Interface to use for the LAN Instance";
     pub-interface = mkOpt str cfg.interface "Interface to use for the Public Instance";
+    lan-ip = mkOpt str "10.8.0.69" "IP to use for the LAN Instance";
+    pub-ip = mkOpt str "10.8.0.70" "IP to use for the Public Instance";
   };
 
   config = mkIf cfg.enable {
     campground = {
       services = {
+        traefik = {
+          enable = true;
+          entrypoints = { web.address = "${cfg.pub-ip}:80"; };
+          dynamicConfigOptions = {
+            http.routers.searx = {
+              rule = "Host(`searx.aicampground.com`)";
+              entryPoints = [ "web" ];
+              service = "searx";
+            };
+
+            http.services.searx = {
+              loadBalancer.servers = [
+                { url = "http://webb:3249"; }
+                { url = "http://daly:3249"; }
+                { url = "http://chesty:3249"; }
+                { url = "http://lucas:3249"; }
+                { url = "http://reckless:3249"; }
+              ];
+            };
+
+            http.routers.bitwarden = {
+              rule = "Host(`bw.aicampground.com`)";
+              entryPoints = [ "web" ];
+              service = "bitwarden";
+            };
+
+            http.services.bitwarden = {
+              loadBalancer.servers = [
+                { url = "http://webb:9012"; }
+              ];
+            };
+
+            http.routers.mattermost = {
+              rule = "Host(`mattermost.aicampground.com`)";
+              entryPoints = [ "web" ];
+              service = "mattermost";
+            };
+
+            http.routers.mm = {
+              rule = "Host(`mm.aicampground.com`)";
+              entryPoints = [ "web" ];
+              service = "mattermost";
+            };
+
+            http.services.mattermost = {
+              loadBalancer.servers = [
+                { url = "http://webb:8065"; }
+              ];
+            };
+          };
+        };
         keepalived = {
           enable = true;
           instances = {
             "pub-campground" = {
               interface = cfg.pub-interface;
-              ips = [ "10.8.0.69" ];
+              ips = [ cfg.pub-ip ];
               state = "MASTER";
               priority = 50;
               virtualRouterId = 51;
             };
             "lan-campground" = {
               interface = cfg.lan-interface;
-              ips = [ "10.8.0.70" ];
+              ips = [ cfg.lan-ip ];
               state = "MASTER";
               priority = 50;
               virtualRouterId = 52;
