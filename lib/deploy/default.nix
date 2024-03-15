3443a053663922544f7748bd7191c4de62ea7b8f
Matt Camp <matt@aicampground.com>
2024-03-15 17:02:12 -0500

    wip

--- lib/deploy/default.nix
+++ lib/deploy/default.nix
@@ -1,54 +1,51 @@
-{ lib
-, inputs
-}:
+{ lib, inputs }:
+
 let
   inherit (inputs) deploy-rs;
 in
 rec {
-  mkDeploy =
-    { self
-    , overrides ? { }
-    ,
-    }:
+  ## Create deployment configuration for use with deploy-rs.
+  ##
+  ## ```nix
+  ## mkDeploy {
+  ##   inherit self;
+  ##   overrides = {
+  ##     my-host.system.sudo = "doas -u";
+  ##   };
+  ## }
+  ## ```
+  ##
+  #@ { self: Flake, overrides: Attrs ? {} } -> Attrs
+  mkDeploy = { self, overrides ? { } }:
     let
       hosts = self.nixosConfigurations or { };
       names = builtins.attrNames hosts;
-      nodes =
-        lib.foldl
-          (result: name:
-            let
-              host = hosts.${name};
-              user = host.config.campground.user.name or null;
-              inherit (host.pkgs) system;
-            in
-            result
-            // {
-              ${name} =
-                (overrides.${name} or { })
-                // {
-                  hostname = overrides.${name}.hostname or "${name}";
-                  profiles =
-                    (overrides.${name}.profiles or { })
-                    // {
-                      system =
-                        (overrides.${name}.profiles.system or { })
-                        // {
-                          path = deploy-rs.lib.${system}.activate.nixos host;
-                        }
-                        // lib.optionalAttrs (user != null) {
-                          user = "root";
-                          sshUser = user;
-                        }
-                        // lib.optionalAttrs
-                          (host.config.campground.security.doas.enable or false)
-                          {
-                            sudo = "doas -u";
-                          };
-                    };
-                };
-            })
-          { }
-          names;
+      nodes = lib.foldl
+        (result: name:
+          let
+            host = hosts.${name};
+            user = host.config.plusultra.user.name or null;
+            inherit (host.pkgs) system;
+          in
+          result // {
+            ${name} = (overrides.${name} or { }) // {
+              hostname = overrides.${name}.hostname or "${name}";
+              profiles = (overrides.${name}.profiles or { }) // {
+                system = (overrides.${name}.profiles.system or { }) // {
+                  path = deploy-rs.lib.${system}.activate.nixos host;
+                } // lib.optionalAttrs (user != null) {
+                  user = "root";
+                  sshUser = user;
+                } // lib.optionalAttrs
+                  (host.config.plusultra.security.doas.enable or false)
+                  {
+                    sudo = "doas -u";
+                  };
+              };
+            };
+          })
+        { }
+        names;
     in
     { inherit nodes; };
 }
