1ff895c29496fcc5227f6936025227f4faf40cfd
Matt Camp <matt@aicampground.com>
2024-03-19 20:46:57 -0500

    wip

--- ci-scripts/get_lan-public_systems.sh
+++ ci-scripts/get_lan-public_systems.sh
@@ -1,23 +1,23 @@
-#!/usr/bin/env -S nix shell nixpkgs#figlet --command bash
+#!/usr/bin/bash
 
-echo "WHAT THE FUCK" | figlet
-# #!/usr/bin/env -S nix shell nixpkgs#jq --command bash
-#
-# # The first argument passed to the script
-# TYPE="$1"
-#
-# # Determine the hosting type based on the argument
-# if [ "$TYPE" = "public" ]; then
-#   ENABLED_PATH=".config.campground.suites.public-hosting.enable"
-# elif [ "$TYPE" = "lan" ]; then
-#   ENABLED_PATH=".config.campground.suites.lan-hosting.enable"
-# else
-#   echo "Invalid type: $TYPE"
-#   exit 1
-# fi
-#
-# nix eval --json '.#nixosConfigurations' --apply "
-#   configurations: (builtins.filter (name: 
-#     configurations.\${name}$ENABLED_PATH == true
-#   ) (builtins.attrNames configurations))
-# " | jq -r '.[]'
+# Manually setup the environment
+export PATH=$(nix shell nixpkgs#jq --command echo $PATH)
+
+# The first argument passed to the script
+TYPE="$1"
+
+# Determine the hosting type based on the argument
+if [ "$TYPE" = "public" ]; then
+  ENABLED_PATH=".config.campground.suites.public-hosting.enable"
+elif [ "$TYPE" = "lan" ]; then
+  ENABLED_PATH=".config.campground.suites.lan-hosting.enable"
+else
+  echo "Invalid type: $TYPE"
+  exit 1
+fi
+
+nix eval --json '.#nixosConfigurations' --apply "
+  configurations: (builtins.filter (name: 
+    configurations.\${name}$ENABLED_PATH == true
+  ) (builtins.attrNames configurations))
+" | jq -r '.[]'
