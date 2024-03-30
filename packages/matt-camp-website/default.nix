63df6c5031fc1ff4712cfb4dee78957b5b486639
Matt Camp <matt@aicampground.com>
2024-03-30 10:22:29 -0500

    clean up

--- packages/matt-camp-website/default.nix
+++ packages/matt-camp-website/default.nix
@@ -4,40 +4,16 @@
 let
   inherit (lib) mapAttrsToList concatStringsSep;
   inherit (lib.campground) override-meta;
-  website = mkYarnPackage {
+  matt-camp-website = mkYarnPackage {
     name = "matt-com.com";
     src = ./.;
     packageJSON = ./package.json;
     yarnLock = ./yarn.lock;
-    # yarnNix = ./yarn.nix;
     doDist = true;
   buildPhase = ''
     export NODE_OPTIONS=--openssl-legacy-provider
     yarn build
-    # quasar build
   '';
   };
 in
-website
-# pkgs.stdenv.mkDerivation {
-#   name = "example-ts-node";
-#   version = "0.1.0";
-#   src = website;
-#   buildInputs = [ pkgs.nodejs pkgs.yarn website ];
-#   buildPhase = ''
-#     mkdir -p $out
-#     # cp -r ${website.src} $out
-#     cp -r ${website} $out
-#     # yarn
-#     # quasar build
-#   '';
-#   # installPhase = ''
-#   #   # runHook preInstall
-#   #   mkdir -p $out
-#   #   cp ${website} $out/website
-#   #   # cp -r node_modules $out/node_modules
-#   #   # cp package.json $out/package.json
-#   #   # cp -r dist $out/dist
-#   #   # runHook postInstall
-#   # '';
-# }
+matt-camp-website
