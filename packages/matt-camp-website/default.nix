4a5009a8636b1601c8f3b9b462e11cef37dda7ae
Matt Camp <matt@aicampground.com>
2024-03-29 20:02:44 -0500

    wip

--- packages/matt-camp-website/default.nix
+++ packages/matt-camp-website/default.nix
@@ -4,13 +4,30 @@
 let
   inherit (lib) mapAttrsToList concatStringsSep;
   inherit (lib.campground) override-meta;
-in
-mkYarnPackage {
+  website = mkYarnPackage {
     name = "matt-com.com";
     src = ./.;
     packageJSON = ./package.json;
     yarnLock = ./yarn.lock;
-    # NOTE: this is optional and generated dynamically if omitted
     yarnNix = ./yarn.nix;
+    doDist = true;
+  };
+in
+pkgs.stdenv.mkDerivation {
+  name = "example-ts-node";
+  version = "0.1.0";
+  src = website;
+  buildInputs = [ pkgs.nodejs pkgs.yarn website ];
+  buildPhase = ''
+    # each phase has pre/postHooks. When you make your own phase be sure to still call the hooks
+    yarn
+    quasar build
+  '';
+  installPhase = ''
+    runHook preInstall
+    cp -r node_modules $out/node_modules
+    cp package.json $out/package.json
+    cp -r dist $out/dist
+    runHook postInstall
+  '';
 }
-
