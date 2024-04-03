b76e915d9cbba433f1fa3b9581c7047981eae095
Matt Camp <matt@aicampground.com>
2024-04-02 22:13:59 -0500

    wip

--- packages/slides-immutable-infra-with-nix/default.nix
+++ packages/slides-immutable-infra-with-nix/default.nix
@@ -5,13 +5,15 @@ let
   inherit (lib) mapAttrsToList concatStringsSep;
   inherit (lib.campground) override-meta;
   nix-slides = mkYarnPackage {
+    version = "0.1.0";
     name = "nix-slides";
     src = ./immutable-infra-with-nix/.;
     packageJSON = ./immutable-infra-with-nix/package.json;
     yarnLock = ./immutable-infra-with-nix/yarn.lock;
     # doDist = true;
   buildPhase = ''
-    yarn build
+    export NODE_OPTIONS=--openssl-legacy-provider
+    yarn 
   '';
   };
 in
