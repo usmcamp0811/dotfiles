ddf766c56196659d1ed0a95f123dc762f4b85fed
Matt Camp <matt@aicampground.com>
2024-04-02 21:42:50 -0500

    making slides

---
+++ packages/slides-immutable-infra-with-nix/default.nix
@@ -0,0 +1,19 @@
+{ lib, writeText, mkYarnPackage, substituteAll, gum, inputs, pkgs
+, hosts ? { }, ... }:
+
+let
+  inherit (lib) mapAttrsToList concatStringsSep;
+  inherit (lib.campground) override-meta;
+  nix-slides = mkYarnPackage {
+    name = "nix-slides";
+    src = ./.;
+    packageJSON = ./immutable-infra-with-nix/package.json;
+    yarnLock = ./immutable-infra-with-nix/yarn.lock;
+    doDist = true;
+  buildPhase = ''
+    export NODE_OPTIONS=--openssl-legacy-provider
+    yarn dev
+  '';
+  };
+in
+nix-slides
