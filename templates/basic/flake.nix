5c01cda50a530d08460d9fd537f7f1cfa1e9680c
Matt Camp <matt@aicampground.com>
2024-04-13 13:15:18 -0500

    add hooks to the basic example template too

--- templates/basic/flake.nix
+++ templates/basic/flake.nix
@@ -3,43 +3,50 @@
 
   inputs.devshell.url = "github:numtide/devshell";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
 
-  outputs = {
-    self,
-    flake-utils,
-    devshell,
-    nixpkgs,
-  }:
+  outputs =
+    { self
+    , flake-utils
+    , devshell
+    , nixpkgs
+    ,
+    }:
     flake-utils.lib.eachDefaultSystem (system: {
-      devShells.default = let
-        pkgs = import nixpkgs {
-          inherit system;
+      devShells.default =
+        let
+          pkgs = import nixpkgs {
+            inherit system;
 
-          overlays = [devshell.overlays.default];
-        };
+            overlays = [ devshell.overlays.default ];
+          };
 
-        randomFiglet = pkgs.writeShellScript "randomFiglet" ''
-          phrases=("Nix Your Problems Away!" "One Flake to Rule Them All!" "Why Fix It, When You Can Nix It?" "Nixify Life" "Just Nix It" "Powered by Nix")
-          random_index=$((RANDOM % 6))
-          case $random_index in
-            0) ${pkgs.figlet}/bin/figlet "Nixify Life";;
-            1) ${pkgs.figlet}/bin/figlet "Just Nix It";;
-            2) ${pkgs.figlet}/bin/figlet "Powered by Nix";;
-            3) ${pkgs.figlet}/bin/figlet "Nix Your Problems Away!";;
-            4) ${pkgs.figlet}/bin/figlet "One Flake to Rule Them All!";;
-            5) ${pkgs.figlet}/bin/figlet "Why Fix It, When You Can Nix It?";;
-          esac
-        '';
-      in
+          randomFiglet = pkgs.writeShellScript "randomFiglet" ''
+            phrases=("Nix Your Problems Away!" "One Flake to Rule Them All!" "Why Fix It, When You Can Nix It?" "Nixify Life" "Just Nix It" "Powered by Nix")
+            random_index=$((RANDOM % 6))
+            case $random_index in
+              0) ${pkgs.figlet}/bin/figlet "Nixify Life";;
+              1) ${pkgs.figlet}/bin/figlet "Just Nix It";;
+              2) ${pkgs.figlet}/bin/figlet "Powered by Nix";;
+              3) ${pkgs.figlet}/bin/figlet "Nix Your Problems Away!";;
+              4) ${pkgs.figlet}/bin/figlet "One Flake to Rule Them All!";;
+              5) ${pkgs.figlet}/bin/figlet "Why Fix It, When You Can Nix It?";;
+            esac
+          '';
+        in
         pkgs.devshell.mkShell {
-          imports = [(pkgs.devshell.importTOML ./devshell.toml)];
+          imports = [
+            (pkgs.devshell.importTOML ./devshell.toml)
+            self.checks.${system}.pre-commit-check.enabledPackages
+          ];
           name = "basic-shell";
           motd = ''
             {214}👁️  Welcome to a Basic devshell 👁️{reset}
             $( ${randomFiglet} | ${pkgs.lolcat}/bin/lolcat -f )
             $(type -p menu &>/dev/null && menu)
+            ${self.checks.${system}.pre-commit-check.shellHook}
           '';
-          commands = [];
+          commands = [ ];
           env = [
             {
               name = "LD_LIBRARY_PATH";
@@ -47,5 +54,13 @@
             }
           ];
         };
+      checks = {
+        pre-commit-check = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            nixpkgs-fmt.enable = true;
+          };
+        };
+      };
     });
 }
