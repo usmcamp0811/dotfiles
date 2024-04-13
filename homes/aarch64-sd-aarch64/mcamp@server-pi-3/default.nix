f78168eaddbe7ac76d16d67bc547dd772d29f7ba
Matt Camp <matt@aicampground.com>
2024-04-13 14:55:34 -0500

    trying to make a pi

---
+++ homes/aarch64-sd-aarch64/mcamp@server-pi-3/default.nix
@@ -0,0 +1,22 @@
+{
+  lib,
+  pkgs,
+  config,
+  osConfig ? {},
+  format ? "unknown",
+  ...
+}:
+with lib.campground; {
+  campground = {
+    user = {
+      enable = true;
+      name = config.snowfallorg.user.name;
+    };
+
+    cli = {
+      zsh = enabled;
+      home-manager = enabled;
+    };
+  };
+  home.stateVersion = "23.05";
+}
