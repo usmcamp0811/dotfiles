93e461e5bb39ec0677bcbacce4eb21e691e9a25c
Matt Camp <matt@aicampground.com>
2024-04-13 12:35:05 -0500

    hook testing

--- shells/default/default.nix
+++ shells/default/default.nix
@@ -1,37 +1,35 @@
-{
-  mkShell,
-  pkgs,
-  config,
-  system,
-  inputs,
-  lib,
-  ...
+{ mkShell
+, pkgs
+, config
+, system
+, inputs
+, lib
+, ...
 }:
 with lib;
 with lib.campground; let
   inherit (lib.campground) override-meta;
   inherit (inputs.self.checks.${system}.pre-commit-check) shellHook;
 in
-  mkShell {
-    # buildInputs = [
-    #   pkgs.deadnix
-    #   pkgs.hydra-check
-    #   pkgs.nix-diff
-    #   pkgs.nix-index
-    #   pkgs.nix-prefetch-git
-    #   pkgs.nixpkgs-fmt
-    #   pkgs.nixpkgs-hammering
-    #   pkgs.nixpkgs-lint
-    #   pkgs.snowfallorg.flake
-    #   pkgs.statix
-    #   pkgs.campground.vault-scripts
-    # ] ++ inputs.self.checks.${system}.pre-commit-check.enabledPackages;
+mkShell {
+  buildInputs = [
+    pkgs.deadnix
+    pkgs.hydra-check
+    pkgs.nix-diff
+    pkgs.nix-index
+    pkgs.nix-prefetch-git
+    pkgs.nixpkgs-fmt
+    pkgs.nixpkgs-hammering
+    pkgs.nixpkgs-lint
+    pkgs.snowfallorg.flake
+    pkgs.statix
+    pkgs.campground.vault-scripts
+  ] ++ inputs.self.checks.${system}.pre-commit-check.enabledPackages;
 
-    buildInputs = inputs.self.checks.${system}.pre-commit-check.enabledPackages;
-    shellHook = ''
-      ${shellHook}
-      echo 🏕️ Welcome to the Campground
-      # Additional setup can go here
+  shellHook = ''
+    ${shellHook}
+    echo 🏕️ Welcome to the Campground
+    # Additional setup can go here
 
-    '';
-  }
+  '';
+}
