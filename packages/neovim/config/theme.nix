59963d860d67477805ae9f5eab0e875e437ea35a
Matt Camp <matt@aicampground.com>
2024-04-13 23:01:38 -0500

    update name and lock file

--- packages/neovim/config/theme.nix
+++ packages/neovim/config/theme.nix
@@ -1,14 +1,14 @@
-{pkgs, ...}: {
+{ ... }: {
   # use the ayu theme
-  imports = [./plugins/ayu.nix];
+  imports = [ ./plugins/ayu.nix ];
 
   # leaving below as examples and also in case I wanna change quickly
   colorschemes = {
     catppuccin = {
       enable = false;
-      background = {dark = "mocha";};
+      settings = { background = { dark = "mocha"; }; };
     };
-    nord = {enable = false;};
-    onedark = {enable = false;};
+    nord = { enable = false; };
+    onedark = { enable = false; };
   };
 }
