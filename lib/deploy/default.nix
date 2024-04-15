8a679d3c9e034f162d096b5b85fdd86c462436e7
Matt Camp <matt@aicampground.com>
2024-04-14 19:01:04 -0500

    try to fix deploy issue

--- lib/deploy/default.nix
+++ lib/deploy/default.nix
@@ -24,34 +24,36 @@ in rec {
     names = builtins.attrNames hosts;
     nodes = lib.foldl (result: name: let
       host = hosts.${name};
-      user = host.config.campground.user.name or null;
       inherit (host.pkgs) system;
+      # Condition to check if the system type is x86_64-linux
+      isActive = system == "x86_64-linux";
     in
-      result
-      // {
-        ${name} =
-          (overrides.${name} or {})
-          // {
-            hostname = overrides.${name}.hostname or "${name}";
-            profiles =
-              (overrides.${name}.profiles or {})
-              // {
-                system =
-                  (overrides.${name}.profiles.system or {})
-                  // {
-                    path = deploy-rs.lib.${system}.activate.nixos host;
-                  }
-                  // lib.optionalAttrs (user != null) {
-                    user = "root";
-                    sshUser = "root";
-                  }
-                  // lib.optionalAttrs
-                  (host.config.campground.security.doas.enable or false) {
-                    sudo = "doas -u";
-                  };
-              };
-          };
-      }) {}
+      if !isActive then result
+      else
+        result // {
+          ${name} =
+            (overrides.${name} or {})
+            // {
+              hostname = overrides.${name}.hostname or "${name}";
+              profiles =
+                (overrides.${name}.profiles or {})
+                // {
+                  system =
+                    (overrides.${name}.profiles.system or {})
+                    // {
+                      path = deploy-rs.lib.${system}.activate.nixos host;
+                    }
+                    // lib.optionalAttrs (host.config.users.users.${host.config.campground.user.name or "root"}.isNormalUser or false) {
+                      user = "root";
+                      sshUser = "root";
+                    }
+                    // lib.optionalAttrs
+                    (host.config.campground.security.doas.enable or false) {
+                      sudo = "doas -u";
+                    };
+                };
+            };
+        }) {}
     names;
   in {inherit nodes;};
 }
