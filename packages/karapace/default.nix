02ad703441f647f91b787656f4d30727d230a9b2
Matt Camp <matt@aicampground.com>
2024-04-18 21:27:10 -0500

    it builds

--- packages/karapace/default.nix
+++ packages/karapace/default.nix
@@ -2,8 +2,6 @@
   lib,
   writeText,
   writeShellApplication,
-  buildPythonPackage, 
-  fetchFromGitHub,
   substituteAll,
   inputs,
   pkgs,
@@ -23,20 +21,83 @@
   pname = "karapace";
   version = "3.12.0"; 
 
-  karapace = buildPythonPackage rec {
+  pypkgs-build-requirements = {
+    accept-types = ["setuptools"];
+  };
+
+# accept-types = "^0.4.1"
+# aiohttp = "^3.9.5"
+# aiokafka = "^0.10.0"
+# avro = "^1.11.3"
+# jsonschema = "^4.21.1"
+# kafka-python = "^2.0.2"
+# networkx = "^3.3"
+# protobuf = "^5.26.1"
+# pyjwt = "^2.8.0"
+# python-dateutil = "^2.9.0.post0"
+# lz4 = "^4.3.3"
+# python-snappy = "^0.7.1"
+# zstandard = "^0.22.0"
+# sentry-sdk = "^1.45.0"
+# ujson = "^5.9.0"
+
+  p2n-overrides = pkgs.poetry2nix.defaultPoetryOverrides.extend (self: super:
+    builtins.mapAttrs (package: build-requirements:
+      (builtins.getAttr package super).overridePythonAttrs (old: {
+        buildInputs =
+          (old.buildInputs or [])
+          ++ (builtins.map (pkg:
+            if builtins.isString pkg
+            then builtins.getAttr pkg super
+            else pkg)
+          build-requirements);
+      }))
+    pypkgs-build-requirements);
+
+
+  python-env = pkgs.poetry2nix.mkPoetryEnv {
+    projectDir = ./.;
+    overrides = p2n-overrides;
+    python = pkgs.python311;
+  };
+
+
+  karapace = pkgs.python3Packages.buildPythonApplication {
     inherit pname;
     inherit version;
 
-    src = fetchFromGitHub {
+    src = pkgs.fetchFromGitHub {
       owner = "Aiven-Open";
       repo = "karapace";
       rev = version;
-      sha256 = "0000000000000000000000000000000000000000000000000000";  # Placeholder hash
+      sha256 = "sha256-Gw4R8QZOfP0cqxMPaes0MGOt0Qd4wJn9SWlcGq+D9b8=";  # Placeholder hash
     };
 
-    # propagatedBuildInputs = [ 
-    #   # Add Python dependencies here, if they are not handled by setup.py
-    # ];
+    preBuild = ''
+      export KARAPACE_VERSION="${version}"
+    '';
+
+    propagatedBuildInputs = [
+      python-env
+      pkgs.python311Packages.zstandard
+      pkgs.python311Packages.python-snappy
+      pkgs.python311Packages.typing-extensions
+      pkgs.python311Packages.cachetools
+      pkgs.python311Packages.confluent-kafka
+      pkgs.python311Packages.aiohttp
+      pkgs.python311Packages.aiokafka
+      pkgs.python311Packages.avro
+      pkgs.python311Packages.aiohttp
+      pkgs.python311Packages.jsonschema
+      pkgs.python311Packages.networkx
+      pkgs.python311Packages.protobuf
+      pkgs.python311Packages.pyjwt
+      pkgs.python311Packages.ujson
+      pkgs.python311Packages.sentry-sdk
+      pkgs.python311Packages.python-dateutil
+      pkgs.python311Packages.kafka-python
+    ];
+    doCheck = false;
 
     meta = with lib; {
       description = "Karapace - a Kafka Schema Registry and REST Proxy";
@@ -47,4 +108,5 @@
   };
 
 in
+# python-env
   override-meta new-meta karapace
