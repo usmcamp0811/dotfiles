be7d19119a4ce7f9cc12b0762d71ed1b9c99e024
Matt Camp <matt@aicampground.com>
2024-04-20 14:51:23 -0500

    kafka connect configure

--- modules/nixos/services/kafka-connect/default.nix
+++ modules/nixos/services/kafka-connect/default.nix
@@ -4,9 +4,32 @@ with lib;
 with lib.campground;
 let 
   cfg = config.campground.services.kafka-connect;
+  kafka-connect-config = builtins.concatStringsSep "\n" (
+    builtins.mapAttrsToList (name: value: "${name}=${toString value}") cfg.config
+  );
 in {
   options.campground.services.kafka-connect = with types; {
     enable = mkBoolOpt false "Whether or not to enable Kafka Connect.";
+    config = mkOption {
+      type = types.attrs;
+      default = {
+        "bootstrap.servers" = "localhost:9092";
+        "group.id" = "connect-cluster";
+        "key.converter" = "org.apache.kafka.connect.json.JsonConverter";
+        "value.converter" = "org.apache.kafka.connect.json.JsonConverter";
+        "key.converter.schemas.enable" = true;
+        "value.converter.schemas.enable" = true;
+        "offset.storage.topic" = "connect-offsets";
+        "offset.storage.replication.factor" = 1;
+        "config.storage.topic" = "connect-configs";
+        "config.storage.replication.factor" = 1;
+        "status.storage.topic" = "connect-status";
+        "status.storage.replication.factor" = 1;
+        "offset.flush.interval.ms" = "10000";
+        "listeners" = "HTTP://:8323";
+      };
+      description = "Kafka Connect configuration.";
+    };
   };
 
   config = mkIf cfg.enable {
@@ -25,9 +48,11 @@ in {
       requires = [ "apache-kafka.service" ];
       wantedBy = [ "multi-user.target" ];
       script = ''
+        # Write the Kafka Connect configuration file
+        echo ${kafka-connect-config} > /var/lib/apache-kafka/kafka-connect.cfg
         export KAFKA_HEAP_OPTS="-Xmx1G -Xms1G"
         export KAFKA_LOG_DIR="/var/log/apache-kafka"
-        ${pkgs.apacheKafka}/bin/connect-distributed.sh ${pkgs.apacheKafka}/config/connect-distributed.properties
+        ${pkgs.apacheKafka}/bin/connect-distributed.sh /var/lib/apache-kafka/kafka-connect.cfg
       '';
       serviceConfig = {
         Restart = "always";
