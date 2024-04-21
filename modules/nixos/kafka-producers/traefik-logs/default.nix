03947b13c5906a1fad467c8761886fb9028f3c23
Matt Camp <matt@aicampground.com>
2024-04-21 10:27:01 -0500

    doh

--- modules/nixos/kafka-producers/traefik-logs/default.nix
+++ modules/nixos/kafka-producers/traefik-logs/default.nix
@@ -28,7 +28,7 @@ in {
       };
       script = ''
         ${pkgs.inotify-tools}/bin/inotifywait -m $TRAEFIK_LOG -e modify --format '%w%f' | while read path; do
-          (tail -n 1 $TRAEFIK_LOG | ${pkgs.kt}/bin/kt produce ) && (sed -i '$d' testlog >/dev/null)
+          (tail -n 1 $TRAEFIK_LOG | ${pkgs.kt}/bin/kt produce ) && (sed -i '$d' $TRAEFIK_LOG >/dev/null)
           echo "Logged"
         done
       '';
