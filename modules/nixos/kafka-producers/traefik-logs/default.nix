858c8e84ae85bb024d5ee7007a362242463dc75a
Matt Camp <matt@aicampground.com>
2024-04-22 18:08:24 -0500

    trying ot catch all the things

--- modules/nixos/kafka-producers/traefik-logs/default.nix
+++ modules/nixos/kafka-producers/traefik-logs/default.nix
@@ -28,7 +28,7 @@ in {
       };
       script = ''
         while true; do
-          ${pkgs.inotify-tools}/bin/inotifywait -m $TRAEFIK_LOG -e modify --format '%w%f' | while read path; do
+          ${pkgs.inotify-tools}/bin/inotifywait -m $TRAEFIK_LOG -e access -e modify -e open --format '%w%f' | while read path; do
             (tail -n 1 $TRAEFIK_LOG | ${pkgs.kt}/bin/kt produce -literal) && (sed -i '$d' $TRAEFIK_LOG >/dev/null)
           done
         done
