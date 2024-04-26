33802e0dcfead60bd51405b7d28856655b4bc7ae
Matt Camp <matt@aicampground.com>
2024-04-25 20:38:48 -0500

    this builds

--- packages/example-flink-job/default.nix
+++ packages/example-flink-job/default.nix
@@ -36,17 +36,16 @@ let
   src = ./.;
 
   # ${pkgs.flink}/bin/flink run -py ${src}/job/job.py -pyclientexec ${python-env}/bin/python
-  # ${python-env}/bin/python ${src}/job/job.py --jobname "example-flink-job" --inputtopic "example-topic" --outputtopic "example-output" --errortopic "example-error" --kafka_server "10.8.0.70:9092"
+  # ${pkgs.flink}/bin/flink run -py ${src}/job/job.py -pyexec ${python-env}/bin/python3 --jobname "example-flink-job" --inputtopic "example-topic" --outputtopic "example-output" --errortopic "example-error" --kafka_server "10.8.0.70:9092"
   flink-job = pkgs.writeShellScriptBin "flink-job" ''
-    export JAVA_HOME=${pkgs.jdk11.home}
-    export FLINK_HOME=${pkgs.flink}
-    export CLASSPATH="${pkgs.campground.flink-connector-kafka}:$CLASSPATH"
-    ${pkgs.flink}/bin/flink run -py ${src}/job/job.py -pyclientexec ${python-env}/bin/python -j ${pkgs.campground.flink-connector-kafka}/lib --jobname "example-flink-job" --inputtopic "example-topic" --outputtopic "example-output" --errortopic "example-error" --kafka_server "10.8.0.70:9092"
+      export JAVA_HOME=${pkgs.jdk11.home}
+      export CLASSPATH=${pkgs.campground.flink-connector-kafka}/lib
+    ${python-env}/bin/python ${src}/job/job.py --jobname "example-flink-job" --inputtopic "example-topic" --outputtopic "example-output" --errortopic "example-error" --kafka_server "10.8.0.70:9092"
   '';
 
   run-tests = pkgs.writeShellScriptBin "run-tests" ''
     export JAVA_HOME=${pkgs.jdk11.home}
-    export FLINK_HOME=${pkgs.campground.flink-connector-kafka}/lib
+    export CLASSPATH=${pkgs.campground.flink-connector-kafka}/lib
     # Resolves the symlink to find the actual path of the script
     SCRIPT=$(readlink -f "$0" || realpath "$0")
     SCRIPT_DIR=$(dirname "$SCRIPT")
@@ -82,12 +81,12 @@ let
   example-flink-job = pkgs.stdenv.mkDerivation {
     name = "example-flink-job";
     src = src;
-    propagatedBuildInputs = [
-      pkgs.openjdk11
-      pkgs.flink
-      python-env
-      pkgs.campground.flink-connector-kafka
-    ];
+    # propagatedBuildInputs = [
+    #   pkgs.openjdk11
+    #   pkgs.flink
+    #   python-env
+    #   pkgs.campground.flink-connector-kafka
+    # ];
     phases = [ "installPhase" ];
     installPhase = ''
       mkdir -p $out/bin
---
+++ packages/flink/default.nix
@@ -0,0 +1,34 @@
+{ lib, writeText, writeShellApplication, substituteAll, gum, inputs, pkgs
+, hosts ? { }, ... }:
+let
+  inherit (lib) mapAttrsToList concatStringsSep;
+  inherit (lib.campground) override-meta;
+
+  pname = "flink-sql-connector-kafka";
+  version = "3.0.2-1.18";
+  kafkaPlugin = pkgs.fetchurl {
+    url =
+      "https://repo.maven.apache.org/maven2/org/apache/flink/${pname}/${version}/${pname}-${version}.jar";
+    sha256 = "sha256-b9NgGdoshvz4VFj0N2F6vKWjQnwjAoJg99OQ6tidnVI=";
+  };
+
+  flink = pkgs.stdenv.mkDerivation rec {
+    inherit pname version;
+    src = pkgs.flink;
+
+    installPhase = ''
+      mkdir -p $out/opt/flink/opt
+      cp -r $src/* $out/
+      cp ${kafkaPlugin} $out/opt/flink/opt/${pname}-${version}.jar
+    '';
+  };
+
+  new-meta = with lib; {
+    description = "A distributed stream processing framework";
+    homepage = "https://flink.apache.org";
+    downloadPage = "https://flink.apache.org/downloads.html";
+    license = licenses.asl20;
+    platforms = platforms.all;
+    maintainers = with maintainers; [ mattcamp ];
+  };
+in override-meta new-meta flink
