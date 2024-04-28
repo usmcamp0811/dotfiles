2e49ac530e03c9143bbd5ef9f882fdd5cbb58fc2
Matt Camp <matt@aicampground.com>
2024-04-28 09:17:07 -0500

    wip

--- modules/nixos/services/flink/default.nix
+++ modules/nixos/services/flink/default.nix
@@ -1,4 +1,4 @@
-{ lib, config, pkgs, ... }:
+{ host ? "", lib, config, pkgs, ... }:
 with lib;
 with lib.campground;
 let cfg = config.campground.services.flink;
@@ -6,24 +6,22 @@ in {
   options.campground.services.flink = with types; {
     enable = mkBoolOpt false "Apache Flink service";
     package = mkOpt package pkgs.flink "The Flink package to use.";
-    masters = mkOpt (lib.types.listOf lib.types.str)  [ "lucas" ] "Mast Flink Node";
-    workers = mkOpt (lib.types.listOf lib.types.str) [
-      "lucas"
-    ] "Worker Nodes";
+    masters = mkOpt (lib.types.listOf lib.types.str) [ host ] "Mast Flink Node";
+    workers = mkOpt (lib.types.listOf lib.types.str) [ host ] "Worker Nodes";
     flink-conf = mkOpt str ''
       env.java.opts.all: --add-exports=java.base/sun.net.util=ALL-UNNAMED --add-exports=java.rmi/sun.rmi.registry=ALL-UNNAMED --add-exports=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED --add-exports=jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED --add-exports=jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED --add-exports=jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED --add-exports=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED --add-exports=java.security.jgss/sun.security.krb5=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.net=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED --add-opens=java.base/java.nio=ALL-UNNAMED --add-opens=java.base/sun.nio.ch=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.text=ALL-UNNAMED --add-opens=java.base/java.time=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.util.concurrent=ALL-UNNAMED --add-opens=java.base/java.util.concurrent.atomic=ALL-UNNAMED --add-opens=java.base/java.util.concurrent.locks=ALL-UNNAMED
-      jobmanager.rpc.address: lucas
+      jobmanager.rpc.address: ${host}
       jobmanager.rpc.port: 6123
       jobmanager.bind-host: 0.0.0.0
       jobmanager.memory.process.size: 1600m
       taskmanager.bind-host: 0.0.0.0
-      taskmanager.host: lucas
+      taskmanager.host: ${host}
       taskmanager.memory.process.size: 1728m
       taskmanager.numberOfTaskSlots: 4
       parallelism.default: 1
       jobmanager.execution.failover-strategy: region
       rest.port: 8081
-      rest.address: lucas
+      rest.address: ${host}
       rest.bind-port: 8080-8090
       rest.bind-address: 0.0.0.0
       env.log.dir: /var/lib/flink/flink-logs
@@ -58,11 +56,11 @@ in {
         # User = "flink";
         # Group = "flink";
         # ExecStart = "${cfg.package}/opt/flink/bin/start-cluster.sh";
-        # ExecStop = "${cfg.package}/opt/flink/bin/stop-cluster.sh";
+        ExecStop = "${pkgs.flink}/opt/flink/bin/jobmanager.sh stop";
         Restart = "on-failure";
         PermissionsStartOnly = true;
       };
-        # "${cfg.package}/opt/flink/bin/standalone-job.sh start-foreground";
+      # "${cfg.package}/opt/flink/bin/standalone-job.sh start-foreground";
       script = ''
         export PATH=${pkgs.openssh}/bin:$PATH
         cp ${pkgs.campground.example-flink-job}/src/job/job.py /tmp/pyflink/
@@ -78,8 +76,12 @@ in {
         mkdir -p /var/lib/flink/conf
         mkdir -p /var/lib/flink/flink-logs
         cp -r ${pkgs.flink}/opt/flink/conf/* /var/lib/flink/conf/
-        echo "${ lib.concatStringsSep "\n" cfg.masters }" > /var/lib/flink/conf/masters
-        echo "${ lib.concatStringsSep "\n" cfg.workers }" > /var/lib/flink/conf/workers
+        echo "${
+          lib.concatStringsSep "\n" cfg.masters
+        }" > /var/lib/flink/conf/masters
+        echo "${
+          lib.concatStringsSep "\n" cfg.workers
+        }" > /var/lib/flink/conf/workers
         echo "${cfg.flink-conf}" > /var/lib/flink/conf/flink-conf.yaml
       '';
     };
--- packages/example-flink-job/default.nix
+++ packages/example-flink-job/default.nix
@@ -35,13 +35,15 @@ let
 
   src = ./.;
 
+  # --jobname example-flink-job --inputtopic example-topic --outputtopic example-output --errortopic example-error --kafka_server lucas:9092
   flink-job = pkgs.writeShellScriptBin "flink-job" ''
+    # ${pkgs.flink}/opt/flink/bin/jobmanager.sh start
     ${pkgs.flink}/bin/flink run \
       -py ${src}/job/job.py \
       -pyclientexec ${python-env}/bin/python \
       -pypath ${python-env} \
-      --jarfile ${pkgs.campground.flink-connector-kafka} \
-      --jobname example-flink-job --inputtopic example-topic --outputtopic example-output --errortopic example-error --kafka_server lucas:9092
+      --jarfile ${pkgs.campground.flink-connector-kafka}
+    # ${pkgs.flink}/opt/flink/bin/jobmanager.sh stop
   '';
 
   run-tests = pkgs.writeShellScriptBin "run-tests" ''
