51b60dea15777c77056aaecbc717c4983b1e1402
Matt Camp <matt@aicampground.com>
2024-04-27 20:01:17 -0500

    wip

--- modules/nixos/services/flink/default.nix
+++ modules/nixos/services/flink/default.nix
@@ -8,22 +8,21 @@ in {
     package = mkOpt package pkgs.flink "The Flink package to use.";
     config = mkOpt str ''
       env.java.opts.all: --add-exports=java.base/sun.net.util=ALL-UNNAMED --add-exports=java.rmi/sun.rmi.registry=ALL-UNNAMED --add-exports=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED --add-exports=jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED --add-exports=jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED --add-exports=jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED --add-exports=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED --add-exports=java.security.jgss/sun.security.krb5=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.net=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED --add-opens=java.base/java.nio=ALL-UNNAMED --add-opens=java.base/sun.nio.ch=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.text=ALL-UNNAMED --add-opens=java.base/java.time=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.util.concurrent=ALL-UNNAMED --add-opens=java.base/java.util.concurrent.atomic=ALL-UNNAMED --add-opens=java.base/java.util.concurrent.locks=ALL-UNNAMED
-      jobmanager.rpc.address: localhost
+      jobmanager.rpc.address: lucas
       jobmanager.rpc.port: 6123
       jobmanager.bind-host: 0.0.0.0
       jobmanager.memory.process.size: 1600m
       taskmanager.bind-host: 0.0.0.0
-      taskmanager.host: localhost
+      taskmanager.host: lucas
       taskmanager.memory.process.size: 1728m
       taskmanager.numberOfTaskSlots: 4
       parallelism.default: 1
       jobmanager.execution.failover-strategy: region
       rest.port: 8081
-      rest.address: localhost
+      rest.address: lucas
       rest.bind-port: 8080-8090
       rest.bind-address: 0.0.0.0
-      env.java.home: /nix/store/6375rn8kiq9pn4pgdkdiqgvg8b0gdycy-openjdk-19.0.2+7
-      env.log.dir: /tmp/flink-logs
+      env.log.dir: /var/lib/flink/flink-logs
     '' "Additional configuration attributes for Flink.";
   };
 
@@ -47,7 +46,7 @@ in {
       wantedBy = [ "multi-user.target" ];
       environment = {
         FLINK_CONF_DIR = "/var/lib/flink/conf";
-        JAVA_HOME = pkgs.jre;
+        JAVA_HOME = pkgs.openjdk11;
       };
       serviceConfig = {
         User = "flink";
@@ -59,6 +58,7 @@ in {
       };
       preStart = ''
         mkdir -p /var/lib/flink/conf
+        mkdir -p /var/lib/flink/flink-logs
         cp -r ${pkgs.flink}/opt/flink/conf/* /var/lib/flink/conf/
         echo "${cfg.config}" > /var/lib/flink/conf/flink-conf.yaml
       '';
