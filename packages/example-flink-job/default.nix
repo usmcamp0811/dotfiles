782ab3ae8030dd3703a9b4c6d48bb41238554aa8
Matt Camp <matt@aicampground.com>
2024-05-05 11:47:13 -0500

    wip

--- packages/example-flink-job/default.nix
+++ packages/example-flink-job/default.nix
@@ -56,6 +56,7 @@ let
         echo "BROKER already set to $BROKER"
     fi
 
+    export PATH=${pkgs.campground.example-flink-job.python}/bin/python:$PATH
     export PYTHONPATH="${pkgs.campground.example-flink-job.python}/lib/python3.11/site-packages"
     export PYFLINK_PYTHON="${pkgs.campground.example-flink-job.python}/bin/python"
     ${pkgs.flink}/bin/flink run \
@@ -85,6 +86,7 @@ let
         echo "BROKER already set to $BROKER"
     fi
 
+    export PATH=${pkgs.campground.example-flink-job.python}/bin/python:$PATH
     export PYTHONPATH="${pkgs.campground.example-flink-job.python}/lib/python3.11/site-packages"
     export PYFLINK_PYTHON="${pkgs.campground.example-flink-job.python}/bin/python"
     ${pkgs.flink}/bin/flink run \
