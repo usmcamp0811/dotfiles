5905ac35fd4dd64ac1e474a5a08cd326123f1816
Matt Camp <matt@aicampground.com>
2024-05-05 21:32:55 -0500

    trying a different version of hte connector

--- packages/flink-connector-kafka/default.nix
+++ packages/flink-connector-kafka/default.nix
@@ -5,10 +5,14 @@ let
   inherit (lib.campground) override-meta;
 
   kafka-jar = "flink-sql-connector-kafka";
-  jar-version = "3.0.2-1.18";
+
+  # jar-version = "3.0.2-1.18";
+    # sha256 = "sha256-b9NgGdoshvz4VFj0N2F6vKWjQnwjAoJg99OQ6tidnVI=";
+  jar-version = "1.15.4";
+
   kafkaPlugin = pkgs.fetchurl {
     url =
       "https://repo.maven.apache.org/maven2/org/apache/flink/${kafka-jar}/${jar-version}/${kafka-jar}-${jar-version}.jar";
-    sha256 = "sha256-b9NgGdoshvz4VFj0N2F6vKWjQnwjAoJg99OQ6tidnVI=";
+    sha256 = "sha256-G4kaFhTXCvjYGGYngP8BB9YOZDyi/bo51U5axcBT/dQ=";
   };
 in kafkaPlugin
