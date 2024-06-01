024181d79e1dc84d72d697d7abeb446907551ca1
Matt Camp <matt@aicampground.com>
2024-06-01 15:59:45 -0500

    updating to 24.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Campground Config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     #nuenv
