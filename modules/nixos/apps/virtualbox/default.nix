57013e7d2f20c25b18d96cc71ced8e1f1acd06bd
Matt <gitlab@aicampground.com>
2024-06-02 03:08:10 +0000

    Nix os 24.05

--- modules/nixos/apps/virtualbox/default.nix
+++ modules/nixos/apps/virtualbox/default.nix
@@ -1,13 +1,7 @@
-{
-  options,
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+{ options, config, lib, pkgs, ... }:
 with lib;
-with lib.campground; let
-  cfg = config.campground.apps.virtualbox;
+with lib.campground;
+let cfg = config.campground.apps.virtualbox;
 in {
   options.campground.apps.virtualbox = with types; {
     enable = mkBoolOpt false "Whether or not to enable Virtualbox.";
@@ -20,8 +14,7 @@ in {
       headless = true;
     };
     virtualisation.virtualbox.guest.enable = true;
-    virtualisation.virtualbox.guest.x11 = true;
-    campground.user.extraGroups = ["vboxusers"];
-    environment.systemPackages = [pkgs.virtualbox];
+    campground.user.extraGroups = [ "vboxusers" ];
+    environment.systemPackages = [ pkgs.virtualbox ];
   };
 }
