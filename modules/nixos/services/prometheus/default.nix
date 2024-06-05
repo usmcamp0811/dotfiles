59878e5f8b9e3d84761ceebda7a2f7bf76c16604
Matt Camp <matt@aicampground.com>
2024-06-04 21:44:37 -0500

    ok

--- modules/nixos/services/prometheus/default.nix
+++ modules/nixos/services/prometheus/default.nix
@@ -1,12 +1,13 @@
-{ lib, config, ... }:
+{ options, config, lib, ... }:
 with lib;
+with lib.campground;
 let cfg = config.campground.services.prometheus;
 in {
-  options.campground.services.prometheus = {
-    enable = mkEnableOption "Prometheus";
+  options.campground.services.prometheus = with types; {
+    enable = mkBoolOpt false "Enable an Prometheus";
     port = mkOpt int 9001 "Port to Host the Prometheus server on.";
     exporter-port = mkOpt int 9002 "Port to Host the Prometheus exporter on.";
-    host = mkOpt str config.networking.hostName
+    hostName = mkOpt str config.networking.hostName
       "The hostname or ip to use for Prometheus.";
   };
 
@@ -18,14 +19,14 @@ in {
         node = {
           enable = true;
           enabledCollectors = [ "systemd" ];
-          port = cfg.exporter-port;
+          port = 9002;
         };
       };
       scrapeConfigs = [{
-        job_name = "${cfg.host}-system-monitor";
+        job_name = "${cfg.hostName}-system-monitor";
         static_configs = [{
           targets = [
-            "${cfg.host}:${
+            "${cfg.hostName}:${
               toString config.services.prometheus.exporters.node.port
             }"
           ];
