ea16c855b11626eff707d183ba3d36fb845ed2e5
Matt Camp <matt@aicampground.com>
2024-06-05 22:37:47 -0500

    wip

--- modules/nixos/services/grafana/default.nix
+++ modules/nixos/services/grafana/default.nix
@@ -1,28 +1,30 @@
-{ lib
-, config
-, ...
-}:
+{ lib, config, ... }:
 with lib;
-with lib.campground; let
-  cfg = config.campground.services.grafana;
-in
-{
+with lib.campground;
+let cfg = config.campground.services.grafana;
+in {
   options.campground.services.grafana = with types; {
     enable = mkBoolOpt false "Enable an Grafana;";
     port = mkOpt int 7443 "Port to Host the grafana server on.";
-    domain =
-      mkOpt str "grafana.lan.aicampground.com"
-        "Domain to Host the grafana server on.";
+    provision = mkOption {
+      type = attrs;
+      default = {
+        enable = false;
+        datasources = [ ];
+      };
+      description = "Grafana provisioning configuration";
+    };
+    domain = mkOpt str "grafana.lan.aicampground.com"
+      "Domain to Host the grafana server on.";
 
     role-id =
       mkOpt str config.campground.services.vault-agent.settings.vault.role-id
-        "Absolute path to the Vault role-id";
+      "Absolute path to the Vault role-id";
     secret-id =
       mkOpt str config.campground.services.vault-agent.settings.vault.secret-id
-        "Absolute path to the Vault secret-id";
-    vault-path =
-      mkOpt str "secret/campground/grafana"
-        "The Vault path to the KV containing the KVs that are for each database";
+      "Absolute path to the Vault secret-id";
+    vault-path = mkOpt str "secret/campground/grafana"
+      "The Vault path to the KV containing the KVs that are for each database";
     kvVersion = mkOption {
       type = enum [ "v1" "v2" ];
       default = "v2";
@@ -38,6 +40,7 @@ in
   config = mkIf cfg.enable {
     services.grafana = {
       enable = true;
+      provision = cfg.provision;
       settings = {
         security = {
           admin_user = "$__env{ADMIN_USER}";
@@ -61,16 +64,14 @@ in
       settings = {
         vault.address = cfg.vault-address;
         auto_auth = {
-          method = [
-            {
-              type = "approle";
-              config = {
-                role_id_file_path = cfg.role-id;
-                secret_id_file_path = cfg.secret-id;
-                remove_secret_id_file_after_reading = false;
-              };
-            }
-          ];
+          method = [{
+            type = "approle";
+            config = {
+              role_id_file_path = cfg.role-id;
+              secret_id_file_path = cfg.secret-id;
+              remove_secret_id_file_after_reading = false;
+            };
+          }];
         };
       };
       secrets.environment.templates = {
--- systems/x86_64-linux/webb/default.nix
+++ systems/x86_64-linux/webb/default.nix
@@ -64,7 +64,26 @@ with lib.campground;
       ldap-client = { enable = mkForce false; };
       netbird = enabled;
       uptime-kuma = enabled;
-      grafana = enabled;
+      grafana = {
+        enable = true;
+        provision = {
+          enable = true;
+          datasources = [
+            {
+              name = "Prometheus";
+              type = "prometheus";
+              access = "proxy";
+              url = "http://webb:9011";
+            }
+            {
+              name = "Loki";
+              type = "loki";
+              access = "proxy";
+              url = "http://webb:3030";
+            }
+          ];
+        };
+      };
       prometheus = enabled;
       loki = enabled;
       # keycloak = {
