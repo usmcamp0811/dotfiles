58f5a1a239ca579fa834f80e444eee3398a7238a
Matt Camp <matt@aicampground.com>
2024-06-06 22:38:54 -0500

    doh for got to rename the copy 🍝

--- modules/nixos/suites/observability/default.nix
+++ modules/nixos/suites/observability/default.nix
@@ -1,9 +1,9 @@
 { options, config, lib, ... }:
 with lib;
 with lib.campground;
-let cfg = config.campground.suites.public-hosting;
+let cfg = config.campground.suites.observability;
 in {
-  options.campground.suites.public-hosting = with types; {
+  options.campground.suites.observability = with types; {
     enable =
       mkBoolOpt false "Whether or not to enable observability reporters.";
     loki-uri = mkOpt str "webb:3030" "The <host>:<port> of the Loki server";
