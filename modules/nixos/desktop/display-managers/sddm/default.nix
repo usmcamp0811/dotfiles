1e0a86dc0343b217648456e45227f3053e165982
Matt Camp <matt@aicampground.com>
2024-06-07 21:18:12 -0500

    wip add sddm

---
+++ modules/nixos/desktop/display-managers/sddm/default.nix
@@ -0,0 +1,79 @@
+{ config, lib, options, pkgs, ... }:
+with lib;
+with lib.campground;
+let
+  cfg = config.campground.desktop.display-manager.sddm;
+  sddmHome = config.users.users.sddm.home;
+in {
+  options.campground.desktop.display-manager.sddm = with types; {
+    enable = mkBoolOpt false "Whether or not to enable sddm.";
+    autoSuspend =
+      mkBoolOpt false "Whether or not to suspend the machine after inactivity.";
+    defaultSession = mkOpt (nullOr str) null "The default session to use.";
+    theme = mkOpt (nullOr str) null "The theme to use.";
+  };
+
+  config = mkIf cfg.enable {
+    systemd.tmpfiles.rules = [ "d ${sddmHome}/.config 0711 sddm sddm" ];
+
+    services.xserver = {
+      enable = true;
+
+      displayManager = {
+        inherit (cfg) defaultSession;
+
+        sddm = { inherit (cfg) enable autoSuspend theme; };
+      };
+
+      libinput.enable = true;
+    };
+
+    systemd.services.campground-user-icon = {
+      before = [ "display-manager.service" ];
+      wantedBy = [ "display-manager.service" ];
+
+      script =
+        # bash
+        ''
+          config_file=/var/lib/AccountsService/users/${config.campground.user.name}
+          icon_file=/run/current-system/sw/share/icons/user/${config.campground.user.name}/${config.campground.user.icon.fileName}
+
+          if ! [ -d "$(dirname "$config_file")" ]; then
+            mkdir -p "$(dirname "$config_file")"
+          fi
+
+          if ! [ -f "$config_file" ]; then
+            echo "[User]
+            Session=${cfg.defaultSession or "plasma"}
+            SystemAccount=false
+            Icon=$icon_file" > "$config_file"
+          else
+            icon_config=$(sed -E -n -e "/Icon=.*/p" $config_file)
+
+            if [[ "$icon_config" == "" ]]; then
+              echo "Icon=$icon_file" >> $config_file
+            else
+              sed -E -i -e 's#^Icon=.*$#Icon=$icon_file#' $config_file
+            fi
+          fi
+        '';
+
+      serviceConfig = {
+        Type = "simple";
+        User = "root";
+        Group = "root";
+      };
+    };
+
+    system.activationScripts.postInstallSddm = stringAfter [ "users" ] # bash
+      ''
+        echo "Setting sddm permissions for user icon"
+        ${
+          getExe' pkgs.acl "setfacl"
+        } -m u:sddm:x /home/${config.campground.user.name}
+        ${
+          getExe' pkgs.acl "setfacl"
+        } -m u:sddm:r /home/${config.campground.user.name}/.face || true
+      '';
+  };
+}
