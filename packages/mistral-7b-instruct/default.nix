a8062507dc6c58ef8f9e3e5697eb2d62fe64eace
Matt Camp <matt@aicampground.com>
2024-06-08 20:23:07 -0500

    update sha

--- packages/mistral-7b-instruct/default.nix
+++ packages/mistral-7b-instruct/default.nix
@@ -6,13 +6,11 @@ let
 
   name = "mistral-7b-instruct";
 
-  # jar-version = "3.0.2-1.18";
-  # sha256 = "sha256-b9NgGdoshvz4VFj0N2F6vKWjQnwjAoJg99OQ6tidnVI=";
   version = "0.1.Q4_K_M";
 
   mistral-model = pkgs.fetchurl {
     url =
       "https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.1-GGUF/raw/main/mistral-7b-instruct-v${version}.gguf";
-    sha256 = "";
+    sha256 = "sha256-0UK4Qw6ZBhnpS3WK5/gZWlf6Ek7A6lF6I6eV1949UHE=";
   };
 in mistral-model
