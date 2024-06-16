6411204402ea0bbd6aafee7616cd45da1c1be7cf
Matt Camp <matt@aicampground.com>
2024-06-16 07:47:10 -0500

    add simple login

--- modules/home/apps/firefox/default.nix
+++ modules/home/apps/firefox/default.nix
@@ -1,13 +1,7 @@
-{
-  options,
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+{ options, config, lib, pkgs, ... }:
 with lib;
-with lib.campground; let
-  cfg = config.campground.apps.firefox;
+with lib.campground;
+let cfg = config.campground.apps.firefox;
 in {
   options.campground.apps.firefox = with types; {
     enable = mkBoolOpt false "Whether or not to enable Firefox.";
@@ -35,49 +29,46 @@ in {
           search = {
             force = true;
             default = "Searx";
-            order = ["Searx" "Google"];
+            order = [ "Searx" "Google" ];
             engines = {
               "Nix Packages" = {
-                urls = [
-                  {
-                    template = "https://search.nixos.org/packages";
-                    params = [
-                      {
-                        name = "type";
-                        value = "packages";
-                      }
-                      {
-                        name = "query";
-                        value = "{searchTerms}";
-                      }
-                    ];
-                  }
-                ];
-                icon = "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-                definedAliases = ["@np"];
+                urls = [{
+                  template = "https://search.nixos.org/packages";
+                  params = [
+                    {
+                      name = "type";
+                      value = "packages";
+                    }
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }];
+                icon =
+                  "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+                definedAliases = [ "@np" ];
               };
               "NixOS Wiki" = {
-                urls = [
-                  {
-                    template = "https://nixos.wiki/index.php?search={searchTerms}";
-                  }
-                ];
+                urls = [{
+                  template =
+                    "https://nixos.wiki/index.php?search={searchTerms}";
+                }];
                 iconUpdateURL = "https://nixos.wiki/favicon.png";
                 updateInterval = 24 * 60 * 60 * 1000; # every day
-                definedAliases = ["@nw"];
+                definedAliases = [ "@nw" ];
               };
               "Searx" = {
-                urls = [
-                  {
-                    template = "https://searx.aicampground.com/?q={searchTerms}";
-                  }
-                ];
+                urls = [{
+                  template = "https://searx.aicampground.com/?q={searchTerms}";
+                }];
                 iconUpdateURL = "https://nixos.wiki/favicon.png";
                 updateInterval = 24 * 60 * 60 * 1000; # every day
-                definedAliases = ["@searx"];
+                definedAliases = [ "@searx" ];
               };
               "Bing".metaData.hidden = true;
-              "Google".metaData.alias = "@g"; # builtin engines only support specifying one additional alias
+              "Google".metaData.alias =
+                "@g"; # builtin engines only support specifying one additional alias
             };
           };
           extensions = with pkgs.nur.repos.rycee.firefox-addons; [
@@ -85,6 +76,7 @@ in {
             bitwarden
             darkreader
             vimium
+            simplelogin
           ];
         };
       };
