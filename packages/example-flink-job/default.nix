0ee4bbd9ecfda3c9761b826f2d086237ed2b2821
Matt Camp <matt@aicampground.com>
2024-06-17 20:07:18 -0500

    wtf mac

--- packages/example-flink-job/default.nix
+++ packages/example-flink-job/default.nix
@@ -42,55 +42,55 @@ let
   src = ./.;
 
   # TODO: Figure out what is not needed
-  flink-conf = pkgs.writeTextFile {
-    name = "flink-conf.yaml";
-    text = ''
-      env.java.opts.all: --add-exports=java.base/sun.net.util=ALL-UNNAMED --add-exports=java.rmi/sun.rmi.registry=ALL-UNNAMED --add-exports=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED --add-exports=jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED --add-exports=jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED --add-exports=jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED --add-exports=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED --add-exports=java.security.jgss/sun.security.krb5=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.net=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED --add-opens=java.base/java.nio=ALL-UNNAMED --add-opens=java.base/sun.nio.ch=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.text=ALL-UNNAMED --add-opens=java.base/java.time=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.util.concurrent=ALL-UNNAMED --add-opens=java.base/java.util.concurrent.atomic=ALL-UNNAMED --add-opens=java.base/java.util.concurrent.locks=ALL-UNNAMED
-      jobmanager.rpc.address: localhost
-      jobmanager.rpc.port: 6123
-      jobmanager.bind-host: 0.0.0.0
-      jobmanager.memory.process.size: 1600m
-      taskmanager.bind-host: 0.0.0.0
-      taskmanager.host: localhost
-      taskmanager.memory.process.size: 1728m
-      taskmanager.numberOfTaskSlots: 1
-      parallelism.default: 1
-      jobmanager.execution.failover-strategy: region
-      rest.address: localhost
-      rest.port: 8081
-      rest.bind-address: 0.0.0.0
-      env.log.dir: /tmp/flink-logs
-      env.java.home: ${pkgs.openjdk11}
-      env.path: ${python-env}/bin/:$PATH
-      python.path: ${python-env}/lib/python3.11/site-packages
-      python.executable: ${python-env}/bin/python
-      python.client.executable: ${python-env}/bin/python
-    '';
-  };
+  # flink-conf = pkgs.writeTextFile {
+  #   name = "flink-conf.yaml";
+  #   text = ''
+  #     env.java.opts.all: --add-exports=java.base/sun.net.util=ALL-UNNAMED --add-exports=java.rmi/sun.rmi.registry=ALL-UNNAMED --add-exports=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED --add-exports=jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED --add-exports=jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED --add-exports=jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED --add-exports=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED --add-exports=java.security.jgss/sun.security.krb5=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.net=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED --add-opens=java.base/java.nio=ALL-UNNAMED --add-opens=java.base/sun.nio.ch=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.text=ALL-UNNAMED --add-opens=java.base/java.time=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.util.concurrent=ALL-UNNAMED --add-opens=java.base/java.util.concurrent.atomic=ALL-UNNAMED --add-opens=java.base/java.util.concurrent.locks=ALL-UNNAMED
+  #     jobmanager.rpc.address: localhost
+  #     jobmanager.rpc.port: 6123
+  #     jobmanager.bind-host: 0.0.0.0
+  #     jobmanager.memory.process.size: 1600m
+  #     taskmanager.bind-host: 0.0.0.0
+  #     taskmanager.host: localhost
+  #     taskmanager.memory.process.size: 1728m
+  #     taskmanager.numberOfTaskSlots: 1
+  #     parallelism.default: 1
+  #     jobmanager.execution.failover-strategy: region
+  #     rest.address: localhost
+  #     rest.port: 8081
+  #     rest.bind-address: 0.0.0.0
+  #     env.log.dir: /tmp/flink-logs
+  #     env.java.home: ${pkgs.openjdk11}
+  #     env.path: ${python-env}/bin/:$PATH
+  #     python.path: ${python-env}/lib/python3.11/site-packages
+  #     python.executable: ${python-env}/bin/python
+  #     python.client.executable: ${python-env}/bin/python
+  #   '';
+  # };
 
-  flink-conf-dir = pkgs.stdenv.mkDerivation {
-    name = "flink-conf-drv";
-    src = src;
-    phases = [ "installPhase" ];
-    installPhase = ''
-      mkdir -p $out/conf
-      # Iterate over each file in the source directory
-      for file in "${pkgs.flink}/opt/flink/conf"/*; do
-          # Get the basename of the file
-          basefile=$(basename "$file")
-          if [ "$basefile" == "flink-conf.yaml" ]; then
-              continue
-          fi
-          if [ "$basefile" == "config.yaml" ]; then
-              continue
-          fi
-          # Create the symbolic link in the destination directory
-          ln -s "$file" "$out/conf/$basefile"
-      done
-      cp ${flink-conf} $out/conf/flink-conf.yaml
-      cp ${flink-conf} $out/conf/config.yaml
-    '';
-  };
+  # flink-conf-dir = pkgs.stdenv.mkDerivation {
+  #   name = "flink-conf-drv";
+  #   src = src;
+  #   phases = [ "installPhase" ];
+  #   installPhase = ''
+  #     mkdir -p $out/conf
+  #     # Iterate over each file in the source directory
+  #     for file in "${pkgs.flink}/opt/flink/conf"/*; do
+  #         # Get the basename of the file
+  #         basefile=$(basename "$file")
+  #         if [ "$basefile" == "flink-conf.yaml" ]; then
+  #             continue
+  #         fi
+  #         if [ "$basefile" == "config.yaml" ]; then
+  #             continue
+  #         fi
+  #         # Create the symbolic link in the destination directory
+  #         ln -s "$file" "$out/conf/$basefile"
+  #     done
+  #     cp ${flink-conf} $out/conf/flink-conf.yaml
+  #     cp ${flink-conf} $out/conf/config.yaml
+  #   '';
+  # };
 
   job = pkgs.writeShellScriptBin "job" ''
     # Check if FLINK_CONF_DIR is unset or empty
@@ -174,7 +174,6 @@ let
       cp -r ${pkgs.flink}/opt/flink $out/opt/
       cp -r ${python-env}/bin/* $out/bin/
       cp ${job}/bin/job $out/bin/example-flink-job
-      cp -r ${flink-conf-dir}/conf $out/
     '';
 
     # passthru = {
