22666706766b16ff225e7f2e34ade98a3f962958
Matt Camp <matt@aicampground.com>
2024-06-17 20:11:47 -0500

    where is the issue

--- packages/example-flink-job/default.nix
+++ packages/example-flink-job/default.nix
@@ -41,35 +41,6 @@ let
 
   src = ./.;
 
-  job = pkgs.writeShellScriptBin "job" ''
-    # Check if FLINK_CONF_DIR is unset or empty
-    if [ -z "$FLINK_CONF_DIR" ]; then
-        echo "FLINK_CONF_DIR set to $FLINK_CONF_DIR"
-    else
-        echo "FLINK_CONF_DIR already set to $FLINK_CONF_DIR"
-    fi
-    if [ -z "$KAFKA_BROKER" ]; then
-        export KAFKA_BROKER="localhost:9092";
-        echo "KAFKA_BROKER set to $KAFKA_BROKER"
-    else
-        echo "KAFKA_BROKER already set to $KAFKA_BROKER"
-    fi
-
-    export PATH=${python-env}/bin/:$PATH
-    export PYTHONPATH="${python-env}/lib/python3.11/site-packages"
-    export PYFLINK_PYTHON="${python-env}/bin/python"
-    export JAVA_HOME=${pkgs.openjdk11};
-    export FLINK_HOME=${pkgs.flink}/opt/flink
-
-    ${pkgs.flink}/opt/flink/bin/jobmanager.sh start
-    ${pkgs.flink}/opt/flink/bin/taskmanager.sh start
-
-    ${pkgs.flink}/bin/flink run \
-      -py ${src}/job/job.py \
-      -pyclientexec python \
-      --jarfile ${pkgs.campground.flink-connector-kafka} &
-  '';
-
   example-flink-job = pkgs.stdenv.mkDerivation {
     name = "example-flink-job";
     src = src;
@@ -82,7 +53,6 @@ let
       cp -r $src/* $out/src/
       cp -r ${pkgs.flink}/opt/flink $out/opt/
       cp -r ${python-env}/bin/* $out/bin/
-      cp ${job}/bin/job $out/bin/example-flink-job
     '';
 
     # passthru = {
