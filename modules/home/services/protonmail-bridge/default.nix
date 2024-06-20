217c0d6589f3ec09f48a99c2d9ed8ba8a807ef1c
Matt Camp <matt@aicampground.com>
2024-06-19 22:02:46 -0500

    got it working

--- modules/home/services/protonmail-bridge/default.nix
+++ modules/home/services/protonmail-bridge/default.nix
@@ -1,13 +1,7 @@
-{
-  options,
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+{ options, config, lib, pkgs, ... }:
 with lib;
-with lib.campground; let
-  cfg = config.campground.services.protonmail-bridge;
+with lib.campground;
+let cfg = config.campground.services.protonmail-bridge;
 in {
   options.campground.services.protonmail-bridge = {
     enable = mkOption {
@@ -23,14 +17,23 @@ in {
     };
 
     logLevel = mkOption {
-      type = types.enum [ "panic" "fatal" "error" "warn" "info" "debug" "debug-client" "debug-server" ];
+      type = types.enum [
+        "panic"
+        "fatal"
+        "error"
+        "warn"
+        "info"
+        "debug"
+        "debug-client"
+        "debug-server"
+      ];
       default = "info";
       description = "The log level";
     };
 
   };
   config = mkIf cfg.enable {
-    home.packages = [pkgs.protonmail-bridge pkgs.pass-secret-service ];
+    home.packages = [ pkgs.protonmail-bridge pkgs.pass-wayland ];
 
     services.pass-secret-service.enable = true;
 
@@ -41,13 +44,14 @@ in {
       };
       Service = {
         Restart = "always";
-        Environment = "PATH=${pkgs.pass-secret-service}/bin:${pkgs.protonmail-bridge}/bin:/run/current-system/sw/bin";
-        ExecStart = "${pkgs.protonmail-bridge}/bin/protonmail-bridge --no-window --log-level ${cfg.logLevel}" + optionalString (cfg.nonInteractive) " --noninteractive";
+        Environment =
+          "PATH=${pkgs.pass-wayland}/bin:${pkgs.protonmail-bridge}/bin:/run/current-system/sw/bin";
+        ExecStart =
+          "${pkgs.protonmail-bridge}/bin/protonmail-bridge --no-window --log-level ${cfg.logLevel}"
+          + optionalString (cfg.nonInteractive) " --noninteractive";
       };
 
-      Install = {
-        WantedBy = [ "default.target" ];
-      };
+      Install = { WantedBy = [ "default.target" ]; };
     };
   };
 }
