6368a1ee95f17b5f63df3ce953569ee421013bab
Matt Camp <matt@aicampground.com>
2024-06-26 20:26:11 -0500

    add collabora for nextcloud office

---
+++ modules/nixos/services/collabora/default.nix
@@ -0,0 +1,29 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+with lib;
+with lib.campground; let
+  cfg = config.campground.services.collabora;
+in {
+  options.campground.services.collabora = with types; {
+    enable = mkBoolOpt false "Enable collabora;";
+  };
+
+  config =
+    mkIf cfg.enable {
+    virtualisation.oci-containers.containers.collabora = {
+      image = "docker.io/collabora/code";
+      ports = [ "9980:9980" ];
+      autoStart = true;
+      environment = {
+        # This limits it to this NC instance AFAICT
+        aliasgroup1 = "https://${campground.services.nextcloud.domain}:443";
+        # Must disable SSL as it's behind a reverse proxy
+        extra_params = "--o:ssl.enable=false";
+      };
+    };
+    };
+}
