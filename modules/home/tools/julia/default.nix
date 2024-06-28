6142b8b2ffed06ec6d4ee34737d79a4e6e1bee99
Matt Camp <matt@aicampground.com>
2024-06-27 22:22:04 -0500

    update julia module

--- modules/home/tools/julia/default.nix
+++ modules/home/tools/julia/default.nix
@@ -1,25 +1,17 @@
-{ options
-, config
-, lib
-, pkgs
-, ...
-}:
+{ options, config, lib, pkgs, ... }:
 with lib;
-with lib.campground; let
-  cfg = config.campground.tools.julia;
-in
-{
+with lib.campground;
+let cfg = config.campground.tools.julia;
+in {
   options.campground.tools.julia = with types; {
     enable = mkBoolOpt false "Whether or not to enable common Julia.";
   };
 
   config = mkIf cfg.enable {
-    # home.sessionVariables = {
-    #   LD_LIBRARY_PATH = "${pkgs.gcc.cc.lib}/lib:${pkgs.zlib}/lib:$LD_LIBRARY_PATH";
-    # };
     home.packages = with pkgs; [
-      # julia-wrapped
-      # jupyter
+      pkgs.campground.julia
+      pkgs.campground.julia.jupyter-console
+      pkgs.campground.julia.jupyter-qtconsole
     ];
   };
 }
