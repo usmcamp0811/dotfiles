92e47d8cc4ef5f69eaf0c634d013bc82a394f364
Matt Camp <matt@aicampground.com>
2024-06-27 22:05:11 -0500

    wip

---
+++ packages/jupyter/default.nix
@@ -0,0 +1,21 @@
+{ lib, writeText, writeShellApplication, substituteAll, gum, inputs, pkgs
+, hosts ? { }, ... }:
+let
+  inherit (lib) mapAttrsToList concatStringsSep;
+  inherit (lib.campground) override-meta;
+  julia-env = pkgs.julia.withPackages [ "IJulia" "CSV" "DataFrames" ];
+  python = pkgs.python311.withPackages
+    (pythonPackages: with pythonPackages; [ jupyter qtconsole ]);
+  startJupyterWithJulia = writeShellApplication {
+    name = "start-jupyter-with-julia";
+    runtimeInputs = [ python julia-env ];
+    text = ''
+      #!${pkgs.runtimeShell}
+      # Ensure Julia kernel is installed
+      # # Start Jupyter console with Julia kernel
+      JULIA_VERSION="myjulia-$(julia -e 'println(string(VERSION.major) * "." * string(VERSION.minor))')"
+      ${julia-env}/bin/julia -e 'using IJulia; installkernel("myjulia")'
+      jupyter console --kernel "$JULIA_VERSION" "$@"
+    '';
+  };
+in startJupyterWithJulia
