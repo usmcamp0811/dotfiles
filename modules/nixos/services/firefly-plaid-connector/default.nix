6fdf4ca13415245e78eb472f8918f58cc2b98b66
Matt Camp <matt@aicampground.com>
2024-07-06 13:15:45 -0500

    wip

--- modules/nixos/services/firefly-plaid-connector/default.nix
+++ modules/nixos/services/firefly-plaid-connector/default.nix
@@ -1,53 +1,20 @@
 { lib, config, pkgs, ... }:
 with lib;
 with lib.campground;
-let cfg = config.campground.services.firefly-plaid-connector;
+let
+  cfg = config.campground.services.firefly-plaid-connector;
+  ff = config.campground.services.firefly;
+  application_yaml = ./application_yaml;
 in {
   options.campground.services.firefly-plaid-connector = with types; {
     enable = mkBoolOpt false "Enable Firefly III.";
-    firefly-plaid-connector-user =
-      mkOpt str "firefly-plaid-connector" "user for Firefly III.";
-    firefly-plaid-connector-group =
-      mkOpt str "firefly-plaid-connector" "user for Firefly III.";
-    dataDir = mkOpt str "/var/lib/${cfg.firefly-plaid-connector-user}"
-      "Data directory for Firefly III.";
-    settings = mkOption {
-      type = attrs;
-      default = {
-        SITE_OWNER = "matt@aicampground.com";
-        APP_URL = "https://${cfg.virtualHost}";
-        APP_DEBUG = true;
-        DB_SOCKET = "/run/postgresql";
-        DB_NAME = "firefly-plaid-connector";
-        DB_CONNECTION = "pgsql";
-        APP_KEY_FILE = "/var/lib/firefly-plaid-connector/key.file";
-        APP_ENV = "production";
-        TRUSTED_PROXIES = "10.0.0.0/8,192.168.0.0/16,172.16.0.0/12";
-      };
-      description = "Settings for Firefly III.";
-    };
-    virtualHost = mkOpt str "firefly-plaid-connector.lan.aicampground.com"
-      "Virtual host for Firefly III.";
-    package = mkOpt types.package pkgs.firefly-plaid-connector-iii
-      "Package for Firefly III.";
-    poolConfig = mkOpt attrs {
-      "listen.owner" = mkDefault "nginx";
-      "listen.group" = mkDefault "nginx";
-      pm = "dynamic";
-      "pm.max_children" = 32;
-      "pm.max_requests" = 500;
-      "pm.max_spare_servers" = 4;
-      "pm.min_spare_servers" = 2;
-      "pm.start_servers" = 2;
-    } "Pool configuration for Firefly III.";
-
     role-id =
       mkOpt str config.campground.services.vault-agent.settings.vault.role-id
       "Absolute path to the Vault role-id";
     secret-id =
       mkOpt str config.campground.services.vault-agent.settings.vault.secret-id
       "Absolute path to the Vault secret-id";
-    vault-path = mkOpt str "secret/campground/firefly-plaid-connector"
+    vault-path = mkOpt str "secret/campground/plaid"
       "The Vault path to the KV containing the KVs that are for each database";
     kvVersion = mkOption {
       type = enum [ "v1" "v2" ];
@@ -62,39 +29,80 @@ in {
   };
 
   config = mkIf cfg.enable {
-    # campground.services = {
-    #   vault-agent = {
-    #     services = {
-    #       "get-firefly-plaid-connector-key" = {
-    #         settings = {
-    #           # replace with the address of your vault
-    #           vault.address = cfg.vault-address;
-    #           auto_auth = {
-    #             method = [{
-    #               type = "approle";
-    #               config = {
-    #                 role_id_file_path = cfg.role-id;
-    #                 secret_id_file_path = cfg.secret-id;
-    #                 remove_secret_id_file_after_reading = false;
-    #               };
-    #             }];
-    #           };
-    #         };
-    #         secrets = {
-    #           file = {
-    #             files = {
-    #               "key.file" = {
-    #                 text = ''
-    #                   {{ with secret "${cfg.vault-path}" }}{{ if eq "${cfg.kvVersion}" "v1" }}{{ .Data.key }}{{ else }}{{ .Data.data.key }}{{ end }}{{ end }}'';
-    #                 permissions = "0600";
-    #                 change-action = "restart";
-    #               };
-    #             };
-    #           };
-    #         };
-    #       };
-    #     };
-    #   };
-    # };
+
+    services.docker.enable = true;
+
+    # Define the oneshot service (if needed)
+    systemd.services.setup-firefly-paid-connector = {
+      description = "Setup for Firefly Paid Connector";
+      wantedBy = [ "multi-user.target" ];
+      environment = {
+        TIMEZONE = "US/Central";
+        FIREFLY_URL = ff.virtualHost;
+      };
+      after = [ "network.target" ];
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = ''
+          #!/bin/bash
+          echo "Running setup script for Firefly Paid Connector..."
+          cat ${application_yaml} | ${pkgs.envsubst}/bin/envsubst > ${ff.dataDir}/application.yaml
+          chown ${ff.firefly-user}:${ff.firefly-group} ${ff.dataDir}/application.yaml
+        '';
+      };
+    };
+
+    # Define the container
+    virtualisation.oci-containers = {
+      containers.firefly-paid-connector = {
+        image = "ghcr.io/dvankley/firefly-plaid-connector-2:latest";
+        hostname = "plaidconnector";
+        volumes = [{
+          source = "${ff.dataDir}/application.yaml";
+          target = "/opt/fpc-config/application.yml";
+          type = "bind";
+          readOnly = true;
+        }];
+        environment = {
+          SPRING_CONFIG_LOCATION = "/opt/fpc-config/application.yml";
+        };
+        autoStart = true;
+        restartPolicy = "always";
+        startTimeout = 90;
+        after = [
+          "setup-firefly-paid-connector.service"
+        ]; # Make container service depend on oneshot service
+      };
+    };
+
+    campground.services.vault-agent.services.setup-firefly-paid-connector = {
+      settings = {
+        vault.address = cfg.vault-address;
+        auto_auth = {
+          method = [{
+            type = "approle";
+            config = {
+              role_id_file_path = cfg.role-id;
+              secret_id_file_path = cfg.secret-id;
+              remove_secret_id_file_after_reading = false;
+            };
+          }];
+        };
+      };
+      secrets.environment.templates = {
+        plaid = {
+          text = ''
+            {{ with secret "${cfg.vault-path}" }}
+            PLAID_CLIENT_ID='{{ if eq "${cfg.kvVersion}" "v1" }}{{ .Data.plaid_clinet_id }}{{ else }}{{ .Data.data.plaid_clinet_id }}{{ end }}'
+            PLAID_SECRET='{{ if eq "${cfg.kvVersion}" "v1" }}{{ .Data.plaid_secret }}{{ else }}{{ .Data.data.plaid_secret }}{{ end }}'
+            USAA_ACCOUT_ID='{{ if eq "${cfg.kvVersion}" "v1" }}{{ .Data.usaa_item_id }}{{ else }}{{ .Data.data.usaa_item_id  }}{{ end }}'
+            USAA_ACCESS_TOKEN='{{ if eq "${cfg.kvVersion}" "v1" }}{{ .Data.usaa_access_token  }}{{ else }}{{ .Data.data.usaa_access_token }}{{ end }}'
+            AMEX_ACCOUT_ID='{{ if eq "${cfg.kvVersion}" "v1" }}{{ .Data.amex_item_id }}{{ else }}{{ .Data.data.amex_item_id  }}{{ end }}'
+            AMEX_ACCESS_TOKEN='{{ if eq "${cfg.kvVersion}" "v1" }}{{ .Data.amex_access_token  }}{{ else }}{{ .Data.data.amex_access_token }}{{ end }}'
+            {{ end }}
+          '';
+        };
+      };
+    };
   };
 }
