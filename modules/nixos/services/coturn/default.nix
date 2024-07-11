7dcd5b4fefe1b41eeb6b355946bc3c0cb2138a1f
Matt Camp <matt@aicampground.com>
2024-07-10 21:30:46 -0500

    wip netbird

---
+++ modules/nixos/services/coturn/default.nix
@@ -0,0 +1,50 @@
+{ lib, config, pkgs, ... }:
+with lib;
+with lib.campground;
+let cfg = config.campground.services.coturn;
+in {
+  options.campground.services.coturn = with types; {
+    enable = mkBoolOpt false "Enable Coturn;";
+  };
+
+  config = mkIf cfg.enable {
+    services.coturn = {
+      enable = true;
+      use-auth-secret = true;
+      static-auth-secret-file = config.sops.secrets.auth-secret.path;
+      realm = "turn.ataraxiadev.com";
+      min-port = 49152;
+      max-port = 49262;
+      no-cli = true;
+      cert =
+        "${config.security.acme.certs.${cert-fqdn}.directory}/fullchain.pem";
+      pkey = "${config.security.acme.certs.${cert-fqdn}.directory}/key.pem";
+      no-tcp-relay = true;
+      extraConfig = ''
+        fingerprint
+        external-ip=${external-ip}
+        userdb=/var/lib/coturn/turnserver.db
+        no-tlsv1
+        no-tlsv1_1
+        no-rfc5780
+        no-stun-backward-compatibility
+        response-origin-only-with-rfc5780
+        no-multicast-peers
+      '' + lib.strings.concatMapStringsSep "\n" (x: "denied-peer-ip=${x}")
+        coturn-denied-ips;
+    };
+    systemd.services.coturn.serviceConfig.StateDirectory = "coturn";
+    systemd.services.coturn.serviceConfig.Group = lib.mkForce "acme";
+
+    networking = {
+      firewall = {
+        allowedUDPPortRanges = with config.services.coturn; [{
+          from = min-port;
+          to = max-port;
+        }];
+        allowedUDPPorts = turn-ports;
+        allowedTCPPorts = turn-ports;
+      };
+    };
+  };
+}
--- modules/nixos/services/netbird/default.nix
+++ modules/nixos/services/netbird/default.nix
@@ -5,11 +5,97 @@ let cfg = config.campground.services.netbird;
 in {
   options.campground.services.netbird = with types; {
     enable = mkBoolOpt false "Enable Netbird;";
+    domain = mkOpt str "netbird.aicampground.com" "Domain for Netbird to use";
   };
 
   config = mkIf cfg.enable {
-    services.netbird.enable = true;
-    environment.systemPackages = [ pkgs.netbird-ui ];
+
+    services.netbird.server = {
+      enable = true;
+      domain = cfg.domain;
+      enableNginx = true;
+      coturn.enable = false;
+      signal.logLevel = "INFO";
+      dashboard.settings = {
+        AUTH_AUTHORITY = issuer;
+        AUTH_CLIENT_ID = client-id;
+        AUTH_SUPPORTED_SCOPES = scopes;
+      };
+      management = {
+        disableAnonymousMetrics = lib.mkForce true;
+        logLevel = "INFO";
+        dnsDomain = "netbird.local";
+        singleAccountModeDomain = "netbird.local";
+        oidcConfigEndpoint = "${issuer}/.well-known/openid-configuration";
+
+        turnDomain = config.services.coturn.realm;
+        turnPort = config.services.coturn.listening-port;
+        settings = {
+          DataStoreEncryptionKey._secret = store-key;
+          DeviceAuthorizationFlow = {
+            Provider = "hosted";
+            ProviderConfig = {
+              Audience = client-id;
+              ClientID = client-id;
+              DeviceAuthEndpoint =
+                "https://auth.ataraxiadev.com/application/o/device/";
+              RedirectURLs = null;
+              Scope = "openid";
+              TokenEndpoint =
+                "https://auth.ataraxiadev.com/application/o/token/";
+              UseIDToken = false;
+            };
+          };
+          HttpConfig = {
+            AuthAudience = client-id;
+            AuthIssuer = "https://auth.ataraxiadev.com/application/o/netbird/";
+            AuthKeysLocation =
+              "https://auth.ataraxiadev.com/application/o/netbird/jwks/";
+            # AuthUserIDClaim = "";
+            IdpSignKeyRefreshEnabled = false;
+          };
+          IdpManagerConfig = {
+            ManagerType = "authentik";
+            ClientConfig = {
+              ClientID = client-id;
+              GrantType = "client_credentials";
+              Issuer = "https://auth.ataraxiadev.com/application/o/netbird/";
+              TokenEndpoint =
+                "https://auth.ataraxiadev.com/application/o/token/";
+            };
+            ExtraConfig = {
+              Password._secret = svc-pass;
+              Username = "Netbird";
+            };
+          };
+          PKCEAuthorizationFlow = {
+            ProviderConfig = {
+              Audience = client-id;
+              AuthorizationEndpoint =
+                "https://auth.ataraxiadev.com/application/o/authorize/";
+              ClientID = client-id;
+              Scope = scopes;
+              TokenEndpoint =
+                "https://auth.ataraxiadev.com/application/o/token/";
+              UseIDToken = false;
+            };
+          };
+          TURNConfig = {
+            Secret._secret = config.sops.secrets.auth-secret.path;
+            TimeBasedCredentials = true;
+            # Not used, supress nix warnind about world-readable password
+            # Password._secret = config.sops.secrets.auth-secret.path;
+          };
+        };
+      };
+    };
+    services.nginx.virtualHosts.${domain} = {
+      useACMEHost = "ataraxiadev.com";
+      enableACME = false;
+      forceSSL = true;
+    };
+
+    persist.state.directories = [ "/var/lib/netbird-mgmt" ];
 
   };
 }
