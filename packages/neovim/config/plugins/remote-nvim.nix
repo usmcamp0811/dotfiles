4c2d300df35c9d9cf58da4e0afe21670ca05498c
Matt Camp <matt@aicampground.com>
2024-07-12 18:45:43 -0500

    remote nvim

--- packages/neovim/config/plugins/remote-nvim.nix
+++ packages/neovim/config/plugins/remote-nvim.nix
@@ -2,18 +2,18 @@
 let
 
   remote-nvim = pkgs.vimUtils.buildVimPlugin {
-    name = "markdown-nvim";
+    name = "remote-nvim";
     src = pkgs.fetchFromGitHub {
       owner = "amitds1997";
       repo = "remote-nvim.nvim";
       rev = "v0.3.11";
-      sha256 = "";
+      sha256 = "sha256-ado876vs1D1tEQu+Q3jDUaJA9hf/9Y5JLCWu3rf219s=";
     };
   };
 
 in {
   extraPlugins = [ remote-nvim ];
-  # extraConfigLua = ''
-  #   require('render-markdown').setup({})
-  # '';
+  extraConfigLua = ''
+    require("remote-nvim").setup()
+  '';
 }
