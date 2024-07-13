5801829737fc7b7d28862f7c57e08d4832fb2f20
Matt Camp <matt@aicampground.com>
2024-07-13 09:37:28 -0500

    fuck yes bitches!

--- packages/julia/default.nix
+++ packages/julia/default.nix
@@ -12,47 +12,65 @@ let
     "IJulia"
     "CSV"
   ];
-  python = pkgs.python311.withPackages (pythonPackages:
-    with pythonPackages; [
-      ipython
-      jupyter
-      qtconsole
-      jupyter_console
-      ipykernel
-    ]);
   startJupyterWithJulia = writeShellApplication {
     name = "julia-console";
-    runtimeInputs = [ python julia-env pkgs.jupyter ];
+    runtimeInputs = [ pkgs.jupyter-all julia-env ];
     text = ''
       #!${pkgs.runtimeShell}
       # Ensure Julia kernel is installed
-      # # Start Jupyter console with Julia kernel
       export LD_LIBRARY_PATH=${pkgs.openssl.out}/lib:$LD_LIBRARY_PATH
-      export PYTHONPATH=$PWD/${python}/${python.sitePackages}/:$PYTHONPATH
-      JULIA_VERSION="campground-julia-$(${julia-env}/bin/julia  -e 'println(string(VERSION.major) * "." * string(VERSION.minor))')"
-      ${julia-env}/bin/julia -e 'using IJulia; installkernel("campground-julia")'
-      ${python}/bin/jupyter console --kernel "$JULIA_VERSION" "$@"
+      export PATH=${pkgs.jupyter-all}/bin:$PATH
+      export PYTHONPATH=${pkgs.jupyter-all}/lib/python3.11/site-packages:$PYTHONPATH
+      JULIA_VERSION=$(${julia-env}/bin/julia -e 'println("campground-julia-" * string(VERSION.major) * "." * string(VERSION.minor))')
+      ${julia-env}/bin/julia -e "using IJulia; installkernel(\"campground-julia\", julia=\`${julia-env}/bin/julia\`)"
+      ${pkgs.jupyter-all}/bin/jupyter console --kernel "$JULIA_VERSION" "$@"
     '';
   };
   startQtJupyterWithJulia = writeShellApplication {
     name = "julia-qtconsole";
-    runtimeInputs = [ python julia-env ];
+    runtimeInputs = [ pkgs.jupyter-all julia-env ];
     text = ''
       #!${pkgs.runtimeShell}
       # Ensure Julia kernel is installed
       # # Start Jupyter console with Julia kernel
       export LD_LIBRARY_PATH=${pkgs.openssl.out}/lib:$LD_LIBRARY_PATH
-      JULIA_VERSION="campground-julia-$(${julia-env}/bin/julia -e 'println(string(VERSION.major) * "." * string(VERSION.minor))')"
-      ${julia-env}/bin/julia -e 'using IJulia; installkernel("campground-julia")'
-      ${python}/bin/jupyter qtconsole --kernel "$JULIA_VERSION" "$@"
+      export PATH=${pkgs.jupyter-all}/bin:$PATH
+      export PYTHONPATH=${pkgs.jupyter-all}/lib/python3.11/site-packages:$PYTHONPATH
+      JULIA_VERSION=$(${julia-env}/bin/julia -e 'println("campground-julia-" * string(VERSION.major) * "." * string(VERSION.minor))')
+      ${julia-env}/bin/julia -e "using IJulia; installkernel(\"campground-julia\", julia=\`${julia-env}/bin/julia\`)"
+      ${pkgs.jupyter-all}/bin/jupyter qtconsole --kernel "$JULIA_VERSION" "$@"
     '';
+  };
+  jupyter = pkgs.stdenv.mkDerivation rec {
+    pname = "jupyter-console-launcher";
+    version = "1.0.0";
+
+    src = pkgs.runCommandNoCC "empty" { } "mkdir $out";
+
+    buildInputs = [ pkgs.jupyter-all ];
+
+    installPhase = ''
+          mkdir -p $out/bin
+          cat > $out/bin/jupyter-console <<EOF
+      #!/usr/bin/env bash
+      jupyter console
+      EOF
+          chmod +x $out/bin/jupyter-console
+    '';
+
+    meta = with pkgs.lib; {
+      description =
+        "A script to launch Jupyter console with all required dependencies";
+      license = licenses.mit;
+      maintainers = with maintainers; [ yourname ];
+    };
   };
 in pkgs.stdenv.mkDerivation rec {
   pname = "julia";
   version = pkgs.julia.version;
   src = ./.;
 
-  buildInputs = [ python julia-env pkgs.openssl ];
+  buildInputs = [ pkgs.jupyter-all julia-env pkgs.openssl ];
 
   installPhase = ''
     mkdir -p $out/bin
@@ -65,6 +83,5 @@ in pkgs.stdenv.mkDerivation rec {
   passthru = {
     jupyter-qtconsole = startQtJupyterWithJulia;
     jupyter-console = startJupyterWithJulia;
-    python = python;
   };
 }
