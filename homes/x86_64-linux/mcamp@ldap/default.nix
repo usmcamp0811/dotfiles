14e09e7cd26da18a9168819dca2e872f41a5c5e7
Matt Camp <matt@aicampground.com>
2024-07-15 08:21:42 -0500

    cause need this

--- homes/x86_64-linux/mcamp@ldap/default.nix
+++ homes/x86_64-linux/mcamp@ldap/default.nix
@@ -1,12 +1,4 @@
-{
-  inputs,
-  lib,
-  pkgs,
-  config,
-  osConfig ? {},
-  format ? "unknown",
-  ...
-}:
+{ inputs, lib, pkgs, config, osConfig ? { }, format ? "unknown", ... }:
 with lib;
 with lib.campground; {
   campground = {
@@ -36,15 +28,18 @@ with lib.campground; {
           monitors = [
             {
               name = "HDMI-A-3";
-              wallpaper = "${pkgs.campground.wallpapers}/share/wallpapers/hsv-saturnV.jpg";
+              wallpaper =
+                "${pkgs.campground.wallpapers}/share/wallpapers/hsv-saturnV.jpg";
             }
             {
               name = "HDMI-A-2";
-              wallpaper = "${pkgs.campground.wallpapers}/share/wallpapers/hsv-saturnV.jpg";
+              wallpaper =
+                "${pkgs.campground.wallpapers}/share/wallpapers/hsv-saturnV.jpg";
             }
             {
               name = "eDP-1";
-              wallpaper = "${pkgs.campground.wallpapers}/share/wallpapers/hsv-saturnV.jpg";
+              wallpaper =
+                "${pkgs.campground.wallpapers}/share/wallpapers/hsv-saturnV.jpg";
             }
           ];
 
@@ -82,7 +77,22 @@ with lib.campground; {
       neovim = enabled;
     };
     services = {
-      openssh = enabled;
+      openssh = {
+        enable = true;
+        extraConfigs = ''
+          Host github.com-usmcamp0811
+            HostName github.com
+            User git
+            IdentityFile ~/.ssh/id_ed25519
+            IdentitiesOnly yes
+
+          Host github.com-mcamp-ata
+            HostName github.com
+            User git
+            IdentityFile ~/.ssh/id_rsa.ata
+            IdentitiesOnly yes
+        '';
+      };
       syncthing = enabled;
     };
 
