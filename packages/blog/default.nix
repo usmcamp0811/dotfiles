daf2469ccea4b80ee06ecf0c25c1bb46368145c0
Matt Camp <matt@aicampground.com>
2024-07-18 21:33:05 -0500

    i can now build the blog

--- packages/blog/default.nix
+++ packages/blog/default.nix
@@ -1,11 +1,28 @@
 { pkgs, lib, ... }:
 let
+  campground-blog = ./Campground/.;
+
+  theme = pkgs.fetchFromGitHub {
+    owner = "CaiJimmy";
+    repo = "hugo-theme-stack";
+    rev = "master";
+    sha256 = "sha256-IMbEgE2+mCxwCpbvUnbnm7oED5+PkyRQlxbB+Oxl7yQ=";
+  };
+
   blog = pkgs.stdenv.mkDerivation rec {
     name = "blog";
     version = "0.1.0";
-    src = ./.;
+    src = campground-blog;
     buildInputs = [ pkgs.hugo ];
+    buildPhase = ''
+      mkdir -p $out
+      mkdir -p $out/public
+      mkdir -p $out/themes/hugo-theme-stack
+      cp -r ${campground-blog}/* $out
+      cp -r ${theme}/* $out/themes/hugo-theme-stack
+      cd $out
+      ${pkgs.hugo}/bin/hugo
+    '';
 
-    installPhase = "";
   };
 in blog
