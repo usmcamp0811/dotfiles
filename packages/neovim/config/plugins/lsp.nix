52046de75f0da8c62bf9e75414c617c70dd5c2ff
Matt Camp <matt@aicampground.com>
2024-07-23 21:58:02 -0500

    adding formater

--- packages/neovim/config/plugins/lsp.nix
+++ packages/neovim/config/plugins/lsp.nix
@@ -5,6 +5,21 @@
     conjure = { enable = true; };
     vimtex = { enable = true; };
     ts-context-commentstring = { enable = true; };
+    conform-nvim = {
+      enable = true;
+      formattersByFt = {
+        lua = [ "stylua" ];
+        # Conform will run multiple formatters sequentially
+        python = [ "isort" "black" ];
+        # Use a sub-list to run only the first available formatter
+        javascript = [[ "prettierd" "prettier" ]];
+        # Use the "*" filetype to run formatters on all filetypes.
+        "*" = [ "codespell" ];
+        # Use the "_" filetype to run formatters on filetypes that don't
+        # have other formatters configured.
+        "_" = [ "trim_whitespace" ];
+      };
+    };
     comment = {
       enable = true;
       settings = {
