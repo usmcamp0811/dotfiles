8599141cc7e22e3f01072414a41119234fd9f534
Matt Camp <matt@aicampground.com>
2024-07-25 09:03:18 -0500

    table api wip

---
+++ packages/example-flink-job/job/tablejob.py
@@ -0,0 +1,63 @@
+import logging
+import os
+import sys
+
+from pyflink.table import EnvironmentSettings, TableEnvironment
+from pyflink.table.window import Tumble
+
+
+def run_example_flink_job(t_env: TableEnvironment, broker: str):
+    # Define Kafka source
+    t_env.execute_sql(
+        f"""
+        CREATE TABLE kafka_source (
+            username STRING,
+            `timestamp` TIMESTAMP(3),
+            WATERMARK FOR `timestamp` AS `timestamp` - INTERVAL '1' SECOND
+        ) WITH (
+            'connector' = 'kafka',
+            'topic' = 'example-input-topic',
+            'properties.bootstrap.servers' = '{broker}',
+            'properties.group.id' = 'test_group_1',
+            'format' = 'json',
+            'json.fail-on-missing-field' = 'false',
+            'json.ignore-parse-errors' = 'true'
+        )
+    """
+    )
+
+    # Define Kafka sink
+    t_env.execute_sql(
+        f"""
+        CREATE TABLE kafka_sink (
+            username STRING,
+            login_count BIGINT
+        ) WITH (
+            'connector' = 'kafka',
+            'topic' = 'example-output-topic',
+            'properties.bootstrap.servers' = '{broker}',
+            'format' = 'json'
+        )
+    """
+    )
+
+    # Read from Kafka source
+    source_table = t_env.from_path("kafka_source")
+
+    # Define Tumble Window
+    result_table = (
+        source_table.window(Tumble.over("10.minutes").on("timestamp").alias("w"))
+        .group_by("username, w")
+        .select("username, COUNT(username) as login_count")
+    )
+
+    # Write result to Kafka sink
+    result_table.execute_insert("kafka_sink").wait()
+
+
+if __name__ == "__main__":
+    broker = os.getenv("KAFKA_BROKER", "localhost:9092")
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO, format="%(message)s")
+    env_settings = EnvironmentSettings.new_instance().in_streaming_mode().build()
+    t_env = TableEnvironment.create(env_settings)
+    run_example_flink_job(t_env, broker)
