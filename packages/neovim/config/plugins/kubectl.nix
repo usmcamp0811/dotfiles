37e97b67eb21e6ca7b931392a721d6cec851fb18
Matt Camp <matt@aicampground.com>
2024-07-25 18:29:25 -0500

    forgot the sha

--- packages/neovim/config/plugins/kubectl.nix
+++ packages/neovim/config/plugins/kubectl.nix
@@ -6,7 +6,7 @@ let
       owner = "Ramilito";
       repo = "kubectl.nvim";
       rev = "0.2.0";
-      sha256 = "";
+      sha256 = "sha256-cPS/PVwZZRgnmOy0WH3yVnZV8rrVCTMFu/hr33M+CB8=";
     };
   };
 in {
