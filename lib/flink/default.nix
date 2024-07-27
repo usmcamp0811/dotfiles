dd0454882c0f1396b7c82c81c937c8c8b63fe218
Matt Camp <matt@aicampground.com>
2024-07-27 13:37:45 -0500

    wip

--- lib/flink/default.nix
+++ lib/flink/default.nix
@@ -1,33 +1,51 @@
-{ pkgs, lib, inputs, snowfall-inputs, }: rec {
-  # Function to create the flink-conf-dir derivation
-  createFlinkConfDir = flinkConf:
+{ lib, inputs, snowfall-inputs, }: rec {
+  ## Create a flink-conf-dir derivation
+  ##
+  ## This function generates a derivation that sets up the Flink configuration directory.
+  ##
+  ## Parameters:
+  ## - `flinkConf`: The Flink configuration to be used.
+  ##
+  ## Example usage:
+  ## ```nix
+  ## createFlinkConfDir {
+  ##   pkgs = import <nixpkgs> {};
+  ##   flinkConf = ./flink-conf.yaml;
+  ## }
+  ## ```
+  createFlinkConfDir = { pkgs, flinkConf, }:
     pkgs.stdenv.mkDerivation {
       name = "flink-conf-drv";
       phases = [ "installPhase" ];
       installPhase = ''
         mkdir -p $out/conf
-        # Iterate over each file in the source directory
         for file in "${pkgs.flink}/opt/flink/conf"/*; do
-            # Get the basename of the file
             basefile=$(basename "$file")
-            if [ "$basefile" == "flink-conf.yaml" ]; then
-                continue
+            if [ "$basefile" != "flink-conf.yaml" ] && [ "$basefile" != "config.yaml" ]; then
+                ln -s "$file" "$out/conf/$basefile"
             fi
-            if [ "$basefile" == "config.yaml" ]; then
-                continue
-            fi
-            # Create the symbolic link in the destination directory
-            ln -s "$file" "$out/conf/$basefile"
         done
         cp ${flinkConf} $out/conf/flink-conf.yaml
         cp ${flinkConf} $out/conf/config.yaml
       '';
     };
 
-  # Function to create the set-flink-conf script
-  createSetFlinkConf = flinkConfDir:
+  ## Create a set-flink-conf script
+  ##
+  ## This function generates a script that sets the FLINK_CONF_DIR environment variable.
+  ##
+  ## Parameters:
+  ## - `flinkConfDir`: The directory containing Flink configuration files.
+  ##
+  ## Example usage:
+  ## ```nix
+  ## createSetFlinkConf {
+  ##   pkgs = import <nixpkgs> {};
+  ##   flinkConfDir = ./flink-conf-dir;
+  ## }
+  ## ```
+  createSetFlinkConf = { pkgs, flinkConfDir, }:
     pkgs.writeScript "set-flink-conf" ''
-      # Check if FLINK_CONF_DIR is unset or empty
       if [ -z "$FLINK_CONF_DIR" ]; then
           export FLINK_CONF_DIR="${flinkConfDir}/conf";
           echo "FLINK_CONF_DIR set to $FLINK_CONF_DIR"
@@ -36,11 +54,34 @@
       fi
     '';
 
-  # General function to create shell scripts with flink configuration
-  writeFlinkShellScriptBin = flinkConf: name: script:
+  ## Create shell scripts with flink configuration
+  ##
+  ## This function generates shell scripts that set up the Flink configuration environment.
+  ##
+  ## Parameters:
+  ## - `flinkConf`: The Flink configuration to be used.
+  ## - `name`: The name of the script.
+  ## - `script`: The script to be executed with the Flink configuration.
+  ##
+  ## Example usage:
+  ## ```nix
+  ## writeFlinkShellScriptBin {
+  ##   pkgs = import <nixpkgs> {};
+  ##   flinkConf = ./flink-conf.yaml;
+  ##   name = "flink-job";
+  ##   script = "flink run my-job.jar";
+  ## }
+  ## ```
+  writeFlinkShellScriptBin = { pkgs, flinkConf, name, script, }:
     let
-      flinkConfDir = createFlinkConfDir flinkConf;
-      setFlinkConf = createSetFlinkConf flinkConfDir;
+      flinkConfDir = createFlinkConfDir {
+        pkgs = pkgs;
+        flinkConf = flinkConf;
+      };
+      setFlinkConf = createSetFlinkConf {
+        pkgs = pkgs;
+        flinkConfDir = flinkConfDir;
+      };
     in pkgs.writeShellScriptBin name ''
       source ${setFlinkConf}
       ${script}
