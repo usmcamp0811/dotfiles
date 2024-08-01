30c2435cb68d94352cbfc57b5c4bcd7cbd63d956
Matt Camp <matt@aicampground.com>
2024-08-01 08:43:11 -0500

    put that shit  int the wrong place

--- lib/flink/default.nix
+++ lib/flink/default.nix
@@ -19,7 +19,7 @@
 
   ## Create shell scripts with Flink configuration
   writeFlinkApplication =
-    { pkgs, flinkConf, name, tag ? "latest", text, extraRuntimeInputs ? [ ], }:
+    { pkgs, flinkConf, name, text, extraRuntimeInputs ? [ ], }:
     let
       flinkConfDir = createFlinkConfDir {
         pkgs = pkgs;
@@ -57,32 +57,33 @@
     };
 
   ## Create a Flink derivation
-  mkFlinkDerivation = { pkgs, name, flinkConf ? pkgs.writeTextFile {
-    name = "flink-conf.yaml";
-    text = ''
-      env.java.opts.all: --add-exports=java.base/sun.net.util=ALL-UNNAMED --add-exports=java.rmi/sun.rmi.registry=ALL-UNNAMED --add-exports=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED --add-exports=jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED --add-exports=jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED --add-exports=jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED --add-exports=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED --add-exports=java.security.jgss/sun.security.krb5=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.net=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED --add-opens=java.base/java.nio=ALL-UNNAMED --add-opens=java.base/sun.nio.ch=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.text=ALL-UNNAMED --add-opens=java.base/java.time=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.util.concurrent=ALL-UNNAMED --add-opens=java.base/java.util.concurrent.atomic=ALL-UNNAMED --add-opens=java.base/java.util.concurrent.locks=ALL-UNNAMED
-      jobmanager.rpc.address: localhost
-      jobmanager.rpc.port: 6123
-      jobmanager.bind-host: 0.0.0.0
-      jobmanager.memory.process.size: 1600m
-      taskmanager.bind-host: 0.0.0.0
-      taskmanager.host: localhost
-      taskmanager.memory.process.size: 1728m
-      taskmanager.numberOfTaskSlots: 3
-      parallelism.default: 1
-      jobmanager.execution.failover-strategy: region
-      rest.address: localhost
-      rest.port: 8081
-      rest.bind-address: 0.0.0.0
-      env.log.dir: /tmp/flink-logs
-      env.java.home: ${pkgs.openjdk11}
-      env.path: ${python-env}/bin/:$PATH
-      python.path: ${python-env}/lib/python3.11/site-packages:${src}
-      python.executable: ${python-env}/bin/python
-      python.client.executable: ${python-env}/bin/python
-      pipeline.jars: ${pkgs.campground.flink-connector-kafka}
-    '';
-  }, python-env, src, flink-job-script ? "jobs/job.py"
+  mkFlinkDerivation = { pkgs, name, tag ? "latest", flinkConf ?
+      pkgs.writeTextFile {
+        name = "flink-conf.yaml";
+        text = ''
+          env.java.opts.all: --add-exports=java.base/sun.net.util=ALL-UNNAMED --add-exports=java.rmi/sun.rmi.registry=ALL-UNNAMED --add-exports=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED --add-exports=jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED --add-exports=jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED --add-exports=jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED --add-exports=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED --add-exports=java.security.jgss/sun.security.krb5=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.net=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED --add-opens=java.base/java.nio=ALL-UNNAMED --add-opens=java.base/sun.nio.ch=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.text=ALL-UNNAMED --add-opens=java.base/java.time=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.util.concurrent=ALL-UNNAMED --add-opens=java.base/java.util.concurrent.atomic=ALL-UNNAMED --add-opens=java.base/java.util.concurrent.locks=ALL-UNNAMED
+          jobmanager.rpc.address: localhost
+          jobmanager.rpc.port: 6123
+          jobmanager.bind-host: 0.0.0.0
+          jobmanager.memory.process.size: 1600m
+          taskmanager.bind-host: 0.0.0.0
+          taskmanager.host: localhost
+          taskmanager.memory.process.size: 1728m
+          taskmanager.numberOfTaskSlots: 3
+          parallelism.default: 1
+          jobmanager.execution.failover-strategy: region
+          rest.address: localhost
+          rest.port: 8081
+          rest.bind-address: 0.0.0.0
+          env.log.dir: /tmp/flink-logs
+          env.java.home: ${pkgs.openjdk11}
+          env.path: ${python-env}/bin/:$PATH
+          python.path: ${python-env}/lib/python3.11/site-packages:${src}
+          python.executable: ${python-env}/bin/python
+          python.client.executable: ${python-env}/bin/python
+          pipeline.jars: ${pkgs.campground.flink-connector-kafka}
+        '';
+      }, python-env, src, flink-job-script ? "jobs/job.py"
     , additionalInstallPhase ? "", additionalPassThru ? { }, }:
     let
       flink-with-kafka-connector = pkgs.flink.overrideAttrs (oldAttrs: {
