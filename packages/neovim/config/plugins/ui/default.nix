a86d2b7ac2c3f49f5729de8b26b024c6f617bd84
Matt Camp <matt@aicampground.com>
2024-08-08 21:00:21 -0500

    wip

--- packages/neovim/config/plugins/ui/default.nix
+++ packages/neovim/config/plugins/ui/default.nix
@@ -140,145 +140,6 @@
       options.desc = "Search Neorg Wiki";
       action = "<cmd>cd ~/vimwiki/home | :Telescope live_grep theme=ivy<cr>";
     }
-    # {
-    #   mode = "n";
-    #   key = "m";
-    #   options = { desc = "Open your Mind"; };
-    #   action = ":MindOpenMain<CR>";
-    # }
-    # {
-    #   mode = "n";
-    #   key = ",";
-    #   options = { desc = "Alpha"; };
-    #   action = "<cmd>Alpha<cr>";
-    # }
-    # {
-    #   mode = "n";
-    #   key = "b";
-    #   options = { desc = "Buffers"; };
-    #   action = "<cmd>BufferLinePick<cr>";
-    # }
-    # {
-    #   mode = "n";
-    #   key = "q";
-    #   options = { desc = "Quit"; };
-    #   action = "<cmd>q!<CR>";
-    # }
-    # {
-    #   mode = "n";
-    #   key = "cx";
-    #   options = { desc = "Switch Slime to X11"; };
-    #   action = "<cmd>lua SlimeXSwitch()<CR>";
-    # }
-    # {
-    #   mode = "n";
-    #   key = "cr";
-    #   options = { desc = "Restart Jupyter"; };
-    #   action = ":MoltenRestart!<CR>";
-    # }
-    # {
-    #   mode = "n";
-    #   key = "cs";
-    #   options = { desc = "Start Jupyter"; };
-    #   action = ":MoltenInit<CR>";
-    # }
-    # {
-    #   mode = "n";
-    #   key = "cD";
-    #   options = { desc = "Stop Jupyter"; };
-    #   action = ":MoltenDeinit<CR>";
-    # }
-    # {
-    #   mode = "n";
-    #   key = "cd";
-    #   options = { desc = "Delete Current Cell"; };
-    #   action = ":MoltenDelete<CR>";
-    # }
-    # {
-    #   mode = "n";
-    #   key = "co";
-    #   options = { desc = "Show Output"; };
-    #   action = ":MoltenShowOutput<CR>";
-    # }
-    # {
-    #   mode = "n";
-    #   key = "ci";
-    #   options = { desc = "Interrupt Jupyter"; };
-    #   action = ":MoltenInterrupt<CR>";
-    # }
-    # {
-    #   mode = "n";
-    #   key = "c<CR>";
-    #   options = { desc = "Run Cell"; };
-    #   action = ":MoltenReevaluateCell<CR>";
-    # }
-    # {
-    #   mode = "n";
-    #   key = "Cc";
-    #   options = { desc = "Open Calendar"; };
-    #   action = "<cmd>Calendar<CR>";
-    # }
-    # {
-    #   mode = "n";
-    #   key = "Cw";
-    #   options = { desc = "Week View"; };
-    #   action = "<cmd>Calendar -view=week<CR>";
-    # }
-    # {
-    #   mode = "n";
-    #   key = "Cd";
-    #   options = { desc = "Day View"; };
-    #   action = "<cmd>Calendar -view=day<CR>";
-    # }
-    # {
-    #   mode = "n";
-    #   key = "Cs";
-    #   options = { desc = "Day View"; };
-    #   action = "<cmd>Calendar -view=days<CR>";
-    # }
-    # {
-    #   mode = "n";
-    #   key = "Co";
-    #   options = { desc = "Clock"; };
-    #   action = "<cmd>Calendar -view=clock<CR>";
-    # }
-    # {
-    #   mode = "n";
-    #   key = "Cf";
-    #   options = { desc = "Open Side Calendar"; };
-    #   action = "<cmd>Calendar -view=year -split=vertical -width=25<CR>";
-    # }
-    # {
-    #   mode = "n";
-    #   key = "h";
-    #   options = { desc = "No Highlight"; };
-    #   action = "<cmd>nohlsearch<CR>";
-    # }
-    # {
-    #   mode = "n";
-    #   key = "f";
-    #   options = { desc = "Find files"; };
-    #   action =
-    #     "<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown{previewer = false})<cr>";
-    # }
-    {
-      mode = "n";
-      key = "F";
-      options = { desc = "Find Text"; };
-      action = "<cmd>Telescope live_grep theme=ivy<cr>";
-    }
-    # {
-    #   mode = "n";
-    #   key = "r";
-    #   options = { desc = "Search Recent Files"; };
-    #   action = ":Telescope oldfiles <CR>";
-    # }
-    # {
-    #   mode = "n";
-    #   key = "w";
-    #   options = { desc = "Search Neorg Wiki"; };
-    #   action = "<cmd>cd ~/vimwiki/home | :Telescope live_grep theme=ivy<cr>";
-    # }
   ];
   plugins = {
     # UI Enhancements
@@ -286,13 +147,12 @@
     leap = {
       enable = true;
       addDefaultMappings = true;
-      maxPhaseOneTargets = null; # or an integer value if needed
+      maxPhaseOneTargets = null;
       highlightUnlabeledPhaseOneTargets = false;
       maxHighlightedTraversalTargets = 10;
       caseSensitive = false;
       equivalenceClasses = [ " 	\r\n" ];
       substituteChars = { };
-      # safeLabels and labels can be defined if needed
       specialKeys = {
         nextTarget = "<enter>";
         prevTarget = "<tab>";
@@ -305,76 +165,44 @@
     which-key = {
       enable = true;
 
-      plugins = {
-        marks = true;
-
-        registers = true;
-
-        spelling = {
-          enabled = true;
-          suggestions = 20;
+      settings = {
+        layout = {
+          height = {
+            min = 4;
+            max = 25;
+          };
+          width = {
+            min = 20;
+            max = 50;
+          };
+          spacing = 3;
+          align = "left";
         };
-
-        presets = {
-          operators = true;
-          motions = true;
-          textObjects = true;
-          windows = true;
-          nav = true;
-          z = true;
-          g = true;
+        triggers = null; # or specify a list of triggers if needed
+        operators = { " " = "Comments"; };
+        triggersBlackList = {
+          i = [ "j" "k" ];
+          v = [ "j" "k" ];
         };
-      };
-      triggers = "auto";
-      operators = { " " = "Comments"; };
-      triggersBlackList = {
-        i = [ "j" "k" ];
-        v = [ "j" "k" ];
-      };
-      icons = {
-        breadcrumb = "»";
-        separator = "➜";
-        group = "+";
-      };
-
-      popupMappings = {
-        scrollDown = "<c-d>";
-        scrollUp = "<c-u>";
-      };
-      window = {
-        border = "rounded";
-        position = "bottom";
-        margin = {
-          top = 1;
-          right = 0;
-          bottom = 1;
-          left = 0;
+        icons = {
+          breadcrumb = "»";
+          separator = "➜";
+          group = "+";
         };
-        padding = {
-          top = 2;
-          right = 2;
-          bottom = 2;
-          left = 2;
+        popupMappings = {
+          scrollDown = "<c-d>";
+          scrollUp = "<c-u>";
         };
-        winblend = 0;
-      };
-
-      layout = {
-        height = {
-          min = 4;
-          max = 25;
+        win = {
+          border = "rounded";
+          title_pos = "bottom";
+          padding = [ 2 2 ]; # Horizontal and vertical padding
+          winblend = 0;
         };
-        width = {
-          min = 20;
-          max = 50;
-        };
-        spacing = 3;
-        align = "left";
       };
     };
     vim-bbye.enable = true;
     bufferline.enable = true;
-    # lsp-lines.enable = true;
     lspsaga.enable = true;
     trouble.enable = true;
     mark-radar.enable = true;
