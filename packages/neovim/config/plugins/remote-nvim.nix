6716884a80dccd75b680d34a5493cf1f501566f1
Matt Camp <matt@aicampground.com>
2024-08-10 07:51:53 -0500

    this builds

--- packages/neovim/config/plugins/remote-nvim.nix
+++ packages/neovim/config/plugins/remote-nvim.nix
@@ -1,27 +1,31 @@
 { pkgs, ... }:
 let
 
-  nui = pkgs.vimUtils.buildVimPlugin {
-    name = "nui";
-    src = pkgs.fetchFromGitHub {
-      owner = "MunifTanjim";
-      repo = "nui.nvim";
-      rev = "v0.3.0";
-      sha256 = "sha256-1/Uo8Bk7QPiQqAWL9jgYLtq6JNj1dnNDBSHn15FHlCM=";
-    };
-  };
-  remote-nvim = pkgs.vimUtils.buildVimPlugin {
-    name = "remote-nvim";
-    src = pkgs.fetchFromGitHub {
-      owner = "amitds1997";
-      repo = "remote-nvim.nvim";
-      rev = "v0.3.11";
-      sha256 = "sha256-ado876vs1D1tEQu+Q3jDUaJA9hf/9Y5JLCWu3rf219s=";
-    };
-  };
-in {
-  extraPlugins = [ remote-nvim nui ];
-  extraConfigLua = ''
-    require("remote-nvim").setup()
-  '';
+in
+# nui = pkgs.vimUtils.buildVimPlugin {
+#   name = "nui";
+#   src = pkgs.fetchFromGitHub {
+#     owner = "MunifTanjim";
+#     repo = "nui.nvim";
+#     rev = "v0.3.0";
+#     sha256 = "";
+#   };
+# };
+# remote-nvim = pkgs.vimUtils.buildVimPlugin {
+#   name = "remote-nvim";
+#   src = pkgs.fetchFromGitHub {
+#     owner = "amitds1997";
+#     repo = "remote-nvim.nvim";
+#     rev = "v0.3.11";
+#     sha256 = "sha256-ado876vs1D1tEQu+Q3jDUaJA9hf/9Y5JLCWu3rf219s=";
+#   };
+# };
+{
+  extraPlugins = [
+    # remote-nvim
+    # nui
+  ];
+  # extraConfigLua = ''
+  #   require("remote-nvim").setup()
+  # '';
 }
