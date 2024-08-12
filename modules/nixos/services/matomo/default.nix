4d08e903f63446a5bfce50973d6ea3b1068df228
Matt Camp <matt@aicampground.com>
2024-08-11 19:26:30 -0500

    trying out matomo

---
+++ modules/nixos/services/matomo/default.nix
@@ -0,0 +1,47 @@
+{ lib, config, ... }:
+with lib;
+with lib.campground;
+let
+  cfg = config.campground.services.matomo;
+in
+{
+  options.campground.services.matomo = with types; {
+    enable = mkBoolOpt false "Enable Matomo;";
+    rootDomain = mkOpt str "lan.aicampground.com" "Root domain to use for Matomo";
+  };
+
+  config = mkIf cfg.enable {
+    # campground.services.postgresql = {
+    #   enable = true;
+    #   authentication = [ "local matomo matomo trust" ];
+    #   databases = [
+    #     {
+    #       name = "matomo";
+    #       user = "matomo";
+    #     }
+    #   ];
+    # };
+
+    services.matomo = {
+      enable = true;
+      hostname = cfg.rootDomain;
+      nginx = {
+        serverAliases = [
+          "matomo.${cfg.rootDomain}"
+          "stats.${cfg.rootDomain}"
+        ];
+        serverName = "matomo.${cfg.rootDomain}";
+        listen = [
+          {
+            addr = "0.0.0.0";
+            port = 16969; # Change this to your desired port
+          }
+        ];
+        enableACME = false;
+
+      };
+
+    };
+
+  };
+}
