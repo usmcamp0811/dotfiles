8b0149cd0c4f54bf5ac703d049b5d61497e68219
Matt Camp <matt@aicampground.com>
2024-08-12 22:02:00 -0500

    add little spies to mattcamp

--- packages/matt-camp-website/src/router/index.js
+++ packages/matt-camp-website/src/router/index.js
@@ -1,14 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+import VueMatomo from 'vue-matomo'
 import routes from './routes'
 
 Vue.use(VueRouter)
 
-/*
- * If not building with SSR mode, you can
- * directly export the Router instantiation
- */
+// Configure VueMatomo
+Vue.use(VueMatomo, {
+  // Configure your matomo server and site by providing
+  host: 'https://matomo.aicampground.com/',
+  siteId: 1,
+
+  // Enable router integration
+  router: Router,
+
+  // Enable link tracking (optional)
+  enableLinkTracking: true,
+
+  // Other optional options
+  requireConsent: false, // if you require users to consent to tracking
+  trackInitialView: true, // tracks the initial view
+  disableCookies: false,  // disable cookies
+
+  // Custom tracking methods if needed
+  trackerFileName: 'matomo', // If you renamed the matomo.js file, adjust this setting
+  enableHeartBeatTimer: true, // recommended: activates the heartbeat of the tracker
+  heartBeatTimerInterval: 15 // in seconds
+})
 
 export default function (/* { store, ssrContext } */) {
   const Router = new VueRouter({
