09de083a6d77580de47803497d4fca89df46b176
Matt Camp <matt@aicampground.com>
2024-08-14 22:51:55 -0500

    wip

--- packages/mac-remote-builder/default.nix
+++ packages/mac-remote-builder/default.nix
@@ -1,14 +1,48 @@
 { pkgs, ... }:
 let
   src = ./.;
+  docker-image-name = "nix-remote-builder";
+
+  stop-builder = pkgs.writeShellScriptBin "stop" ''
+    ${pkgs.docker}/bin/docker stop ${docker-image-name}
+  '';
 
   build-image = pkgs.writeShellScriptBin "build" ''
-    AUTHORIZED_KEY=${"1:-" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAclfREva2i4LsnBQPY3ZSsZzeuS5DGn11u0abBR8cFv mcamp@butler"}
-    ${pkgs.docker}/bin/docker build -f ${src}/Dockerfile -t nix-build --build-arg AUTHORIZED_KEY="$AUTHORIZED_KEY" .
+    AUTHORIZED_KEY=''${"1:-" 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAclfREva2i4LsnBQPY3ZSsZzeuS5DGn11u0abBR8cFv mcamp@butler'}
+    ${pkgs.docker}/bin/docker build -f ${src}/Dockerfile -t nix-builder --build-arg AUTHORIZED_KEY="$AUTHORIZED_KEY" .
+  '';
+  nix_conf = ''
+    accept-flake-config = true
+    experimental-features = nix-command flakes
   '';
 
   start-builder = pkgs.writeShellScriptBin "start-builder" ''
-    ${pkgs.docker}/bin/docker -f ${src}/Dockerfile -t nix-build .
+    ACCESS_TOKENS=${1}
+    PORT=${"2:-2222"}
+    NETRC_FILE=${"3:-" "$HOME/.netrc"}
+
+    # Create a temporary directory for the nix.conf file
+    TEMP_DIR=$(mktemp -d)
+    NIX_CONF="$TEMP_DIR/nix.conf"
+
+    echo "accept-flake-config = true" > $NIC_CONF
+    echo "experimental-features = nix-command flakes" >> $NIC_CONF
+
+    if [ -n "$ACCESS_TOKENS" ]; then
+      echo "access-tokens = $ACCESS_TOKENS" >> $NIC_CONF
+    fi
+
+
+
+    ${pkgs.docker}/bin/docker run -it -p $PORT:22 \
+      -n ${docker-image-name} \
+      -v $NIX_CONF:/etc/nix/nix.conf:ro \
+      -v $NETRC_FILE:/root/.netrc:ro \
+      nix-builder
   '';
 in
-build-image
+{
+  build = build-image;
+  start = start-builder;
+  stop = stop-builder;
+}
