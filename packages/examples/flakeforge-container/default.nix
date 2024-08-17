90cc7af6cc59c1f7f1a1f45eddc25e9bdc3adb7c
Matt Camp <matt@aicampground.com>
2024-08-17 16:14:08 -0500

    wip

--- packages/examples/flakeforge-container/default.nix
+++ packages/examples/flakeforge-container/default.nix
@@ -4,6 +4,6 @@ let
   my-bash-image =
     inputs.flakeforge.packages.x86_64-linux.flakeforgeTools.streamLayeredImageConf {
       name = "bash-stream-layered";
-      contents = [ nixpkgs.legacyPackages.x86_64-linux.bashInteractive ];
+      contents = [ pkgs.bashInteractive ];
     };
 in my-bash-image
