60f29b2a080f80a86b6f640b6e6818e4f26f3da4
Matt Camp <matt@aicampground.com>
2024-08-17 18:17:38 -0500

    wip

--- packages/examples/flakeforge-container/default.nix
+++ packages/examples/flakeforge-container/default.nix
@@ -9,7 +9,10 @@ let
   flask-app = pkgs.streamLayeredImageConf {
     name = "flakeforge-example-flask-app";
     tag = "latest";
-    contents = [ pkgs.campground.example-flask-app ];
+    contents = [
+      pkgs.campground.example-flask-app
+      pkgs.bashInteractive
+    ];
     config = {
       Entrypoint = [ "${pkgs.campground.example-flask-app}/bin/run-app" ];
     };
