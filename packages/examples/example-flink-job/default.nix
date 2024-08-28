9fccb79d71e58d93281e903b6c5d7cf480639df4
Matt Camp <matt@aicampground.com>
2024-08-27 22:04:04 -0500

    cleaning up

--- packages/examples/example-flink-job/default.nix
+++ packages/examples/example-flink-job/default.nix
@@ -1,46 +1,7 @@
-{ lib, pkgs, hosts ? { }, ... }:
+{ lib, pkgs, ... }:
 with lib;
 with lib.campground;
 let
-  new-meta = with lib; {
-    description = "An Example PyFlink Job";
-    license = licenses.asl20;
-    maintainers = with maintainers; [ matt-camp ];
-    mainProgram = "example-flink-job";
-  };
-
-  pypkgs-build-requirements = {
-    avro = [ "setuptools" ];
-    avro-python3 =
-      [ "setuptools" "python-snappy" "zstandard" "isort" "pycodestyle" ];
-    apache-flink = [ "setuptools" ];
-    mocker = [ "setuptools" ];
-    apache-flink-libraries = [ "setuptools" ];
-  };
-
-  p2n-overrides = pkgs.poetry2nix.defaultPoetryOverrides.extend (self: super:
-    builtins.mapAttrs (package: build-requirements:
-      let
-        override = super."${package}".overridePythonAttrs (oldAttrs: {
-          buildInputs = (oldAttrs.buildInputs or [ ])
-            ++ (builtins.map (req: super."${req}") build-requirements);
-        });
-      in if package == "apache-flink-libraries" then
-        override.overrideAttrs (oldAttrs: {
-          postInstall = ''
-            ${oldAttrs.postInstall or ""}
-            rm -rf $out/lib/python3.11/site-packages/pyflink/__pycache__/version.cpython-311.pyc
-          '';
-        })
-      else
-        override) pypkgs-build-requirements);
-
-  python-env = pkgs.poetry2nix.mkPoetryEnv {
-    projectDir = src;
-    python = pkgs.python311;
-    overrides = p2n-overrides;
-    preferWheels = true; # Prefer wheels to speed up the build process
-  };
 
   src = ./.;
 
@@ -52,15 +13,29 @@ let
     ${example-flink-job.run-job}/bin/run-job ${src}/jobs/table_job.py
   '';
 
-  example-flink-job = mkFlinkDerivation {
-    inherit pkgs python-env;
+  example-flink-job = mkPyFlinkDerivation {
+    inherit pkgs;
     name = "example-flink-job";
     src = src;
     flink-job-script = "jobs/stream_job.py";
+    pypkgs-build-requirements = {
+      avro = [ "setuptools" ];
+      avro-python3 = [
+        "setuptools"
+        "python-snappy"
+        "zstandard"
+        "isort"
+        "pycodestyle"
+      ];
+      apache-flink = [ "setuptools" ];
+      mocker = [ "setuptools" ];
+      apache-flink-libraries = [ "setuptools" ];
+    };
     additionalPassThru = {
       stream-job = stream-job;
       table-job = table-job;
     };
   };
 
-in example-flink-job
+in
+example-flink-job
