efd7085977af5a2a3fc43167160cf7c372458455
Matt Camp <matt@aicampground.com>
2024-09-04 19:19:33 -0500

    this

--- systems/x86_64-linux/webb/default.nix
+++ systems/x86_64-linux/webb/default.nix
@@ -149,10 +149,9 @@ in
             url = "localhost:5432"; # Connect via TCP instead of using a socket
             user = "firefly"; # The correct user
             database = "firefly"; # The firefly database
-            jsonData =
-              {
-                sslmode = "disable"
-              };
+            jsonData = {
+              sslmode = "disable";
+            };
           }
         ];
       };
