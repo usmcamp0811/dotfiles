d1c564f3ae17d219fbe76d0eeec89e5f76ad41b3
Matt Camp <matt@aicampground.com>
2024-09-05 21:51:23 -0500

    wip

--- packages/airflow/default.nix
+++ packages/airflow/default.nix
@@ -1,35 +1,33 @@
-{
-  lib,
-  pkgs,
-  writeText,
-}:
+{ lib, pkgs, writeText, }:
 
 let
-  airflowVersion = "2.10.0";
+  airflowVersion = "2.10.0"; # Use the correct version/tag from GitHub
 
-  airflow = pkgs.python311Packages.buildPythonPackage {
+  airflow = pkgs.python311Packages.buildPythonApplication {
     pname = "apache-airflow";
     version = airflowVersion;
 
-    src = pkgs.fetchPypi {
-      pname = "apache-airflow";
-      version = airflowVersion;
-      sha256 = "sha256-+ycJlxbY8DYECMjKhtadv+1ERVg0tw0VBiUKvlIbU1o=";
+    src = pkgs.fetchFromGitHub {
+      owner = "apache";
+      repo = "airflow";
+      rev = airflowVersion;
+      sha256 =
+        "sha256-4pKRshuDyxA2Pad35DYH+TlDRwsJuLwnFIYdGSieBcw="; # Placeholder hash, update with the correct one
     };
-    doCheck = false;
 
     propagatedBuildInputs = with pkgs.python311Packages; [
       requests
       click
       flask
+      # Add other dependencies here, or extract them from the Airflow repo
     ];
 
     meta = with lib; {
       description = "Apache Airflow workflow management platform";
       homepage = "https://airflow.apache.org/";
+      # license = licenses.apache20;
       maintainers = with maintainers; [ matt-camp ]; # Add your maintainer info
     };
   };
 
-in
-airflow
+in airflow
