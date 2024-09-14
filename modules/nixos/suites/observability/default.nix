6e0a9f3b67e7218f975b1740ab568f86bb6abb0b
Matt Camp <matt@aicampground.com>
2024-09-13 22:14:09 -0500

    lets see if this works

--- modules/nixos/suites/observability/default.nix
+++ modules/nixos/suites/observability/default.nix
@@ -1,26 +1,39 @@
-{ options, config, lib, ... }:
+{
+  options,
+  config,
+  lib,
+  ...
+}:
 with lib;
 with lib.campground;
 
-let cfg = config.campground.suites.observability;
+let
+  cfg = config.campground.suites.observability;
 
-in {
+in
+{
   options.campground.suites.observability = with types; {
-    enable =
-      mkBoolOpt false "Whether or not to enable observability reporters.";
+    enable = mkBoolOpt false "Whether or not to enable observability reporters.";
     loki-uri = mkOpt str "webb:3030" "The <host>:<port> of the Loki server";
     prometheus = mkBoolOpt false "Whether or not to enable Prometheus server.";
     loki = mkBoolOpt false "Whether or not to enable Loki server.";
     grafana = mkBoolOpt false "Whether or not to enable Grafana server.";
-    hostnames =
-      mkOpt (listOf str) [ "mattis" "lucas" "chesty" "daly" "reckless" "webb" ]
-      "List of hostnames for scrape configs.";
+    hostnames = mkOpt (listOf str) [
+      "mattis"
+      "lucas"
+      "chesty"
+      "daly"
+      "reckless"
+      "webb"
+    ] "List of hostnames for scrape configs.";
   };
 
   config = mkIf cfg.enable {
     campground = {
       services = {
-        loki = { enable = cfg.loki; };
+        loki = {
+          enable = cfg.loki;
+        };
         prometheus = {
           enable = cfg.prometheus;
           exporter-enable = true;
@@ -29,21 +42,37 @@ in {
         promtail = {
           enable = true;
           loki-uri = cfg.loki-uri;
-          # additionalScrapeConfigs = [{
-          #   job_name = "traefik";
-          #   journal = {
-          #     max_age = "12h";
-          #     path = "/var/log/journal";
-          #     labels = {
-          #       job = "traefik";
-          #       host = config.networking.hostName;
-          #     };
-          #   };
-          #   relabel_configs = [{
-          #     source_labels = [ "__journal__systemd_unit" ];
-          #     target_label = "unit";
-          #   }];
-          # }];
+          additionalScrapeConfigs = [
+            {
+              job_name = "borgbackup-jobs";
+              journal = {
+                max_age = "12h";
+                labels = {
+                  job = "systemd-journal";
+                  host = config.networking.hostName;
+                };
+              };
+              relabel_configs = [
+                # Match specific borgbackup services and count their successful deactivations
+                {
+                  source_labels = [ "__journal__systemd_unit" ];
+                  regex = "borgbackup-job-webb_rsync.service|borgbackup-job-daly_rsync.service|borgbackup-job-campground.service";
+                  action = "keep";
+                }
+                {
+                  source_labels = [ "__journal_message" ];
+                  regex = "Deactivated successfully";
+                  action = "keep";
+                }
+                {
+                  source_labels = [ "__journal__systemd_unit" ];
+                  target_label = "borgbackup_service";
+                  action = "replace";
+                }
+              ];
+            }
+          ];
+
         };
       };
     };
