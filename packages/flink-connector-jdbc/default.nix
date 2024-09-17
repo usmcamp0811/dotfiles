c69fa8484e127d411d4cd535686c9a2ef6cf50dc
Matt Camp <matt@aicampground.com>
2024-09-17 13:46:36 -0500

    better

--- packages/flink-connector-jdbc/default.nix
+++ packages/flink-connector-jdbc/default.nix
@@ -4,12 +4,12 @@ let
   inherit (lib) mapAttrsToList concatStringsSep;
   inherit (lib.campground) override-meta;
 
-  kafka-jar = "flink-connector-jdbc_2.12";
-  jar-version = "1.13.6";
+  kafka-jar = "flink-connector-jdbc";
+  jar-version = "3.2.0-1.19";
 
   kafkaPlugin = pkgs.fetchurl {
     url =
       "https://repo.maven.apache.org/maven2/org/apache/flink/${kafka-jar}/${jar-version}/${kafka-jar}-${jar-version}.jar";
-    sha256 = "sha256-uuRbj0towaLvRjtIvw/QF2Wl0gzcCLMGTJb0P/ngdz4=";
+    sha256 = "sha256-KBNBkElPzvOYH1NgNc+nxPEqt4NCrBrKouDCPGNIQVg=";
   };
 in kafkaPlugin
