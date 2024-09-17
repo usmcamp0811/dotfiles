803b0c100f7e004f651afc12270e8cdee0df2d56
Matt Camp <matt@aicampground.com>
2024-09-16 21:13:44 -0500

    here we go

--- modules/nixos/services/prometheus/default.nix
+++ modules/nixos/services/prometheus/default.nix
@@ -1,51 +1,38 @@
-{
-  options,
-  config,
-  lib,
-  ...
-}:
+{ options, config, lib, ... }:
 with lib;
 with lib.campground;
 
 let
   cfg = config.campground.services.prometheus;
 
-  generateScrapeConfigs =
-    hostnames:
-    lib.concatMap (hostname: [
-      {
-        job_name = "${hostname}-system-monitor";
-        static_configs = [ { targets = [ "${hostname}:${toString cfg.exporter-port}" ]; } ];
-        relabel_configs = [
-          {
-            source_labels = [ "__address__" ];
-            regex = "([^:]+):.*";
-            target_label = "instance";
-            replacement = "$1";
-          }
-        ];
-      }
-    ]) hostnames;
+  generateScrapeConfigs = hostnames:
+    lib.concatMap (hostname: [{
+      job_name = "${hostname}-system-monitor";
+      static_configs =
+        [{ targets = [ "${hostname}:${toString cfg.exporter-port}" ]; }];
+      relabel_configs = [{
+        source_labels = [ "__address__" ];
+        regex = "([^:]+):.*";
+        target_label = "instance";
+        replacement = "$1";
+      }];
+    }]) hostnames;
 
-in
-{
+in {
   options.campground.services.prometheus = with types; {
     enable = mkBoolOpt false "Enable Prometheus";
     exporter-enable = mkBoolOpt false "Enable Prometheus Systemd Exporter";
     port = mkOpt int 9011 "Port to Host the Prometheus server on.";
     exporter-port = mkOpt int 9012 "Port to Host the Prometheus exporter on.";
     exporter-host = mkOpt str "webb" "The hostname or IP running Prometheus.";
-    hostName = mkOpt str config.networking.hostName "The hostname or IP to use for Prometheus.";
-    additionalScrapeConfigs = mkOpt (listOf (
-      attrsOf anything
-    )) [ ] "Additional scrape configs for Prometheus.";
+    hostName = mkOpt str config.networking.hostName
+      "The hostname or IP to use for Prometheus.";
+    additionalScrapeConfigs = mkOpt (listOf (attrsOf anything)) [ ]
+      "Additional scrape configs for Prometheus.";
     hostnames = mkOpt (listOf str) [ ] "List of hostnames for scrape configs.";
-    additionalCollectors = mkOpt (listOf str) [
-      "zfs"
-      "sysctl"
-      "powersupplyclass"
-      "nfs"
-    ] "List of additional Collectors";
+    additionalCollectors =
+      mkOpt (listOf str) [ "zfs" "sysctl" "powersupplyclass" "nfs" ]
+      "List of additional Collectors";
     scriptFiles = mkOption {
       type = types.attrsOf types.package;
       default = { };
@@ -65,27 +52,24 @@ in
           port = cfg.scriptExporterPort;
           openFirewall = true;
 
-          # Process scriptFiles into a list of script paths
-          scriptFiles = builtins.mapAttrsToList (
-            name: scriptDerivation:
-            # Get the script file path from the derivation
-            let
-              scriptPath =
-                if builtins.pathExists (scriptDerivation + "/bin/${name}") then
-                  scriptDerivation + "/bin/${name}"
-                else if builtins.pathExists (scriptDerivation + "/${name}") then
-                  scriptDerivation + "/${name}"
-                else
-                  (throw "Cannot find script file for '${name}' in derivation '${scriptDerivation}'");
-            in
-            scriptPath
-          ) cfg.scriptFiles;
-
-          # Set up endpoints for each script
-          endpoints = lib.mapAttrs (name: _: {
-            script = name;
-            timeout = "5s"; # You can make this configurable if needed
-          }) cfg.scriptFiles;
+          # Define scripts under settings.scripts
+          settings = {
+            scripts = lib.mapAttrs (name: scriptDerivation:
+              # Get the script file path from the derivation
+              let
+                scriptPath =
+                  if builtins.pathExists "${scriptDerivation}/bin/${name}" then
+                    "${scriptDerivation}/bin/${name}"
+                  else if builtins.pathExists "${scriptDerivation}/${name}" then
+                    "${scriptDerivation}/${name}"
+                  else
+                    (throw
+                      "Cannot find script file for '${name}' in derivation '${scriptDerivation}'");
+              in {
+                script = scriptPath;
+                timeout = "5s"; # You can make this configurable if needed
+              }) cfg.scriptFiles;
+          };
         };
         node = {
           enable = cfg.exporter-enable;
@@ -93,7 +77,8 @@ in
           port = cfg.exporter-port;
         };
       };
-      scrapeConfigs = generateScrapeConfigs cfg.hostnames ++ cfg.additionalScrapeConfigs;
+      scrapeConfigs = generateScrapeConfigs cfg.hostnames
+        ++ cfg.additionalScrapeConfigs;
     };
   };
 }
--- modules/nixos/suites/observability/default.nix
+++ modules/nixos/suites/observability/default.nix
@@ -1,40 +1,26 @@
-{
-  options,
-  config,
-  pkgs,
-  lib,
-  ...
-}:
+{ options, config, pkgs, lib, ... }:
 with lib;
 with lib.campground;
 
-let
-  cfg = config.campground.suites.observability;
+let cfg = config.campground.suites.observability;
 
-in
-{
+in {
   options.campground.suites.observability = with types; {
-    enable = mkBoolOpt false "Whether or not to enable observability reporters.";
+    enable =
+      mkBoolOpt false "Whether or not to enable observability reporters.";
     loki-uri = mkOpt str "webb:3030" "The <host>:<port> of the Loki server";
     prometheus = mkBoolOpt false "Whether or not to enable Prometheus server.";
     loki = mkBoolOpt false "Whether or not to enable Loki server.";
     grafana = mkBoolOpt false "Whether or not to enable Grafana server.";
-    hostnames = mkOpt (listOf str) [
-      "mattis"
-      "lucas"
-      "chesty"
-      "daly"
-      "reckless"
-      "webb"
-    ] "List of hostnames for scrape configs.";
+    hostnames =
+      mkOpt (listOf str) [ "mattis" "lucas" "chesty" "daly" "reckless" "webb" ]
+      "List of hostnames for scrape configs.";
   };
 
   config = mkIf cfg.enable {
     campground = {
       services = {
-        loki = {
-          enable = cfg.loki;
-        };
+        loki = { enable = cfg.loki; };
         prometheus = {
           enable = cfg.prometheus;
           exporter-enable = true;
@@ -53,67 +39,62 @@ in
             '';
 
             # Using pkgs.writeShellApplication
-            shellAppScript = pkgs.writeShellApplication {
-              name = "shellAppScript";
-              runtimeDeps = [ ]; # Add any runtime dependencies if needed
-              text = ''
-                #!/bin/bash
-                echo "shell_app_metric 3"
-              '';
-            };
+            # shellAppScript = pkgs.writeShellApplication {
+            #   name = "shellAppScript";
+            #   runtimeDeps = [ ]; # Add any runtime dependencies if needed
+            #   text = ''
+            #     #!/bin/bash
+            #     echo "shell_app_metric 3"
+            #   '';
+            # };
           };
-          additionalScrapeConfigs = [
-            {
-              job_name = "borgbackup-job-${config.networking.hostName}_rsync";
-              static_configs = [
-                {
-                  targets = [ "${config.networking.hostName}:9012" ]; # Assuming the node exporter runs on port 9012
-                }
-              ];
-              metrics_path = "/metrics";
-              relabel_configs = [
-                {
-                  source_labels = [ "__meta_systemd_service" ];
-                  regex = "borgbackup-job-${config.networking.hostName}_rsync";
-                  action = "keep";
-                }
-              ];
-            }
-          ];
+          additionalScrapeConfigs = [{
+            job_name = "borgbackup-job-${config.networking.hostName}_rsync";
+            static_configs = [{
+              targets = [
+                "${config.networking.hostName}:9012"
+              ]; # Assuming the node exporter runs on port 9012
+            }];
+            metrics_path = "/metrics";
+            relabel_configs = [{
+              source_labels = [ "__meta_systemd_service" ];
+              regex = "borgbackup-job-${config.networking.hostName}_rsync";
+              action = "keep";
+            }];
+          }];
         };
         promtail = {
           enable = true;
           loki-uri = cfg.loki-uri;
-          additionalScrapeConfigs = [
-            {
-              job_name = "borgbackup-jobs";
-              journal = {
-                max_age = "12h";
-                labels = {
-                  job = "systemd-journal";
-                  host = config.networking.hostName;
-                };
+          additionalScrapeConfigs = [{
+            job_name = "borgbackup-jobs";
+            journal = {
+              max_age = "12h";
+              labels = {
+                job = "systemd-journal";
+                host = config.networking.hostName;
               };
-              relabel_configs = [
-                # Match specific borgbackup services and count their successful deactivations
-                {
-                  source_labels = [ "__journal__systemd_unit" ];
-                  regex = "borgbackup-job-webb_rsync.service|borgbackup-job-daly_rsync.service|borgbackup-job-campground.service";
-                  action = "keep";
-                }
-                {
-                  source_labels = [ "__journal_message" ];
-                  regex = "Deactivated successfully";
-                  action = "keep";
-                }
-                {
-                  source_labels = [ "__journal__systemd_unit" ];
-                  target_label = "borgbackup_service";
-                  action = "replace";
-                }
-              ];
-            }
-          ];
+            };
+            relabel_configs = [
+              # Match specific borgbackup services and count their successful deactivations
+              {
+                source_labels = [ "__journal__systemd_unit" ];
+                regex =
+                  "borgbackup-job-webb_rsync.service|borgbackup-job-daly_rsync.service|borgbackup-job-campground.service";
+                action = "keep";
+              }
+              {
+                source_labels = [ "__journal_message" ];
+                regex = "Deactivated successfully";
+                action = "keep";
+              }
+              {
+                source_labels = [ "__journal__systemd_unit" ];
+                target_label = "borgbackup_service";
+                action = "replace";
+              }
+            ];
+          }];
 
         };
       };
