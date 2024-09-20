89de3f5442c7d6ce270a9fc98a32a241cccc7ff6
Matt Camp <matt@aicampground.com>
2024-09-20 14:55:58 -0500

    have i told you how much i hate flink

--- lib/pyflink/default.nix
+++ lib/pyflink/default.nix
@@ -192,12 +192,11 @@
         name = "run-job";
         text = ''
           PYFILES="${src},${src}/${pyFolderName}"
-          export CLASSPATH="${flink-with-kafka-connector}/opt/flink/lib"
-
           ${flink-with-kafka-connector}/bin/flink run \
             -py "$1" \
             -pyclientexec ${python-env.python}/bin/python \
             --pyFiles="$PYFILES" \
+            --classpath="file://${flink-with-kafka-connector}/opt/flink/lib" \
             --jarfile ${getFlinkKafkaConnector pkgs} ${
               builtins.concatStringsSep " "
               (map (jar: "--jarfile=${jar}") additionalJars)
