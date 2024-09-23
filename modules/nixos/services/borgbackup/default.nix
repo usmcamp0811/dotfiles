ff3c7390e51bcd69d4a0fd3f957bb84113ab8058
Matt Camp <matt@aicampground.com>
2024-09-22 19:32:52 -0500

    doh

--- modules/nixos/services/borgbackup/default.nix
+++ modules/nixos/services/borgbackup/default.nix
@@ -7,10 +7,10 @@ let
     serviceConfig.ExecStartPost = ''
       mkdir -p /var/lib/node_exporter/textfile_collector
       if [ $? -eq 0 ]; then
-        echo "borg_backup_success{job=\"${name}\"} 1" > /var/lib/node_exporter/textfile_collector/borg-backup-${name}.prom
-        echo "borg_backup_last_run{job=\"${name}\"} $(date +%s)" >> /var/lib/node_exporter/textfile_collector/borg-backup-${name}.prom
+        echo "borg_backup_success{job=\"${jobName}\"} 1" > /var/lib/node_exporter/textfile_collector/borg-backup-${jobName}.prom
+        echo "borg_backup_last_run{job=\"${jobName}\"} $(date +%s)" >> /var/lib/node_exporter/textfile_collector/borg-backup-${jobName}.prom
       else
-        echo "borg_backup_success{job=\"${name}\"} 0" > /var/lib/node_exporter.textfile_collector/borg-backup-${name}.prom
+        echo "borg_backup_success{job=\"${jobName}\"} 0" > /var/lib/node_exporter.textfile_collector/borg-backup-${jobName}.prom
       fi
     '';
   };
