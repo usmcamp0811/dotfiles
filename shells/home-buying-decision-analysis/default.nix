d2cba9b5ae3b7efa1dea13b07d6b2d37f24ebe7e
Matt Camp <matt@aicampground.com>
2024-09-24 23:11:36 -0500

    wip

--- shells/home-buying-decision-analysis/default.nix
+++ shells/home-buying-decision-analysis/default.nix
@@ -1,8 +1,17 @@
-{ lib, writeText, writeShellApplication, substituteAll, gum, inputs, pkgs
-, hosts ? { }, ... }:
+{
+  lib,
+  writeText,
+  writeShellApplication,
+  substituteAll,
+  gum,
+  inputs,
+  pkgs,
+  hosts ? { },
+  ...
+}:
+with lib;
+with lib.campground;
 let
-  inherit (lib) mapAttrsToList concatStringsSep;
-  inherit (lib.campground) override-meta;
   src = ./.;
   julia-env = pkgs.julia.withPackages.override { extraLibs = [ python-env ]; } [
     "IJulia"
@@ -11,39 +20,48 @@ let
     "PyCall"
   ];
 
-  pypkgs-build-requirements = { redfin = [ "setuptools" ]; };
+  # builtins.mapAttrs (package: build-requirements:
+  #   (builtins.getAttr package super).overridePythonAttrs (old: {
+  #     buildInputs = (old.buildInputs or [ ]) ++ (builtins.map (pkg:
+  #       if builtins.isString pkg then builtins.getAttr pkg super else pkg)
+  #       build-requirements);
+  #   })) pypkgs-build-requirements);
 
-  p2n-overrides = pkgs.poetry2nix.defaultPoetryOverrides.extend (self: super:
-    builtins.mapAttrs (package: build-requirements:
-      (builtins.getAttr package super).overridePythonAttrs (old: {
-        buildInputs = (old.buildInputs or [ ]) ++ (builtins.map (pkg:
-          if builtins.isString pkg then builtins.getAttr pkg super else pkg)
-          build-requirements);
-      })) pypkgs-build-requirements);
-
-  python-env = pkgs.poetry2nix.mkPoetryEnv {
-    projectDir = ./.;
-    overrides = p2n-overrides;
-    python = pkgs.python311;
+  python-env = mkPythonDerivation {
+    inherit pkgs;
+    src = ./.;
+    name = "redfin_search";
+    pypkgs-build-requirements = {
+      redfin = [ "setuptools" ];
+    };
   };
+
   startJupyterWithJulia = writeShellApplication {
     name = "start-jupyter-with-julia";
-    runtimeInputs = [ python-env julia-env ];
+    runtimeInputs = [
+      python-env
+      julia-env
+    ];
     text = ''
       # Ensure Julia kernel is installed
       # Start Jupyter console with Julia kernel
       export KERNEL_NAME="home-project-julia"
       JULIA_VERSION="$KERNEL_NAME-$(julia -e 'println(string(VERSION.major) * "." * string(VERSION.minor))')"
-      export PYTHONPATH="${python-env}/lib/python3.11/site-packages:${python-env}/lib/site-packages"
+      export PYTHONPATH="${python-env.python}/lib/python3.11/site-packages:${python-env}/lib/site-packages"
       ${julia-env}/bin/julia -e 'using IJulia; installkernel(ENV["KERNEL_NAME"])' 
-      ${python-env}/bin/jupyter console --kernel "$JULIA_VERSION" "$@"
+      ${python-env.python}/bin/jupyter console --kernel "$JULIA_VERSION" "$@"
     '';
   };
-in pkgs.mkShell {
-  buildInputs = [ pkgs.poetry julia-env python-env startJupyterWithJulia ];
+in
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.poetry
+    julia-env
+    python-env.python
+    startJupyterWithJulia
+  ];
   env = {
-    PYTHONPATH =
-      "${python-env}/lib/python3.11/site-packages:${python-env}/lib/site-packages";
+    PYTHONPATH = "${python-env.python}/lib/python3.11/site-packages:${python-env}/lib/site-packages";
   };
   shellHook = ''
     echo "Poetry environment and Julia project set up."
