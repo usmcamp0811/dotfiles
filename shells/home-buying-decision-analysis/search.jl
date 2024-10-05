13a1be6be646e4ed2c3757d880881e692ab03cd0
Matt Camp <matt@aicampground.com>
2024-10-04 21:07:45 -0500

    wip

--- shells/home-buying-decision-analysis/search.jl
+++ shells/home-buying-decision-analysis/search.jl
@@ -18,7 +18,8 @@ function coerce_dataframe(df::DataFrame)
     return df
 end
 
-function get_properties(city::String; listing_type::String="for_sale", past_days::Int=30, min_price::Int=300_000, max_price::Int=560_000)
+function get_properties(city::String; listing_type::String="for_sale", past_days::Int=30,
+min_price::Int=300_000, max_price::Int=560_000)
 
     # Fetch properties
     properties = hh.scrape_property(
@@ -37,24 +38,26 @@ function get_properties(city::String; listing_type::String="for_sale", past_days
 end
 
 
-function get_properties(cities::Array; listing_type::String="for_sale", past_days::Int=30, min_price::Int=300_000, max_price::Int=560_000, save_csv::Bool=true, csv_name::String="properties.csv")
+function get_properties(cities::Array; listing_type::String="for_sale", past_days::Int=30,
+min_price::Int=300_000, max_price::Int=560_000, save_csv::Bool=true, csv_name::String="properties.csv")
   all_properties = DataFrame()  # Initialize an empty DataFrame to collect all properties
-  
+
   # Check if file exists to avoid writing the header multiple times
   file_exists = isfile(csv_name)
-  
+
   for city in cities
     properties = get_properties(city; max_price=max_price)  # Get properties for the city
-    
+
     if save_csv
       # Write the header only if the file doesn't exist yet
       CSV.write(csv_name, properties, append=file_exists, writeheader=!file_exists)
       file_exists = true  # Set file exists to true after first write
     else
-      all_properties = vcat(all_properties, properties)  # Concatenate properties DataFrame if not saving to CSV
+      all_properties = vcat(all_properties, properties)  # Concatenate properties DataFrame if not saving
+      to CSV
     end
   end
-  
+
   if !save_csv
     return all_properties  # Return the collected properties if not saving to CSV
   end
@@ -62,22 +65,22 @@ end
 
 
 cities = ["Pittsburgh, PA", "Louisville, KY", "Chattanooga, TN"]
-cities = ["Louisville, KY", "St. Louis, MO", "Baltimore, MD", 
-          "Indianapolis, IN", "Philadelphia, PA", "Lancaster, PA", 
+cities = ["Louisville, KY", "St. Louis, MO", "Baltimore, MD",
+          "Indianapolis, IN", "Philadelphia, PA", "Lancaster, PA",
           "Washington, DC", "Seattle, WA", "Denver, CO", "Portsmouth, VA",
-          "Cleveland, OH", "Pittsburgh, PA", "Milwaukee, WI", 
-          "Buffalo, NY", "New Orleans, LA", "Minneapolis, MN", 
-          "Cincinnati, OH", "Kansas City, MO", "Richmond, VA", 
-          "Omaha, NE", "Providence, RI", "Rochester, NY", 
-          "Columbus, OH", "Detroit, MI", "Madison, WI", 
-          "Chattanooga, TN", "Salt Lake City, UT", "Worcester, MA", 
-          "Grand Rapids, MI", "Albany, NY", "Hartford, CT", 
-          "Knoxville, TN", "Dayton, OH", "Durham, NC", 
-          "Asheville, NC", "Greenville, SC", "Boise, ID", 
-          "Spokane, WA", "Fort Wayne, IN", "Toledo, OH", 
-          "Little Rock, AR", "Birmingham, AL", "Springfield, MA", 
-          "Augusta, GA", "Mobile, AL", "Tucson, AZ", 
-          "Fayetteville, AR", "Des Moines, IA", "Syracuse, NY", 
+          "Cleveland, OH", "Pittsburgh, PA", "Milwaukee, WI",
+          "Buffalo, NY", "New Orleans, LA", "Minneapolis, MN",
+          "Cincinnati, OH", "Kansas City, MO", "Richmond, VA",
+          "Omaha, NE", "Providence, RI", "Rochester, NY",
+          "Columbus, OH", "Detroit, MI", "Madison, WI",
+          "Chattanooga, TN", "Salt Lake City, UT", "Worcester, MA",
+          "Grand Rapids, MI", "Albany, NY", "Hartford, CT",
+          "Knoxville, TN", "Dayton, OH", "Durham, NC",
+          "Asheville, NC", "Greenville, SC", "Boise, ID",
+          "Spokane, WA", "Fort Wayne, IN", "Toledo, OH",
+          "Little Rock, AR", "Birmingham, AL", "Springfield, MA",
+          "Augusta, GA", "Mobile, AL", "Tucson, AZ",
+          "Fayetteville, AR", "Des Moines, IA", "Syracuse, NY",
           "Winston-Salem, NC", "Memphis, TN"]
 
 
@@ -91,7 +94,8 @@ function get_nearby_places(address::String; walking_distance_minutes::Int=15, pl
     distance_meters = walking_distance_minutes * 80  # Rough estimate of walking speed: 80 meters per minute
 
     # Geocode the address using OpenStreetMap Nominatim API
-    query_url = "https://nominatim.openstreetmap.org/search?q=$(replace(address, ' ' => '+'))&format=json&limit=1"
+    query_url = "https://nominatim.openstreetmap.org/search?q=$(replace(address, ' ' =>
+    '+'))&format=json&limit=1"
     response = HTTP.get(query_url)
     location = JSON.parse(String(response.body))[1]
     lat, lon = location["lat"], location["lon"]
@@ -105,7 +109,8 @@ function get_nearby_places(address::String; walking_distance_minutes::Int=15, pl
     node[$query_type](around:$distance_meters,$lat,$lon);
     out body;
     """
-    overpass_url = replace(overpass_url, "\$lat" => lat, "\$lon" => lon, "\$distance_meters" => string(distance_meters))
+    overpass_url = replace(overpass_url, "\$lat" => lat, "\$lon" => lon, "\$distance_meters" =>
+    string(distance_meters))
 
     # Send the query
     overpass_api_url = "http://overpass-api.de/api/interpreter"
@@ -113,7 +118,8 @@ function get_nearby_places(address::String; walking_distance_minutes::Int=15, pl
     places = JSON.parse(String(overpass_response.body))["elements"]
 
     # Return place details (name, lat, lon)
-    nearby_places = [(place["tags"]["name"], place["lat"], place["lon"]) for place in places if haskey(place["tags"], "name")]
+    nearby_places = [(place["tags"]["name"], place["lat"], place["lon"]) for place in places if
+    haskey(place["tags"], "name")]
     return nearby_places
 end
 
@@ -129,6 +135,25 @@ address = "5501 S Fawcett Ave, Tacoma, WA 98408"
 address = "4504 SE 65th Ave, Portland, OR 97206"
 address = "580 5th St, Lakeport, CA 95453"
 address = "211 Beech Street, Trinidad, CO 81082"
+address = "1232 Juniata St, Pittsburgh, PA 15233"
+address = "117 Vernon Ave, Louisville, KY 40206"
+address = "98 Ruth Street Pittsburgh, PA 15211"
+address = "3914 Howley Street Pittsburgh, PA 15224"
+address = "3423 Denny Street Pittsburgh, PA 15201"
+address = "1206 Linden Place Pittsburgh, PA 15212"
 grocery_stores = get_nearby_places(address, walking_distance_minutes=15, place_type="grocery")
-restaurants = get_nearby_places(address, walking_distance_minutes=17, place_type="restaurant")
+restaurants = get_nearby_places(address, walking_distance_minutes=15, place_type="restaurant")
 
+using HTTP
+
+url = "https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/searchRestaurants?locationId=304554"
+
+headers = Dict(
+    "x-rapidapi-key" => fastapikey,
+    "x-rapidapi-host" => "tripadvisor16.p.rapidapi.com"
+)
+
+response = HTTP.get(url, headers)
+data = String(response.body)
+
+println(data)
