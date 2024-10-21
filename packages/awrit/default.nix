2ac44cec3bd45a0f3f5531bc8c478858a072486f
Matt Camp <matt@aicampground.com>
2024-10-20 22:49:42 -0500

    still no go

--- packages/awrit/default.nix
+++ packages/awrit/default.nix
@@ -12,28 +12,26 @@ stdenv.mkDerivation {
 
   buildInputs = [ cmake ];
 
-  # Patch the CMakeLists.txt file before building
   patchPhase = ''
     substituteInPlace CMakeLists.txt \
-      --replace "GetCEFFileInfoForVesion" "GetCEFFileInfoForVersion"
+      --replace-fail "GetCEFFileInfoForVesion" "GetCEFFileInfoForVersion"
     substituteInPlace CMakeLists.txt \
-      --replace 'file(READ "''${CMAKE_BINARY_DIR}/index.json" INDEX_CONTENT)' \
-                'file(READ "''${CMAKE_BINARY_DIR}/index.json" INDEX_CONTENT)\nif(INDEX_CONTENT STREQUAL "")\n  message(FATAL_ERROR "Failed to download index.json or it is empty.")\nendif()'
+      --replace-fail "file(READ "''${CMAKE_BINARY_DIR}/index.json" INDEX_CONTENT)" \
+                "file(READ "''${CMAKE_BINARY_DIR}/index.json" INDEX_CONTENT)\nif(INDEX_CONTENT STREQUAL "")\n  message(FATAL_ERROR "Failed to download index.json or it is empty.")\nendif()"
     substituteInPlace CMakeLists.txt \
-      --replace 'string(JSON SHA GET ''${CEF_FILE_INFO} "''${CEF_VERSIONS}" ''${VERSION_IDX} files ''${FILE_IDX} sha1)' \
-                'string(JSON SHA GET "''${FILES}" ''${FILE_IDX} sha1)'
+      --replace-fail "string(JSON SHA GET ''${CEF_FILE_INFO} "''${CEF_VERSIONS}" ''${VERSION_IDX} files ''${FILE_IDX} sha1)" \
+                "string(JSON SHA GET "''${FILES}" ''${FILE_IDX} sha1)"
     substituteInPlace CMakeLists.txt \
-      --replace 'string(JSON NAME GET ''${CEF_FILE_INFO} "''${CEF_VERSIONS}" ''${VERSION_IDX} files ''${FILE_IDX} name)' \
-                'string(JSON NAME GET "''${FILES}" ''${FILE_IDX} name)'
+      --replace-fail "string(JSON NAME GET ''${CEF_FILE_INFO} "''${CEF_VERSIONS}" ''${VERSION_IDX} files ''${FILE_IDX} name)" \
+                "string(JSON NAME GET "''${FILES}" ''${FILE_IDX} name)"
     substituteInPlace CMakeLists.txt \
-      --replace 'FetchContent_Declare(cef_src' \
-                'if(DEFINED CEF_FILE_NAME AND DEFINED CEF_FILE_SHA)\n  FetchContent_Declare(cef_src'
+      --replace-fail "FetchContent_Declare(cef_src" \
+                "if(DEFINED CEF_FILE_NAME AND DEFINED CEF_FILE_SHA)\n  FetchContent_Declare(cef_src"
     substituteInPlace CMakeLists.txt \
-      --replace 'SOURCE_SUBDIR cmake)' \
-                'SOURCE_SUBDIR cmake)\nelse()\n  message(FATAL_ERROR "CEF file name or SHA is not defined. Cannot proceed with download.")\nendif()'
+      --replace-fail "SOURCE_SUBDIR cmake)" \
+                "SOURCE_SUBDIR cmake)\nelse()\n  message(FATAL_ERROR "CEF file name or SHA is not defined. Cannot proceed with download.")\nendif()"
   '';
 
-  # Standard build phases
   cmakeFlags = [ "-DCMAKE_BUILD_TYPE=Release" ];
   installPhase = ''
     mkdir -p $out/bin
