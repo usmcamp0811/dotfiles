38b14884ff3fd566978ef19e0f1aa7e490023f30
Matt Camp <matt@aicampground.com>
2024-10-20 22:32:58 -0500

    wip

--- packages/awrit/default.nix
+++ packages/awrit/default.nix
@@ -1,43 +1,48 @@
-{ lib, writeText, inputs, pkgs, hosts ? { }, ... }:
-let
-  inherit (lib) mapAttrsToList concatStringsSep;
-  inherit (lib.campground) override-meta;
+{ pkgs, stdenv, lib, cmake, fetchFromGitHub }:
 
-  awrit = pkgs.stdenv.mkDerivation rec {
-    pname = "awrit";
-    version = "main"; # Change this to a specific version if needed
-
-    src = pkgs.fetchFromGitHub {
-      owner = "chase";
-      repo = "awrit";
-      rev = "main";
-      sha256 = "sha256-9OlH5qx1zxulwQmNoaX3eLtw1MFEsTh/DUaK43xqDSM=";
-    };
-
-    buildInputs = [ pkgs.cmake pkgs.ninja ];
-
-    buildPhase = ''
-      cmake -G "Ninja" -DCMAKE_BUILD_TYPE=Release -S . -B build
-      cmake --build build
-    '';
-
-    installPhase = ''
-      cmake --install build --prefix $out
-    '';
-
-    meta = with lib; {
-      description = "Actual Web Rendering in Terminal";
-      homepage = "https://github.com/chase/awrit";
-      license = licenses.mit;
-      platforms = platforms.linux;
-    };
+stdenv.mkDerivation {
+  pname = "awrit";
+  version = "1.0.0";
+  src = fetchFromGitHub {
+    owner = "Chase";
+    repo = "awrit";
+    rev = "main";
+    sha256 = "sha256-9OlH5qx1zxulwQmNoaX3eLtw1MFEsTh/DUaK43xqDSM=";
   };
 
-  new-meta = with lib; {
-    description = "A Kafka Headquarters";
-    homepage = "https://github.com/tchiotludo/awrit";
-    license = pkgs.lib.licenses.mit;
-    maintainers = with maintainers; [ mattcamp ];
+  buildInputs = [ cmake ];
+
+  # Patch the CMakeLists.txt file before building
+  patchPhase = ''
+    substituteInPlace CMakeLists.txt \
+      --replace "GetCEFFileInfoForVesion" "GetCEFFileInfoForVersion"
+    substituteInPlace CMakeLists.txt \
+      --replace 'file(READ "''${CMAKE_BINARY_DIR}/index.json" INDEX_CONTENT)' \
+                'file(READ "''${CMAKE_BINARY_DIR}/index.json" INDEX_CONTENT)\nif(INDEX_CONTENT STREQUAL "")\n  message(FATAL_ERROR "Failed to download index.json or it is empty.")\nendif()'
+    substituteInPlace CMakeLists.txt \
+      --replace 'string(JSON SHA GET ''${CEF_FILE_INFO} "''${CEF_VERSIONS}" ''${VERSION_IDX} files ''${FILE_IDX} sha1)' \
+                'string(JSON SHA GET "''${FILES}" ''${FILE_IDX} sha1)'
+    substituteInPlace CMakeLists.txt \
+      --replace 'string(JSON NAME GET ''${CEF_FILE_INFO} "''${CEF_VERSIONS}" ''${VERSION_IDX} files ''${FILE_IDX} name)' \
+                'string(JSON NAME GET "''${FILES}" ''${FILE_IDX} name)'
+    substituteInPlace CMakeLists.txt \
+      --replace 'FetchContent_Declare(cef_src' \
+                'if(DEFINED CEF_FILE_NAME AND DEFINED CEF_FILE_SHA)\n  FetchContent_Declare(cef_src'
+    substituteInPlace CMakeLists.txt \
+      --replace 'SOURCE_SUBDIR cmake)' \
+                'SOURCE_SUBDIR cmake)\nelse()\n  message(FATAL_ERROR "CEF file name or SHA is not defined. Cannot proceed with download.")\nendif()'
+  '';
+
+  # Standard build phases
+  cmakeFlags = [ "-DCMAKE_BUILD_TYPE=Release" ];
+  installPhase = ''
+    mkdir -p $out/bin
+    cp awrit $out/bin/
+  '';
+
+  meta = with lib; {
+    description = "Awrit project with patched CMakeLists.txt";
+    license = licenses.bsd3;
+    maintainers = with maintainers; [ your_github_username ];
   };
-in
-override-meta new-meta awrit
+}
