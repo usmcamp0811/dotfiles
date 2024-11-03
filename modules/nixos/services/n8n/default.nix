cdae5d75de36b02aeb9fa29a08a11c09bcce7b2d
Matt Camp <matt@aicampground.com>
2024-11-03 14:15:06 -0600

    wip

--- modules/nixos/services/n8n/default.nix
+++ modules/nixos/services/n8n/default.nix
@@ -4,6 +4,7 @@ with lib.campground;
 let
   # TODO: One day maybe pass credentials automatically into n8n via Vault
   cfg = config.campground.services.n8n;
+  format = pkgs.formats.json { };
 in {
   options.campground.services.n8n = with types; {
     enable = mkBoolOpt false "Enable n8n.";
@@ -15,16 +16,13 @@ in {
         "WEBHOOK_URL for n8n, in case we’re running behind a reverse proxy. This cannot be set through configuration and must reside in an environment variable.";
     };
     settings = mkOption {
-      type = attrsOf str;
+      type = format.type;
       default = {
-        N8N_COMMUNITY_NODES_ENABLED = "true"; # Enables community nodes
-        N8N_NODES_EXCLUDE = ""; # Leave blank to include all nodes
-        N8N_NODES_INCLUDE = ""; # Leave blank to include all nodes
-        N8N_DEFAULT_TIMEOUT =
-          "120"; # Default timeout for node operations in seconds
-        N8N_PERSONALIZATION_ENABLED =
-          "false"; # Disables personalization analytics (optional)
+        generic = { timezone = config.time.timeZone; };
+        endpoints = { metrics = { enable = true; }; };
+        # community = { nodes = { enable = true; }; };
       };
+
       description =
         "Additional configuration settings for n8n, passed as environment variables. For supported values, see https://docs.n8n.io/hosting/environment-variables/configuration-methods/";
     };
