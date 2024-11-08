b97e253d812b54586e60e3e73ec74aa75ee8284c
Matt Camp <matt@aicampground.com>
2024-11-08 07:37:36 -0600

    add openwebui

---
+++ modules/nixos/services/open-webui/default.nix
@@ -0,0 +1,72 @@
+{ lib, config, ... }:
+with lib;
+with lib.campground;
+let cfg = config.campground.services.open-webui;
+in {
+  options.campground.services.open-webui = with types; {
+    enable = mkBoolOpt false "Enable Open-WebUI.";
+
+    stateDir = mkOption {
+      type = types.str;
+      default = "/var/lib/open-webui";
+      description = ''
+        Directory for Open-WebUI state files.
+      '';
+    };
+
+    port = mkOption {
+      type = types.int;
+      default = 8080;
+      description = ''
+        The port on which Open-WebUI listens.
+      '';
+    };
+
+    package = mkOption {
+      type = types.str;
+      default = "open-webui";
+      description = ''
+        The package to be used for Open-WebUI service.
+      '';
+    };
+
+    openFirewall = mkBoolOpt false "Open firewall for Open-WebUI.";
+
+    host = mkOption {
+      type = types.str;
+      default = "0.0.0.0";
+      description = ''
+        The host address for Open-WebUI.
+      '';
+    };
+
+    environmentFile = mkOption {
+      type = types.nullOr types.str;
+      default = null;
+      description = ''
+        Path to a file containing environment variables for the Open-WebUI service.
+      '';
+    };
+
+    environment = mkOption {
+      type = types.attrsOf types.str;
+      default = { };
+      description = ''
+        Additional environment variables for the Open-WebUI service.
+      '';
+    };
+  };
+
+  config = mkIf cfg.enable {
+    services.open-webui = {
+      enable = true;
+      stateDir = cfg.stateDir;
+      port = cfg.port;
+      package = cfg.package;
+      openFirewall = cfg.openFirewall;
+      host = cfg.host;
+      environmentFile = cfg.environmentFile;
+      environment = cfg.environment;
+    };
+  };
+}
