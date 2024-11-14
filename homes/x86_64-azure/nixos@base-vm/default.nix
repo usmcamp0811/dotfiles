83b443b21ab094a5f75c7ca88510a60e85cdcaf6
Matt Camp <matt@aicampground.com>
2024-11-14 13:18:55 -0600

    build azure

---
+++ homes/x86_64-azure/nixos@base-vm/default.nix
@@ -0,0 +1,17 @@
+{ lib, pkgs, config, osConfig ? { }, format ? "unknown", ... }:
+with lib;
+with lib.campground; {
+  campground = {
+    user = {
+      enable = true;
+      name = config.snowfallorg.user.name;
+    };
+
+    cli = {
+      zsh = enabled;
+      home-manager = enabled;
+      env = enabled;
+    };
+  };
+  home.stateVersion = "23.05";
+}
