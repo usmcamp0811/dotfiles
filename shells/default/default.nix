c9bb679f5135c484743cc8b1cb0851a52d4bf7fe
Matt Camp <matt@aicampground.com>
2024-12-02 21:01:10 -0600

    wip

--- shells/default/default.nix
+++ shells/default/default.nix
@@ -36,7 +36,7 @@ mkShell {
 
     # Initialize fzf keybindings for shell history
     export FZF_DEFAULT_OPTS="--height 40% --reverse --border"
-
+    source $ZSH/oh-my-zsh.sh
     source ${pkgs.fzf}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
     ZSH_AUTOSUGGEST_STRATEGY=(history)
     exec zsh
