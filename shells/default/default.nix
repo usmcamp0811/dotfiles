54da344c5e820e4b400ad538ac4a9a465252e724
Matt Camp <matt@aicampground.com>
2024-12-02 18:56:00 -0600

    this is how to do fzf in a shell

--- shells/default/default.nix
+++ shells/default/default.nix
@@ -4,7 +4,8 @@ with lib.campground;
 let
   # inherit (lib.campground) override-meta;
   inherit (inputs.self.hooks.${system}.pre-commit-check) shellHook;
-in mkShell {
+in
+mkShell {
   buildInputs = [
     pkgs.deadnix
     pkgs.hydra-check
@@ -18,12 +19,26 @@ in mkShell {
     pkgs.statix
     pkgs.campground.vault-scripts
     pkgs.vault
+    pkgs.zsh
+    pkgs.oh-my-zsh
+    pkgs.fzf
   ] ++ inputs.self.hooks.${system}.pre-commit-check.enabledPackages;
-
+  pure = true;
   shellHook = ''
     ${shellHook}
     echo 🏕️ Welcome to the Campground
     # Additional setup can go here
+    export ZSH=$HOME/.oh-my-zsh
+    export ZSH_THEME="agnoster"  # Replace with your desired theme
+    export ZDOTDIR=$PWD/.zshrc  # Use a project-specific .zshrc
+
+    # Initialize fzf keybindings for shell history
+    export FZF_DEFAULT_OPTS="--height 40% --reverse --border"
+    bindkey '^R' fzf-history-widget
+
+
+
+    exec zsh
 
   '';
 }
