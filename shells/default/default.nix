4a4dcc5d66faf683892f9c577247eddfa57a0741
Matt Camp <matt@aicampground.com>
2024-12-02 21:27:52 -0600

    bash

--- shells/default/default.nix
+++ shells/default/default.nix
@@ -17,8 +17,7 @@ in mkShell {
     pkgs.statix
     pkgs.campground.vault-scripts
     pkgs.vault-bin
-    pkgs.zsh
-    pkgs.oh-my-zsh
+    pkgs.bashInteractive # Use an interactive version of bash
     pkgs.fzf
     pkgs.util-linux # Provides `ps` and other standard utilities
     pkgs.coreutils # General Unix utilities
@@ -26,39 +25,31 @@ in mkShell {
 
   pure = true;
 
-  # Set Zsh as the default shell for this nix-shell environment
   shellHook = ''
     ${shellHook}
     echo 🏕️ Welcome to the Campground
 
-    # Set ZSH configuration directory
-    export ZSH=${pkgs.oh-my-zsh}/share/oh-my-zsh
+    # Set up fzf for bash history search
+    export FZF_DEFAULT_OPTS="--height 40% --reverse --border"
 
-    # Set your desired oh-my-zsh theme
-    export ZSH_THEME="fino"
+    # Bind Ctrl-R to fzf for history search
+    # This replaces the default reverse-i-search with fzf
+    if [ -n "$BASH_VERSION" ]; then
+      # Load fzf keybindings and history search setup
+      . ${pkgs.fzf}/share/fzf/key-bindings.bash
 
-    # Source oh-my-zsh
-    if [ -n "$ZSH_VERSION" ]; then
-      echo "You are already in zsh"
-    else
-      echo "Starting zsh..."
-      exec zsh
+      # Customizing history search using fzf
+      bind '"\C-r": "\C-e\fuzzy_search_history\n"'
+      fuzzy_search_history() {
+        local selected
+        selected=$(HISTTIMEFORMAT= fzf --height 40% --reverse --tac --border --preview 'echo {}' < <(history))
+        if [[ -n $selected ]]; then
+          READLINE_LINE=$(echo "$selected" | sed 's/^[ ]*[0-9]*[ ]*//')
+          READLINE_POINT=${ # READLINE_LINE}
+          fi}
     fi
 
-    # Only source fzf and zsh settings if we're in zsh
-    if [ -n "$ZSH_VERSION" ]; then
-      # Source fzf keybindings and completion
-      export FZF_DEFAULT_OPTS="--height 40% --reverse --border"
-      . ${pkgs.fzf}/share/fzf/completion.zsh
-      . ${pkgs.fzf}/share/fzf/key-bindings.zsh
-
-      # Source zsh-autosuggestions if available
-      if [ -f ${pkgs.fzf}/share/zsh-autosuggestions/zsh-autosuggestions.zsh ]; then
-        . ${pkgs.fzf}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-        ZSH_AUTOSUGGEST_STRATEGY=(history)
-      else
-        echo "Warning: zsh-autosuggestions not found"
-      fi
-    fi
+    # Include fzf completion (optional, helps with tab completion enhancements)
+    [ -f ${pkgs.fzf}/share/fzf/completion.bash ] && . ${pkgs.fzf}/share/fzf/completion.bash
   '';
 }
