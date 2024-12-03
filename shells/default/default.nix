c3cc0351a496cf6c96c7f27632627d392f330059
Matt Camp <matt@aicampground.com>
2024-12-02 21:15:22 -0600

    maybe

--- shells/default/default.nix
+++ shells/default/default.nix
@@ -25,21 +25,36 @@ mkShell {
     pkgs.fzf
   ] ++ inputs.self.hooks.${system}.pre-commit-check.enabledPackages;
   pure = true;
+
   shellHook = ''
     ${shellHook}
     echo 🏕️ Welcome to the Campground
 
+    # Set ZSH configuration directory
     export ZSH=${pkgs.oh-my-zsh}/share/oh-my-zsh
 
-    # Additional setup can go here
-    export ZSH_THEME="fino"  # Replace with your desired theme
+    # Set your desired oh-my-zsh theme
+    export ZSH_THEME="fino"
 
-    # Initialize fzf keybindings for shell history
-    export FZF_DEFAULT_OPTS="--height 40% --reverse --border"
-    exec zsh
+    # Source oh-my-zsh
     . $ZSH/oh-my-zsh.sh
+
+    # Source fzf keybindings and other settings
+    export FZF_DEFAULT_OPTS="--height 40% --reverse --border"
+
+    # Source fzf zsh keybindings and completion scripts
+    . ${pkgs.fzf}/share/fzf/completion.zsh
+    . ${pkgs.fzf}/share/fzf/key-bindings.zsh
+
+    # Source zsh-autosuggestions if available
     . ${pkgs.fzf}/share/zsh-autosuggestions/zsh-autosuggestions.zsh
     ZSH_AUTOSUGGEST_STRATEGY=(history)
 
+    # Finally, switch to zsh
+    if [ -n "$ZSH_VERSION" ]; then
+      echo "You are already in zsh"
+    else
+      zsh
+    fi
   '';
 }
