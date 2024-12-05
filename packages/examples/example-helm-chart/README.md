5a0deef51f44b5f506e24dbabc0bc71a1eff43c8
Matt Camp <matt@aicampground.com>
2024-12-04 18:40:18 -0600

    example helm chart init

---
+++ packages/examples/example-helm-chart/README.md
@@ -0,0 +1,4 @@
+# Example Helm Chart
+
+This is an attempt at doing Kubernetes things in a more of a Nix way.
+More to come... stand-by to stand-by...
---
+++ packages/examples/example-helm-chart/default.nix
@@ -0,0 +1,19 @@
+{ lib, pkgs, ... }:
+with lib;
+with lib.campground;
+let
+  example-chart = pkgs.stdenv.mkDerivation {
+    name = "example-chart";
+    src = ./.;
+    installPhase = ''
+      mkdir -p $out/src/chart
+      cp -r $src/chart/* $out/src/chart
+      mkdir -p $out/src/kubernetes
+      cp -r $src/kubernetes/* $out/src/kubernetes
+      mkdir -p $out/src/data
+      cp -r $src/data/* $out/src/data
+    '';
+    meta = { mainProgram = "Cyclops"; };
+  }; # // subCharts;
+in
+example-chart
