f9030800ed627ca9d2c04add6cb56cf0d7dd5a4f
Matt Camp <matt@aicampground.com>
2024-12-04 22:50:07 -0600

    add a helper function for secret yamls

--- lib/file/default.nix
+++ lib/file/default.nix
@@ -1,4 +1,4 @@
-{lib, ...}: rec {
+{ lib, ... }: rec {
   ## Append text to the contents of a file
   ##
   ## ```nix
@@ -20,4 +20,56 @@
   fileWithText' = file: text: ''
     ${text}
     ${builtins.readFile file}'';
+
+  #
+  # Creates a shell script that injects secrets into a YAML file.
+  #
+  # This function generates a script that can be used to inject secrets
+  # into a YAML file by substituting environment variables into the
+  # input YAML. The script leverages `vault` for secret management,
+  # `envsubst` to replace variables, and `yq` to manipulate YAML.
+  #
+  # Parameters:
+  # - `name`: (string) The name of the script binary to be created.
+  # - `env`: (attrs) Environment attributes, unused in this function but kept for future customization.
+  # - `secrets`: (string) Path to a script that sets environment variables, typically using values fetched from Vault.
+  #   The secrets script should have lines similar to:
+  #   ```
+  #   export AWS_ACCESS_KEY_ID=$(vault kv get -field=AWS_ACCESS_KEY_ID secret/campground/mlflow)
+  #   ```
+  #   This script sets the necessary environment variables used in the YAML interpolation.
+  # - `inputYaml`: (string) Path to the YAML file in which secrets should be injected.
+  #
+  # Example usage:
+  # ```
+  # mkSecretYaml {
+  #   name = "inject-secrets";
+  #   env = {};  # Attributes for additional environment settings.
+  #   secrets = ./secrets.sh;  # Path to the script containing environment variables.
+  #   inputYaml = ./values.yaml;  # Path to the YAML file to be processed.
+  # }
+  # ```
+  #
+  # The generated script does the following:
+  # - Loads secrets from the provided script.
+  # - Reads and substitutes environment variables into the input YAML.
+  # - Formats the resulting YAML using `bat` with syntax highlighting.
+  #
+  # Note:
+  # This function is particularly useful for injecting secrets into
+  # Helm `values.yaml` files, but it can be used for any YAML that
+  # requires secrets or dynamic values.
+  mkSecretYaml = { name, env, secrets, inputYaml }:
+    pkgs.writeShellScriptBin name ''
+      # fail fast!
+      set -e
+      export PATH=$PATH:${pkgs.vault-bin}/bin
+      if [ -z "$VAULT_ADDR" ]; then
+          echo "Error: VAULT_ADDR is not set. Please set it and try again."
+          exit 1
+      fi
+
+      source "${secrets}"
+      ${pkgs.bat}/bin/bat -p ${inputYaml} | ${pkgs.envsubst}/bin/envsubst | ${pkgs.yq}/bin/yq -y | ${pkgs.bat}/bin/bat --language yaml
+    '';
 }
