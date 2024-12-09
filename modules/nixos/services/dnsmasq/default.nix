26ec37060f841bbdd8509c7b971d7ad3b8f6fdd0
Matt Camp <matt@aicampground.com>
2024-12-08 19:17:54 -0600

    add dnsmasq

---
+++ modules/nixos/services/dnsmasq/default.nix
@@ -0,0 +1,32 @@
+{ lib, config, ... }:
+with lib;
+with lib.campground;
+let cfg = config.campground.services.dnsmasq;
+in {
+  options.campground.services.dnsmasq = with types; {
+    enable = mkBoolOpt false "Enable dnsmasq.";
+
+    settings = mkOption {
+      type = attrsOf (attrsOf str);
+      default = {
+        listenAddress = "127.0.0.1"; # Default to localhost
+        interface = ""; # Optional interface binding
+        extraConfig = "";
+      };
+      description = ''
+        Configuration for dnsmasq. Refer to the dnsmasq documentation for details on supported options.
+      '';
+    };
+  };
+
+  config = mkIf cfg.enable {
+    services.dnsmasq = {
+      enable = true;
+      extraConfig = cfg.settings.extraConfig;
+
+      # Optional interface binding and listen address
+      listenAddress = cfg.settings.listenAddress;
+      interface = cfg.settings.interface;
+    };
+  };
+}
