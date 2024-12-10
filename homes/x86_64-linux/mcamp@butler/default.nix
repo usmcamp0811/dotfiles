9126b5ba636e44086780cae8affbe12896930476
Matt Camp <matt@aicampground.com>
2024-12-09 23:09:23 -0600

    wip

--- homes/x86_64-linux/mcamp@butler/default.nix
+++ homes/x86_64-linux/mcamp@butler/default.nix
@@ -75,6 +75,12 @@ with lib.campground; {
     services = {
       openssh = {
         enable = true;
+        authorizedKeys = [
+          "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCz1jfoM5vFTtLxbHhhFQbKbMxy589qWE3vnWAYJJ9vUSh8k2uqLrwT/5z9dd5/+cvCsz3EcRJR7zlE1hGkyYfGgkEUhEvC2g5uVHtNlEAyAycHqJ/EmjdUdKITSnF92GEA4EpyOZWjlxPnOVsaqNqw0E3hekDJN6e88jjR3wszOf4bVmpDfg64tM6GHfArI7fbkiR4NhgZm5u90yVr+7orrUKQddkmqVxEovqU0iJmXRRwTRLKYWN5B1kqEGsy2z82DLO6GHCvLEb0kXt7SX00Y+1yIoToMpJLRCaHcdcGsNszdO/X8GYHK0IFN43+1M47FF0Dd0nBx7T98tgute7cg7yl5JM0ocJRUILbPXXan6iY6YMz7aJrsspvcXzh8WiY1zJkVWuWCdbYM9DtlXrT5AbWx3h/RZntxadd/mGKKgsS5x8N/aBLLgwZExjumBzNo9XoCpJ+C2/TjEFLpk/6vS+jM1klla/okiyr7OZ9DdRddjgFA8yNcWfsO8blE/c= mcamp@ATA-NUC"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINLbrIDbLSEpfOc4onBP8y6aKCNEN5rEe0J3h7klfKzG mcamp@butler"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAclfREva2i4LsnBQPY3ZSsZzeuS5DGn11u0abBR8cFv mcamp@butler"
+        ];
+
         extraConfigs = ''
           Host github.com-usmcamp0811
             HostName github.com
