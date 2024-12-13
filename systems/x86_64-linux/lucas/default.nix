f7ed9ed41bfd11426934de3f50bcef980341c7e0
Matt Camp <matt@aicampground.com>
2024-12-12 22:36:22 -0600

    wip

--- systems/x86_64-linux/lucas/default.nix
+++ systems/x86_64-linux/lucas/default.nix
@@ -65,19 +65,14 @@ with lib.campground; {
           api_addr = "http://127.0.0.1:8200"
         '';
 
-        # policies = builtins.foldl'
-        #   (policies: file:
-        #     policies // {
-        #       "${snowfall.path.get-file-name-without-extension file}" = file;
-        #     })
-        #   { }
-        #   (builtins.filter (snowfall.path.has-file-extension "hcl")
-        #     (builtins.map
-        #       (path:
-        #         ../daly/vault/policies + "/${
-        #         builtins.baseNameOf (builtins.unsafeDiscardStringContext path)
-        #       }")
-        #       (snowfall.fs.get-files ../daly/vault/policies)));
+        policies = builtins.foldl' (policies: file:
+          policies // {
+            "${snowfall.path.get-file-name-without-extension file}" = file;
+          }) { } (builtins.filter (snowfall.path.has-file-extension "hcl")
+            (builtins.map (path:
+              ../daly/vault/policies + "/${
+                builtins.baseNameOf (builtins.unsafeDiscardStringContext path)
+              }") (snowfall.fs.get-files ../daly/vault/policies)));
       };
       n8n = { enable = true; };
       chromadb = { enable = true; };
