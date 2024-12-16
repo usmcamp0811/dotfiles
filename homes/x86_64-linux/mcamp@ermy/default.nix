45f886925d6afa8b7a7261fb1cbca45734ef495f
Matt Camp <matt@aicampground.com>
2024-12-15 18:07:27 -0600

    wip

---
+++ homes/x86_64-linux/mcamp@ermy/default.nix
@@ -0,0 +1,43 @@
+{ lib, pkgs, config, osConfig ? { }, format ? "unknown", ... }:
+with lib.campground; {
+  campground = {
+    user = {
+      enable = true;
+      name = "mcamp";
+      fullName = "Matt Camp";
+      email = "matt@aicampground.com";
+    };
+
+    cli = {
+      zsh = enabled;
+      env = enabled;
+      home-manager = enabled;
+      ranger = enabled;
+    };
+    services = {
+      # picom = enabled;
+      # openssh = enabled;
+      # syncthing = enabled;
+    };
+
+    # apps = {
+    #   firefox = enabled;
+    #   brave = enabled;
+    #   libreoffice = enabled;
+    #   alacritty = enabled;
+    #   kitty = enabled;
+    #   rofi = enabled;
+    #   mpv = enabled;
+    #   #TODO: Add Qutebrowser
+    # };
+    tools = {
+      git = enabled;
+      # direnv = enabled;
+      # julia = enabled;
+      # python = enabled;
+      # vault = enabled;
+    };
+  };
+
+  home.stateVersion = "23.05";
+}
