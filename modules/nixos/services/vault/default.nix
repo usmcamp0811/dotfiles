19e58443624474045a5d9363a63cfdbb144272fe
Matt Camp <matt@aicampground.com>
2024-12-18 18:39:33 -0600

    WIP: Auto unseal Vault

--- modules/nixos/services/vault/default.nix
+++ modules/nixos/services/vault/default.nix
@@ -52,27 +52,27 @@ let
 
   unseal-script = pkgs.writeShellScriptBin "clevis-unseal-vault" ''
     # Path to the encrypted file containing the unseal key
-    encrypted_file="/path/to/encrypted/unseal-key.jwe"
+    encrypted_file="${cfg.tang-useal-key}"
 
     # Vault address (e.g., local or cluster address)
     vault_addr="http://127.0.0.1:8200"
 
     # Check if Vault is sealed
     is_sealed() {
-        vault status -address="$vault_addr" 2>/dev/null | grep -q "Sealed.*true"
+        ${package}/bin/vault status -address="$vault_addr" 2>/dev/null | grep -q "Sealed.*true"
         return $?
     }
 
     # Unseal Vault using the key from the encrypted file
     unseal_vault() {
         echo "Attempting to unseal Vault..."
-        unseal_key=$(clevis decrypt < "$encrypted_file")
+        unseal_key=$(${pkgs.clevis}/bin/clevis decrypt < "$encrypted_file")
         if [ $? -ne 0 ]; then
             echo "Error: Failed to decrypt the unseal key."
             exit 1
         fi
 
-        vault operator unseal -address="$vault_addr" "$unseal_key"
+        ${package}/bin/vault operator unseal -address="$vault_addr" "$unseal_key"
         if [ $? -eq 0 ]; then
             echo "Vault successfully unsealed."
         else
@@ -89,32 +89,65 @@ let
         echo "Vault is already unsealed. No action required."
     fi
   '';
+
+  write-policies-commands = mapAttrsToList
+    (name: policy: ''
+      echo Writing policy '${name}': '${policy}'
+      vault policy write '${name}' '${policy}'
+    '')
+    policies;
+  write-policies = concatStringsSep "\n" write-policies-commands;
+
+  known-policies = mapAttrsToList (name: _value: name) policies;
+
+  remove-unknown-policies = ''
+    current_policies=$(vault policy list -format=json | jq -r '.[]')
+    known_policies=(${
+      concatStringsSep " "
+      (builtins.map (policy: ''"${policy}"'') known-policies)
+    })
+
+    while read current_policy; do
+      is_known=false
+
+      for known_policy in "''${known_policies[@]}"; do
+        if [ "$known_policy" = "$current_policy" ]; then
+          is_known=true
+          break
+        fi
+      done
+
+      if [ "$is_known" = "false" ] && [ "$current_policy" != "default" ] && [ "$current_policy" != "root" ]; then
+        echo "Removing policy: $current_policy"
+        vault policy delete "$current_policy"
+      else
+        echo "Keeping policy: $current_policy"
+      fi
+    done <<< "$current_policies"
+  '';
 in
 {
   options.campground.services.vault = {
     enable = mkEnableOption "Vault";
-
     ui = mkBoolOpt true "Whether the UI should be enabled.";
+    auto-unseal =
+      mkBoolOpt false "Whether or not to auto unseal with Clevis & Tang";
+    tang-unseal-key = mkOpt (types.nullOr types.str) null
+      "Location of a Tang encrypted unseal key";
     storage = {
       backend = mkOpt types.str "file" "The storage backend for Vault.";
       path = mkOpt types.str "/var/lib/vault/data" "Path";
       config = mkOpt (types.nullOr types.str) null "Config";
     };
-
     address = mkOpt types.str "0.0.0.0:8200" "Where to access vault UI at";
-
     settings = mkOpt types.str "" "Configuration for Vault's config file.";
-
     mutable-policies = mkBoolOpt false
       "Whether policies not specified in Nix should be removed.";
-
     policies = mkOpt (types.attrsOf (types.either types.str types.path)) { }
       "Policies to install when Vault runs.";
-
     policy-agent = {
       user = mkOpt types.str "root" "The user to run the Vault Agent as.";
       group = mkOpt types.str "root" "The group to run the Vault Agent as.";
-
       auth = {
         roleIdFilePath = mkOpt types.str "/var/lib/vault/vault/role-id"
           "The file to read the role-id from.";
@@ -141,10 +174,18 @@ in
 
     systemd.services.vault = { };
 
-    # systemd.tmpfiles.rules =
-    #   if cfg.storage.backend == "file" then [
-    #     "d ${cfg.storage.path} 0750 vault vault -"
-    #   ] else [];
+    systemd.services.vault-unseal =
+      mkIf (cfg.auto-unseal && cfg.unseal-key != null) {
+        description =
+          "Run when Vault service restarts and needs to be unsealed";
+        wants = [ "vault.service" ]; # Ensures dependency on the Vault service
+        after = [ "vault.service" ]; # Ensures this runs after the Vault service
+        serviceConfig = {
+          Type = "oneshot";
+          ExecStart = unseal-script;
+        };
+        startWhen = "Vault service restarts";
+      };
 
     systemd.services.vault-policies =
       mkIf (has-policies || !cfg.mutable-policies) {
@@ -169,85 +210,47 @@ in
           VAULT_ADDR = "http://${config.services.vault.address}";
         };
 
-        script =
-          let
-            write-policies-commands = mapAttrsToList
-              (name: policy: ''
-                echo Writing policy '${name}': '${policy}'
-                vault policy write '${name}' '${policy}'
-              '')
-              policies;
-            write-policies = concatStringsSep "\n" write-policies-commands;
-
-            known-policies = mapAttrsToList (name: _value: name) policies;
-
-            remove-unknown-policies = ''
-              current_policies=$(vault policy list -format=json | jq -r '.[]')
-              known_policies=(${
-                concatStringsSep " "
-                (builtins.map (policy: ''"${policy}"'') known-policies)
-              })
-
-              while read current_policy; do
-                is_known=false
-
-                for known_policy in "''${known_policies[@]}"; do
-                  if [ "$known_policy" = "$current_policy" ]; then
-                    is_known=true
-                    break
-                  fi
-                done
-
-                if [ "$is_known" = "false" ] && [ "$current_policy" != "default" ] && [ "$current_policy" != "root" ]; then
-                  echo "Removing policy: $current_policy"
-                  vault policy delete "$current_policy"
-                else
-                  echo "Keeping policy: $current_policy"
-                fi
-              done <<< "$current_policies"
-            '';
-          in
-          ''
-            if ! [ -f '${cfg.policy-agent.auth.roleIdFilePath}' ]; then
-              echo 'role-id file not found: ${cfg.policy-agent.auth.roleIdFilePath}'
-              exit 0
-            fi
-
-            if ! [ -f '${cfg.policy-agent.auth.secretIdFilePath}' ]; then
-              echo 'secret-id file not found: ${cfg.policy-agent.auth.secretIdFilePath}'
-              exit 0
-            fi
-
-            role_id="$(cat '${cfg.policy-agent.auth.roleIdFilePath}')"
-            secret_id="$(cat '${cfg.policy-agent.auth.secretIdFilePath}')"
-
-            seal_status=$(curl -s "$VAULT_ADDR/v1/sys/seal-status" | jq ".sealed")
-
-            echo "Seal Status: $seal_status"
-
-            if [ seal_status = "true" ]; then
-              echo "Vault is currently sealed, cannot install policies."
-              exit 1
-            fi
-
-            echo "Getting token..."
-
-            token=$(vault write -field=token auth/approle/login \
-              role_id="$role_id" \
-              secret_id="$secret_id" \
-            )
-
-            echo "Logging in..."
-
-            export VAULT_TOKEN="$(vault login -method=token -token-only token="$token")"
-
-            echo "Writing policies..."
-
-            ${write-policies}
-
-            ${optionalString (!cfg.mutable-policies) remove-unknown-policies}
+        script = ''
+          if ! [ -f '${cfg.policy-agent.auth.roleIdFilePath}' ]; then
+            echo 'role-id file not found: ${cfg.policy-agent.auth.roleIdFilePath}'
             exit 0
-          '';
+          fi
+
+          if ! [ -f '${cfg.policy-agent.auth.secretIdFilePath}' ]; then
+            echo 'secret-id file not found: ${cfg.policy-agent.auth.secretIdFilePath}'
+            exit 0
+          fi
+
+          role_id="$(cat '${cfg.policy-agent.auth.roleIdFilePath}')"
+          secret_id="$(cat '${cfg.policy-agent.auth.secretIdFilePath}')"
+
+          seal_status=$(curl -s "$VAULT_ADDR/v1/sys/seal-status" | jq ".sealed")
+
+          echo "Seal Status: $seal_status"
+
+          if [ seal_status = "true" ]; then
+            echo "Vault is currently sealed, cannot install policies."
+            exit 1
+          fi
+
+          echo "Getting token..."
+
+          token=$(vault write -field=token auth/approle/login \
+            role_id="$role_id" \
+            secret_id="$secret_id" \
+          )
+
+          echo "Logging in..."
+
+          export VAULT_TOKEN="$(vault login -method=token -token-only token="$token")"
+
+          echo "Writing policies..."
+
+          ${write-policies}
+
+          ${optionalString (!cfg.mutable-policies) remove-unknown-policies}
+          exit 0
+        '';
       };
   };
 }
