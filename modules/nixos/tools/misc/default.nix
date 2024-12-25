12d5946b0da6fa7ac601ad8f0a38d98a71e943be
Matt Camp <matt@aicampground.com>
2024-12-25 17:52:05 -0600

    wip

--- modules/nixos/tools/misc/default.nix
+++ modules/nixos/tools/misc/default.nix
@@ -1,14 +1,29 @@
 { options, config, lib, pkgs, ... }:
 with lib;
 with lib.campground;
-let cfg = config.campground.tools.misc;
-in {
+let
+  cfg = config.campground.tools.misc;
+  # flake-src = ../../../..;
+
+  # dynamicValue =
+  #   pkgs.runCommand "get-sys-configs" { buildInputs = [ pkgs.nix ]; } ''
+  #     set -e  # Stop on errors
+  #     nix repl 2>/dev/null <<EOF > $out || (echo "Error in nix repl execution" > $out && exit 0)
+  #       :lf ${toString flake-src}
+  #       sys = lib.findVaultPathsAndFields outputs.nixosConfigurations.butler.config.campground
+  #       builtins.toJSON sys
+  #     EOF
+  #   '';
+  # dynamicValueText = builtins.readFile dynamicValue;
+in
+{
   options.campground.tools.misc = with types; {
     enable = mkBoolOpt false "Whether or not to enable common utilities.";
   };
 
   config = mkIf cfg.enable {
     campground.home.configFile."wgetrc".text = "";
+    # campground.home.configFile."tst.json".text = dynamicValueText;
 
     environment.systemPackages = with pkgs; [
       fzf
