1452b1ce6d42ebfe2485b92904027d6c68f1596d
Matt Camp <matt@aicampground.com>
2024-12-25 22:28:05 -0600

    making a function

---
+++ lib/hosting/default.nix
@@ -0,0 +1,20 @@
+{ lib, inputs, ... }:
+with lib; rec {
+  lookupServiceEndpoint = { nixosConfigurations, serviceName }:
+    let
+      # Helper function to construct the URL for a service
+      getServiceUrl = host: cfg:
+        if cfg.config.campground.services.${serviceName}.enable or false then
+          let port = cfg.config.campground.services.${serviceName}.port or null;
+          in if port != null then "http://${host}:${toString port}" else null
+        else
+          null;
+
+      # Collect all URLs for hosts with the service enabled
+      serviceUrls = builtins.filter (url: url != null)
+        (map (host: getServiceUrl host inputs.self.nixosConfigurations.${host})
+          (builtins.attrNames inputs.self.nixosConfigurations));
+    in
+    serviceUrls;
+
+}
