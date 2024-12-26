d586ebcb77d0e8b2097d2dc8c745491466a2d6c3
Matt Camp <matt@aicampground.com>
2024-12-25 19:03:00 -0600

    cool this is what i need to do.. but not htis

--- modules/nixos/tools/misc/default.nix
+++ modules/nixos/tools/misc/default.nix
@@ -1,20 +1,11 @@
-{ options, config, lib, pkgs, ... }:
+{ options, config, inputs, lib, pkgs, ... }:
 with lib;
 with lib.campground;
 let
   cfg = config.campground.tools.misc;
-  # flake-src = ../../../..;
-
-  # dynamicValue =
-  #   pkgs.runCommand "get-sys-configs" { buildInputs = [ pkgs.nix ]; } ''
-  #     set -e  # Stop on errors
-  #     nix repl 2>/dev/null <<EOF > $out || (echo "Error in nix repl execution" > $out && exit 0)
-  #       :lf ${toString flake-src}
-  #       sys = lib.findVaultPathsAndFields outputs.nixosConfigurations.butler.config.campground
-  #       builtins.toJSON sys
-  #     EOF
-  #   '';
-  # dynamicValueText = builtins.readFile dynamicValue;
+  flake-src = ../../../..;
+  configs = inputs.self.nixosConfigurations;
+  sys = lib.campground.findVaultPathsAndFields configs.butler.config.campground;
 in
 {
   options.campground.tools.misc = with types; {
@@ -23,7 +14,7 @@ in
 
   config = mkIf cfg.enable {
     campground.home.configFile."wgetrc".text = "";
-    # campground.home.configFile."tst.json".text = dynamicValueText;
+    campground.home.configFile."tst.json".text = builtins.toJSON sys;
 
     environment.systemPackages = with pkgs; [
       fzf
