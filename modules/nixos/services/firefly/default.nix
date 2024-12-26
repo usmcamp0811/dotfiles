c605aedb3ebd731ef30f0355f4c57acc8f2eb4a2
Matt Camp <matt@aicampground.com>
2024-12-25 22:05:51 -0600

    more port adding

--- modules/nixos/services/firefly/default.nix
+++ modules/nixos/services/firefly/default.nix
@@ -5,6 +5,7 @@ let cfg = config.campground.services.firefly;
 in {
   options.campground.services.firefly = with types; {
     enable = mkBoolOpt false "Enable Firefly III.";
+    port = mkOpt int 16244 "Port for firefly";
     firefly-user = mkOpt str "firefly" "user for Firefly III.";
     firefly-group = mkOpt str "firefly" "user for Firefly III.";
     dataDir = mkOpt str "/var/lib/${cfg.firefly-user}"
@@ -27,23 +28,24 @@ in {
     virtualHost =
       mkOpt str "firefly.lan.aicampground.com" "Virtual host for Firefly III.";
     package = mkOpt types.package pkgs.firefly-iii "Package for Firefly III.";
-    poolConfig = mkOpt attrs {
-      "listen.owner" = mkDefault "nginx";
-      "listen.group" = mkDefault "nginx";
-      pm = "dynamic";
-      "pm.max_children" = 32;
-      "pm.max_requests" = 500;
-      "pm.max_spare_servers" = 4;
-      "pm.min_spare_servers" = 2;
-      "pm.start_servers" = 2;
-    } "Pool configuration for Firefly III.";
+    poolConfig = mkOpt attrs
+      {
+        "listen.owner" = mkDefault "nginx";
+        "listen.group" = mkDefault "nginx";
+        pm = "dynamic";
+        "pm.max_children" = 32;
+        "pm.max_requests" = 500;
+        "pm.max_spare_servers" = 4;
+        "pm.min_spare_servers" = 2;
+        "pm.start_servers" = 2;
+      } "Pool configuration for Firefly III.";
 
     role-id =
       mkOpt str config.campground.services.vault-agent.settings.vault.role-id
-      "Absolute path to the Vault role-id";
+        "Absolute path to the Vault role-id";
     secret-id =
       mkOpt str config.campground.services.vault-agent.settings.vault.secret-id
-      "Absolute path to the Vault secret-id";
+        "Absolute path to the Vault secret-id";
     vault-path = mkOpt str "secret/campground/firefly"
       "The Vault path to the KV containing the KVs that are for each database";
     kvVersion = mkOption {
@@ -68,7 +70,7 @@ in {
       virtualHosts.${cfg.virtualHost} = {
         listen = [{
           addr = "0.0.0.0";
-          port = 16244;
+          port = cfg.port;
         }];
       };
     };
