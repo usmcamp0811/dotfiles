c4ee8301aabba8aacbf9c7a99acfb2cf9dbf8147
Matt Camp <matt@aicampground.com>
2024-12-25 22:34:51 -0600

    this works

--- lib/hosting/default.nix
+++ lib/hosting/default.nix
@@ -1,6 +1,8 @@
 { lib, inputs, ... }:
 with lib; rec {
+
   lookupServiceEndpoint = { nixosConfigurations, serviceName }:
+
     let
       # Helper function to construct the URL for a service
       getServiceUrl = host: cfg:
@@ -12,8 +14,8 @@ with lib; rec {
 
       # Collect all URLs for hosts with the service enabled
       serviceUrls = builtins.filter (url: url != null)
-        (map (host: getServiceUrl host inputs.self.nixosConfigurations.${host})
-          (builtins.attrNames inputs.self.nixosConfigurations));
+        (map (host: getServiceUrl host nixosConfigurations.${host})
+          (builtins.attrNames nixosConfigurations));
     in
     serviceUrls;
 
