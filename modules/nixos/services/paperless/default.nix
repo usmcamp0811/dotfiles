e2cb3b1e6de6c224a3210f255cedbf7dcabe68f9
Matt Camp <matt@aicampground.com>
2024-12-31 13:03:52 -0600

    wip

--- modules/nixos/services/paperless/default.nix
+++ modules/nixos/services/paperless/default.nix
@@ -1,8 +1,34 @@
 { lib, config, pkgs, ... }:
 with lib;
 with lib.campground;
-let cfg = config.campground.services.paperless;
-in {
+let
+  cfg = config.campground.services.paperless;
+  package = config.services.paperless.package;
+  # Extract the default script
+  hacked-web-script =
+    let secretKeyFile = "${cfg.dataDir}/nixos-paperless-secret-key";
+    in ''
+      # Set the PAPERLESS_SOCIALACCOUNT_PROVIDERS variable
+      PAPERLESS_SOCIALACCOUNT_PROVIDERS=$(cat /var/lib/vault/paperless.oidc)
+      export PAPERLESS_SOCIALACCOUNT_PROVIDERS
+      if [[ ! -f '${secretKeyFile}' ]]; then
+        (
+          umask 0377
+          tr -dc A-Za-z0-9 < /dev/urandom | head -c64 | ${pkgs.moreutils}/bin/sponge '${secretKeyFile}'
+        )
+      fi
+      PAPERLESS_SECRET_KEY="$(cat '${secretKeyFile}')"
+      export PAPERLESS_SECRET_KEY
+      if [[ ! $PAPERLESS_SECRET_KEY ]]; then
+        echo "PAPERLESS_SECRET_KEY is empty, refusing to start."
+        exit 1
+      fi
+      exec ${package.python.pkgs.gunicorn}/bin/gunicorn \
+        -c ${package}/lib/paperless-ngx/gunicorn.conf.py paperless.asgi:application
+    '';
+
+in
+{
   options.campground.services.paperless = with types; {
     enable = mkBoolOpt false "Enable Mattermost;";
     dataDir = mkOpt str "/var/lib/paperless" "Location to store data";
@@ -14,6 +40,7 @@ in {
     gotenbergPort = mkOpt str "3000" "Gotenberg Port";
     tikaPort = mkOpt str "9998" "Tika Port";
     domainName = mkOpt str "https://docs.lan.aicampground.com" "domain to use";
+    oidc-auth = mkBoolOpt true "Enable Authentik Login";
     role-id =
       mkOpt str config.campground.services.vault-agent.settings.vault.role-id
         "Absolute path to the Vault role-id";
@@ -77,6 +104,8 @@ in {
           "http://127.0.0.1:${cfg.gotenbergPort}";
 
         PAPERLESS_EMAIL_TASK_CRON = "*/5 * * * *";
+        PAPERLESS_APPS =
+          mkIf cfg.oidc-auth "allauth.socialaccount.providers.openid_connect";
       };
     };
 
@@ -88,15 +117,20 @@ in {
           "root"; # Use the root user to create the folder and set permissions
         ExecStartPre =
           "${pkgs.coreutils}/bin/chown root:root /var/lib/vault"; # Set folder ownership to root
-        ExecStart =
-          "${pkgs.coreutils}/bin/cp /tmp/detsys-vault/paperless.pass /var/lib/vault/paperless.pass";
-        ExecStartPost =
-          "${pkgs.coreutils}/bin/chown paperless:paperless /var/lib/vault/paperless.pass"; # Change file ownership to vaultwarden
       };
+      script = ''
+        ${pkgs.coreutils}/bin/cp /tmp/detsys-vault/paperless.pass /var/lib/vault/paperless.pass
+        ${pkgs.coreutils}/bin/cp /tmp/detsys-vault/paperless.oidc /var/lib/vault/paperless.oidc
+      '';
+      postStart = ''
+        ${pkgs.coreutils}/bin/chown paperless:paperless /var/lib/vault/paperless.pass
+        ${pkgs.coreutils}/bin/chown paperless:paperless /var/lib/vault/paperless.oidc
+      '';
       wantedBy = [ "multi-user.target" ];
-      before = [ "paperless.service" ];
+      before = [ "paperless-web.service" ];
     };
-
+    systemd.services.paperless-web.script =
+      mkIf cfg.oidc-auth hacked-web-script;
     virtualisation.oci-containers.containers.gotenberg = {
       user = "gotenberg:gotenberg";
       image = "gotenberg/gotenberg:7.8.1";
@@ -116,28 +150,39 @@ in {
       ports = [ "127.0.0.1:${cfg.tikaPort}:9998" ];
     };
 
-    campground.services.vault-agent.services.paperlessPasswordFile = {
-      settings = {
-        vault.address = cfg.vault-address;
-        auto_auth = {
-          method = [{
-            type = "approle";
-            config = {
-              role_id_file_path = cfg.role-id;
-              secret_id_file_path = cfg.secret-id;
-              remove_secret_id_file_after_reading = false;
-            };
-          }];
+    campground.services.vault-agent.services = {
+      paperlessPasswordFile = {
+        settings = {
+          vault.address = cfg.vault-address;
+          auto_auth = {
+            method = [{
+              type = "approle";
+              config = {
+                role_id_file_path = cfg.role-id;
+                secret_id_file_path = cfg.secret-id;
+                remove_secret_id_file_after_reading = false;
+              };
+            }];
+          };
         };
-      };
-      secrets = {
-        file = {
-          files = {
-            "paperless.pass" = {
-              text = ''
-                {{ with secret "${cfg.vault-path}" }}{{ if eq "${cfg.kvVersion}" "v1" }}{{ .Data.password }}{{ else }}{{ .Data.data.password }}{{ end }}{{ end }}'';
-              permissions = "0600";
-              change-action = "restart";
+        secrets = {
+          file = {
+            files = {
+              "paperless.oidc" = {
+                text = ''
+                  {{ with secret "${cfg.vault-path}" }}
+                  {"openid_connect":{"OAUTH_PKCE_ENABLED":true,"APPS":[{"provider_id":"authentik","name":"Authentik","client_id":"{{ if eq "${cfg.kvVersion}" "v1" }}{{ .Data.CLIENT_ID }}{{ else }}{{ .Data.data.CLIENT_ID }}{{ end }}","secret":"{{ if eq "${cfg.kvVersion}" "v1" }}{{ .Data.CLIENT_SECRET }}{{ else }}{{ .Data.data.CLIENT_SECRET }}{{ end }}","settings":{"server_url":"https://auth.aicampground.com/application/o/paperless/.well-known/openid-configuration"}}]}}
+                  {{ end }}
+                '';
+                permissions = "0600";
+                change-action = "restart";
+              };
+              "paperless.pass" = {
+                text = ''
+                  {{ with secret "${cfg.vault-path}" }}{{ if eq "${cfg.kvVersion}" "v1" }}{{ .Data.password }}{{ else }}{{ .Data.data.password }}{{ end }}{{ end }}'';
+                permissions = "0600";
+                change-action = "restart";
+              };
             };
           };
         };
