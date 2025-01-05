ae3c0af7ed7e9347a3db5219d28ab91f9966d187
Matt Camp <matt@aicampground.com>
2025-01-05 14:44:28 -0600

    initial wag at lambda functions

---
+++ lib/lambda/default.nix
@@ -0,0 +1,46 @@
+{ lib, ... }:
+with lib; rec {
+  getLambdaHandler = src:
+    builtins.replaceStrings [ ".py" ] [ "" ] (builtins.baseNameOf src);
+
+  mkAWSLambdaPythonImage =
+    { name ? "aws-lambda-with-nix"
+    , system
+    , pkgs
+    , pythonSrc
+    , pythonEnv ? pkgs.python3.withPackages (ps: [ ps.awslambdaric ])
+    }:
+    let
+      appSource = pkgs.runCommand "buildApp" { inherit pythonSrc; } ''
+        mkdir -p $out
+        cp $pythonSrc $out/$(basename $pythonSrc)
+      '';
+
+      handler = getLambdaHandler pythonSrc;
+
+      awsLambdaRie = pkgs.writeShellScript "aws-lambda-rie" ''
+        ${pkgs.aws-lambda-rie}/bin/aws-lambda-rie \
+          --runtime-interface-emulator-address 0.0.0.0:''${LAMBDA_PORT:-9001} \
+          ${pythonEnv}/bin/python -m awslambdaric ${handler}
+      '';
+
+      devServer = pkgs.writeShellScript "devserver" ''
+        ls *.py flake.nix | ${pkgs.entr}/bin/entr -r ${awsLambdaRie}
+      '';
+
+    in
+    pkgs.dockerTools.buildLayeredImage
+      {
+        name = name;
+        config = {
+          EntryPoint = [ "${pythonEnv}/bin/python" "-m" "awslambdaric" ];
+
+          WorkingDir = "${appSource}";
+
+          Cmd = [ "app.handler" ];
+        };
+      } // {
+      inherit devServer appSource;
+    };
+
+}
