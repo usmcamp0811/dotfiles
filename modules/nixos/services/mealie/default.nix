463f509405def10b3b65ed3458783556554af066
Matt Camp <matt@aicampground.com>
2025-01-09 13:51:41 -0600

    ai model

--- modules/nixos/services/mealie/default.nix
+++ modules/nixos/services/mealie/default.nix
@@ -132,6 +132,7 @@ in {
                 OIDC_SIGNUP_ENABLED=true
                 OPENAI_BASE_URL=http://reckless:11434/v1
                 OPENAI_API_KEY=mysecretkey
+                OPENAI_MODEL=phi3.5:latest
                 OIDC_CONFIGURATION_URL={{ with secret "${cfg.vault-path}" }}{{ if eq "${cfg.kvVersion}" "v1" }}{{ .Data.OIDC_CONFIGURATION_URL }}{{ else }}{{ .Data.data.OIDC_CONFIGURATION_URL }}{{ end }}{{ end }}
                 OIDC_CLIENT_ID={{ with secret "${cfg.vault-path}" }}{{ if eq "${cfg.kvVersion}" "v1" }}{{ .Data.OIDC_CLIENT_ID }}{{ else }}{{ .Data.data.OIDC_CLIENT_ID }}{{ end }}{{ end }}
                 OIDC_CLIENT_SECRET={{ with secret "${cfg.vault-path}" }}{{ if eq "${cfg.kvVersion}" "v1" }}{{ .Data.OIDC_CLIENT_SECRET }}{{ else }}{{ .Data.data.OIDC_CLIENT_SECRET }}{{ end }}{{ end }}
