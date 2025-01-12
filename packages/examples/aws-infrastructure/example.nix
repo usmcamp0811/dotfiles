447c53f7d78673b8b8a0b41fb079aba6ee45b60b
Matt Camp <matt@aicampground.com>
2025-01-11 22:27:02 -0600

    wipwip

--- packages/examples/aws-infrastructure/example.nix
+++ packages/examples/aws-infrastructure/example.nix
@@ -21,15 +21,15 @@
     };
 
     lambda = {
-      # another-example-job = {
-      #   lambda-image = pkgs.campground.aws-lambda-image;
-      #   environment.variables = {
-      #     LATITUDE = "38.9072";
-      #     LONGITUDE = "-77.0369";
-      #     S3_BUCKET = "weather-data";
-      #     S3_KEY = "forecasts/washington_dc_forecast.json";
-      #   };
-      # };
+      another-example-job = {
+        lambda-image = pkgs.campground.aws-lambda-image;
+        environment.variables = {
+          LATITUDE = "38.9072";
+          LONGITUDE = "-77.0369";
+          S3_BUCKET = "weather-data";
+          S3_KEY = "forecasts/washington_dc_forecast.json";
+        };
+      };
       weather-job = {
         enable = true;
         variables = {
