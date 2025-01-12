c6b3c0ee96dbb2aaa14d73b2068d2c500425400c
Matt Camp <matt@aicampground.com>
2025-01-11 22:08:28 -0600

    doh

--- modules/terraform/aws/storage/ecr/default.nix
+++ modules/terraform/aws/storage/ecr/default.nix
@@ -1,6 +1,6 @@
 { config, lib, pkgs, ... }:
 with lib;
-with lib.cyclops;
+with lib.campground;
 with types;
 
 let
@@ -14,7 +14,8 @@ let
       force_delete = mkBoolOpt true "Delete images if destroyed";
     };
   };
-in {
+in
+{
   options.aws.storage.ecr = {
     enable = mkBoolOpt false "Enable AWS Elastic Container Registry";
     registeries = mkOpt (listOf registryOptions) [{ name = "default-ecr"; }]
@@ -22,34 +23,38 @@ in {
   };
 
   config = mkIf cfg.enable {
-    resource.aws_ecr_repository = mkMerge (map (registery: {
-      "${registery.name}" = {
-        inherit (registery) name image_tag_mutability;
-        image_scanning_configuration = {
-          scan_on_push = registery.scan_on_push;
+    resource.aws_ecr_repository = mkMerge (map
+      (registery: {
+        "${registery.name}" = {
+          inherit (registery) name image_tag_mutability;
+          image_scanning_configuration = {
+            scan_on_push = registery.scan_on_push;
+          };
+          force_delete = registery.force_delete;
         };
-        force_delete = registery.force_delete;
-      };
-    }) cfg.registeries);
+      })
+      cfg.registeries);
 
-    resource.aws_ecr_lifecycle_policy = mkMerge (map (registery: {
-      "${registery.name}" = {
-        repository = registery.name;
-        depends_on = [ "resource.aws_ecr_repository.${registery.name}" ];
-        policy = builtins.toJSON {
-          rules = [{
-            rulePriority = 1;
-            description = "Keep only 10 images";
-            selection = {
-              countType = "imageCountMoreThan";
-              countNumber = 10;
-              tagStatus = "tagged";
-              tagPrefixList = [ "prod" ];
-            };
-            action = { type = "expire"; };
-          }];
+    resource.aws_ecr_lifecycle_policy = mkMerge (map
+      (registery: {
+        "${registery.name}" = {
+          repository = registery.name;
+          depends_on = [ "resource.aws_ecr_repository.${registery.name}" ];
+          policy = builtins.toJSON {
+            rules = [{
+              rulePriority = 1;
+              description = "Keep only 10 images";
+              selection = {
+                countType = "imageCountMoreThan";
+                countNumber = 10;
+                tagStatus = "tagged";
+                tagPrefixList = [ "prod" ];
+              };
+              action = { type = "expire"; };
+            }];
+          };
         };
-      };
-    }) cfg.registeries);
+      })
+      cfg.registeries);
   };
 }
