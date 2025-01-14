d07e93388a5c8b148baa779ed42f469494bb6f1f
Matt Camp <matt@aicampground.com>
2025-01-13 19:44:19 -0600

    WIPip

--- modules/terraform/aws/lambda/default.nix
+++ modules/terraform/aws/lambda/default.nix
@@ -17,8 +17,7 @@ let
       lambdaImg = lambdaConfig.lambda-image;
     };
 
-in
-{
+in {
   options.aws.lambda = {
     enable = mkBoolOpt false "Enable AWS Lambda Jobs";
     jobs = mkOption {
@@ -30,7 +29,7 @@ in
             mkOpt str "campground_ecr" "The name of the registry to use";
           environment.variables =
             mkOpt (types.attrsOf types.str) { foo = "bar"; }
-              "Environment Variables for the Lambda Function";
+            "Environment Variables for the Lambda Function";
           depends_on =
             mkOpt (listOf str) [ ] "List of things the lambda job depends on";
           timeout = mkOpt int 60 "timeout for the lambda job";
@@ -46,29 +45,26 @@ in
     aws.storage.ecr.enable = true;
 
     aws.storage.ecr.registeries = mapAttrsToList
-      (name: lambdaConfig: { name = lambdaConfig.registry-name; })
-      cfg.jobs;
+      (name: lambdaConfig: { name = lambdaConfig.registry-name; }) cfg.jobs;
 
-    resource.null_resource = mapAttrs
-      (name: lambdaConfig: {
-        provisioner = {
-          local-exec = {
-            command = "${build-push-lambda-image lambdaConfig}/bin/build-push ${
+    resource.null_resource = mapAttrs (name: lambdaConfig: {
+      provisioner = {
+        local-exec = {
+          command = "${build-push-lambda-image lambdaConfig}/bin/build-push ${
               config.resource.aws_ecr_repository."${lambdaConfig.registry-name}"
               "repository_url"
             }";
-          };
         };
-        depends_on = [ "aws_ecr_repository.${lambdaConfig.registry-name}" ];
-        triggers = {
-          always_run = true;
-          registry_url =
-            config.resource.aws_ecr_repository."${lambdaConfig.registry-name}"
-              "repository_url";
-          package_hash = lambdaImageTag lambdaConfig;
-        };
-      })
-      cfg.jobs;
+      };
+      depends_on = [ "aws_ecr_repository.${lambdaConfig.registry-name}" ];
+      triggers = {
+        always_run = true;
+        registry_url =
+          config.resource.aws_ecr_repository."${lambdaConfig.registry-name}"
+          "repository_url";
+        package_hash = lambdaImageTag lambdaConfig;
+      };
+    }) cfg.jobs;
 
     data.aws_iam_policy_document.assume_role = {
       statement = {
@@ -83,29 +79,29 @@ in
       };
     };
 
+    # Updated IAM role resource with dynamic name
     resource.aws_iam_role.iam_for_lambda = {
-      name = "iam_for_lambda";
+      name = "iam_for_lambda-${config.environment.name}";
       assume_role_policy =
         config.data.aws_iam_policy_document.assume_role "json";
     };
 
-    resource.aws_lambda_function = mapAttrs
-      (name: lambdaConfig: {
-        package_type = "Image";
-        image_uri = "${
+    # Updated Lambda function resource with explicit dependency on IAM role
+    resource.aws_lambda_function = mapAttrs (name: lambdaConfig: {
+      package_type = "Image";
+      image_uri = "${
           config.resource.aws_ecr_repository."${lambdaConfig.registry-name}"
           "repository_url"
         }:${lambdaConfig.lambda-image.imageName}-latest";
-        function_name = name;
-        timeout = lambdaConfig.timeout;
-        memory_size = lambdaConfig.memory_size;
-        role = config.resource.aws_iam_role.iam_for_lambda "arn";
-        environment = { variables = lambdaConfig.environment.variables; };
-        depends_on = [
-          "resource.null_resource.${name}"
-          "resource.aws_iam_role.iam_for_lambda"
-        ] ++ lambdaConfig.depends_on;
-      })
-      cfg.jobs;
+      function_name = name;
+      timeout = lambdaConfig.timeout;
+      memory_size = lambdaConfig.memory_size;
+      role = config.resource.aws_iam_role.iam_for_lambda "arn";
+      environment = { variables = lambdaConfig.environment.variables; };
+      depends_on = [
+        "resource.null_resource.${name}"
+        "resource.aws_iam_role.iam_for_lambda"
+      ] ++ lambdaConfig.depends_on;
+    }) cfg.jobs;
   };
 }
