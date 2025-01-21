4ec59c6c05237ca1e423630b5b7041867ab3d707
Matt Camp <matt@aicampground.com>
2025-01-20 21:12:23 -0600

    add oidc overlay for matomo

--- overlays/campground/default.nix
+++ overlays/campground/default.nix
@@ -1,5 +1,22 @@
 { campground-nvim, old-nixpkgs, ... }:
 final: prev: {
   campground-nvim = campground-nvim.packages.${prev.system}.nvim;
+
   neovide = old-nixpkgs.legacyPackages.${prev.system}.neovide;
+
+  matomo_5 = prev.matomo_5.overrideAttrs (old: rec {
+    # Fetch the plugin using fetchFromGitHub
+    loginOIDCPlugin = prev.fetchFromGitHub {
+      owner = "dominik-th";
+      repo = "matomo-plugin-LoginOIDC";
+      rev = "5.0.0";
+      sha256 = "sha256-L1ET2EoO6lm648Xf6UcpT1NR5DU4yBhlzaQyrECFjzQ=";
+    };
+
+    # Add the plugin to the installPhase
+    installPhase = old.installPhase + ''
+      echo "Including LoginOIDC plugin in Matomo package..."
+      cp -r ${loginOIDCPlugin} $out/share/plugins/LoginOIDC
+    '';
+  });
 }
