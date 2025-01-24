c4ed9639a78b449a7e51b92263cbbd0cc8a88614
Matt Camp <matt@aicampground.com>
2025-01-23 22:01:39 -0600

    it runs

--- packages/applications/zotifarr/default.nix
+++ packages/applications/zotifarr/default.nix
@@ -1,30 +1,35 @@
 { pkgs, ... }:
-
-pkgs.stdenv.mkDerivation {
-  pname = "zotifarr";
-  version = "1.0.0"; # Replace with the actual version if known
-
+let
+  python-env = pkgs.python311.withPackages (ps: [
+    ps.APScheduler
+    ps.blinker
+    ps.click
+    ps.flask
+    ps.itsdangerous
+    ps.jinja2
+    ps.markupsafe
+    ps.portalocker
+    ps.tenacity
+    ps.tzlocal
+    ps.werkzeug
+  ]);
   src = pkgs.fetchFromGitHub {
     owner = "Xoconoch";
     repo = "zotifarr";
     rev = "main";
     sha256 = "sha256-aVAnvcolmKw2DMGu5ptgQiwDSFUy6JtbPPrV7FzDCW8=";
   };
+  run_app = pkgs.writeShellScriptBin "zotifarr" ''
+    ${python-env}/bin/python ${src}/app.py
+  '';
+in
+pkgs.stdenv.mkDerivation {
+  pname = "zotifarr";
+  version = "0.1.0";
+  inherit src;
 
   buildInputs = [
-    (pkgs.python311.withPackages (ps: [
-      ps.APScheduler
-      ps.blinker
-      ps.click
-      ps.flask
-      ps.itsdangerous
-      ps.jinja2
-      ps.markupsafe
-      ps.portalocker
-      ps.tenacity
-      ps.tzlocal
-      ps.werkzeug
-    ]))
+    python-env
     pkgs.iptables
     pkgs.nano
     pkgs.curl
@@ -34,12 +39,15 @@ pkgs.stdenv.mkDerivation {
 
   installPhase = ''
     mkdir -p $out/app
-    cp -r * $out/app
+    cp -r ${src}/* $out/app
+    mkdir -p $out/bin
+    cp ${run_app}/bin/zotifarr $out/bin/zotifarr
   '';
 
   meta = {
-    description = "Python Flask application with utilities.";
+    description =
+      "Rip from spotify directly to your music library regardless of your account.";
     license = pkgs.lib.licenses.mit;
-    maintainers = [ "your-github-username" ];
+    maintainers = [ "Matt" ];
   };
 }
