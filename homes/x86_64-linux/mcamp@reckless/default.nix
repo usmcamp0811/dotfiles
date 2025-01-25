f61a347bf3b938b5c5768a965916de1025f9e685
Matt Camp <matt@aicampground.com>
2025-01-25 14:37:40 -0600

    wip

--- homes/x86_64-linux/mcamp@reckless/default.nix
+++ homes/x86_64-linux/mcamp@reckless/default.nix
@@ -16,60 +16,6 @@ with lib.campground; {
     };
     services.protonmail-bridge = enabled;
     apps.thunderbird = enabled;
-    # desktop = {
-    #   addons = {
-    #     waynergy = enabled;
-    #     rofi = enabled;
-    #     swaynotificationcenter = enabled;
-    #     networkmanagerapplet = enabled;
-    #     swayidle = enabled;
-    #     swaylock = enabled;
-    #     input-leap = enabled;
-    #     qt = enabled;
-    #     kitty = enabled;
-    #     waybar = {
-    #       enable = true;
-    #       display = "HDMI-A-3";
-    #     };
-    #     hyprpaper = {
-    #       enable = true;
-    #       monitors = [
-    #         {
-    #           name = "HDMI-A-1";
-    #           wallpaper =
-    #             "${pkgs.campground.wallpapers}/share/wallpapers/pittsburgh-wallpaper.jpeg";
-    #         }
-    #         {
-    #           name = "HDMI-A-2";
-    #           wallpaper =
-    #             "${pkgs.campground.wallpapers}/share/wallpapers/pittsburgh-wallpaper.jpeg";
-    #         }
-    #         {
-    #           name = "HDMI-A-3";
-    #           wallpaper =
-    #             "${pkgs.campground.wallpapers}/share/wallpapers/pittsburgh-wallpaper.jpeg";
-    #         }
-    #       ];
-    #
-    #       wallpapers = [
-    #         "${pkgs.campground.wallpapers}/share/wallpapers/hsv-saturnV.jpg"
-    #         "${pkgs.campground.wallpapers}/share/wallpapers/pittsburgh-wallpaper.jpeg"
-    #       ];
-    #     };
-    #     gbar = enabled;
-    #     wofi = enabled;
-    #   };
-    #   wallpapers = enabled;
-    #   qtile = {
-    #     enable = true;
-    #     wallpaper = "hsv-saturnV.jpg";
-    #   };
-    #   hyprland = {
-    #     enable = true;
-    #     startup = [ "${getExe pkgs.ckb-next} -b" ];
-    #   };
-    # };
-
   };
 
   home.stateVersion = "23.05";
---
+++ modules/home/archetypes/headless/default.nix
@@ -0,0 +1,33 @@
+{ lib, config, pkgs, ... }:
+with lib;
+with lib.campground;
+let cfg = config.campground.archetypes.headless;
+in {
+  options.campground.archetypes.headless = with types; {
+    enable = mkEnableOption "desktop home enviornment";
+  };
+
+  config = mkIf cfg.enable {
+    campground = {
+      cli = {
+        zsh = enabled;
+        bash = enabled;
+        env = enabled;
+        home-manager = enabled;
+        k9s = enabled;
+        broot = enabled;
+        ranger = enabled;
+        neovim = enabled;
+      };
+      services = {
+        openssh = enabled;
+        syncthing = enabled;
+      };
+      tools = {
+        git = enabled;
+        direnv = enabled;
+        vault = enabled;
+      };
+    };
+  };
+}
