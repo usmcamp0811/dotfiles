e2ee76e05810d89b5c59ebf6fbec13c938b69f37
Matt Camp <matt@aicampground.com>
2025-01-25 14:39:07 -0600

    wip

--- homes/x86_64-linux/mcamp@webb/default.nix
+++ homes/x86_64-linux/mcamp@webb/default.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, config, osConfig ? { }, format ? "unknown", ... }:
+{ lib, ... }:
 with lib.campground; {
   campground = {
     user = {
@@ -7,40 +7,7 @@ with lib.campground; {
       fullName = "Matt Camp";
       email = "matt@aicampground.com";
     };
-
-    cli = {
-      zsh = enabled;
-      bash = enabled;
-      env = enabled;
-      home-manager = enabled;
-      k9s = enabled;
-      broot = enabled;
-      ranger = enabled;
-      neovim = enabled;
-    };
-    services = {
-      # picom = enabled;
-      openssh = enabled;
-      syncthing = enabled;
-    };
-
-    # apps = {
-    #   firefox = enabled;
-    #   brave = enabled;
-    #   libreoffice = enabled;
-    #   alacritty = enabled;
-    #   kitty = enabled;
-    #   rofi = enabled;
-    #   mpv = enabled;
-    #   #TODO: Add Qutebrowser
-    # };
-    tools = {
-      git = enabled;
-      direnv = enabled;
-      # julia = enabled;
-      # python = enabled;
-      vault = enabled;
-    };
+    archetypes.headless = enabled;
   };
 
   home.stateVersion = "23.05";
