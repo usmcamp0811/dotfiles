501fd200261a42ae4cdc2c434b532519a8afbd30
Matt Camp <matt@aicampground.com>
2025-02-01 15:14:02 -0600

    Revert "nb down wg up"
    This reverts commit affb42cf26e503a382299cecf6e1ad787e506f62.

--- systems/x86_64-linux/webb/default.nix
+++ systems/x86_64-linux/webb/default.nix
@@ -68,7 +68,7 @@ in
     services = {
       # onlyoffice = { enable = true; };
       lemmy = enabled;
-      # netbird.server = enabled;
+      netbird.server = enabled;
       vault = {
         enable = true;
         ui = true;
@@ -291,33 +291,33 @@ in
           "host    all        all       ::/0 reject" # Reject all other IPv6 connections
         ];
       };
-      wireguard = {
-        enable = true;
-        port = 1149;
-        interface-name = "campnet";
-        ips = [ "10.100.0.1/24" ];
-        fetchWireguardKeys = true;
-        peers = [
-          {
-            # daly
-            publicKey = "qUnW//Iq8eq2D5dKMfsIa0zCewUSOSVaLtpO7AxWXAE=";
-            allowedIPs = [ "10.100.0.10/32" ];
-          }
-          {
-            # butler
-            publicKey = "Thdtm9iUmcZFgFMiJUm0T0EaBe/gvfmcBHrSi5Gvfm8=";
-            presharedKeyFile = "/var/lib/wireguard/wg0-preshared-key";
-            allowedIPs = [ "10.100.0.2/32" ];
-          }
-          {
-            # phone
-            publicKey = "cq5+lO9tjEom1pUuXtb9rfAfSN6DZxDZkKWdVQ6Cokw=";
-            presharedKeyFile = "/var/lib/wireguard/wg0-preshared-key";
-            allowedIPs = [ "10.100.0.3/32" ];
-          }
-
-        ];
-      };
+      # wireguard = {
+      #   enable = true;
+      #   port = 1149;
+      #   interface-name = "campnet";
+      #   ips = [ "10.100.0.1/24" ];
+      #   fetchWireguardKeys = true;
+      #   # peers = [
+      #   #   {
+      #   #     # daly
+      #   #     publicKey = "qUnW//Iq8eq2D5dKMfsIa0zCewUSOSVaLtpO7AxWXAE=";
+      #   #     allowedIPs = [ "10.100.0.10/32" ];
+      #   #   }
+      #   #   {
+      #   #     # butler
+      #   #     publicKey = "Thdtm9iUmcZFgFMiJUm0T0EaBe/gvfmcBHrSi5Gvfm8=";
+      #   #     presharedKeyFile = "/var/lib/wireguard/wg0-preshared-key";
+      #   #     allowedIPs = [ "10.100.0.2/32" ];
+      #   #   }
+      #   #   {
+      #   #     # phone
+      #   #     publicKey = "cq5+lO9tjEom1pUuXtb9rfAfSN6DZxDZkKWdVQ6Cokw=";
+      #   #     presharedKeyFile = "/var/lib/wireguard/wg0-preshared-key";
+      #   #     allowedIPs = [ "10.100.0.3/32" ];
+      #   #   }
+      #
+      #   # ];
+      # };
       matomo = enabled;
       zfs-key-server = {
         enable = true;
