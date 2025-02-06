f7ac86922a087d8f06f0e3812f74bee24c6257dc
Matt Camp <matt@aicampground.com>
2025-02-05 22:16:57 -0600

    bugs

--- modules/nixos/stig/default.nix
+++ modules/nixos/stig/default.nix
@@ -4,11 +4,11 @@ with lib.campground;
 let cfg = config.campground.stig;
 in {
   options.campground.stig = with types; {
-    enale = mkBoolOpt false "Stig the machine";
+    enable = mkBoolOpt false "Stig the machine";
     srg = mkOpt (listOf str) [ ] "SRGs that are enabled";
     cci = mkOpt (listOf str) [ ] "CCIs that are enabled";
   };
-  config = {
+  config = mkIf cfg.enable {
     assertions = [
       {
         assertion = !cfg.firewall.enable -> cfg.firewall.justification != null;
@@ -27,22 +27,25 @@ in {
           "You must provide a justification if audit enforcement is disabled.";
       }
       {
-        assertion = !cfg.login_attempts.enable -> cfg.justification != null;
+        assertion = !cfg.login_attempts.enable
+          -> cfg.login_attempts.justification != null;
         message =
           "You must provide a justification if login attempt restrictions are disabled.";
       }
       {
-        assertion = !cfg.banner.enable -> cfg.justification != null;
+        assertion = !cfg.banner.enable -> cfg.banner.justification != null;
         message =
           "You must provide a justification if the graphical login banner is disabled.";
       }
       {
-        assertion = !cfg.session_limit.enable -> cfg.justification != null;
+        assertion = !cfg.session_limit.enable -> cfg.session_limit.justification
+          != null;
         message =
           "You must provide a justification if session limit enforcement is disabled.";
       }
       {
-        assertion = !cfg.session_lock.enable -> cfg.justification != null;
+        assertion = !cfg.session_lock.enable -> cfg.session_lock.justification
+          != null;
         message =
           "You must provide a justification if session lock enforcement is disabled.";
       }
