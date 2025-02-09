aa249d669e855dcb0df6a80e87e811d199fa11f1
Matt Camp <matt@aicampground.com>
2025-02-08 22:22:26 -0600

    seems to at least not piss nix off

--- modules/nixos/stig/fips_crypto_compliance/default.nix
+++ modules/nixos/stig/fips_crypto_compliance/default.nix
@@ -7,15 +7,36 @@ mkStigModule {
   name = "fips_crypto_compliance";
   srgList = [ "SRG-OS-000478-GPOS-00223" ];
   stigConfig = {
+    # Ensure kernel boots in FIPS mode
     boot.kernelParams = [ "fips=1" ];
-    security.pki.enable = true;
-    security.pki.certificates = [ "/etc/ssl/certs/ca-certificates.crt" ];
-    environment.etc."openssl.cnf".text = ''
-      [ openssl_conf ]
-      alg_section = evp_properties
 
-      [ evp_properties ]
-      fips_mode = yes
+    # Enforce FIPS mode in OpenSSL
+    environment.etc."openssl.cnf".text = ''
+      openssl_conf = openssl_init
+
+      [openssl_init]
+      providers = provider_sect
+
+      [provider_sect]
+      default = default_sect
+      fips = fips_sect
+
+      [default_sect]
+      activate = 1
+
+      [fips_sect]
+      activate = 1
     '';
+
+    # Enforce FIPS for system-wide crypto
+    security.apparmor.enable = true;
+    security.apparmor.profiles."system-wide-fips" = ''
+      # Deny non-FIPS crypto
+      deny /proc/sys/crypto/fips_enabled rw,
+      deny /sys/module/fips_mode rw,
+    '';
+
+    # Ensure `libgcrypt` operates in FIPS mode
+    environment.etc."gcrypt/fips_enabled".text = "1";
   };
 }
