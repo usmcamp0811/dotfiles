d0d22a0a9fc982aaba44349206295532d512c282
Matt Camp <matt@aicampground.com>
2025-02-11 21:44:42 -0600

    more stig fixing

--- modules/nixos/stig/audit_storage_notification/default.nix
+++ modules/nixos/stig/audit_storage_notification/default.nix
@@ -8,13 +8,11 @@ mkStigModule {
   srgList = [ "SRG-OS-000046-GPOS-00022" "SRG-OS-000343-GPOS-00134" ];
   cciList = [ "CCI-000139" "CCI-001855" ];
   stigConfig = {
-    environment.etc."audit/auditd.conf".text = [
-      ''
-        space_left_action = syslog
-        admin_space_left_action = syslog
-        action_mail_acct = root@localhost
-      ''
-    ];
+    environment.etc."audit/auditd.conf".text = ''
+      space_left_action = syslog
+      admin_space_left_action = syslog
+      action_mail_acct = root@localhost
+    '';
   };
 
 }
