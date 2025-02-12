8da842e823c49e61b260bc53421885fdc9a6d4e0
Matt Camp <matt@aicampground.com>
2025-02-11 21:40:54 -0600

    more stig fixing

--- modules/nixos/stig/audit_storage_monitoring/default.nix
+++ modules/nixos/stig/audit_storage_monitoring/default.nix
@@ -11,18 +11,16 @@ mkStigModule {
     "SRG-OS-000047-GPOS-00023"
   ];
   stigConfig = {
-    environment.etc."audit/auditd.conf".text = [
-      ''
-        # Notify system administrators when audit storage usage reaches thresholds
-        space_left = 25%                  # Action at 75% storage usage
-        space_left_action = syslog         # Notify SA/ISSO at 75%
+    environment.etc."audit/auditd.conf".text = ''
+      # Notify system administrators when audit storage usage reaches thresholds
+      space_left = 25%                  # Action at 75% storage usage
+      space_left_action = syslog         # Notify SA/ISSO at 75%
 
-        admin_space_left = 10%             # Action at 90% storage usage
-        admin_space_left_action = syslog   # Notify SA/ISSO at 90%
+      admin_space_left = 10%             # Action at 90% storage usage
+      admin_space_left_action = syslog   # Notify SA/ISSO at 90%
 
-        disk_full_action = HALT            # Stop system if storage is full
-        disk_error_action = HALT           # Stop system if audit process fails
-      ''
-    ];
+      disk_full_action = HALT            # Stop system if storage is full
+      disk_error_action = HALT           # Stop system if audit process fails
+    '';
   };
 }
