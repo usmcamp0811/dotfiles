b198d82fbc5d65462d0ef309dd68051c4dd3a351
Matt Camp <matt@aicampground.com>
2025-02-11 21:36:28 -0600

    this builds but not sure i have the srg list correct

--- modules/nixos/stig/fips_crypto_compliance/default.nix
+++ modules/nixos/stig/fips_crypto_compliance/default.nix
@@ -5,12 +5,23 @@ with lib.campground;
 mkStigModule {
   inherit config;
   name = "fips_crypto_compliance";
-  srgList = [ "SRG-OS-000478-GPOS-00223" ];
+  srgList = [
+    "SRG-OS-000476-GPOS-00221"
+    "SRG-OS-000042-GPOS-00020"
+    "SRG-OS-000274-GPOS-00104"
+    "SRG-OS-000275-GPOS-00105"
+    "SRG-OS-000276-GPOS-00106"
+    "SRG-OS-000277-GPOS-00107"
+    "SRG-OS-000477-GPOS-00222"
+    "SRG-OS-000304-GPOS-00121"
+    "SRG-OS-000478-GPOS-00223"
+  ];
+  cciList = [ "CCI-000172" "CCI-000135" "CCI-000015" ];
   stigConfig = {
-    # Ensure kernel boots in FIPS mode
+    ## 1. Enable Kernel FIPS Mode
     boot.kernelParams = [ "fips=1" ];
 
-    # Enforce FIPS mode in OpenSSL
+    ## 2. Ensure OpenSSL Uses FIPS Mode
     environment.etc."openssl.cnf".text = ''
       openssl_conf = openssl_init
 
@@ -28,15 +39,27 @@ mkStigModule {
       activate = 1
     '';
 
-    # Enforce FIPS for system-wide crypto
+    ## 3. Enforce FIPS via AppArmor (Fixed)
     security.apparmor.enable = true;
-    security.apparmor.policies."system-wide-fips" = ''
-      # Deny non-FIPS crypto
-      deny /proc/sys/crypto/fips_enabled rw,
-      deny /sys/module/fips_mode rw,
-    '';
+    security.apparmor.policies."fips-restrictions" = {
+      profile = ''
+        # Deny non-FIPS crypto
+        deny /proc/sys/crypto/fips_enabled rw,
+        deny /sys/module/fips_mode rw,
+      '';
+    };
 
-    # Ensure `libgcrypt` operates in FIPS mode
+    ## 4. Ensure `libgcrypt` Operates in FIPS Mode
     environment.etc."gcrypt/fips_enabled".text = "1";
+
+    ## 5. Audit Log Configuration
+    security.audit.rules = [
+      "-w /etc/sudoers -p wa -k identity"
+      "-w /etc/passwd -p wa -k identity"
+      "-w /etc/shadow -p wa -k identity"
+      "-w /etc/gshadow -p wa -k identity"
+      "-w /etc/group -p wa -k identity"
+      "-w /etc/security/opasswd -p wa -k identity"
+    ];
   };
 }
