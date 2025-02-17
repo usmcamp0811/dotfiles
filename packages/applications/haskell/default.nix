8a7abe5cc16efe935ecc3f764c76f2de19f77835
Matt Camp <matt@aicampground.com>
2025-02-16 18:52:34 -0600

    wip

--- packages/applications/haskell/default.nix
+++ packages/applications/haskell/default.nix
@@ -4,10 +4,10 @@ with lib.campground;
 let
   inherit (lib) mapAttrsToList concatStringsSep;
   inherit (lib.campground) override-meta;
-
-in
-createHaskellConsole "haskell-jupyter" "jupyter console" {
-  inherit pkgs;
   haskellEnv = pkgs.haskellPackages.ghcWithPackages (p: with p; [ ihaskell ]);
-  kernelName = "haskell";
-}
+  jupyterConsole = createHaskellConsole "haskell-jupyter" "jupyter console" {
+    inherit pkgs haskellEnv;
+    kernelName = "haskell";
+  };
+in
+haskellEnv // { jupyter = juptyterConsole; }
