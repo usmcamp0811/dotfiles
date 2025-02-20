66999f4e3da675b24cb6236ef6da4a84c46a63d5
Matt Camp <matt@aicampground.com>
2025-02-19 18:12:00 -0600

    rust tools

---
+++ modules/home/tools/rust/default.nix
@@ -0,0 +1,20 @@
+{ config, lib, pkgs, ... }:
+with lib;
+with lib.campground;
+let cfg = config.campground.tools.rust;
+in {
+  options.campground.tools.rust = with types; {
+    enable = mkBoolOpt false "Whether or not to enable common Rust.";
+  };
+
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      cargo
+      evcxr
+      rust-analyzer
+      rustfmt
+      clippy
+      rustc
+    ];
+  };
+}
