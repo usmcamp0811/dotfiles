a2b68d8c1f4548477c66b5aae01b54a074d548c9
Matt Camp <matt@aicampground.com>
2025-02-22 10:48:49 -0600

    sweet

--- packages/learning-rust/todo-list/src/main.rs
+++ packages/learning-rust/todo-list/src/main.rs
@@ -2,31 +2,42 @@ mod task;
 mod task_manager;
 use crate::task_manager::TaskManager;
 use clap::Parser;
+use dirs::home_dir;
 use serde::{Deserialize, Serialize};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    #[arg(long, short, help = "Path to the input file")]
+    #[arg(
+        long,
+        short,
+        help = "Path to the input file",
+        default_value = "~/.todo.json"
+    )]
     filename: String,
+
+    #[arg(long, short, help = "Text for the Task Title", required = true)]
+    title: String,
+}
+
+fn expand_tilde(path: String) -> String {
+    if path.starts_with("~") {
+        if let Some(home) = home_dir() {
+            return path.replacen("~", home.to_str().unwrap(), 1);
+        }
+    }
+    path
 }
 
 fn main() {
     let args = Args::parse();
-    let filename = args.filename;
+    let filename = expand_tilde(args.filename);
+    let title = args.title;
     let mut manager = TaskManager::load_from_file(&filename).unwrap_or_else(|_| TaskManager::new());
-    let id1 = manager.add_task("Learn Rust".to_string());
-    let id2 = manager.add_task("Build a todo app".to_string());
-    let id3 = manager.add_task("Make an interface for the app".to_string());
-    let id4 = manager.add_task("Make a GUI for the app".to_string());
+    manager.add_task(title);
 
     println!("\nListing Tasks...");
     manager.list_tasks();
-
-    println!("\nMarking first task as complete...");
-    manager.complete_task(&id1);
-
-    manager.remove_task(&id3);
 
     if let Err(e) = manager.save_to_file(&filename) {
         eprintln!("Error saving tasks: {}", e);
