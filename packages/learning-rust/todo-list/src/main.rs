1851721e9e6b93d2c648e482373983fd3d5a8f20
Matt Camp <matt@aicampground.com>
2025-02-22 10:31:57 -0600

    cool now my rust app can do command line args

--- packages/learning-rust/todo-list/src/main.rs
+++ packages/learning-rust/todo-list/src/main.rs
@@ -1,11 +1,20 @@
 mod task;
 mod task_manager;
 use crate::task_manager::TaskManager;
+use clap::Parser;
 use serde::{Deserialize, Serialize};
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(long, short, help = "Path to the input file")]
+    filename: String,
+}
+
 fn main() {
-    let filename = "todo.json";
-    let mut manager = TaskManager::load_from_file(filename).unwrap_or_else(|_| TaskManager::new());
+    let args = Args::parse();
+    let filename = args.filename;
+    let mut manager = TaskManager::load_from_file(&filename).unwrap_or_else(|_| TaskManager::new());
     let id1 = manager.add_task("Learn Rust".to_string());
     let id2 = manager.add_task("Build a todo app".to_string());
     let id3 = manager.add_task("Make an interface for the app".to_string());
@@ -19,7 +28,7 @@ fn main() {
 
     manager.remove_task(&id3);
 
-    if let Err(e) = manager.save_to_file("todo.json") {
+    if let Err(e) = manager.save_to_file(&filename) {
         eprintln!("Error saving tasks: {}", e);
     }
 }
