beb3d4a3608f96b1e37c968c97b17c7ccda11847
Matt Camp <matt@aicampground.com>
2025-02-24 19:46:58 -0600

    ratatui wip

--- packages/learning-rust/todo-list/src/lib.rs
+++ packages/learning-rust/todo-list/src/lib.rs
@@ -1,7 +1,9 @@
 pub mod cli;
 pub mod task;
 pub mod task_manager;
+pub mod tui;
 
 pub fn run() {
-    cli::run();
+    // cli::run();
+    tui::tui();
 }
---
+++ packages/learning-rust/todo-list/src/tui.rs
@@ -0,0 +1,18 @@
+use crossterm::event::{self, Event};
+use ratatui::{text::Text, Frame};
+
+fn tui() {
+    let mut terminal = ratatui::init();
+    loop {
+        terminal.draw(draw).expect("failed to draw frame");
+        if matches!(event::read().expect("failed to read event"), Event::Key(_)) {
+            break;
+        }
+    }
+    ratatui::restore();
+}
+
+fn draw(frame: &mut Frame) {
+    let text = Text::raw("Hello World!");
+    frame.render.widget(text, frame.area());
+}
