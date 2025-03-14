0d17a67943faf5edefcb83a9d3ba578509e5efa1
Matt Camp <matt@aicampground.com>
2025-03-13 22:03:49 -0500

    wip

--- modules/nixos/services/k0s/default.nix
+++ modules/nixos/services/k0s/default.nix
@@ -29,6 +29,21 @@ let
             storage:
               create_default_storage_class: true
               type: openebs_local_storage
+            helm:
+              repositories:
+              - name: stable
+                url: https://charts.helm.sh/stable
+              - name: prometheus-community
+                url: https://prometheus-community.github.io/helm-charts
+              charts:
+              - name: prometheus-stack
+                chartname: prometheus-community/prometheus
+                version: "11.16.8"
+                values: |
+                  storageSpec:
+                    emptyDir:
+                      medium: Memory
+        namespace: default
           installConfig:
             users:
               etcdUser: ${cfg.users.etcdUser}
