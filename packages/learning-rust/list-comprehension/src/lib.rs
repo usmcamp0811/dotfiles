2b582ad5a07331f78c87f7cab4504878c832fba8
Matt Camp <matt@aicampground.com>
2025-03-15 16:07:03 -0500

    some additional tests

--- packages/learning-rust/list-comprehension/src/lib.rs
+++ packages/learning-rust/list-comprehension/src/lib.rs
@@ -8,4 +8,28 @@ mod tests {
         let result = comp![x for x in [1,2,3]].collect::<Vec<_>>();
         assert_eq!(result.iter().map(|&x| x).collect::<Vec<_>>(), vec![1, 2, 3]);
     }
+
+    #[test]
+    fn if_even_test() {
+        let ifresult = comp![x for x in [1,2,3,4] if x % 2 == 0].collect::<Vec<_>>();
+        assert_eq!(ifresult.iter().map(|&x| x).collect::<Vec<_>>(), vec![2, 4]);
+    }
+
+    #[test]
+    fn if_odd_test() {
+        let result = comp![x for x in 1..10 if x % 2 != 0].collect::<Vec<_>>();
+        assert_eq!(
+            result.iter().map(|&x| x).collect::<Vec<_>>(),
+            vec![1, 3, 5, 7, 9]
+        );
+    }
+
+    #[test]
+    fn if_if_test() {
+        let result = comp![x for x in 1..100 if x % 2 != 0 if x < 10].collect::<Vec<_>>();
+        assert_eq!(
+            result.iter().map(|&x| x).collect::<Vec<_>>(),
+            vec![1, 3, 5, 7, 9]
+        );
+    }
 }
