7904ef2ccfe6dd6899841483c1ec985d7346a8c1
Matt Camp <matt@aicampground.com>
2025-03-16 15:11:23 -0500

    maybe change this later so its not always admin but

--- modules/nixos/apps/k9s/default.nix
+++ modules/nixos/apps/k9s/default.nix
@@ -1,24 +1,17 @@
-{
-  options,
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+{ options, config, lib, pkgs, ... }:
 with lib;
-with lib.campground; let
-  cfg = config.campground.apps.k9s;
+with lib.campground;
+let cfg = config.campground.apps.k9s;
 in {
   options.campground.apps.k9s = with types; {
     enable = mkBoolOpt false "Whether or not to enable K9s.";
     role-id =
       mkOpt str config.campground.services.vault-agent.settings.vault.role-id
-      "Absolute path to the Vault role-id";
+        "Absolute path to the Vault role-id";
     secret-id =
       mkOpt str config.campground.services.vault-agent.settings.vault.secret-id
-      "Absolute path to the Vault secret-id";
-    vault-path =
-      mkOpt str "secret/campground/k8s"
+        "Absolute path to the Vault secret-id";
+    vault-path = mkOpt str "secret/campground/k0s"
       "The Vault path to the KV containing the Kubeconfig.";
     vault-address = mkOption {
       type = str;
@@ -26,14 +19,14 @@ in {
       description = "The address of your Vault";
     };
     kvVersion = mkOption {
-      type = enum ["v1" "v2"];
+      type = enum [ "v1" "v2" ];
       default = "v2";
       description = "KV store version";
     };
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [k9s];
+    environment.systemPackages = with pkgs; [ k9s ];
 
     # TODO: Maybe move to a new group-secrets service that gets group level things from Vault
     systemd.services.copyKUBECONFIG = {
@@ -41,26 +34,25 @@ in {
       serviceConfig = {
         Type = "oneshot";
         User = "root";
-        ExecStart = "${pkgs.bash}/bin/bash -c 'mkdir -p /etc/k8s/ && cp /tmp/detsys-vault/kubeconfig /etc/k8s/config && chgrp k8s /etc/k8s/config'";
+        ExecStart =
+          "${pkgs.bash}/bin/bash -c 'mkdir -p /etc/k8s/ && cp /tmp/detsys-vault/kubeconfig /etc/k8s/config && chgrp k8s /etc/k8s/config'";
       };
-      wantedBy = ["multi-user.target"];
-      after = ["nscd.service"];
+      wantedBy = [ "multi-user.target" ];
+      after = [ "nscd.service" ];
     };
 
     campground.services.vault-agent.services.copyKUBECONFIG = {
       settings = {
         vault.address = cfg.vault-address;
         auto_auth = {
-          method = [
-            {
-              type = "approle";
-              config = {
-                role_id_file_path = cfg.role-id;
-                secret_id_file_path = cfg.secret-id;
-                remove_secret_id_file_after_reading = false;
-              };
-            }
-          ];
+          method = [{
+            type = "approle";
+            config = {
+              role_id_file_path = cfg.role-id;
+              secret_id_file_path = cfg.secret-id;
+              remove_secret_id_file_after_reading = false;
+            };
+          }];
         };
       };
       secrets = {
@@ -70,9 +62,9 @@ in {
               text = ''
                 {{ with secret "${cfg.vault-path}" }}
                 {{ if eq "${cfg.kvVersion}" "v1" }}
-                {{ .Data.config }}
+                {{ .Data.admin_kubeconfig }}
                 {{ else }}
-                {{ .Data.data.config }}
+                {{ .Data.data.admin_kubeconfig }}
                 {{ end }}
                 {{ end }}
               '';
