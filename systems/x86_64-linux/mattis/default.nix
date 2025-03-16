5c79122b0c709cea48cc0b44292f300d52d08605
Matt Camp <matt@aicampground.com>
2025-03-16 15:28:49 -0500

    worker

--- systems/x86_64-linux/mattis/default.nix
+++ systems/x86_64-linux/mattis/default.nix
@@ -29,6 +29,11 @@ in
         enable = true;
         interface = "enp0s20f0u1";
       };
+      kubernetes = {
+        enable = true;
+        role = "worker";
+        interface = "enp0s20f0u1";
+      };
     };
 
     archetypes = {
