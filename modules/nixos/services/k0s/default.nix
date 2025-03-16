03eedee77cf39706bb7c06acb6457e1348796c4a
Matt Camp <matt@aicampground.com>
2025-03-16 14:50:16 -0500

    wip

--- modules/nixos/services/k0s/default.nix
+++ modules/nixos/services/k0s/default.nix
@@ -57,7 +57,7 @@ let
               peerRouterIPs: ""
               metricsPort: 9090  
             podCIDR: 10.244.0.0/16
-            provider: flannel
+            provider: calico
             serviceCIDR: 10.96.0.0/12
           podSecurityPolicy:
             defaultPolicy: 00-k0s-privileged
