01591943e3c8d6722796f1e6a2c48f821de128d7
Matt Camp <matt@aicampground.com>
2025-03-17 21:17:26 -0500

    wip

--- modules/nixos/services/k0s/default.nix
+++ modules/nixos/services/k0s/default.nix
@@ -59,6 +59,8 @@ let
             defaultPolicy: 00-k0s-privileged
           storage:
             type: etcd
+            etcd:
+              peerAddress: ${cfg.apiAddress}
           telemetry:
             enabled: true
       '';
