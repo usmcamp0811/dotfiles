61f5b47c819a67c355aa9cd22809f5b413e1e49e
Matt Camp <matt@aicampground.com>
2025-03-23 13:43:41 -0500

    gonna try to make a db with rust

---
+++ packages/learning-rust/rust-db/default.nix
@@ -0,0 +1,15 @@
+{ lib, pkgs, ... }:
+let
+
+  rust-db = pkgs.rustPlatform.buildRustPackage {
+    pname = "rust-db";
+    version = "0.1.0";
+
+    src = ./.;
+
+    cargoLock = { lockFile = ./Cargo.lock; };
+    nativeBuildInputs = with pkgs; [ pkg-config ];
+    buildInputs = with pkgs; [ openssl ];
+  };
+in
+rust-db
