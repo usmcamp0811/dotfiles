a9dbddd6f182790ae2c2757e840561267fca11db
Matt Camp <matt@aicampground.com>
2025-03-28 22:16:42 -0500

    example nix2container runs

--- overlays/docker/default.nix
+++ overlays/docker/default.nix
@@ -1,5 +1,5 @@
 { compose2nix, nix2container, ... }:
 final: prev: {
   compose2nix = compose2nix.packages.${prev.system}.default;
-  nix2containerPkgs = nix2container.packages.${preve.system};
+  nix2containerPkgs = nix2container.packages.${prev.system};
 }
--- packages/examples/nix2container-hello/default.nix
+++ packages/examples/nix2container-hello/default.nix
@@ -10,60 +10,58 @@
 with lib;
 with lib.campground;
 let
-  inherit (lib) mapAttrsToList concatStringsSep;
-  inherit (lib.campground) override-meta;
 
-  nginxConfContent = pkgs.writeText "nginx.conf" ''
-    daemon off;
+  nginxPort = "80";
 
-    events {
-        worker_connections 1024;
-    }
-
-    http {
-
-        server {
-            listen 80;
-
-            location / {
-                root /www/data;
-            }
-        }
-    }
-  '';
-
-  indexHTMLContent = pkgs.writeText "index.html" ''
+  nginxWebRoot = pkgs.writeTextDir "index.html" ''
     <!doctype html>
     <html>
-      <head>
-        <title>Hello nginx</title>
-        <meta charset="utf-8" />
-      </head>
-      <body>
-        <h1>
-          Hello World!
-        </h1>
-      </body>
+      <head><title>Hello nginx</title></head>
+      <body><h1>Hello Nix2Container!</h1></body>
     </html>
   '';
-  nginx-image = nix2container.buildImage {
-    name = "layered";
-    config = {
-      WorkingDir = "/www/data";
-      entrypoint = [ "${pkgs.nginx}/bin/nginx" ];
-    };
-    copyToRoot = pkgs.buildEnv {
-      name = "image-root";
-      pathsToLink = [ "/bin" ];
-      paths = [ pkgs.coreutils pkgs.nginx ];
-    };
-    runAsRoot = ''
-      mkdir -p www/data
-      mkdir -p etc/nginx/
-      mkdir -p var/log/nginx/
-      cat ${indexHTMLContent} > www/data/index.html
-      cat ${nginxConfContent} > etc/nginx/nginx.conf '';
-    maxLayers = 3;
-  };
+
+  nginxConf = pkgs.writeText "nginx.conf" ''
+    user nobody nobody;
+    daemon off;
+    error_log /dev/stdout info;
+    pid /dev/null;
+
+    events {}
+
+    http {
+      access_log /dev/stdout;
+      server {
+        listen ${nginxPort};
+        index index.html;
+        location / {
+          root ${nginxWebRoot};
+        }
+      }
+    }
+  '';
+
+  nginxVar = pkgs.runCommand "nginx-var" { } ''
+    mkdir -p $out/var/log/nginx
+    mkdir -p $out/var/cache/nginx
+  '';
 in
-nginx-image
+pkgs.nix2containerPkgs.nix2container.buildImage {
+  name = "nix2container-hello";
+  tag = "latest";
+  copyToRoot = pkgs.buildEnv {
+    name = "image-root";
+    paths = [
+      pkgs.dockerTools.fakeNss
+      pkgs.nginx
+      pkgs.coreutils
+      pkgs.stdenv.cc.cc
+      nginxVar
+    ];
+    pathsToLink = [ "/bin" "/lib64" "/etc" "/var" "/tmp" ];
+  };
+  config = {
+    Cmd = [ "${pkgs.nginx}/bin/nginx" "-c" nginxConf ];
+    ExposedPorts."${nginxPort}/tcp" = { };
+  };
+}
