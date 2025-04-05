84a927eaabed48aeec88b7b7b541ccd664c40f13
Matt Camp <matt@aicampground.com>
2025-04-05 16:57:14 -0500

    err

--- modules/home/cli/yazi/init.lua
+++ modules/home/cli/yazi/init.lua
@@ -42,10 +42,10 @@ require("bunny"):setup({
 		{ key = "v", path = "/var" },
 		{ key = "t", path = "/tmp" },
 		{ key = "c", path = "/config", desc = "Nix Config" },
-		{ key = { "N" }, path = "/nix/store", desc = "Nix store" },
+		{ key = "N", path = "/nix/store", desc = "Nix store" },
+		{ key = "n", path = "~/code/campground-nvim", desc = "Nvim Config" },
 		{ key = { "h", "h" }, path = "~", desc = "Home" },
 		{ key = { "h", "c" }, path = "~/code", desc = "Code" },
-		{ key = { "n" }, path = "~/code/campground-nvim", desc = "Nvim Config" },
 		{ key = { "h", "w" }, path = "~/work-code", desc = "Work Code" },
 		{ key = { "h", "d" }, path = "~/Documents", desc = "Documents" },
 		-- key and path attributes are required, desc is optional
