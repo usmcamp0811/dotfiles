adb04d456d32a857b21ae4e4cc792c42417d4a04
Matt Camp <matt@aicampground.com>
2025-04-05 18:26:20 -0500

    works

--- flake.nix
+++ flake.nix
@@ -208,62 +208,62 @@
     };
 
     mime-preview-yazi = {
-      url = "github:DreamMaoMao/mime-preview.yazi?rev=5ee0e78d7a46b756b77bec029722d31823869f08";
+      url = "github:DreamMaoMao/mime-preview.yazi";
       flake = false;
     };
 
     mime-ext-yazi = {
-      url = "github:DreamMaoMao/mime-ext.yazi?rev=47b5f85b0b6156f689641132eb9d29f7301b9b6a";
+      url = "github:DreamMaoMao/mime-ext.yazi";
       flake = false;
     };
 
     mediainfo-yazi = {
-      url = "github:boydaihungst/mediainfo.yazi?rev=2c3f675d3722672eb3a3422590375d1145843a40";
+      url = "github:boydaihungst/mediainfo.yazi";
       flake = false;
     };
 
     office-yazi = {
-      url = "github:macydnah/office.yazi?rev=bcd9e9e78835c96eb2bb8b8841e4753704b99b17";
+      url = "github:macydnah/office.yazi";
       flake = false;
     };
 
     eza-preview-yazi = {
-      url = "github:pierreay/eza-preview.yazi?rev=eeed668291bdf6cf91baf5124b952dcb2a2bfc6f";
+      url = "github:pierreay/eza-preview.yazi";
       flake = false;
     };
 
     glow-yazi = {
-      url = "github:Reledia/glow.yazi?rev=5ce76dc92ddd0dcef36e76c0986919fda3db3cf5";
+      url = "github:Reledia/glow.yazi";
       flake = false;
     };
 
     hexyl-yazi = {
-      url = "github:Reledia/hexyl.yazi?rev=39d3d4e23ad7cec8888f648ddf55af4386950ce7";
+      url = "github:Reledia/hexyl.yazi";
       flake = false;
     };
 
     ouch-yazi = {
-      url = "github:ndtoan96/ouch.yazi?rev=ce6fb75431b9d0d88efc6ae92e8a8ebb9bc1864a";
+      url = "github:ndtoan96/ouch.yazi";
       flake = false;
     };
 
     rich-preview-yazi = {
-      url = "github:MrDwarf7/rich-preview.yazi?rev=6567dc7776be71bb76f96918df55c3154479da1b";
+      url = "github:MrDwarf7/rich-preview.yazi";
       flake = false;
     };
 
     duckdb-yazi = {
-      url = "github:wylie102/duckdb.yazi?rev=6959a9b7bcfa2b6591d6e94e9ffa08adf09bffe5";
+      url = "github:wylie102/duckdb.yazi";
       flake = false;
     };
 
     yaziline-yazi = {
-      url = "github:llanosrocas/yaziline.yazi?rev=e06c47f7fc7a1c679e3935b45013108dadd09c96";
+      url = "github:llanosrocas/yaziline.yazi";
       flake = false;
     };
 
     yatline-yazi = {
-      url = "github:imsi32/yatline.yazi?rev=600ed1fb1d04e1292da04280a65f8deca04dc36a";
+      url = "github:imsi32/yatline.yazi";
       flake = false;
     };
 
