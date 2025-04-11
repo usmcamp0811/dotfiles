15d64062c1cada462626355fce483794c2ce1b2c
Matt Camp <matt@aicampground.com>
2025-04-10 20:11:25 -0500

    wip

--- modules/nixos/services/k0s/default.nix
+++ modules/nixos/services/k0s/default.nix
@@ -350,7 +350,7 @@ in
           description = "k0s Worker - Zero Friction Kubernetes";
           documentation = [ "https://docs.k0sproject.io" ];
           path = with pkgs; [ kmod util-linux mount ];
-          after = [ "network-online.target" ];
+          after = [ "network-online.target" "k0s-worker-token.service" ];
           wants = [ "network-online.target" ];
           wantedBy = [ "multi-user.target" ];
           startLimitIntervalSec = 5;
