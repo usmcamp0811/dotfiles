06b2abc60eaed61fe33efa38d69f5f71e30b69d1
Matt Camp <matt@aicampground.com>
2025-04-12 15:57:36 -0500

    wip

--- modules/nixos/services/k0s/default.nix
+++ modules/nixos/services/k0s/default.nix
@@ -433,12 +433,18 @@ in
             LimitNOFILE = 999999;
             Restart = "always";
             ExecStart = ''
-              ${cfg.package}/bin/k0s controller --data-dir=${cfg.dataDir}${
-                optionalString cfg.isLeader " --config=${cfg.dataDir}/k0s.yaml"
-              }${
+              ${cfg.package}/bin/k0s controller --data-dir=${cfg.dataDir} cfg.isLeader --config=${cfg.dataDir}/k0s.yaml
+              ${
                 optionalString (!cfg.isLeader) " --token-file=${cfg.dataDir}/k0s-token-controller"
               }
             '';
+            # ExecStart = ''
+            #   ${cfg.package}/bin/k0s controller --data-dir=${cfg.dataDir}${
+            #     optionalString cfg.isLeader " --config=${cfg.dataDir}/k0s.yaml"
+            #   }${
+            #     optionalString (!cfg.isLeader) " --token-file=${cfg.dataDir}/k0s-token-controller"
+            #   }
+            # '';
           };
           preStart = ''
             HOST_IP=$(${pkgs.iproute2}/bin/ip -4 addr show ${cfg.interface} | ${pkgs.gawk}/bin/awk '/inet / {print $2}' | ${pkgs.coreutils}/bin/cut -d/ -f1 | ${pkgs.coreutils}/bin/head -n1)
