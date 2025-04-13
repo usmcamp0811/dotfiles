4737072a911ff13d3ec72cde987852476429203f
Matt Camp <matt@aicampground.com>
2025-04-12 21:57:21 -0500

    wip

--- lib/kubernetes/default.nix
+++ lib/kubernetes/default.nix
@@ -1,26 +1,31 @@
 { lib, ... }:
 with lib; rec {
-  lookupK0sControllers = { nixosConfigurations, port ? 6443 }:
+  lookupK0sControllers =
+    { nixosConfigurations
+    , port ? 6443
+    ,
+    }:
     let
       getK0sController = host: cfg:
         let
           enabled = cfg.config.campground.suites.kubernetes.enable or false;
           role = cfg.config.campground.suites.kubernetes.role or null;
         in
-        if enabled && elem role [ "controller" "controller+worker" ] then {
+        if enabled && elem role [ "controller" "controller+worker" ]
+        then {
           name = host;
           value = {
             ip = cfg.config.networking.hostName or host;
             inherit port;
-            options = [ "check" "check-ssl" "verify" "none" ];
+            options = [ "check" ];
+            # options = ["check" "check-ssl" "verify" "none"];
           };
-        } else
-          null;
+        }
+        else null;
 
       controllers = builtins.listToAttrs (builtins.filter (x: x != null)
         (map (host: getK0sController host nixosConfigurations.${host})
           (builtins.attrNames nixosConfigurations)));
     in
     controllers;
-
 }
