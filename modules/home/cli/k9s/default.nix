bf29518d0bfaaad4fd97857751b9cf2e05b5fce9
Matt Camp <matt@aicampground.com>
2025-04-12 22:36:13 -0500

    wip

--- modules/home/cli/k9s/default.nix
+++ modules/home/cli/k9s/default.nix
@@ -1,22 +1,26 @@
-{ options, config, lib, pkgs, ... }:
+{ options
+, config
+, lib
+, pkgs
+, ...
+}:
 with lib;
-with lib.campground;
-let cfg = config.campground.cli.k9s;
-in {
+with lib.campground; let
+  cfg = config.campground.cli.k9s;
+in
+{
   options.campground.cli.k9s = with types; {
     enable = mkBoolOpt false "Whether or not to enable K9s.";
   };
 
   config = mkIf cfg.enable {
     campground.cli.aliases = {
-
       k = ''
-        ${pkgs.kubectl}/bin/kubectl
+        ${pkgs.kubectl}/bin/kubectl $@
       '';
       kwatch = ''
-        watch ${pkgs.kubectl}/bin/kubectl
+        watch ${pkgs.kubectl}/bin/kubectl $@
       '';
-
     };
     home.packages = with pkgs; [ k9s kubernetes-helm kubectl ];
   };
