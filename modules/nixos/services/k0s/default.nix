9d462e60ccc1e1f06f45d2bc3ef0121b3c04561a
Matt Camp <matt@aicampground.com>
2025-04-13 19:42:01 -0500

    flannel isnt by default

--- modules/nixos/services/k0s/default.nix
+++ modules/nixos/services/k0s/default.nix
@@ -63,7 +63,7 @@ let
                 peerRouterIPs: ""
                 metricsPort: 9090
               podCIDR: 10.244.0.0/16
-              provider: flannel
+              provider: calico
               serviceCIDR: 10.96.0.0/12
             podSecurityPolicy:
               defaultPolicy: 00-k0s-privileged
@@ -229,7 +229,6 @@ in
       openiscsi
       nfs-utils
       campground.k0s
-      cni-plugin-flannel
     ];
 
     systemd.services = mkMerge [
