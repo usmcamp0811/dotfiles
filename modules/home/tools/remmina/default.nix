ebccc3a8810d05963999fa29cdddbf2a51a2a15c
Matt Camp <matt@aicampground.com>
2025-04-24 14:15:53 -0500

    remmina auto launch

--- modules/home/tools/remmina/default.nix
+++ modules/home/tools/remmina/default.nix
@@ -1,16 +1,32 @@
-{ options, config, lib, pkgs, ... }:
+{ options
+, config
+, lib
+, pkgs
+, ...
+}:
 with lib;
-with lib.campground;
-let cfg = config.campground.tools.remmina;
-
-in {
+with lib.campground; let
+  cfg = config.campground.tools.remmina;
+in
+{
   options.campground.tools.remmina = with types; {
     enable = mkBoolOpt false "Whether or not to enable Remmina.";
   };
 
   config = mkIf cfg.enable {
-    # TOOD: Auto configure 
     home.packages = with pkgs; [ remmina ];
 
+    xdg.mimeApps.enable = true;
+    xdg.mimeApps.defaultApplications = {
+      "application/x-rdp" = "org.remmina.Remmina.desktop";
+    };
+
+    xdg.desktopEntries.remmina = {
+      name = "Remmina";
+      exec = "${pkgs.bash}/bin/bash -c \"G_MESSAGES_DEBUG=all ${pkgs.remmina}/bin/remmina -c %u\"";
+      type = "Application";
+      mimeType = [ "application/x-rdp" ];
+      noDisplay = true;
+    };
   };
 }
