f766ee3f5de2cb84a48eb5376d7eca4a5da24a2a
Matt Camp <matt@aicampground.com>
2025-04-25 20:36:55 -0500

    wip

--- modules/nixos/services/k3s/default.nix
+++ modules/nixos/services/k3s/default.nix
@@ -59,11 +59,6 @@ in
   };
 
   config = mkIf cfg.enable {
-    # boot.kernelParams = ["iptables.legacy=1"];
-    # environment.sessionVariables = {
-    #   IPTABLES_BACKEND = "legacy";
-    # };
-    # environment.etc."alternatives/iptables".source = pkgs.iptables-legacy;
 
     services.k3s = {
       enable = true;
@@ -71,107 +66,106 @@ in
       role = cfg.role;
       tokenFile = mkIf (cfg.tokenFile == null && cfg.role == "agent") "/var/lib/vault/k3s/k3s-token";
       serverAddr = cfg.serverAddr;
-      # extraFlags = cfg.extraFlagsG
-    };
+      extraFlags = cfg.extraFlagsG
+        };
 
-    systemd.services.store-k3s-token = mkIf cfg.storeK3sToken {
-      description = "Store K3s node-token in Vault";
+      systemd.services.store-k3s-token = mkIf cfg.storeK3sToken {
+        description = "Store K3s node-token in Vault";
+        after = [
+          "k3s.service"
+          "vault-agent.service"
+          "network-online.target"
+        ];
+        requires = [ "k3s.service" ];
+        wantedBy = [ "multi-user.target" ];
+        serviceConfig = {
+          Type = "oneshot";
+          User = "root";
+          ExecStart = pkgs.writeShellScript "store-k3s-token" ''
+            set -e
+
+            echo "Waiting for K3s to be fully ready..."
+
+            until [ -f /var/lib/rancher/k3s/server/node-token ]; do
+              sleep 5
+            done
+
+            echo "Reading K3s node-token..."
+            NODE_TOKEN=$(< /var/lib/rancher/k3s/server/node-token)
+
+            VAULT_PATH="${cfg.vault-path}"
+            export VAULT_ADDR="${cfg.vault-address}"
+            HOSTNAME=${config.networking.hostName}
+
+            ROLE_ID=$(cat /var/lib/vault/$HOSTNAME/role-id)
+            SECRET_ID=$(cat /var/lib/vault/$HOSTNAME/secret-id)
+
+            echo "Logging in to Vault using AppRole..."
+            VAULT_TOKEN=$(${pkgs.vault}/bin/vault write -field=token auth/approle/login role_id="$ROLE_ID" secret_id="$SECRET_ID")
+            export VAULT_TOKEN
+
+            echo "Storing K3s node-token in Vault at $VAULT_PATH"
+            ${pkgs.vault}/bin/vault kv put "$VAULT_PATH" node_token="$NODE_TOKEN"
+
+            echo "Done storing K3s token."
+          '';
+          RemainAfterExit = true;
+        };
+      };
+
+      environment.systemPackages = [ cfg.package ];
+
+      systemd.services.get-k3s-token = mkIf !cfg.storeK3sToken {
+      description = "Get kss tokens from Vault";
       after = [
-        "k3s.service"
         "vault-agent.service"
         "network-online.target"
       ];
-      requires = [ "k3s.service" ];
-      wantedBy = [ "multi-user.target" ];
+      before = [ "k3s" ];
+      wantedBy = [
+        "multi-user.target"
+      ];
       serviceConfig = {
         Type = "oneshot";
         User = "root";
-        ExecStart = pkgs.writeShellScript "store-k3s-token" ''
+        ExecStart = pkgs.writeShellScript "get-k3s-tokens" ''
           set -e
-
-          echo "Waiting for K3s to be fully ready..."
-
-          until [ -f /var/lib/rancher/k3s/server/node-token ]; do
-            sleep 5
-          done
-
-          echo "Reading K3s node-token..."
-          NODE_TOKEN=$(< /var/lib/rancher/k3s/server/node-token)
-
-          VAULT_PATH="${cfg.vault-path}"
-          export VAULT_ADDR="${cfg.vault-address}"
-          HOSTNAME=${config.networking.hostName}
-
-          ROLE_ID=$(cat /var/lib/vault/$HOSTNAME/role-id)
-          SECRET_ID=$(cat /var/lib/vault/$HOSTNAME/secret-id)
-
-          echo "Logging in to Vault using AppRole..."
-          VAULT_TOKEN=$(${pkgs.vault}/bin/vault write -field=token auth/approle/login role_id="$ROLE_ID" secret_id="$SECRET_ID")
-          export VAULT_TOKEN
-
-          echo "Storing K3s node-token in Vault at $VAULT_PATH"
-          ${pkgs.vault}/bin/vault kv put "$VAULT_PATH" node_token="$NODE_TOKEN"
-
-          echo "Done storing K3s token."
+          mkdir -p /var/lib/vault/k3s/
+          cp /tmp/detsys-vault/k3s-token /var/lib/vault/k3s/k3s-token
         '';
+
         RemainAfterExit = true;
       };
     };
-
-    environment.systemPackages = [ cfg.package ];
-
-    systemd.services.get-k3s-token = mkIf !cfg.storeK3sToken {
-    description = "Get kss tokens from Vault";
-    after = [
-      "vault-agent.service"
-      "network-online.target"
-    ];
-    before = [ "k3s" ];
-    wantedBy = [
-      "multi-user.target"
-    ];
-    serviceConfig = {
-      Type = "oneshot";
-      User = "root";
-      ExecStart = pkgs.writeShellScript "get-k3s-tokens" ''
-        set -e
-        mkdir -p /var/lib/vault/k3s/
-        cp /tmp/detsys-vault/k3s-token /var/lib/vault/k3s/k3s-token
-      '';
-
-      RemainAfterExit = true;
-    };
-  };
-  campground.services.vault-agent.services.get-k3s-token = mkIf (cfg.tokenFile == null && cfg.role == "agent") {
-    settings = {
-      vault.address = cfg.vault-address;
-      auto_auth = {
-        method = [
-          {
-            type = "approle";
-            config = {
-              role_id_file_path = cfg.role-id;
-              secret_id_file_path = cfg.secret-id;
-              remove_secret_id_file_after_reading = false;
-            };
-          }
-        ];
+    campground.services.vault-agent.services.get-k3s-token = mkIf (cfg.tokenFile == null && cfg.role == "agent") {
+      settings = {
+        vault.address = cfg.vault-address;
+        auto_auth = {
+          method = [
+            {
+              type = "approle";
+              config = {
+                role_id_file_path = cfg.role-id;
+                secret_id_file_path = cfg.secret-id;
+                remove_secret_id_file_after_reading = false;
+              };
+            }
+          ];
+        };
       };
-    };
 
-    secrets = {
-      file = {
-        files = {
-          "k3s-token" = {
-            text = ''
-              {{ with secret "${cfg.vault-path}" }}{{ if eq "${cfg.kvVersion}" "v1" }}{{ .Data.k3s_token }}{{ else }}{{ .Data.data.k3s_token }}{{ end }}{{ end }}
-            '';
-            permissions = "0400";
-            change-action = "restart";
+      secrets = {
+        file = {
+          files = {
+            "k3s-token" = {
+              text = ''
+                {{ with secret "${cfg.vault-path}" }}{{ if eq "${cfg.kvVersion}" "v1" }}{{ .Data.k3s_token }}{{ else }}{{ .Data.data.k3s_token }}{{ end }}{{ end }}
+              '';
+              permissions = "0400";
+              change-action = "restart";
+            };
           };
         };
       };
     };
-  };
-};
-}
+  }
