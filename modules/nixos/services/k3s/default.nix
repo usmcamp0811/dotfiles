aadf5205321900b4ca696a7831518a3de593e403
Matt Camp <matt@aicampground.com>
2025-04-26 19:24:46 -0500

    not sure about snapshotter with k3s

--- modules/nixos/services/k3s/default.nix
+++ modules/nixos/services/k3s/default.nix
@@ -116,11 +116,13 @@ in
       tokenFile = mkIf (!cfg.clusterInit) "/var/lib/rancher/k3s/server/node-token";
       serverAddr = cfg.serverAddr;
       extraFlags = mkDefault cfg.extraFlags;
+      setKubeConfig = true;
+      snapshotter = "nix";
       configPath =
         if cfg.role == "server"
         then "/etc/rancher/k3s/config.yml"
         else null;
-      # extraFlags = mkDefault (cfg.extraFlags ++ ["--snapshotter overlayfs"]);
+      moreFlags = cfg.extraFlags;
     };
 
     systemd.services.store-k3s-token = mkIf cfg.clusterInit {
