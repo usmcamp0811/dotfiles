9796518b5e9964f5baedda4dc6911c2113d44e24
Matt Camp <matt@aicampground.com>
2025-05-09 07:39:03 -0500

    mkslide

---
+++ lib/slidev/default.nix
@@ -0,0 +1,62 @@
+{ lib
+, inputs
+, ...
+}: rec {
+  # adapted from https://github.com/charles-bord/nix-forest-slides/tree/master
+  mkSlide =
+    { lib
+    , stdenv
+    , slidev
+    , markdown
+    , themes
+    , slides
+    , assets ? [ ]
+    , urlBase ? "/"
+    ,
+    }:
+    stdenv.mkDerivation {
+      pname = "slidev-presentation";
+      version = "0.1.0";
+      src = ./.;
+
+      nativeBuildInputs = [ slidev ];
+
+      buildInputs = [ ];
+
+      buildPhase =
+        let
+          assetsGlobsStr =
+            builtins.concatStringsSep " " (builtins.map (pkg: "${pkg}/*") assets);
+        in
+        ''
+          runHook preBuild
+
+          mkdir themes
+          cp -r ${themes}/packages/* themes/
+          chmod -R u+w themes/
+
+          mkdir public
+          ln -s ${assetsGlobsStr} public/
+
+          ln -s ${slidev}/node_modules node_modules
+
+          cp ${markdown} ./slides.md
+          slidev build --base "${urlBase}"
+
+          runHook postBuild
+        '';
+
+      installPhase = ''
+        runHook preInstall
+        cp -r dist $out
+        runHook postInstall
+      '';
+
+      meta = {
+        description = "Slidev Presentation SPA";
+        homepage = "https://sli.dev/";
+        license = lib.licenses.mit;
+        maintainers = with lib.maintainers; [ ];
+      };
+    };
+}
