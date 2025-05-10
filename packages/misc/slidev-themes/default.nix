7dadeed0d2c0004aa28a832e047d04a0b1873ed5
Matt Camp <matt@aicampground.com>
2025-05-10 14:07:02 -0500

    make themes packages

---
+++ packages/misc/slidev-themes/default.nix
@@ -0,0 +1,42 @@
+{ pkgs
+, lib
+, inputs
+, ...
+}:
+with lib;
+with lib.campground; let
+  neversink-theme = buildTheme {
+    inherit pkgs;
+    pname = "slidev-theme-neversink";
+    version = "0.3.6";
+    src = pkgs.fetchFromGitHub {
+      owner = "gureckis";
+      repo = "slidev-theme-neversink";
+      rev = "v0.3.6";
+      hash = "sha256-JcdkZBcf059Pk5lqwGIlcTHmfIM54no98adeHe+TNBs=";
+    };
+    depsHash = "sha256-NKQ/MISoYnQFYMfcb8vOTE+YF1/AUHYRlGU4qNQalVY=";
+  };
+  mokkapps-theme = buildTheme {
+    inherit pkgs;
+    pname = "slidev-theme-mokkapps";
+    version = "0.1.0";
+    src = pkgs.fetchFromGitHub {
+      owner = "Mokkapps";
+      repo = "slidev-theme-mokkapps";
+      rev = "master";
+      hash = "sha256-m2RXHI+vvszYaDxO38mLdxMKZbtUgAMrdSJBCINgQSc=";
+    };
+    depsHash = "sha256-ZJh47LQamNh1kPd8c/JTlkcQp9k2MwKLkIw+f+102DE=";
+  };
+  slidev-themes = pkgs.fetchFromGitHub {
+    owner = "slidevjs";
+    repo = "themes";
+    rev = "v0.22.0";
+    hash = "sha256-t6sg/nSbr2ytMHN1yuQy/kEDLyAYHXFVwcN1naeGhQc=";
+  };
+in
+slidev-themes
+  // {
+  inherit neversink-theme mokkapps-theme;
+}
