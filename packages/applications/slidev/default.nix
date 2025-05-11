f7315922983de991fb463131a9ae1ede8d3ac05b
Matt Camp <matt@aicampground.com>
2025-05-11 13:40:07 -0500

    updated version.. this is the latest i can do newer version dont have frozen locks

--- packages/applications/slidev/default.nix
+++ packages/applications/slidev/default.nix
@@ -2,53 +2,73 @@
 , pkgs
 ,
 }:
-# adapted from https://github.com/charles-bord/nix-forest-slides/tree/master
-pkgs.stdenv.mkDerivation (finalAttrs: {
-  pname = "slidev";
-  version = "0.49.29";
-  src = pkgs.fetchFromGitHub {
-    owner = "slidevjs";
-    repo = "slidev";
-    rev = "v${finalAttrs.version}";
-    hash = "sha256-bOSIJTzPMyS+wbJsLVaMw/wicbsgQADEOyQQjU8MKWw=";
+with lib; let
+  versions = {
+    v0_49_29 = {
+      version = "0.49.29";
+      rev = "v0.49.29";
+      srcHash = "sha256-bOSIJTzPMyS+wbJsLVaMw/wicbsgQADEOyQQjU8MKWw=";
+      depsHash = "sha256-18/6hNsRq0hl1UXY6cchuIEVWTbCiZJG33QHDvnXS1A=";
+    };
+    v0_50_0 = {
+      version = "0.50.0";
+      rev = "v0.50.0";
+      srcHash = "sha256-8LP7bAFWJAxd17u77aqX+j0mqTw59AODlrqot8np21g=";
+      depsHash = "sha256-M9wqO+V5r2+PlxRMBe47fULTyaaeDWq45rR6XtKPsBw=";
+    };
   };
 
-  nativeBuildInputs = [ pkgs.nodejs pkgs.pnpm_9.configHook pkgs.makeWrapper ];
+  buildSlidev = name: cfg:
+    pkgs.stdenv.mkDerivation (finalAttrs: {
+      pname = "slidev";
+      version = cfg.version;
 
-  pnpmDeps = pkgs.pnpm_9.fetchDeps {
-    inherit (finalAttrs) pname version src;
-    hash = "sha256-18/6hNsRq0hl1UXY6cchuIEVWTbCiZJG33QHDvnXS1A=";
-  };
+      src = pkgs.fetchFromGitHub {
+        owner = "slidevjs";
+        repo = "slidev";
+        rev = cfg.rev;
+        hash = cfg.srcHash;
+      };
 
-  buildPhase = ''
-    runHook preBuild
+      nativeBuildInputs = [ pkgs.nodejs pkgs.pnpm_9.configHook pkgs.makeWrapper ];
 
-    pnpm --filter @slidev/cli build
-    pnpm --filter @slidev/parser build
+      pnpmDeps = pkgs.pnpm_9.fetchDeps {
+        pname = "slidev";
+        version = cfg.version;
+        src = finalAttrs.src;
+        hash = cfg.depsHash;
+      };
 
-    runHook postBuild
-  '';
+      buildPhase = ''
+        runHook preBuild
+        pnpm --filter @slidev/cli build
+        pnpm --filter @slidev/parser build
+        runHook postBuild
+      '';
 
-  installPhase = ''
-    runHook preInstall
+      installPhase = ''
+        runHook preInstall
+        mkdir -p $out
+        cp -r packages $out/packages
+        cp -r node_modules $out/node_modules
+        makeWrapper ${pkgs.nodejs}/bin/node $out/bin/slidev \
+          --set NODE_PATH "$out/node_modules" \
+          --add-flags "$out/packages/slidev/bin/slidev.mjs"
+        runHook postInstall
+      '';
 
-    mkdir -p $out
-    cp -r packages $out/packages
-    cp -r node_modules $out/node_modules
+      meta = {
+        description = "Presentation Slides for Developers";
+        homepage = "https://sli.dev/";
+        changelog = "https://github.com/slidevjs/slidev/releases/tag/v${cfg.version}";
+        license = licenses.mit;
+        mainProgram = "slidev";
+      };
+    });
 
-    makeWrapper "${pkgs.nodejs}/bin/node" "$out/bin/slidev" \
-      --set NODE_PATH "$out/node_modules" \
-      --add-flags "$out/packages/slidev/bin/slidev.mjs"
+  all = lib.mapAttrs buildSlidev versions;
 
-    runHook postInstall
-  '';
-
-  meta = {
-    description = "Presentation Slides for Developers";
-    homepage = "https://sli.dev/";
-    changelog = "https://github.com/slidevjs/slidev/releases/tag/v${finalAttrs.version}";
-    license = lib.licenses.mit;
-    mainProgram = "slidev";
-    maintainers = with lib.maintainers; [ ];
-  };
-})
+  # Choose the latest version for default
+  latest = all.v0_50_0;
+in
+latest // all
