4b94e2e37741af0faa2812f99a15a476a34f5ad4
Matt Camp <matt@aicampground.com>
2025-05-16 22:53:48 -0500

    wip

--- lib/slidev/default.nix
+++ lib/slidev/default.nix
@@ -1,72 +1,74 @@
-{
-  lib,
-  inputs,
-  ...
+{ lib
+, inputs
+, ...
 }: rec {
   # adapted from https://github.com/charles-bord/nix-forest-slides/tree/master
-  mkSlide = {
-    lib,
-    stdenv,
-    slidev,
-    markdown,
-    themes ? [],
-    slides ? [],
-    assets ? [],
-    urlBase ? "/",
-    extraNodePackages ? [],
-  }:
+  mkSlide =
+    { lib
+    , stdenv
+    , slidev
+    , markdown
+    , themes ? [ ]
+    , slides ? [ ]
+    , assets ? [ ]
+    , urlBase ? "/"
+    , extraNodePackages ? [ ]
+    ,
+    }:
     stdenv.mkDerivation {
       pname = "slidev-presentation";
       version = "0.1.0";
       src = ./.;
 
-      nativeBuildInputs = [slidev];
+      nativeBuildInputs = [ slidev ];
 
       buildInputs = extraNodePackages;
 
-      buildPhase = let
-        assetsGlobsStr =
-          builtins.concatStringsSep " " (builtins.map (pkg: "${pkg}/*") assets);
-        customThemeDirs = builtins.concatStringsSep "\n" (
-          builtins.map
-          (t: ''
-            mkdir -p themes/${t.pname}
-            cp -r ${t}/* themes/${t.pname}
-          '')
-          themes
-        );
-      in ''
-        runHook preBuild
+      buildPhase =
+        let
+          assetsGlobsStr =
+            builtins.concatStringsSep " " (builtins.map (pkg: "${pkg}/*") assets);
+          customThemeDirs = builtins.concatStringsSep "\n" (
+            builtins.map
+              (t: ''
+                mkdir -p themes/${t.pname}
+                cp -r ${t}/* themes/${t.pname}
+              '')
+              themes
+          );
+        in
+        ''
+          runHook preBuild
 
-        mkdir themes
+          mkdir themes
 
-        ${customThemeDirs}
+          ${customThemeDirs}
 
-        chmod -R u+w themes/
+          chmod -R u+w themes/
 
-        mkdir -p public/assets
-        ${builtins.concatStringsSep "\n" (builtins.map (pkg: "cp -r ${pkg}/* public/assets/") assets)}
+          mkdir -p public/assets
+          ${builtins.concatStringsSep "\n" (builtins.map (pkg: "cp -r ${pkg}/* public/assets/") assets)}
 
-        mkdir -p slides
-        ${builtins.concatStringsSep "\n" (builtins.map (pkg: "cp -r ${pkg}/* slides") slides)}
+          mkdir -p slides
+          ${builtins.concatStringsSep "\n" (builtins.map (pkg: "cp -r ${pkg}/* slides") slides)}
 
-        mkdir -p node_modules
+          mkdir -p node_modules
 
-        # Copy all top-level packages from slidev
-        cp -r ${slidev}/node_modules/* node_modules/
+          # Copy all top-level packages from slidev
+          cp -r ${slidev}/node_modules/* node_modules/
 
-        # Inject extra packages (like sass-embedded)
-        ${builtins.concatStringsSep "\n" (builtins.map (pkg: ''
-            mkdir -p node_modules/${pkg.pname}
-            cp -r ${pkg}/lib/node_modules/${pkg.pname}/* node_modules/${pkg.pname}/
-          '')
-          extraNodePackages)}
+          # Inject extra packages (like sass-embedded)
+          ${builtins.concatStringsSep "\n" (builtins.map (pkg: ''
+              mkdir -p node_modules/${pkg.pname}
+              cp -r ${pkg}/lib/node_modules/${pkg.pname}/* node_modules/${pkg.pname}/
+            '')
+            extraNodePackages)}
 
-        cp ${markdown} ./slides.md
-        slidev build --base "${urlBase}"
+          cp ${markdown} ./slides.md
+          slidev build --base "${urlBase}"
 
-        runHook postBuild
-      '';
+          runHook postBuild
+        '';
 
       installPhase = ''
         runHook preInstall
@@ -80,21 +82,22 @@
         description = "Slidev Presentation SPA";
         homepage = "https://sli.dev/";
         license = lib.licenses.mit;
-        maintainers = with lib.maintainers; [];
+        maintainers = with lib.maintainers; [ ];
       };
     };
-  buildPnpmTheme = {
-    pkgs,
-    pname,
-    version,
-    src,
-    depsHash,
-    pnpm,
-  }:
+  buildPnpmTheme =
+    { pkgs
+    , pname
+    , version
+    , src
+    , depsHash
+    , pnpm
+    ,
+    }:
     pkgs.stdenv.mkDerivation {
       inherit pname version src;
 
-      nativeBuildInputs = [pkgs.nodejs pnpm.configHook];
+      nativeBuildInputs = [ pkgs.nodejs pnpm.configHook ];
 
       pnpmDeps = pnpm.fetchDeps {
         inherit pname version src;
@@ -113,13 +116,14 @@
       };
     };
 
-  buildNpmTheme = {
-    pkgs,
-    pname,
-    version,
-    src,
-    depsHash ? null,
-  }:
+  buildNpmTheme =
+    { pkgs
+    , pname
+    , version
+    , src
+    , depsHash ? null
+    ,
+    }:
     pkgs.buildNpmPackage {
       inherit pname version src;
 
