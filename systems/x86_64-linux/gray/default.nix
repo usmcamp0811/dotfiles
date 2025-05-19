15272d69d60e1e37820baed98a779cc8d58c6259
Matt Camp <matt@aicampground.com>
2025-05-18 20:47:17 -0500

    wip

--- systems/x86_64-linux/gray/default.nix
+++ systems/x86_64-linux/gray/default.nix
@@ -21,7 +21,7 @@ in
   services.tlp = { enable = mkForce false; };
   campground = {
     rmf.example-flask-app = {
-      enable = true;
+      # enable = true;
       settings = {
         port = 8081;
       };
