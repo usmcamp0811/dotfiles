cf3b178f3a66b6ec98fefd1c8161c93900c0544e
Matt Camp <matt@aicampground.com>
2025-05-22 08:16:27 -0500

    wip

--- overlays/campground/default.nix
+++ overlays/campground/default.nix
@@ -54,5 +54,6 @@
   });
 }
   // {
-  inherit (channels.unstable) lemmy-server lemmy-help pds pdsadmin k3s pnpm_9;
+  inherit (channels.unstable) lemmy-server lemmy-help pds pdsadmin k3s pnpm_9 python312Packages python3Packages;
+  inherit (channels.pyarrow) cargo-auditable;
 }
