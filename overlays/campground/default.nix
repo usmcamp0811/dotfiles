dc8aae4a10c3e3f7118ffa3c16bfa63e331e44f1
Matt Camp <matt@aicampground.com>
2025-05-24 12:56:41 -0500

    getting htere

--- overlays/campground/default.nix
+++ overlays/campground/default.nix
@@ -1,4 +1,5 @@
 { kube-gen
+, prev-nixpkgs
 , campground-nvim
 , nixhelm
 , nixtheplanet
@@ -52,16 +53,64 @@
         cp -r ${loginOIDCPlugin} $out/share/plugins/LoginOIDC
       '';
   });
+  mkYarnPackage = old-nixpkgs.legacyPackages.${prev.system}.mkYarnPackage;
+
   cargo-auditable = prev.cargo-auditable.overrideAttrs (old: {
     buildInputs = (old.buildInputs or [ ]) ++ [ prev.python3Packages.requests ];
   });
+  python312Packages =
+    prev.python312Packages
+    // {
+      pyrate-limiter = prev-nixpkgs.outputs.legacyPackages.${prev.system}.python312Packages.pyrate-limiter;
+      requests-ratelimiter = prev-nixpkgs.outputs.legacyPackages.${prev.system}.python312Packages.requests-ratelimiter;
+      lap = final.python3Packages.buildPythonPackage rec {
+        pname = "lap";
+        version = "0.4.0";
+
+        src = final.fetchPypi {
+          inherit pname version;
+          sha256 = "sha256-aQEpE09EOph9KnLbkgvi1SpM67PaqGL7If95hAj8Xl4=";
+        };
+
+        nativeBuildInputs = [ final.python3Packages.setuptools ];
+        doCheck = false;
+
+        meta = with final.lib; {
+          description = "Linear assignment problem solver using LAPJV algorithm";
+          homepage = "https://github.com/gatagat/lap";
+          license = licenses.mit;
+        };
+      };
+    };
+  python313Packages =
+    prev.python313Packages
+    // {
+      pyrate-limiter = prev-nixpkgs.outputs.legacyPackages.${prev.system}.python313Packages.pyrate-limiter;
+      requests-ratelimiter = prev-nixpkgs.outputs.legacyPackages.${prev.system}.python313Packages.requests-ratelimiter;
+      lap = final.python3Packages.buildPythonPackage rec {
+        pname = "lap";
+        version = "0.4.0";
+
+        src = final.fetchPypi {
+          inherit pname version;
+          sha256 = "sha256-aQEpE09EOph9KnLbkgvi1SpM67PaqGL7If95hAj8Xl4=";
+        };
+
+        nativeBuildInputs = [ final.python3Packages.setuptools ];
+        doCheck = false;
+
+        meta = with final.lib; {
+          description = "Linear assignment problem solver using LAPJV algorithm";
+          homepage = "https://github.com/gatagat/lap";
+          license = licenses.mit;
+        };
+      };
+    };
   python3Packages =
     prev.python3Packages
     // {
-      requests-ratelimiter = prev.python3Packages.requests-ratelimiter.overrideAttrs (old: {
-        meta = old.meta // { broken = false; };
-      });
-
+      pyrate-limiter = prev-nixpkgs.outputs.legacyPackages.${prev.system}.python3Packages.pyrate-limiter;
+      requests-ratelimiter = prev-nixpkgs.outputs.legacyPackages.${prev.system}.python3Packages.requests-ratelimiter;
       lap = final.python3Packages.buildPythonPackage rec {
         pname = "lap";
         version = "0.4.0";
@@ -84,5 +133,5 @@
 }
   // {
   inherit (channels.unstable) lemmy-server lemmy-help pds pdsadmin rofi k3s pnpm_9;
-  inherit (channels.prev-nixpkgs) yarn2nix yarn python313Packages python312Packages fetchCargoVendor swaynotificationcenter rustPlatform julia;
+  inherit (channels.prev-nixpkgs) nginx yarn2nix yarn python313Packages python312Packages fetchCargoVendor swaynotificationcenter rustPlatform julia;
 }
