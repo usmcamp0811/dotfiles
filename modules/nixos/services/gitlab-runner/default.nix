ca0b06e8896730c96d838bbad38e559c2cc2b670
Matt Camp <matt@aicampground.com>
2025-05-24 22:25:42 -0500

    update gitlab

--- modules/nixos/services/gitlab-runner/default.nix
+++ modules/nixos/services/gitlab-runner/default.nix
@@ -1,7 +1,11 @@
-{ lib, config, pkgs, inputs, ... }:
+{ lib
+, config
+, pkgs
+, inputs
+, ...
+}:
 with lib;
-with lib.campground;
-let
+with lib.campground; let
   cfg = config.campground.services.gitlab-runner;
 
   CI_SERVER_URL = "${cfg.runner-name}_CI_SERVER_URL";
@@ -13,17 +17,21 @@ in
 {
   options.campground.services.gitlab-runner = {
     enable = mkEnableOption "GitLab Runner";
-    runner-name = mkOpt types.str config.networking.hostName
-      "Name used in Vault to deleniate runners";
+    runner-name =
+      mkOpt types.str config.networking.hostName
+        "Name used in Vault to deleniate runners";
 
-    role-id = mkOpt types.str
-      config.campground.services.vault-agent.settings.vault.role-id
-      "Absolute path to the Vault role-id";
-    secret-id = mkOpt types.str
-      config.campground.services.vault-agent.settings.vault.secret-id
-      "Absolute path to the Vault secret-id";
-    vault-path = mkOpt types.str "secret/campground/gitlab-runner"
-      "The Vault path to the KV containing the KVs that are for each database";
+    role-id =
+      mkOpt types.str
+        config.campground.services.vault-agent.settings.vault.role-id
+        "Absolute path to the Vault role-id";
+    secret-id =
+      mkOpt types.str
+        config.campground.services.vault-agent.settings.vault.secret-id
+        "Absolute path to the Vault secret-id";
+    vault-path =
+      mkOpt types.str "secret/campground/gitlab-runner"
+        "The Vault path to the KV containing the KVs that are for each database";
     kvVersion = mkOption {
       type = types.enum [ "v1" "v2" ];
       default = "v2";
@@ -39,73 +47,71 @@ in
   config = mkIf cfg.enable {
     boot.kernel.sysctl."net.ipv4.ip_forward" = true; # 1
     virtualisation.docker.enable = true;
-    # services.gitlab-runner = {
-    #   enable = true;
-    #   services = {
-    #     # runner for building in docker via host's nix-daemon
-    #     # nix store will be readable in runner, might be insecure
-    #
-    #     nix = with lib; {
-    #       authenticationTokenConfigFile =
-    #         toString /tmp/detsys-vault/config.toml; # 2
-    #       # File should contain at least these two variables:
-    #       # `CI_SERVER_URL`
-    #       # `REGISTRATION_TOKEN`
-    #       # registrationConfigFile = 
-    #       dockerImage = "alpine";
-    #       dockerVolumes = [
-    #         "/nix/store:/nix/store:ro"
-    #         "/nix/var/nix/db:/nix/var/nix/db:ro"
-    #         "/nix/var/nix/daemon-socket:/nix/var/nix/daemon-socket:ro"
-    #         "/root/.config/attic:/root/.config/attic:ro"
-    #       ];
-    #       dockerDisableCache = true;
-    #       preBuildScript = pkgs.writeScript "setup-container" ''
-    #         mkdir -p -m 0755 /nix/var/log/nix/drvs
-    #         mkdir -p -m 0755 /nix/var/nix/gcroots
-    #         mkdir -p -m 0755 /nix/var/nix/profiles
-    #         mkdir -p -m 0755 /nix/var/nix/temproots
-    #         mkdir -p -m 0755 /nix/var/nix/userpool
-    #         mkdir -p -m 1777 /nix/var/nix/gcroots/per-user
-    #         mkdir -p -m 1777 /nix/var/nix/profiles/per-user
-    #         mkdir -p -m 0755 /nix/var/nix/profiles/per-user/root
-    #         mkdir -p -m 0700 "$HOME/.nix-defexpr"
-    #         . ${pkgs.nix}/etc/profile.d/nix-daemon.sh
-    #         # TODO: link to inputs.nixpkgs
-    #         ${pkgs.nix}/bin/nix-channel --add https://nixos.org/channels/nixos-24.11 nixpkgs # 3
-    #         ${pkgs.nix}/bin/nix-channel --update nixpkgs
-    #         ${pkgs.nix}/bin/nix-env -i ${
-    #           concatStringsSep " " (with pkgs; [
-    #             nix
-    #             cacert
-    #             git
-    #             openssh
-    #             deploy-rs
-    #             campground.get-lan-pub-systems
-    #             vault
-    #             ssh-agents
-    #             attic
-    #             sbomnix
-    #           ])
-    #         }
-    #         mkdir -p -m 0755 /etc/nix
-    #         echo "extra-experimental-features = nix-command flakes" >> /etc/nix/nix.conf
-    #         echo "allow-unfree = true" >> /etc/nix/nix.conf
-    #         chmod 644 /etc/nix/nix.conf
-    #       '';
-    #       environmentVariables = {
-    #         ENV = "/etc/profile";
-    #         USER = "root";
-    #         NIX_REMOTE = "daemon";
-    #         PATH =
-    #           "/nix/var/nix/profiles/default/bin:/nix/var/nix/profiles/default/sbin:/bin:/sbin:/usr/bin:/usr/sbin";
-    #         NIX_SSL_CERT_FILE =
-    #           "/nix/var/nix/profiles/default/etc/ssl/certs/ca-bundle.crt";
-    #       };
-    #       # tagList = [ "nix" ];
-    #     };
-    #   };
-    # };
+    services.gitlab-runner = {
+      enable = true;
+      services = {
+        # runner for building in docker via host's nix-daemon
+        # nix store will be readable in runner, might be insecure
+
+        nix = with lib; {
+          authenticationTokenConfigFile =
+            toString /tmp/detsys-vault/config.toml; # 2
+          # File should contain at least these two variables:
+          # `CI_SERVER_URL`
+          # `REGISTRATION_TOKEN`
+          # registrationConfigFile =
+          dockerImage = "alpine";
+          dockerVolumes = [
+            "/nix/store:/nix/store:ro"
+            "/nix/var/nix/db:/nix/var/nix/db:ro"
+            "/nix/var/nix/daemon-socket:/nix/var/nix/daemon-socket:ro"
+            "/root/.config/attic:/root/.config/attic:ro"
+          ];
+          dockerDisableCache = true;
+          preBuildScript = pkgs.writeScript "setup-container" ''
+            mkdir -p -m 0755 /nix/var/log/nix/drvs
+            mkdir -p -m 0755 /nix/var/nix/gcroots
+            mkdir -p -m 0755 /nix/var/nix/profiles
+            mkdir -p -m 0755 /nix/var/nix/temproots
+            mkdir -p -m 0755 /nix/var/nix/userpool
+            mkdir -p -m 1777 /nix/var/nix/gcroots/per-user
+            mkdir -p -m 1777 /nix/var/nix/profiles/per-user
+            mkdir -p -m 0755 /nix/var/nix/profiles/per-user/root
+            mkdir -p -m 0700 "$HOME/.nix-defexpr"
+            . ${pkgs.nix}/etc/profile.d/nix-daemon.sh
+            # TODO: link to inputs.nixpkgs
+            ${pkgs.nix}/bin/nix-channel --add https://nixos.org/channels/nixos-24.11 nixpkgs # 3
+            ${pkgs.nix}/bin/nix-channel --update nixpkgs
+            ${pkgs.nix}/bin/nix-env -i ${
+              concatStringsSep " " (with pkgs; [
+                nix
+                cacert
+                git
+                openssh
+                deploy-rs
+                campground.get-lan-pub-systems
+                vault
+                ssh-agents
+                attic-client
+                sbomnix
+              ])
+            }
+            mkdir -p -m 0755 /etc/nix
+            echo "extra-experimental-features = nix-command flakes" >> /etc/nix/nix.conf
+            echo "allow-unfree = true" >> /etc/nix/nix.conf
+            chmod 644 /etc/nix/nix.conf
+          '';
+          environmentVariables = {
+            ENV = "/etc/profile";
+            USER = "root";
+            NIX_REMOTE = "daemon";
+            PATH = "/nix/var/nix/profiles/default/bin:/nix/var/nix/profiles/default/sbin:/bin:/sbin:/usr/bin:/usr/sbin";
+            NIX_SSL_CERT_FILE = "/nix/var/nix/profiles/default/etc/ssl/certs/ca-bundle.crt";
+          };
+          # tagList = [ "nix" ];
+        };
+      };
+    };
 
     campground = {
       services = {
@@ -116,14 +122,16 @@ in
                 # replace with the address of your vault
                 vault.address = cfg.vault-address;
                 auto_auth = {
-                  method = [{
-                    type = "approle";
-                    config = {
-                      role_id_file_path = cfg.role-id;
-                      secret_id_file_path = cfg.secret-id;
-                      remove_secret_id_file_after_reading = false;
-                    };
-                  }];
+                  method = [
+                    {
+                      type = "approle";
+                      config = {
+                        role_id_file_path = cfg.role-id;
+                        secret_id_file_path = cfg.secret-id;
+                        remove_secret_id_file_after_reading = false;
+                      };
+                    }
+                  ];
                 };
               };
               secrets = {
