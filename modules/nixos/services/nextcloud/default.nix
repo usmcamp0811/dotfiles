8c17f67017fd49a4507b5be2f3a4c91c58326fe5
Matt Camp <matt@aicampground.com>
2025-05-25 20:44:50 -0500

    pump nextcloud version

--- modules/nixos/services/nextcloud/default.nix
+++ modules/nixos/services/nextcloud/default.nix
@@ -1,15 +1,20 @@
-{ lib, config, pkgs, ... }:
+{ lib
+, config
+, pkgs
+, ...
+}:
 with lib;
-with lib.campground;
-let cfg = config.campground.services.nextcloud;
-in {
+with lib.campground; let
+  cfg = config.campground.services.nextcloud;
+in
+{
   options.campground.services.nextcloud = with types; {
     enable = mkBoolOpt false "Enable Nextcloud";
     port = mkOpt int 7443 "Port to host the Nextcloud server on";
     adminuser = mkOpt str "mcamp" "Absolute path to the Vault role-id";
     home = mkOpt str "/var/lib/nextcloud" "App Storage path of nextcloud.";
     dataDir = mkOpt str "/var/lib/nextcloud" "Data Storage path of nextcloud.";
-    package = mkOpt package pkgs.nextcloud30 "The nextcloud package to use.";
+    package = mkOpt package pkgs.nextcloud31 "The nextcloud package to use.";
     domain =
       mkOpt str "cloud.aicampground.com" "Trusted Domain to serve Nextcloud On";
     # OnlyOffice configuration
@@ -20,10 +25,12 @@ in {
     secret-id =
       mkOpt str config.campground.services.vault-agent.settings.vault.secret-id
         "Absolute path to the Vault secret-id";
-    onlyoffice-vault-path = mkOpt str "secret/campground/onlyoffice"
-      "The Vault path to the KV containing the OnlyOffice JWT Token";
-    vault-path = mkOpt str "secret/campground/nextcloud"
-      "The Vault path to the KV containing the KVs that are for each database";
+    onlyoffice-vault-path =
+      mkOpt str "secret/campground/onlyoffice"
+        "The Vault path to the KV containing the OnlyOffice JWT Token";
+    vault-path =
+      mkOpt str "secret/campground/nextcloud"
+        "The Vault path to the KV containing the KVs that are for each database";
     kvVersion = mkOption {
       type = enum [ "v1" "v2" ];
       default = "v2";
@@ -61,7 +68,6 @@ in {
         "pm.start_servers" = "40";
       };
       extraApps = with config.services.nextcloud.package.packages.apps; {
-
         # Office and document management
         inherit onlyoffice richdocuments;
 
@@ -70,8 +76,13 @@ in {
         inherit tasks deck calendar forms notes polls phonetrack;
 
         # Security and authentication
-        inherit impersonate twofactor_webauthn user_oidc end_to_end_encryption
-          notify_push;
+        inherit
+          impersonate
+          twofactor_webauthn
+          user_oidc
+          end_to_end_encryption
+          notify_push
+          ;
 
         # Group management, media, and collaboration tools
         inherit groupfolders memories maps spreed cookbook cospend;
@@ -108,33 +119,36 @@ in {
     campground.services.postgresql = {
       enable = true;
       authentication = [ "local nextcloud nextcloud trust" ];
-      databases = [{
-        name = "nextcloud";
-        user = "nextcloud";
-      }];
+      databases = [
+        {
+          name = "nextcloud";
+          user = "nextcloud";
+        }
+      ];
     };
 
     services.nginx = {
       enable = true;
       virtualHosts = {
         "${cfg.domain}" = {
-          listen = [{
-            addr = "0.0.0.0";
-            port = 13244;
-          }];
+          listen = [
+            {
+              addr = "0.0.0.0";
+              port = 13244;
+            }
+          ];
         };
       };
     };
 
-    systemd.services.nextcloud-setup.serviceConfig.ExecStartPost =
-      pkgs.writeScript "nextcloud-redis.sh" ''
-        #!${pkgs.runtimeShell}
-        nextcloud-occ config:system:set filelocking.enabled --value true --type bool
-        nextcloud-occ config:system:set redis 'host' --value '/var/run/redis-nextcloud/redis.sock' --type string
-        nextcloud-occ config:system:set redis 'port' --value 0 --type integer
-        nextcloud-occ config:system:set memcache.local --value '\OC\Memcache\Redis' --type string
-        nextcloud-occ config:system:set memcache.locking --value '\OC\Memcache\Redis' --type string
-      '';
+    systemd.services.nextcloud-setup.serviceConfig.ExecStartPost = pkgs.writeScript "nextcloud-redis.sh" ''
+      #!${pkgs.runtimeShell}
+      nextcloud-occ config:system:set filelocking.enabled --value true --type bool
+      nextcloud-occ config:system:set redis 'host' --value '/var/run/redis-nextcloud/redis.sock' --type string
+      nextcloud-occ config:system:set redis 'port' --value 0 --type integer
+      nextcloud-occ config:system:set memcache.local --value '\OC\Memcache\Redis' --type string
+      nextcloud-occ config:system:set memcache.locking --value '\OC\Memcache\Redis' --type string
+    '';
 
     services.redis.servers.nextcloud = {
       enable = true;
@@ -150,14 +164,16 @@ in {
       settings = {
         vault.address = cfg.vault-address;
         auto_auth = {
-          method = [{
-            type = "approle";
-            config = {
-              role_id_file_path = cfg.role-id;
-              secret_id_file_path = cfg.secret-id;
-              remove_secret_id_file_after_reading = false;
-            };
-          }];
+          method = [
+            {
+              type = "approle";
+              config = {
+                role_id_file_path = cfg.role-id;
+                secret_id_file_path = cfg.secret-id;
+                remove_secret_id_file_after_reading = false;
+              };
+            }
+          ];
         };
       };
       secrets = {
