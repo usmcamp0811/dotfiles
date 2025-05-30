aade515a778167f3b240680566f94191ade841e3
Matt Camp <matt@aicampground.com>
2025-05-29 20:04:34 -0500

    wip

--- modules/home/cli/zsh/default.nix
+++ modules/home/cli/zsh/default.nix
@@ -31,23 +31,26 @@ in
 
       shellAliases = { update = "sudo nixos-rebuild switch"; };
 
-      oh-my-zsh = {
-        enable = true;
-        theme = "fino";
-        plugins = [ "fzf" ];
-      };
+      # oh-my-zsh = {
+      #   enable = true;
+      #   theme = "fino";
+      #   plugins = ["fzf"];
+      # };
       initContent = lib.mkBefore ''
         source $HOME/.config/shell/zsh/fino.zsh-theme
+
         ${lib.concatMapStringsSep "\n"
           (file: ''[ -r "${file}" ] && source "${file}"'')
           cfg.extraSource}
         [ -r "/var/lib/vault/users/${config.campground.user.name}/passwords" ] && source "/var/lib/vault/users/${config.campground.user.name}/passwords"
-        bindkey -v
 
         for file in ~/.config/shell/private/*.shrc(N); do
           [ -r "$file" ] && source "$file"
         done
         ${pkgs.campground.campfetch}/bin/campfetch
+        bindkey -v
+        bindkey '^?' backward-delete-char          # backspace in insert mode
+        bindkey -M vicmd 'v' edit-command-line     # open $EDITOR in command mode
       '';
     };
     # TODO: Move the aliases.shrc into a nix file so if programs are called in there they are for sure installed and have the correct path
