b2aa37002f4120c84de657f137621f8148c3ac2f
Matt Camp <matt@aicampground.com>
2025-06-01 08:54:02 -0500

    thanks to /u/DaymanTargaryen for letting me know I can't spell :-D

--- modules/home/cli/env/default.nix
+++ modules/home/cli/env/default.nix
@@ -1,28 +1,31 @@
-{ inputs, options, config, pkgs, lib, ... }:
+{ inputs
+, options
+, config
+, pkgs
+, lib
+, ...
+}:
 with lib;
-with lib.campground;
-let
+with lib.campground; let
   cfg-user = config.campground.user;
   is-darwin = pkgs.stdenv.isDarwin;
 
   # aliases = import ./aliases.nix { inherit pkgs; };
   home-directory =
-    if cfg-user.name == null then
-      null
-    else if is-darwin then
-      "/Users/${cfg-user.name}"
-    else
-      "/home/${cfg-user.name}";
+    if cfg-user.name == null
+    then null
+    else if is-darwin
+    then "/Users/${cfg-user.name}"
+    else "/home/${cfg-user.name}";
 in
 {
   options.campground.cli.env = with types;
     mkOption {
       type = attrsOf (oneOf [ str path (listOf (either str path)) ]);
       apply = mapAttrs (_n: v:
-        if isList v then
-          concatMapStringsSep ":" (x: toString x) v
-        else
-          (toString v));
+        if isList v
+        then concatMapStringsSep ":" (x: toString x) v
+        else (toString v));
       default = { };
       description = "A set of environment variables to set.";
     };
@@ -42,34 +45,27 @@ in
       XDG_DATA_HOME = "${home-directory}/.local/share";
       XDG_BIN_HOME = "$HOME/.local/bin";
       TMUX_TMPDIR = "$XDG_RUNTIME_DIR";
-      NODE_REPL_HISTORY =
-        "${config.home.sessionVariables.XDG_DATA_HOME}/node_repl_history";
+      NODE_REPL_HISTORY = "${config.home.sessionVariables.XDG_DATA_HOME}/node_repl_history";
       NVM_DIR = "${config.home.sessionVariables.XDG_DATA_HOME}/nvm";
       PYLINTHOME = "$XDG_CACHE_HOME/pylint";
-      XDG_CACHE_HOME = mkDefault "$HOME/.cahce";
-      PYTHON_EGG_CACHE =
-        "${config.home.sessionVariables.XDG_CACHE_HOME}/python-eggs";
+      XDG_CACHE_HOME = mkDefault "$HOME/.cache";
+      PYTHON_EGG_CACHE = "${config.home.sessionVariables.XDG_CACHE_HOME}/python-eggs";
       WGETRC = "${config.home.sessionVariables.XDG_CONFIG_HOME}/wgetrc";
       CARGO_HOME = "${config.home.sessionVariables.XDG_DATA_HOME}/cargo";
       MANPAGER = "sh -c 'col -bx | ${pkgs.bat}/bin/bat -l man -p'";
       IPYTHONDIR = "${config.home.sessionVariables.XDG_CONFIG_HOME}/jupyter";
-      JUPYTER_CONFIG_DIR =
-        "${config.home.sessionVariables.XDG_CONFIG_HOME}/jupyter";
+      JUPYTER_CONFIG_DIR = "${config.home.sessionVariables.XDG_CONFIG_HOME}/jupyter";
       GOPATH = "${config.home.sessionVariables.XDG_DATA_HOME}/go";
       JULIA_EDITOR = "nvim";
       JULIA_NUM_THREADS = "12";
-      JULIA_LOAD_PATH =
-        "${config.home.sessionVariables.XDG_CONFIG_HOME}/julia:$julia_load_path";
-      JULIA_DEPOT_PATH =
-        "${config.home.sessionVariables.XDG_CONFIG_HOME}/julia:$julia_depot_path";
+      JULIA_LOAD_PATH = "${config.home.sessionVariables.XDG_CONFIG_HOME}/julia:$julia_load_path";
+      JULIA_DEPOT_PATH = "${config.home.sessionVariables.XDG_CONFIG_HOME}/julia:$julia_depot_path";
       SSB_HOME = "${config.home.sessionVariables.XDG_DATA_HOME}/zoom";
       POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD = "true";
       CONDARC = "${config.home.sessionVariables.XDG_CONFIG_HOME}/conda/condarc";
     };
-    home.activation.privateDir =
-      inputs.home-manager.lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        mkdir -p "${config.home.homeDirectory}/.config/shell/private/"
-      '';
-
+    home.activation.privateDir = inputs.home-manager.lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      mkdir -p "${config.home.homeDirectory}/.config/shell/private/"
+    '';
   };
 }
