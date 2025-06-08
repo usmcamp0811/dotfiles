2bd1dcfb243f9638711bc114c940f788e3428171
Matt Camp <matt@aicampground.com>
2025-06-08 11:21:18 -0500

    make cf common

--- modules/nixos/suites/common/default.nix
+++ modules/nixos/suites/common/default.nix
@@ -1,8 +1,13 @@
-{ options, config, lib, ... }:
+{ options
+, config
+, lib
+, ...
+}:
 with lib;
-with lib.campground;
-let cfg = config.campground.suites.common;
-in {
+with lib.campground; let
+  cfg = config.campground.suites.common;
+in
+{
   options.campground.suites.common = with types; {
     enable = mkBoolOpt false "Whether or not to enable common configuration.";
   };
@@ -33,13 +38,20 @@ in {
       };
 
       services = {
+        crystal-forge = {
+          enable = true;
+          client = {
+            enable = true;
+            server_host = "reckless";
+            server_port = 3444;
+          };
+        };
         openssh = {
           enable = true;
           authorizedKeys = [
             "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGw+o+9F4kz+dYyI2I4WudgKjyFOK+L0QW4LhxkG4sMt gitlab-runner@aicampground.com"
             "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKdMWMFyi7Lvjm78KOX3tKZ5bkEZ7bHA56ZKKtTb9wIo mcamp@aicampground.com"
             "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAclfREva2i4LsnBQPY3ZSsZzeuS5DGn11u0abBR8cFv mcamp@butler"
-
           ];
         };
       };
