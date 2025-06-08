e44489a715ae6338a4503aa95e9d1cd0eb341a57
Matt Camp <matt@aicampground.com>
2025-06-08 11:21:05 -0500

    updating to pull key from vault

--- modules/nixos/archetypes/server/default.nix
+++ modules/nixos/archetypes/server/default.nix
@@ -1,8 +1,14 @@
-{ options, config, lib, pkgs, ... }:
+{ options
+, config
+, lib
+, pkgs
+, ...
+}:
 with lib;
-with lib.campground;
-let cfg = config.campground.archetypes.server;
-in {
+with lib.campground; let
+  cfg = config.campground.archetypes.server;
+in
+{
   options.campground.archetypes.server = with types; {
     enable = mkBoolOpt false "Whether or not to enable the server archetype.";
     k8s = mkBoolOpt false "Is this a K8s Node?";
--- modules/nixos/services/crystal-forge/default.nix
+++ modules/nixos/services/crystal-forge/default.nix
@@ -63,7 +63,27 @@ in
         type = types.port;
         default = 3000;
       };
-      private_key = mkOption { type = types.path; };
+    };
+    role-id =
+      mkOpt types.str
+        config.campground.services.vault-agent.settings.vault.role-id
+        "Absolute path to the Vault role-id";
+    secret-id =
+      mkOpt types.str
+        config.campground.services.vault-agent.settings.vault.secret-id
+        "Absolute path to the Vault secret-id";
+    vault-path =
+      mkOpt types.str "secret/campground/crystal-forge"
+        "The Vault path to the KV containing the KVs that are for each database";
+    kvVersion = mkOption {
+      type = types.enum [ "v1" "v2" ];
+      default = "v2";
+      description = "KV store version";
+    };
+    vault-address = mkOption {
+      type = types.str;
+      default = config.campground.services.vault-agent.settings.vault.address;
+      description = "The address of your Vault";
     };
   };
 
@@ -75,8 +95,50 @@ in
         configPath
         database
         server
-        client
         ;
+      client = {
+        inherit (cfg.client) server_port server_host enable;
+        private_key = "/tmp/detsys-vault/${config.networking.hostName}";
+      };
+    };
+    campground.services = {
+      vault-agent = {
+        services = {
+          "crystal-forge-agent" = {
+            settings = {
+              vault.address = cfg.vault-address;
+              auto_auth = {
+                method = [
+                  {
+                    type = "approle";
+                    config = {
+                      role_id_file_path = cfg.role-id;
+                      secret_id_file_path = cfg.secret-id;
+                      remove_secret_id_file_after_reading = false;
+                    };
+                  }
+                ];
+              };
+            };
+            secrets = {
+              file = {
+                files = {
+                  "${config.networking.hostName}.key" = {
+                    text = ''
+                      {{ with secret "${cfg.vault-path}" }}{{ if eq "${cfg.kvVersion}" "v1" }}{{ .Data.${
+                        config.networking.hostName
+                      }}{{ else }}{{ .Data.data.${
+                        config.networking.hostName
+                      } }}{{ end }}{{ end }}'';
+                    permissions = "0600";
+                    change-action = "restart";
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
     };
   };
 }
