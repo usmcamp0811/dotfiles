890635d870d836d4e1798e3c6080d277b5ac7799
Matt Camp <matt@aicampground.com>
2025-06-14 13:30:13 -0500

    just a cf test

--- checks/example-flask-app/default.nix
+++ checks/example-flask-app/default.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 
 pkgs.runCommand "example-flask-app" { src = ./.; } ''
+
   mkdir -p $out
   ${pkgs.campground.example-flask-app.python}/bin/python -c "import flask; print(flask.__version__)" > $out/results.txt
 ''
