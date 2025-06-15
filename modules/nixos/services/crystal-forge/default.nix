e7934e6c7c901b7781b1c26ff6c0d9410d54af1d
Matt Camp <matt@aicampground.com>
2025-06-15 11:02:45 -0500

    update log level option

--- modules/nixos/services/crystal-forge/default.nix
+++ modules/nixos/services/crystal-forge/default.nix
@@ -1,16 +1,20 @@
-{
-  lib,
-  config,
-  pkgs,
-  ...
+{ lib
+, config
+, pkgs
+, ...
 }:
 with lib;
 with lib.campground; let
   cfg = config.campground.services.crystal-forge;
   host = config.networking.hostName;
-in {
+in
+{
   options.campground.services.crystal-forge = {
     enable = mkEnableOption "Enable the Crystal Forge service(s)";
+    log_level = lib.mkOption {
+      type = lib.types.enum [ "off" "error" "warn" "info" "debug" "trace" ];
+      default = "debug";
+    };
     configPath = mkOption {
       type = types.path;
       default = generatedConfigPath;
@@ -51,7 +55,7 @@ in {
       };
       authorized_keys = mkOption {
         type = types.attrsOf types.str;
-        default = {};
+        default = { };
       };
     };
     client = {
@@ -68,23 +72,23 @@ in {
     flakes = {
       watched = lib.mkOption {
         type = lib.types.attrsOf lib.types.str;
-        default = {dotfiles = "git+https://gitlab.com/usmcamp0811/dotfiles";};
+        default = { dotfiles = "git+https://gitlab.com/usmcamp0811/dotfiles"; };
         description = "Flakes to watch and auto-track (name → repo_url).";
       };
     };
     role-id =
       mkOpt types.str
-      config.campground.services.vault-agent.settings.vault.role-id
-      "Absolute path to the Vault role-id";
+        config.campground.services.vault-agent.settings.vault.role-id
+        "Absolute path to the Vault role-id";
     secret-id =
       mkOpt types.str
-      config.campground.services.vault-agent.settings.vault.secret-id
-      "Absolute path to the Vault secret-id";
+        config.campground.services.vault-agent.settings.vault.secret-id
+        "Absolute path to the Vault secret-id";
     vault-path =
       mkOpt types.str "secret/campground/crystal-forge"
-      "The Vault path to the KV containing the KVs that are for each database";
+        "The Vault path to the KV containing the KVs that are for each database";
     kvVersion = mkOption {
-      type = types.enum ["v1" "v2"];
+      type = types.enum [ "v1" "v2" ];
       default = "v2";
       description = "KV store version";
     };
@@ -96,11 +100,11 @@ in {
   };
 
   config = mkIf cfg.enable {
-
     services.crystal-forge = {
       inherit
         (cfg)
         enable
+        log_level
         configPath
         database
         server
