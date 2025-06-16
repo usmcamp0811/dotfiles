ab8d9c963488c487277e667ec625dfca6a14ba0b
Matt Camp <matt@aicampground.com>
2025-06-15 19:06:05 -0500

    so grafana can get to the data

--- systems/x86_64-linux/reckless/default.nix
+++ systems/x86_64-linux/reckless/default.nix
@@ -286,6 +286,7 @@ in
           "local all root trust"
           "local all postgres peer"
           "local atticd atticd trust"
+          "host  crystal_forge  grafana  10.8.0.0/24  trust"
           # "host  campgroundai  campgroundai  0.0.0.0/0 md5"
           "host  all  all  0.0.0.0/0  reject"
           "host  all  all  ::0/0  reject"
