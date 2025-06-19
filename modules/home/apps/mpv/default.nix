fdbefcbed203353c2481db5821190a3bfc50c4b1
Matt Camp <matt@aicampground.com>
2025-06-19 08:55:35 -0500

    remove this alias cause its not something i have workignright now

--- modules/home/apps/mpv/default.nix
+++ modules/home/apps/mpv/default.nix
@@ -1,4 +1,8 @@
-{ lib, pkgs, config, ... }:
+{ lib
+, pkgs
+, config
+, ...
+}:
 let
   inherit (lib) mkEnableOption mkIf;
 
@@ -8,11 +12,10 @@ in
   options.campground.apps.mpv = { enable = mkEnableOption "mpv"; };
 
   config = mkIf cfg.enable {
-
-    campground.cli.aliases = {
-      mpv =
-        "${pkgs.devour}/bin/devour ${pkgs.mpv}/bin/mpv --script=$HOME/.config/mpv/scripts/mpv-cheatsheet.js -ao=pipewire";
-    };
+    # campground.cli.aliases = {
+    #   mpv =
+    #     "${pkgs.devour}/bin/devour ${pkgs.mpv}/bin/mpv --script=$HOME/.config/mpv/scripts/mpv-cheatsheet.js -ao=pipewire";
+    # };
 
     programs.mpv = { enable = true; };
   };
