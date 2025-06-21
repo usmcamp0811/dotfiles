c3c6dd762c690c28ff84276f41ebbf76e96374e0
Matt Camp <matt@aicampground.com>
2025-06-21 13:13:58 -0500

    wip

--- systems/x86_64-linux/reckless/default.nix
+++ systems/x86_64-linux/reckless/default.nix
@@ -288,6 +288,8 @@ in
           "local atticd atticd trust"
           "host  crystal_forge  grafana  10.8.0.0/24  trust"
           "host  crystal_forge  crystal_forge  127.0.0.1/32  trust"
+          "host  postgres  crystal_forge  127.0.0.1/32  trust"
+          "host  postgres  crystal_forge  ::1/128       trust"
           # "host  campgroundai  campgroundai  0.0.0.0/0 md5"
           "host  all  all  0.0.0.0/0  reject"
           "host  all  all  ::0/0  reject"
