472c6889515b4a1566e631ff8f55ee1395c63836
Matt Camp <matt@aicampground.com>
2025-07-19 19:06:33 -0500

    substitutotrs

--- modules/nixos/suites/common/default.nix
+++ modules/nixos/suites/common/default.nix
@@ -19,14 +19,9 @@ in
       nix = {
         enable = true;
         extra-substituters = {
-          # Core NixOS cache (you might already have this as default)
+          # Core NixOS cache
           "https://cache.nixos.org" = {
             key = "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=";
-          };
-
-          # Hyprland cache (for your Wayland setup)
-          "https://hyprland.cachix.org" = {
-            key = "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=";
           };
 
           # Nix community cache
@@ -34,9 +29,39 @@ in
             key = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
           };
 
-          # NUR cache - KEY for Firefox addons!
-          "https://nur.cachix.org" = {
-            key = "nur.cachix.org-1:WG6ry5dehFKjDzEkgxcGdQQGt1IRbJLtL0F6bJwfnr8=";
+          # CUDA maintainers cache
+          "https://cuda-maintainers.cachix.org" = {
+            key = "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E=";
+          };
+
+          # Nixpkgs Wayland cache
+          "https://nixpkgs-wayland.cachix.org" = {
+            key = "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA=";
+          };
+
+          # Hyprland cache
+          "https://hyprland.cachix.org" = {
+            key = "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=";
+          };
+
+          # Nixpkgs Python cache
+          "https://nixpkgs-python.cachix.org" = {
+            key = "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU=";
+          };
+
+          # Anyrun cache (from your second config)
+          "https://anyrun.cachix.org" = {
+            key = "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s=";
+          };
+
+          # Yazi cache (from your second config)
+          "https://yazi.cachix.org" = {
+            key = "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k=";
+          };
+
+          # Numtide cache
+          "https://numtide.cachix.org" = {
+            key = "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE=";
           };
         };
       };
