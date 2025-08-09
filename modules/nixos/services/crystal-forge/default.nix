9696e04a0f841b3adf44ef8b57f8f2073319a0c3
Matt Camp <matt@aicampground.com>
2025-08-09 12:42:18 -0500

    wip

--- modules/nixos/services/crystal-forge/default.nix
+++ modules/nixos/services/crystal-forge/default.nix
@@ -351,21 +351,21 @@ in {
         cp /tmp/detsys-vault/agent.key /var/lib/crystal-forge/agent.key
       '';
     };
-    systemd.services.crystal-forge-server.serviceConfig = {
-      MemoryMax = lib.mkForce "95%"; # Leave 5% for OS
-      MemoryHigh = lib.mkForce "85%"; # Start throttling early
-      MemorySwapMax = lib.mkForce "10G"; # Minimal swap usage
-      OOMScoreAdjust = lib.mkForce "-1000"; # Maximum protection from OOM killer
-      OOMPolicy = "continue"; # Don't kill the service on OOM
-    };
-    systemd.services.crystal-forge-server.environment = {
-      GC_INITIAL_HEAP_SIZE = "128M";
-      GC_MAX_HEAP_SIZE = "12G";
-    };
-    systemd.services.crystal-forge-builder.environment = {
-      GC_INITIAL_HEAP_SIZE = "128M";
-      GC_MAX_HEAP_SIZE = "12G";
-    };
+    # systemd.services.crystal-forge-server.serviceConfig = {
+    #   MemoryMax = lib.mkForce "95%"; # Leave 5% for OS
+    #   MemoryHigh = lib.mkForce "85%"; # Start throttling early
+    #   MemorySwapMax = lib.mkForce "10G"; # Minimal swap usage
+    #   OOMScoreAdjust = lib.mkForce "-1000"; # Maximum protection from OOM killer
+    #   OOMPolicy = "continue"; # Don't kill the service on OOM
+    # };
+    # systemd.services.crystal-forge-server.environment = {
+    #   GC_INITIAL_HEAP_SIZE = "128M";
+    #   GC_MAX_HEAP_SIZE = "12G";
+    # };
+    # systemd.services.crystal-forge-builder.environment = {
+    #   GC_INITIAL_HEAP_SIZE = "128M";
+    #   GC_MAX_HEAP_SIZE = "12G";
+    # };
     systemd.services.crystal-forge-builder.serviceConfig = {
       OOMScoreAdjust = lib.mkForce "-1000"; # Maximum protection from OOM killer
       OOMPolicy = "continue"; # Don't kill the service on OOM
@@ -393,9 +393,6 @@ in {
       # Process Limits
       LimitNOFILE = lib.mkForce "1048576"; # High file descriptor limit
       LimitNPROC = lib.mkForce "32768"; # High process limit
-
-      # Optional: Pin to specific cores if you have many
-      # CPUAffinity = lib.mkForce "0-7"; # Use first 8 cores
     };
 
     # systemd.services.crystal-forge-server.preStart = ''
