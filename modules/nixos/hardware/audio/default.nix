9b541db649c0850565eb6a8598e076de7a2e0229
Matt Camp <matt@aicampground.com>
2025-09-13 19:08:12 -0500

    fix spotty audio

--- modules/nixos/hardware/audio/default.nix
+++ modules/nixos/hardware/audio/default.nix
@@ -1,29 +1,21 @@
-{ options
-, config
-, pkgs
-, lib
-, ...
+# modules/campground/hardware/audio.nix
+{
+  options,
+  config,
+  pkgs,
+  lib,
+  ...
 }:
-# @FIXME(jakehamilton): The transition to wireplumber from media-session has completely
-# broken my setup. I'll need to invest some time to figure out how to override Alsa things
-# again...
 with lib;
 with lib.campground; let
   cfg = config.campground.hardware.audio;
-in
-{
+in {
   options.campground.hardware.audio = with types; {
     enable = mkBoolOpt false "Whether or not to enable audio support.";
-    alsa-monitor = mkOpt attrs { } "Alsa configuration.";
-    nodes =
-      mkOpt (listOf attrs) [ ]
-        "Audio nodes to pass to Pipewire as `context.objects`.";
-    modules =
-      mkOpt (listOf attrs) [ ]
-        "Audio modules to pass to Pipewire as `context.modules`.";
-    extra-packages =
-      mkOpt (listOf package) [ pkgs.qjackctl pkgs.easyeffects ]
-        "Additional packages to install.";
+    alsa-monitor = mkOpt attrs {} "Alsa configuration.";
+    nodes = mkOpt (listOf attrs) [] "Audio nodes to pass to Pipewire as `context.objects`.";
+    modules = mkOpt (listOf attrs) [] "Audio modules to pass to Pipewire as `context.modules`.";
+    extra-packages = mkOpt (listOf package) [pkgs.qjackctl pkgs.easyeffects] "Additional packages to install.";
   };
 
   config = mkIf cfg.enable {
@@ -34,47 +26,56 @@ in
       alsa.enable = true;
       pulse.enable = true;
       jack.enable = true;
-
       wireplumber.enable = true;
+
+      # PipeWire clock/rate/quantum to prevent HDMI underruns
+      extraConfig.pipewire."99-hdmi-tweaks.conf".context.properties = {
+        default.clock.rate = 48000;
+        default.clock.allowed-rates = [48000];
+        default.clock.quantum = 1024;
+        default.clock.min-quantum = 1024;
+        default.clock.max-quantum = 2048;
+      };
+
+      # WirePlumber: no auto-suspend + lower per-app latency for Brave
+      wireplumber.extraConfig."51-no-suspend.lua" = ''
+        alsa_monitor = alsa_monitor or {}
+        alsa_monitor.rules = alsa_monitor.rules or {}
+
+        table.insert(alsa_monitor.rules, {
+          matches = { { { "node.name", "matches", "alsa_output.*" } } },
+          apply_properties = { ["session.suspend-timeout-seconds"] = 0 }
+        })
+
+        table.insert(alsa_monitor.rules, {
+          matches = { { { "application.name", "matches", "Brave*" } } },
+          apply_properties = {
+            ["node.pause-on-idle"] = false,
+            ["node.latency"] = "1024/48000"
+          }
+        })
+      '';
     };
 
-    # environment.etc = {
-    #   "pipewire/pipewire.conf.d/10-pipewire.conf".source =
-    #     pkgs.writeText "pipewire.conf" (builtins.toJSON pipewire-config);
-    #   # "pipewire/pipewire.conf.d/21-alsa.conf".source =
-    #   #   pkgs.writeText "pipewire.conf" (builtins.toJSON alsa-config);
-    #   #
-    #   # "wireplumber/wireplumber.conf".source =
-    #   #   pkgs.writeText "pipewire.conf" (builtins.toJSON pipewire-config);
-    #   #
-    #   # "wireplumber/scripts/config.lua.d/alsa.lua".text = ''
-    #   #   local input = ${lua-format.generate "sample.lua" cfg.alsa-monitor}
-    #   #
-    #   #   if input.rules == nil then
-    #   #    input.rules = {}
-    #   #   end
-    #   #
-    #   #   local rules = input.rules
-    #   #
-    #   #   for _, rule in ipairs(input.rules) do
-    #   #     table.insert(alsa_monitor.rules, rule)
-    #   #   end
-    #   # '';
-    # };
-
+    # Disable legacy PulseAudio daemon
     services.pulseaudio.enable = mkForce false;
 
-    environment.systemPackages = with pkgs;
-      [ pulsemixer pavucontrol ] ++ cfg.extra-packages;
+    # Stop HDA HDMI codec from idling
+    boot.kernelParams = [
+      "snd_hda_intel.power_save=0"
+      "snd_hda_intel.power_save_controller=0"
+    ];
 
-    campground.user.extraGroups = [ "audio" ];
+    environment.systemPackages = with pkgs; [pulsemixer pavucontrol] ++ cfg.extra-packages;
+
+    campground.user.extraGroups = ["audio"];
 
     campground.home.extraOptions = {
       systemd.user.services.mpris-proxy = {
         Unit.Description = "Mpris proxy";
-        Unit.After = [ "network.target" "sound.target" ];
+        Unit.After = ["network.target" "sound.target"];
         Service.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
-        Install.WantedBy = [ "default.target" ];
+        Install.WantedBy = ["default.target"];
       };
     };
   };
