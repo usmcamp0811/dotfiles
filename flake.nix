e77a8de04ffdd1670a66c17dda225724e6b59128
Matt Camp <matt@aicampground.com>
2025-09-28 21:21:02 -0500

    wip

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     services-flake.url = "github:juspay/services-flake";
 
     crystal-forge = {
-      url = "git+ssh://git@gitlab.com/crystal-forge/crystal-forge";
+      url = "git+ssh://git@gitlab.com/crystal-forge/crystal-forge.git?ref=fix-tests";
       inputs.nixpkgs.follows = "unstable";
     };
 
@@ -154,7 +154,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    technofab = { url = "gitlab:TECHNOFAB/nix-packages"; };
+    technofab = {url = "gitlab:TECHNOFAB/nix-packages";};
     technofab.inputs.nixpkgs.follows = "nixpkgs";
 
     # GPG default configuration
@@ -297,23 +297,22 @@
     };
   };
 
-  outputs = inputs:
-    let
-      inherit (inputs) deploy-rs;
+  outputs = inputs: let
+    inherit (inputs) deploy-rs;
 
-      lib = inputs.snowfall-lib.mkLib {
-        inherit inputs;
-        src = ./.;
-        snowfall = {
-          meta = {
-            name = "campground";
-            title = "AI Campground";
-          };
-
-          namespace = "campground";
+    lib = inputs.snowfall-lib.mkLib {
+      inherit inputs;
+      src = ./.;
+      snowfall = {
+        meta = {
+          name = "campground";
+          title = "AI Campground";
         };
+
+        namespace = "campground";
       };
-    in
+    };
+  in
     lib.mkFlake {
       channels-config = {
         allowUnfree = true;
@@ -365,17 +364,17 @@
         nixos-hardware.nixosModules.lenovo-thinkpad-p1
         nixos-hardware.nixosModules.lenovo-thinkpad-p53
       ];
-      systems.hosts.gray.modules = with inputs; [ nixos-hardware.nixosModules.framework-16-7040-amd ];
+      systems.hosts.gray.modules = with inputs; [nixos-hardware.nixosModules.framework-16-7040-amd];
 
       # Fixed bug in Amazon image builder: https://github.com/nix-community/nixos-generators/issues/150
-      systems.hosts.base.modules = [ ({ ... }: { amazonImage.sizeMB = 32 * 1024; }) ];
+      systems.hosts.base.modules = [({...}: {amazonImage.sizeMB = 32 * 1024;})];
 
-      deploy = lib.mkDeploy { inherit (inputs) self; };
+      deploy = lib.mkDeploy {inherit (inputs) self;};
 
       checks =
         builtins.mapAttrs
-          (_system: deploy-lib: deploy-lib.deployChecks inputs.self.deploy)
-          deploy-rs.lib;
+        (_system: deploy-lib: deploy-lib.deployChecks inputs.self.deploy)
+        deploy-rs.lib;
 
       outputs-builder = channels: {
         # this needs to be `hooks` not `checks` because `checks` will get run with `deploy` and
@@ -393,7 +392,7 @@
         };
         nixidyEnvs = inputs.nixidy.lib.mkEnvs {
           pkgs = channels.nixpkgs;
-          envs = { dev.modules = [ ./kubernetes/dev.nix ]; };
+          envs = {dev.modules = [./kubernetes/dev.nix];};
         };
       };
 
