5210d3466f364f47615e74a7a89c30c2be95cd7a
Matt Camp <matt@aicampground.com>
2025-10-29 07:50:47 -0500

    wip

--- modules/nixos/cache/campground/default.nix
+++ modules/nixos/cache/campground/default.nix
@@ -13,7 +13,7 @@ in {
   config = mkIf cfg.enable {
     campground.nix.extra-substituters = {
       # "https://attic.aicampground.com/campground".key = "campground:XZ6LmOgWmChUUb5ZWWn/XnTreAYaNcPTQHxUR3T3dc8=";
-      "s3://nix-cache?endpoint=https://s3-api.lan.aicampground.com&region=us-east-1".key = "campground-cache:rEvsSt+le2/YWewNPH+LNhWHPAqZwONk3VSoei307u4=";
+      "s3://nix-cache?endpoint=https://s3-api.lan.aicampground.com&region=us-east-1".key = "campground:Us1XpC1L8ruQ7UoBBfZqrQravKKTVLIijcuhJ2bK6H4=";
     };
   };
 }
