0a94627b4d1581899570bb257261bf3e6d43ac20
Matt Camp <matt@aicampground.com>
2025-11-27 17:33:26 -0600

    fix nixos-hosts

--- packages/scripts/nixos-hosts/default.nix
+++ packages/scripts/nixos-hosts/default.nix
@@ -11,12 +11,9 @@
   inherit (lib) mapAttrsToList concatStringsSep;
   inherit (lib.campground) override-meta;
 
-  # substituteAll -> replaceVars
+  # replacement helper using lib.replaceVars (expects @var@ in the template)
   substitute = args: let
-    # Read the template file
     src = builtins.readFile args.src;
-
-    # Everything except src becomes a variable
     vars =
       builtins.mapAttrs (_: v: toString v)
       (builtins.removeAttrs args ["src"]);
@@ -25,10 +22,21 @@
 
   formatted-hosts = mapAttrsToList (name: host: "${name},${host.pkgs.system}") hosts;
 
+  # Normal CSV with header + rows (if any)
   hosts-csv = writeText "hosts.csv" ''
     Name,System
     ${concatStringsSep "\n" formatted-hosts}
   '';
+
+  # Always give the script a real file, even if there are no hosts
+  hostsFile =
+    if hosts == {}
+    then
+      # header-only CSV; your script can still handle “no data rows”
+      writeText "hosts-empty.csv" ''
+        Name,System
+      ''
+    else hosts-csv;
 
   nixos-hosts = writeShellApplication {
     name = "nixos-hosts";
@@ -37,12 +45,10 @@
       src = ./nixos-hosts.sh;
 
       help = ./help;
-      hosts =
-        if hosts == {}
-        then ""
-        else hosts-csv;
+      hosts = hostsFile;
     };
 
+    # don’t run any checks that might exec the script at build time
     checkPhase = "";
 
     runtimeInputs = [gum];
