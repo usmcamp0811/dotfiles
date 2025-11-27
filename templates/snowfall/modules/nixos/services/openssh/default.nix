5789a21491bacb40fab61fdf088cfd128108a38e
Matt Camp <matt@aicampground.com>
2025-11-27 10:46:35 -0600

    update ssh module

--- templates/snowfall/modules/nixos/services/openssh/default.nix
+++ templates/snowfall/modules/nixos/services/openssh/default.nix
@@ -1,7 +1,13 @@
-{ config, lib, host ? "", format ? "", inputs ? { }, ... }:
+{
+  config,
+  lib,
+  host ? "",
+  format ? "",
+  inputs ? {},
+  ...
+}:
 with lib;
-with lib.namespace-change-me;
-let
+with lib.namespace-change-me; let
   cfg = config.namespace-change-me.services.openssh;
 
   user = config.users.users.${config.namespace-change-me.user.name};
@@ -11,40 +17,40 @@ let
   # the specialArg `name` to provide the host name.
   name = host;
 
-  default-key =
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAclfREva2i4LsnBQPY3ZSsZzeuS5DGn11u0abBR8cFv mcamp@butler";
+  default-key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAclfREva2i4LsnBQPY3ZSsZzeuS5DGn11u0abBR8cFv mcamp@butler";
 
-  other-hosts = lib.filterAttrs (key: host:
-    key != name && (host.config.namespace-change-me.user.name or null) != null)
-    ((inputs.self.nixosConfigurations or { })
-      // (inputs.self.darwinConfigurations or { }));
+  other-hosts =
+    lib.filterAttrs (key: host:
+      key != name && (host.config.namespace-change-me.user.name or null) != null)
+    ((inputs.self.nixosConfigurations or {})
+      // (inputs.self.darwinConfigurations or {}));
 
-  other-hosts-config = lib.concatMapStringsSep "\n" (name:
-    let
-      remote = other-hosts.${name};
-      remote-user-name = remote.config.namespace-change-me.user.name;
-      remote-user-id =
-        builtins.toString remote.config.users.users.${remote-user-name}.uid;
+  other-hosts-config = lib.concatMapStringsSep "\n" (name: let
+    remote = other-hosts.${name};
+    remote-user-name = remote.config.namespace-change-me.user.name;
+    remote-user-id =
+      builtins.toString remote.config.users.users.${remote-user-name}.uid;
 
-      forward-gpg = optionalString (config.programs.gnupg.agent.enable
-        && remote.config.programs.gnupg.agent.enable) ''
-          RemoteForward /run/user/${remote-user-id}/gnupg/S.gpg-agent /run/user/${user-id}/gnupg/S.gpg-agent.extra
-          RemoteForward /run/user/${remote-user-id}/gnupg/S.gpg-agent.ssh /run/user/${user-id}/gnupg/S.gpg-agent.ssh
-        '';
-    in ''
-      Host ${name}
-        User ${remote-user-name}
-        ForwardAgent yes
-        Port ${builtins.toString cfg.port}
-        ${forward-gpg}
-    '') (builtins.attrNames other-hosts);
+    forward-gpg = optionalString (config.programs.gnupg.agent.enable
+      && remote.config.programs.gnupg.agent.enable) ''
+      RemoteForward /run/user/${remote-user-id}/gnupg/S.gpg-agent /run/user/${user-id}/gnupg/S.gpg-agent.extra
+      RemoteForward /run/user/${remote-user-id}/gnupg/S.gpg-agent.ssh /run/user/${user-id}/gnupg/S.gpg-agent.ssh
+    '';
+  in ''
+    Host ${name}
+      User ${remote-user-name}
+      ForwardAgent yes
+      Port ${builtins.toString cfg.port}
+      ${forward-gpg}
+  '') (builtins.attrNames other-hosts);
 in {
   options.namespace-change-me.services.openssh = with types; {
     enable = mkBoolOpt false "Whether or not to configure OpenSSH support.";
     authorizedKeys =
-      mkOpt (listOf str) [ default-key ] "The public keys to apply.";
+      mkOpt (listOf str) [default-key] "The public keys to apply.";
     port = mkOpt port 2222 "The port to listen on (in addition to 22).";
-    manage-other-hosts = mkOpt bool true
+    manage-other-hosts =
+      mkOpt bool true
       "Whether or not to add other host configurations to SSH config.";
   };
 
@@ -52,38 +58,45 @@ in {
     services.openssh = {
       enable = true;
       settings = {
-        PermitRootLogin = mkForce
-          (if format == "install-iso" then "yes" else "prohibit-password");
-        # PermitRootLogin =
-        #   if format == "install-iso" then "yes" else "without-password";
-        # PasswordAuthentication = true;
-        # X11Forwarding = true;
-        # TODO: flip back to false when all is good
+        PermitRootLogin =
+          mkForce
+          (
+            if format == "install-iso"
+            then "yes"
+            else "prohibit-password"
+          );
       };
 
       extraConfig = ''
         StreamLocalBindUnlink yes
       '';
 
-      ports = [ 22 cfg.port ];
+      ports = [22 cfg.port];
     };
 
-    programs.ssh.forwardX11 = true;
-    programs.ssh.extraConfig = ''
-      Host *
-        HostKeyAlgorithms +ssh-rsa
+    programs.ssh = {
+      # New: explicitly control defaults instead of relying on implicit ones
+      enableDefaultConfig = false;
 
-      ${optionalString cfg.manage-other-hosts other-hosts-config}
-    '';
+      forwardX11 = true;
+
+      extraConfig = ''
+        Host *
+          HostKeyAlgorithms +ssh-rsa
+
+        ${optionalString cfg.manage-other-hosts other-hosts-config}
+      '';
+    };
 
     namespace-change-me.user.extraOptions.openssh.authorizedKeys.keys =
       cfg.authorizedKeys;
 
     namespace-change-me.home.extraOptions = {
       programs.zsh.shellAliases = foldl (aliases: system:
-        aliases // {
+        aliases
+        // {
           "ssh-${system}" = "ssh ${system} -t tmux a";
-        }) { } (builtins.attrNames other-hosts);
+        }) {} (builtins.attrNames other-hosts);
     };
   };
 }
