d43eee8c94d46b12ea1b275d787abe6fd288db2d
Matt Camp <matt@aicampground.com>
2025-11-26 22:49:38 -0600

    flake update 🤞🤞 don't this will just work

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
     terranix.url = "github:terranix/terranix";
     terranix.inputs.nixpkgs.follows = "nixpkgs";
     old-nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-    nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/release-25.11";
     pyarrow.url = "github:nixos/nixpkgs/e8b4c13b8d206f4b01e95499aa7425765a79513e";
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
@@ -113,7 +113,7 @@
 
     # Home Manager (release-24.11)
     # home-manager.url = "github:nix-community/home-manager/release-24.11";
-    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # Vault Integration
