9bbd4e5f8f363b36b85eb1f4a452c4cc68270ed0
Matt Camp <matt@aicampground.com>
2025-12-08 12:15:10 -0600

    vault enabled

--- systems/x86_64-linux/blue-ridge/default.nix
+++ systems/x86_64-linux/blue-ridge/default.nix
@@ -25,11 +25,21 @@ with lib.campground; {
       observability = enabled;
     };
     system = {
-      # passwds = enabled;
+      passwds = enabled;
     };
     services = {
       ntp = enabled;
       tang = enabled;
+      vault-agent = {
+        enable = true;
+        settings = {
+          vault = {
+            address = "https://vault.lan.aicampground.com";
+            role-id = "/var/lib/vault/blue-ridge/role-id";
+            secret-id = "/var/lib/vault/blue-ridge/secret-id";
+          };
+        };
+      };
       openssh = {
         authorizedKeys = [
           "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAGs9njLHA3yyrX6BTf5Z3Xj8jzOh9zVYfJoeai6WhmBtjr34KV0F79YKafvJPS4gasOTFpnKXObvBo0jG3/AIN+dwBohHtFtXSYBgZecFg847XoeN+7cIveqgI2Q1Jn2sFoUTzGiwKxqLRM7ZuTtRJGfoizOxlYHdyovus67jfDxewP5A== mcamp@Butler"
