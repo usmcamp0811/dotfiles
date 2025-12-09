7d06f369f366b3d361830e21eace33b5b78dd460
Matt Camp <matt@aicampground.com>
2025-12-08 19:09:33 -0600

    fix passwds

--- systems/x86_64-linux/blue-ridge/impermanence.nix
+++ systems/x86_64-linux/blue-ridge/impermanence.nix
@@ -38,6 +38,13 @@
       "/var/lib/systemd/random-seed"
       "/var/lib/vault/blue-ridge/role-id"
       "/var/lib/vault/blue-ridge/secret-id"
+
+      # Persist user password hashes
+      "/etc/shadow"
+      "/etc/passwd"
+      "/etc/group"
+      "/etc/subuid"
+      "/etc/subgid"
     ];
 
     users.admin = {
