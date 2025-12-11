fb50d7f85279a7f3b46606b080916c3d3f715970
Matt Camp <matt@aicampground.com>
2025-12-10 18:13:37 -0600

    wip

--- systems/x86_64-linux/blue-ridge/default.nix
+++ systems/x86_64-linux/blue-ridge/default.nix
@@ -22,7 +22,7 @@ with lib.campground; {
   # users.mutableUsers = true;
   # Use campground modules for configuration
   campground = {
-    cli = {zsh = enabled;};
+    cli = {zsh.root = enabled;};
     suites = {
       common = enabled;
       observability = enabled;
