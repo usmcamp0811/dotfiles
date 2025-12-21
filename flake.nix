8ce6c875fbdba6063a7465a924e8e12fb62f1d3c
Matt Camp <matt@aicampground.com>
2025-12-20 19:26:36 -0600

    wip

--- flake.nix
+++ flake.nix
@@ -380,8 +380,8 @@
       # ];
 
       systems.hosts.butler.modules = with inputs; [
-        # nixos-hardware.nixosModules.lenovo-thinkpad-p1
-        # nixos-hardware.nixosModules.lenovo-thinkpad-p53
+        nixos-hardware.nixosModules.lenovo-thinkpad-p1
+        nixos-hardware.nixosModules.lenovo-thinkpad-p53
       ];
 
       systems.hosts.gray.modules = with inputs; [nixos-hardware.nixosModules.framework-16-7040-amd];
