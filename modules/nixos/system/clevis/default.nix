31140e1702d74b3201aa7c8afc26128ebfbd64b3
Matt Camp <matt@aicampground.com>
2026-01-03 21:44:04 -0600

    rebert

--- modules/nixos/system/clevis/default.nix
+++ modules/nixos/system/clevis/default.nix
@@ -11,89 +11,37 @@ with lib.fmf; let
 in {
   options.fmf.system.clevis = with types; {
     enable = mkBoolOpt false "Whether or not to enable Clevis.";
-    mode = mkOption {
-      type = enum ["http-keyfile" "tang"];
-      default = "http-keyfile";
-      description = "Clevis mode: 'http-keyfile' for HTTP keyfile server, 'tang' for Tang server";
-    };
-
-    # Common options
     hostId = mkOpt str "12345678" "The output of head -c 8 /etc/machine-id";
-
-    # HTTP keyfile mode options
-    keyfile-url = mkOpt str "http://key-server:8080/zfs-keyfile" "The URL for the Clevis encrypted Keyfile";
-    luks-device = mkOpt str "/dev/nvme0n1p2" "LUKS device to unlock";
-    luks-name = mkOpt str "luks" "Name for the unlocked LUKS device";
-
-    # Tang mode options
-    tang-luks-device = mkOpt str "/dev/disk/by-partlabel/disk-main-luks" "LUKS device for Tang mode";
-    tang-luks-name = mkOpt str "crypted" "Name for Tang unlocked device";
+    keyfile-url =
+      mkOpt str "http://key-server:8080/zfs-keyfile"
+      "The URL for the Clevis encrypted Keyfile";
   };
 
-  config = mkIf cfg.enable (mkMerge [
-    # Common configuration
-    {
-      environment.systemPackages = with pkgs; [clevis];
-      boot.initrd.availableKernelModules = ["iwlwifi" "igc" "nfsv4"];
-      boot.kernelParams = ["ip=dhcp"];
-    }
-
-    # For non-systemd initrd (legacy)
-    (mkIf (!config.boot.initrd.systemd.enable) {
-      boot.initrd.network = {
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [clevis];
+    # Phase 1 NFS mount
+    boot.initrd.network = {
+      enable = true;
+      postCommands = ''
+        # mkdir -p /mnt/campfs
+        # mount -t nfs -o vers=4 10.8.0.140:/mnt/campfs /mnt/campfs
+        echo $(echo $(${pkgs.curl}/bin/curl -s $cfg.keyfile-url) | ${pkgs.clevis}/bin/clevis decrypt) > /luks.key
+        cat /luks.key
+        cryptsetup luksOpen --key-file /luks.key /dev/nvme0n1p2 luks
+      '';
+      ssh = {
         enable = true;
-        postCommands =
-          if cfg.mode == "tang" then ''
-            # Tang mode: use clevis to unlock directly
-            ${pkgs.clevis}/bin/clevis luks unlock -d ${cfg.tang-luks-device} -n ${cfg.tang-luks-name} || echo "Clevis/Tang unlock failed, falling back to password"
-          ''
-          else ''
-            # HTTP keyfile mode (original behavior)
-            echo $(echo $(${pkgs.curl}/bin/curl -s ${cfg.keyfile-url}) | ${pkgs.clevis}/bin/clevis decrypt) > /luks.key
-            cat /luks.key
-            cryptsetup luksOpen --key-file /luks.key ${cfg.luks-device} ${cfg.luks-name}
-          '';
-        ssh = {
-          enable = true;
-          port = 22;
-          shell = "/bin/cryptsetup-askpass";
-          authorizedKeys = [
-            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINLbrIDbLSEpfOc4onBP8y6aKCNEN5rEe0J3h7klfKzG mcamp@butler"
-          ];
-          hostKeys = ["/etc/ssh/ssh_host_rsa_key" "/etc/ssh/ssh_host_ed25519_key"];
-        };
+        port = 22;
+        shell = "/bin/cryptsetup-askpass";
+        authorizedKeys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINLbrIDbLSEpfOc4onBP8y6aKCNEN5rEe0J3h7klfKzG mcamp@butler"
+        ];
+        hostKeys = ["/etc/ssh/ssh_host_rsa_key" "/etc/ssh/ssh_host_ed25519_key"];
       };
-    })
-
-    # For systemd initrd
-    (mkIf config.boot.initrd.systemd.enable {
-      boot.initrd.systemd.network = {
-        enable = true;
-        networks."10-initrd" = {
-          matchConfig.Type = "ether";
-          networkConfig.DHCP = "yes";
-        };
-      };
-
-      boot.initrd.systemd.services.clevis-unlock = {
-        description = "Unlock LUKS with Clevis";
-        before = [ "cryptsetup-pre.target" ];
-        wants = [ "network-online.target" ];
-        after = [ "network-online.target" ];
-        unitConfig.DefaultDependencies = false;
-        script =
-          if cfg.mode == "tang" then ''
-            ${pkgs.clevis}/bin/clevis luks unlock -d ${cfg.tang-luks-device} -n ${cfg.tang-luks-name} || echo "Clevis/Tang unlock failed"
-          ''
-          else ''
-            ${pkgs.curl}/bin/curl -s ${cfg.keyfile-url} | ${pkgs.clevis}/bin/clevis decrypt > /luks.key
-            ${pkgs.cryptsetup}/bin/cryptsetup luksOpen --key-file /luks.key ${cfg.luks-device} ${cfg.luks-name}
-          '';
-        serviceConfig = {
-          Type = "oneshot";
-          RemainAfterExit = true;
-        };
-      };
-    })
-  ]);
+    };
+    # TODO: This should probably be parameterized and or not here because it could vary per system
+    # use this lspci -v | grep -iA8 'network\|ethernet' to then ask Chad what modules to use here
+    boot.initrd.availableKernelModules = ["iwlwifi" "igc" "nfsv4"];
+    boot.kernelParams = ["ip=dhcp"];
+  };
 }
