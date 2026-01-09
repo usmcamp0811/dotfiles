7103c68e56e2a4bafbcbd13ea3a1067dc1bfd730
Matt Camp <matt@aicampground.com>
2026-01-08 20:36:07 -0600

    iw[

--- modules/nixos/suites/desktop/default.nix
+++ modules/nixos/suites/desktop/default.nix
@@ -32,7 +32,7 @@ in
             wayland = true;
             sddmTheme = {
               enable = true;
-              name = "cyberpunk";  # You can change this to any of the available themes
+              # name can be overridden per-system
             };
           };
         };
