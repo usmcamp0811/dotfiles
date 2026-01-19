c1fc5a9ad9f08e6af1c0c518be4ee224a7dcabd0
Matt Camp <matt@aicampground.com>
2026-01-19 10:48:16 -0600

    wip

--- modules/nixos/services/k8s-control-plane/default.nix
+++ modules/nixos/services/k8s-control-plane/default.nix
@@ -151,6 +151,34 @@ in {
       };
     };
 
+    # Override tmpfiles rules to match custom data-dir
+    # The NixOS k3s module hardcodes /var/lib/rancher/k3s, but we use /var/lib/rancher
+    # So we need to create additional symlinks at the correct location
+    systemd.tmpfiles.settings."10-k3s-datadir-override" =
+      lib.mkIf (cfg.dataDir != "/var/lib/rancher/k3s") (
+        let
+          # Get the manifests and charts from the k3s service config
+          k3sCfg = config.services.k3s;
+          manifestDir = "${cfg.dataDir}/server/manifests";
+          chartDir = "${cfg.dataDir}/server/static/charts";
+
+          # Create tmpfiles rules for each manifest
+          manifestRules = lib.mapAttrs' (name: manifest:
+            lib.nameValuePair "${manifestDir}/${manifest.target}" {
+              "L+".argument = manifest.source;
+            }
+          ) k3sCfg.manifests;
+
+          # Create tmpfiles rules for each chart
+          chartRules = lib.mapAttrs' (name: chart:
+            lib.nameValuePair "${chartDir}/${name}.tgz" {
+              "L+".argument = chart;
+            }
+          ) k3sCfg.charts;
+        in
+          manifestRules // chartRules
+      );
+
     # Firewall
     networking.firewall = {
       enable = lib.mkForce true;
