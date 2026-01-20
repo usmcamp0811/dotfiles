7afa3917dc02a0b3e7da9297599784df9ae3514a
Matt Camp <matt@aicampground.com>
2026-01-19 18:28:04 -0600

    wip

--- modules/nixos/kubernetes/external-secrets/default.nix
+++ modules/nixos/kubernetes/external-secrets/default.nix
@@ -45,51 +45,51 @@ in {
         tar -czf $out -C external-secrets .
       '';
 
-    # systemd.services.vault-k8s-init = mkIf (config.services.k3s.clusterInit) {
-    #   description = "Configure Vault Kubernetes Auth";
-    #   wantedBy = ["multi-user.target"];
-    #   after = ["network.target" "k3s.service"];
-    #   requires = ["k3s.service"];
-    #
-    #   serviceConfig = {
-    #     Type = "oneshot";
-    #     User = "root";
-    #     ExecStart = pkgs.writeShellScript "vault-k8s-init" ''
-    #       set -e
-    #
-    #       K8S_HOST=${config.services.k3s.serverAddr}
-    #       NS=external-secrets
-    #       SA=vault-auth
-    #
-    #       VAULT_PATH="${cfg.vault-path}"
-    #       export VAULT_ADDR="${cfg.vault-address}"
-    #       HOSTNAME=${config.networking.hostName}
-    #
-    #       ROLE_ID=$(cat /var/lib/vault/$HOSTNAME/role-id)
-    #       SECRET_ID=$(cat /var/lib/vault/$HOSTNAME/secret-id)
-    #
-    #       echo "Logging in to Vault using AppRole..."
-    #       VAULT_TOKEN=$(${pkgs.vault}/bin/vault write -field=token auth/approle/login role_id="$ROLE_ID" secret_id="$SECRET_ID")
-    #       export VAULT_TOKEN
-    #
-    #       ${pkgs.k3s}/bin/k3s kubectl -n $NS create token $SA --duration=24h > /tmp/token.jwt
-    #       ${pkgs.k3s}/bin/k3s kubectl -n $NS get configmap kube-root-ca.crt -o jsonpath='{.data.ca\.crt}' > /tmp/ca.crt
-    #
-    #       ${pkgs.vault}/bin/vault write auth/kubernetes/config \
-    #         token_reviewer_jwt="$(< /tmp/token.jwt)" \
-    #         kubernetes_host="$K8S_HOST" \
-    #         kubernetes_ca_cert="$(< /tmp/ca.crt)"
-    #
-    #       ${pkgs.vault}/bin/vault write auth/kubernetes/role/external-secrets \
-    #         bound_service_account_names="$SA" \
-    #         bound_service_account_namespaces="*" \
-    #         policies=${cfg.vault-policy} \
-    #         ttl=24h
-    #       rm -rf /tmp/token.jwt /tmp/ca.crt
-    #     '';
-    #     RemainAfterExit = true;
-    #   };
-    # };
+    systemd.services.vault-k8s-init = mkIf (config.services.k3s.clusterInit) {
+      description = "Configure Vault Kubernetes Auth";
+      wantedBy = ["multi-user.target"];
+      after = ["network.target" "k3s.service"];
+      requires = ["k3s.service"];
+
+      serviceConfig = {
+        Type = "oneshot";
+        User = "root";
+        ExecStart = pkgs.writeShellScript "vault-k8s-init" ''
+          set -e
+
+          K8S_HOST=${config.services.k3s.serverAddr}
+          NS=external-secrets
+          SA=vault-auth
+
+          VAULT_PATH="${cfg.vault-path}"
+          export VAULT_ADDR="${cfg.vault-address}"
+          HOSTNAME=${config.networking.hostName}
+
+          ROLE_ID=$(cat /var/lib/vault/$HOSTNAME/role-id)
+          SECRET_ID=$(cat /var/lib/vault/$HOSTNAME/secret-id)
+
+          echo "Logging in to Vault using AppRole..."
+          VAULT_TOKEN=$(${pkgs.vault}/bin/vault write -field=token auth/approle/login role_id="$ROLE_ID" secret_id="$SECRET_ID")
+          export VAULT_TOKEN
+
+          ${pkgs.k3s}/bin/k3s kubectl -n $NS create token $SA --duration=24h > /tmp/token.jwt
+          ${pkgs.k3s}/bin/k3s kubectl -n $NS get configmap kube-root-ca.crt -o jsonpath='{.data.ca\.crt}' > /tmp/ca.crt
+
+          ${pkgs.vault}/bin/vault write auth/kubernetes/config \
+            token_reviewer_jwt="$(< /tmp/token.jwt)" \
+            kubernetes_host="$K8S_HOST" \
+            kubernetes_ca_cert="$(< /tmp/ca.crt)"
+
+          ${pkgs.vault}/bin/vault write auth/kubernetes/role/external-secrets \
+            bound_service_account_names="$SA" \
+            bound_service_account_namespaces="*" \
+            policies=${cfg.vault-policy} \
+            ttl=24h
+          rm -rf /tmp/token.jwt /tmp/ca.crt
+        '';
+        RemainAfterExit = true;
+      };
+    };
     services.k3s.manifests = {
       external-secrets.content = {
         apiVersion = "helm.cattle.io/v1";
