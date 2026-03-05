c018dcad50522e948f360d827ec518252fc3cb84
Matt Camp <matt@aicampground.com>
2026-03-05 05:28:55 -0600

    terraink

---
+++ packages/applications/terraink/default.nix
@@ -0,0 +1,49 @@
+{ lib, pkgs, ... }:
+let
+  pname = "terraink";
+  version = "0.2.0";
+
+  src = pkgs.fetchFromGitHub {
+    owner = "yousifamanuel";
+    repo = pname;
+    rev = "v${version}";
+    hash = "sha256-87DzA4hmGfWBLaVbrmbIHOlmfdRVxTFX/5bmsIk2C3A=";
+  };
+
+  srcWithLock = pkgs.runCommand "${pname}-src-${version}" {
+    inherit src;
+    lockfile = ./package-lock.json;
+  } ''
+    cp -r "$src" "$out"
+    chmod -R u+w "$out"
+    cp "$lockfile" "$out/package-lock.json"
+  '';
+in pkgs.buildNpmPackage {
+  inherit pname version;
+  src = srcWithLock;
+
+  nativeBuildInputs = [ pkgs.makeWrapper ];
+
+  npmDepsHash = "sha256-yXmueacim+22A+9XqCED9AZKRM0G1krNtOTIFvCOJbA=";
+  npmBuildScript = "build";
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p "$out/share/${pname}"
+    cp -r dist/* "$out/share/${pname}/"
+
+    makeWrapper ${pkgs.caddy}/bin/caddy "$out/bin/${pname}" \
+      --add-flags "file-server --root $out/share/${pname} --listen :4173"
+
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "Generate printable custom map posters";
+    homepage = "https://github.com/yousifamanuel/terraink";
+    license = licenses.mit;
+    mainProgram = pname;
+    platforms = platforms.linux ++ platforms.darwin;
+  };
+}
