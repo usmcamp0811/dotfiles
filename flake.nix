3273024cb7731408ba54002cd07671c8f1651066
Matt Camp <matt@aicampground.com>
2026-05-05 20:07:27 -0500

    cf update

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
     services-flake.url = "github:juspay/services-flake";
     crystal-forge = {
-      url = "gitlab:crystal-forge/crystal-forge/TASK-287-clean-build-names";
+      url = "gitlab:crystal-forge/crystal-forge/TASK-288-rebuild-evaluations-view";
       inputs.nixpkgs.follows = "unstable";
     };
     opencode-anthropic-auth = {
