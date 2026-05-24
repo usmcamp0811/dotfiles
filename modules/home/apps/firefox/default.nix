fdc387a0e26892be2bd54ee5cc594b6e92da8f96
Matt Camp <matt@aicampground.com>
2026-05-23 19:31:54 -0500

    guard stylix firefox profileNames option

--- modules/home/apps/firefox/default.nix
+++ modules/home/apps/firefox/default.nix
@@ -8,84 +8,88 @@ in {
     cac = mkBoolOpt false "Enable CAC Support";
   };
 
-  config = mkIf cfg.enable {
-    stylix.targets.firefox.profileNames = [ "default" ];
+  config = mkIf cfg.enable (mkMerge [
+    (mkIf (hasAttrByPath [ "stylix" "targets" "firefox" "profileNames" ] options) {
+      stylix.targets.firefox.profileNames = [ "default" ];
+    })
 
-    # environment.systemPackages = with pkgs; [
-    #   nssTools
-    #   firefox
-    # ];
+    {
+      # environment.systemPackages = with pkgs; [
+      #   nssTools
+      #   firefox
+      # ];
 
-    programs.firefox = {
-      enable = true;
-      profiles = {
-        default = {
-          id = 0;
-          name = "default";
-          isDefault = true;
-          settings = {
-            "browser.startup.homepage" = "https://searx.aicampground.com";
-            "browser.search.defaultenginename" = "Searx";
-            "browser.search.order.1" = "Searx";
-          };
-          search = {
-            force = true;
-            default = "Searx";
-            order = [ "Searx" "google" ];
-            engines = {
-              "Nix Packages" = {
-                urls = [{
-                  template = "https://search.nixos.org/packages";
-                  params = [
-                    {
-                      name = "type";
-                      value = "packages";
-                    }
-                    {
-                      name = "query";
-                      value = "{searchTerms}";
-                    }
-                  ];
-                }];
-                icon =
-                  "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-                definedAliases = [ "@np" ];
-              };
-              "NixOS Wiki" = {
-                urls = [{
-                  template =
-                    "https://nixos.wiki/index.php?search={searchTerms}";
-                }];
-                icon = "https://nixos.wiki/favicon.png";
-                updateInterval = 24 * 60 * 60 * 1000; # every day
-                definedAliases = [ "@nw" ];
-              };
-              "Searx" = {
-                urls = [{
-                  template = "https://searx.aicampground.com/?q={searchTerms}";
-                }];
-                icon = "https://nixos.wiki/favicon.png";
-                updateInterval = 24 * 60 * 60 * 1000; # every day
-                definedAliases = [ "@searx" ];
-              };
-              "bing".metaData.hidden = true;
-              "google".metaData.alias =
-                "@g"; # builtin engines only support specifying one additional alias
+      programs.firefox = {
+        enable = true;
+        profiles = {
+          default = {
+            id = 0;
+            name = "default";
+            isDefault = true;
+            settings = {
+              "browser.startup.homepage" = "https://searx.aicampground.com";
+              "browser.search.defaultenginename" = "Searx";
+              "browser.search.order.1" = "Searx";
             };
+            search = {
+              force = true;
+              default = "Searx";
+              order = [ "Searx" "google" ];
+              engines = {
+                "Nix Packages" = {
+                  urls = [{
+                    template = "https://search.nixos.org/packages";
+                    params = [
+                      {
+                        name = "type";
+                        value = "packages";
+                      }
+                      {
+                        name = "query";
+                        value = "{searchTerms}";
+                      }
+                    ];
+                  }];
+                  icon =
+                    "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+                  definedAliases = [ "@np" ];
+                };
+                "NixOS Wiki" = {
+                  urls = [{
+                    template =
+                      "https://nixos.wiki/index.php?search={searchTerms}";
+                  }];
+                  icon = "https://nixos.wiki/favicon.png";
+                  updateInterval = 24 * 60 * 60 * 1000; # every day
+                  definedAliases = [ "@nw" ];
+                };
+                "Searx" = {
+                  urls = [{
+                    template = "https://searx.aicampground.com/?q={searchTerms}";
+                  }];
+                  icon = "https://nixos.wiki/favicon.png";
+                  updateInterval = 24 * 60 * 60 * 1000; # every day
+                  definedAliases = [ "@searx" ];
+                };
+                "bing".metaData.hidden = true;
+                "google".metaData.alias =
+                  "@g"; # builtin engines only support specifying one additional alias
+              };
+            };
+            extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
+              ublock-origin
+              bitwarden
+              darkreader
+              vimium
+              simplelogin
+            ];
           };
-          extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
-            ublock-origin
-            bitwarden
-            darkreader
-            vimium
-            simplelogin
-          ];
         };
       };
-    };
-    # # TODO: Add things to exploade cac certs and install them into firefox here
-    # fmf.services.cac.enable = mkIf cfg.cac true;
-  };
+      # # TODO: Add things to exploade cac certs and install them into firefox here
+      # fmf.services.cac.enable = mkIf cfg.cac true;
+    }
+  ]);
 }
 # TODO: Read this and do something with it
 # https://github.com/NixOS/nixpkgs/issues/171978
