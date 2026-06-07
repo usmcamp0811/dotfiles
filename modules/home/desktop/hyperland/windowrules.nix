c940109e7d65cbdc9b878f9669f6a268f0f6cbc4
Matt Camp <matt@aicampground.com>
2026-06-06 19:16:46 -0500

    err

--- modules/home/desktop/hyperland/windowrules.nix
+++ modules/home/desktop/hyperland/windowrules.nix
@@ -11,7 +11,7 @@ in {
   config = mkIf cfg.enable {
     wayland.windowManager.hyprland = {
       settings = {
-        windowrule = [
+        windowrulev2 = [
           # ░█░█░▀█▀░█▀█░█▀▄░█▀█░█░█░░░█▀▄░█░█░█░░░█▀▀░█▀▀
           # ░█▄█░░█░░█░█░█░█░█░█░█▄█░░░█▀▄░█░█░█░░░█▀▀░▀▀█
           # ░▀░▀░▀▀▀░▀░▀░▀▀░░▀▀▀░▀░▀░░░▀░▀░▀▀▀░▀▀▀░▀▀▀░▀▀▀
