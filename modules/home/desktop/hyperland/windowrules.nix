acc6ef16ceb372055a23ef20ca11889f0e9fa3b5
Matt Camp <matt@aicampground.com>
2026-06-06 23:10:39 -0500

    fix(hyprland): rename windowrulev2 to windowrule in extraConfig
    In the latest Hyprland, the 'windowrulev2' keyword is deprecated.
    The modern syntax is just 'windowrule' which accepts v2 format.
    Changed all instances in the raw extraConfig from 'windowrulev2 ='
    to 'windowrule =' to fix deprecation warnings.

--- modules/home/desktop/hyperland/windowrules.nix
+++ modules/home/desktop/hyperland/windowrules.nix
@@ -21,94 +21,94 @@ in {
         # ░█▀▀░█░░░█░█░█▀█░░█░░░█░░█░█░█░█
         # ░▀░░░▀▀▀░▀▀▀░▀░▀░░▀░░▀▀▀░▀░▀░▀▀▀
         ##
-        windowrulev2 = float,class:^(Rofi)$
-        windowrulev2 = float,class:^(viewnior)$
-        windowrulev2 = float,class:^(feh)$
-        windowrulev2 = float,class:^(wlogout)$
-        windowrulev2 = float,class:^(file_progress)$
-        windowrulev2 = float,class:^(confirm)$
-        windowrulev2 = float,class:^(dialog)$
-        windowrulev2 = float,class:^(download)$
-        windowrulev2 = float,class:^(notification)$
-        windowrulev2 = float,class:^(error)$
-        windowrulev2 = float,class:^(splash)$
-        windowrulev2 = float,class:^(confirmreset)$
-        windowrulev2 = float,class:^(polkit-gnome-authentication-agent-1)$
-        windowrulev2 = float,class:^(wdisplays)$
-        windowrulev2 = size 1100 600,class:^(wdisplays)$
-        windowrulev2 = float,class:^(blueman-manager)$
-        windowrulev2 = float,class:^(nm-connection-editor)$
+        windowrule = float,class:^(Rofi)$
+        windowrule = float,class:^(viewnior)$
+        windowrule = float,class:^(feh)$
+        windowrule = float,class:^(wlogout)$
+        windowrule = float,class:^(file_progress)$
+        windowrule = float,class:^(confirm)$
+        windowrule = float,class:^(dialog)$
+        windowrule = float,class:^(download)$
+        windowrule = float,class:^(notification)$
+        windowrule = float,class:^(error)$
+        windowrule = float,class:^(splash)$
+        windowrule = float,class:^(confirmreset)$
+        windowrule = float,class:^(polkit-gnome-authentication-agent-1)$
+        windowrule = float,class:^(wdisplays)$
+        windowrule = size 1100 600,class:^(wdisplays)$
+        windowrule = float,class:^(blueman-manager)$
+        windowrule = float,class:^(nm-connection-editor)$
 
         # floating terminal
-        windowrulev2 = float,title:^(floating_kitty)$
-        windowrulev2 = size 1100 600,title:^(floating_kitty)$
-        windowrulev2 = center,title:^(floating_kitty)$
-        windowrulev2 = animation slide,title:^(floating_kitty)$
+        windowrule = float,title:^(floating_kitty)$
+        windowrule = size 1100 600,title:^(floating_kitty)$
+        windowrule = center,title:^(floating_kitty)$
+        windowrule = animation slide,title:^(floating_kitty)$
 
         # calendar reminders
-        windowrulev2 = float,class:^(thunderbird)$,title:.*(Reminders)$
-        windowrulev2 = size 1100 600,class:^(thunderbird)$,title:.*(Reminders)$
-        windowrulev2 = move 78% 6%,class:^(thunderbird)$,title:.*(Reminders)$
-        windowrulev2 = pin,class:^(thunderbird)$,title:.*(Reminders)$
+        windowrule = float,class:^(thunderbird)$,title:.*(Reminders)$
+        windowrule = size 1100 600,class:^(thunderbird)$,title:.*(Reminders)$
+        windowrule = move 78% 6%,class:^(thunderbird)$,title:.*(Reminders)$
+        windowrule = pin,class:^(thunderbird)$,title:.*(Reminders)$
 
         # thunar file operation progress
-        windowrulev2 = float,class:^(thunar)$,title:^(File Operation Progress)$
-        windowrulev2 = size 800 600,class:^(thunar)$,title:^(File Operation Progress)$
-        windowrulev2 = move 78% 6%,class:^(thunar)$,title:^(File Operation Progress)$
-        windowrulev2 = pin,class:^(thunar)$,title:^(File Operation Progress)$
+        windowrule = float,class:^(thunar)$,title:^(File Operation Progress)$
+        windowrule = size 800 600,class:^(thunar)$,title:^(File Operation Progress)$
+        windowrule = move 78% 6%,class:^(thunar)$,title:^(File Operation Progress)$
+        windowrule = pin,class:^(thunar)$,title:^(File Operation Progress)$
 
         # Workspace 8 (VM) layout
-        windowrulev2 = size 1000 1330,class:^(virt-manager)$,title:^(Virtual Machine Manager)$
-        windowrulev2 = float,class:^(virt-manager)$,title:^(Virtual Machine Manager)$
-        windowrulev2 = move 80% 6%,class:^(virt-manager)$,title:^(Virtual Machine Manager)$
-        windowrulev2 = float,class:^(looking-glass-client)$
-        windowrulev2 = size 2360 1330,class:^(looking-glass-client)$
-        windowrulev2 = move 25% 6%,class:^(looking-glass-client)$
-        windowrulev2 = float,class:^(virt-manager)$,title:^.*(on QEMU/KVM)$
-        windowrulev2 = size 2360 1330,class:^(virt-manager)$,title:^.*(on QEMU/KVM)$
-        windowrulev2 = move 25% 6%,class:^(virt-manager)$,title:^.*(on QEMU/KVM)$
-        windowrulev2 = float,class:^(qemu)$
-        windowrulev2 = size 2360 1330,class:^(qemu)$
-        windowrulev2 = move 25% 6%,class:^(qemu)$
+        windowrule = size 1000 1330,class:^(virt-manager)$,title:^(Virtual Machine Manager)$
+        windowrule = float,class:^(virt-manager)$,title:^(Virtual Machine Manager)$
+        windowrule = move 80% 6%,class:^(virt-manager)$,title:^(Virtual Machine Manager)$
+        windowrule = float,class:^(looking-glass-client)$
+        windowrule = size 2360 1330,class:^(looking-glass-client)$
+        windowrule = move 25% 6%,class:^(looking-glass-client)$
+        windowrule = float,class:^(virt-manager)$,title:^.*(on QEMU/KVM)$
+        windowrule = size 2360 1330,class:^(virt-manager)$,title:^.*(on QEMU/KVM)$
+        windowrule = move 25% 6%,class:^(virt-manager)$,title:^.*(on QEMU/KVM)$
+        windowrule = float,class:^(qemu)$
+        windowrule = size 2360 1330,class:^(qemu)$
+        windowrule = move 25% 6%,class:^(qemu)$
 
         # make Firefox PiP window floating and sticky
-        windowrulev2 = float,title:^(Picture-in-Picture)$
-        windowrulev2 = pin,title:^(Picture-in-Picture)$
+        windowrule = float,title:^(Picture-in-Picture)$
+        windowrule = pin,title:^(Picture-in-Picture)$
 
         # fix xwayland apps
-        windowrulev2 = rounding 0,xwayland:1,floating:1
-        windowrulev2 = center,class:^(.*jetbrains.*)$,title:^(Confirm Exit|Open Project|win424|win201|splash)$
-        windowrulev2 = size 640 400,class:^(.*jetbrains.*)$,title:^(splash)$
+        windowrule = rounding 0,xwayland:1,floating:1
+        windowrule = center,class:^(.*jetbrains.*)$,title:^(Confirm Exit|Open Project|win424|win201|splash)$
+        windowrule = size 640 400,class:^(.*jetbrains.*)$,title:^(splash)$
 
         ##
         # ░█▀█░█▀█░█▀█░█▀▀░▀█▀░▀█▀░█░█
         # ░█░█░█▀▀░█▀█░█░░░░█░░░█░░░█░
         # ░▀▀▀░▀░░░▀░▀░▀▀▀░▀▀▀░░▀░░░▀░
         ##
-        windowrulev2 = opaque,class:^(virt-manager)$,title:.*(on QEMU).*
-        windowrulev2 = opaque,class:^(looking-glass-client)$
-        windowrulev2 = opaque,title:^(.*(Twitch|TNTdrama|YouTube|Bally Sports|Video Entertainment|Plex)).*(Firefox).*$
-        windowrulev2 = dimaround,class:^(gcr-prompter)$
+        windowrule = opaque,class:^(virt-manager)$,title:.*(on QEMU).*
+        windowrule = opaque,class:^(looking-glass-client)$
+        windowrule = opaque,title:^(.*(Twitch|TNTdrama|YouTube|Bally Sports|Video Entertainment|Plex)).*(Firefox).*$
+        windowrule = dimaround,class:^(gcr-prompter)$
 
         # Require input
-        windowrulev2 = bordercolor rgba(ed8796FF),class:^(polkit-gnome-authentication-agent-1)$
-        windowrulev2 = dimaround,class:^(polkit-gnome-authentication-agent-1)$
-        windowrulev2 = stayfocused,class:^(polkit-gnome-authentication-agent-1)$
-        windowrulev2 = stayfocused,class:^(Rofi)$
-        windowrulev2 = nofocus,class:^(steam)$,title:^()$
+        windowrule = bordercolor rgba(ed8796FF),class:^(polkit-gnome-authentication-agent-1)$
+        windowrule = dimaround,class:^(polkit-gnome-authentication-agent-1)$
+        windowrule = stayfocused,class:^(polkit-gnome-authentication-agent-1)$
+        windowrule = stayfocused,class:^(Rofi)$
+        windowrule = nofocus,class:^(steam)$,title:^()$
 
         ##
         # ░▀█▀░█▀▄░█░░░█▀▀░▀█▀░█▀█░█░█░▀█▀░█▀▄░▀█▀░▀█▀
         # ░░█░░█░█░█░░░█▀▀░░█░░█░█░█▀█░░█░░█▀▄░░█░░░█░
         # ░▀▀▀░▀▀░░▀▀▀░▀▀▀░▀▀▀░▀░▀░▀░▀░▀▀▀░▀▀░░▀▀▀░░▀░
         ##
-        windowrulev2 = idleinhibit focus,class:^(steam_app).*
-        windowrulev2 = idleinhibit focus,class:^(gamescope).*
-        windowrulev2 = idleinhibit focus,class:.*(cemu|yuzu|ryujinx|emulationstation|retroarch).*
-        windowrulev2 = idleinhibit fullscreen,title:.*(cemu|yuzu|ryujinx|emulationstation|retroarch).*
-        windowrulev2 = idleinhibit fullscreen,title:^(.*(Twitch|TNTdrama|YouTube|Bally Sports|Video Entertainment|Plex)).*(Firefox).*$
-        windowrulev2 = idleinhibit focus,title:^(.*(Twitch|TNTdrama|YouTube|Bally Sports|Video Entertainment|Plex)).*(Firefox).*$
-        windowrulev2 = idleinhibit focus,class:^(mpv|.+exe)$
+        windowrule = idleinhibit focus,class:^(steam_app).*
+        windowrule = idleinhibit focus,class:^(gamescope).*
+        windowrule = idleinhibit focus,class:.*(cemu|yuzu|ryujinx|emulationstation|retroarch).*
+        windowrule = idleinhibit fullscreen,title:.*(cemu|yuzu|ryujinx|emulationstation|retroarch).*
+        windowrule = idleinhibit fullscreen,title:^(.*(Twitch|TNTdrama|YouTube|Bally Sports|Video Entertainment|Plex)).*(Firefox).*$
+        windowrule = idleinhibit focus,title:^(.*(Twitch|TNTdrama|YouTube|Bally Sports|Video Entertainment|Plex)).*(Firefox).*$
+        windowrule = idleinhibit focus,class:^(mpv|.+exe)$
 
         ##
         # ░█░█░█▀█░█▀▄░█░█░█▀▀░█▀█░█▀█░█▀▀░█▀▀░░░█▀▀░█▀█░█▀█░█▀▀░▀█▀░█▀▀
@@ -117,47 +117,47 @@ in {
         ##
 
         # Secondary Monitor Media
-        windowrulev2 = workspace 1,title:^(.*(Twitch|TNTdrama|YouTube|Bally Sports|Video Entertainment|Plex)).*(Firefox).*$
+        windowrule = workspace 1,title:^(.*(Twitch|TNTdrama|YouTube|Bally Sports|Video Entertainment|Plex)).*(Firefox).*$
         #Browsers
-        # windowrulev2 = workspace 2,title:^(?!.*(Twitch|TNTdrama|YouTube|Bally Sports|Video Entertainment|Plex)).*(Firefox).*$
-        # windowrulev2 = workspace special:inactive,title:^(.*(hidden tabs - Workona)).*(Firefox).*$
+        # windowrule = workspace 2,title:^(?!.*(Twitch|TNTdrama|YouTube|Bally Sports|Video Entertainment|Plex)).*(Firefox).*$
+        # windowrule = workspace special:inactive,title:^(.*(hidden tabs - Workona)).*(Firefox).*$
         # Code
-        windowrulev2 = workspace 3,class:^(Code)$
-        windowrulev2 = workspace 3,class:^(neovide)$
-        windowrulev2 = workspace 3,class:^(GitHub Desktop)$
-        windowrulev2 = workspace 3,class:^(GitKraken)$
-        windowrulev2 = workspace 3,class:^(kitty)$,title:^(nvim).*
+        windowrule = workspace 3,class:^(Code)$
+        windowrule = workspace 3,class:^(neovide)$
+        windowrule = workspace 3,class:^(GitHub Desktop)$
+        windowrule = workspace 3,class:^(GitKraken)$
+        windowrule = workspace 3,class:^(kitty)$,title:^(nvim).*
         # Gaming
-        windowrulev2 = workspace 4,class:^(Steam|steam)$
-        windowrulev2 = workspace 4,class:^(Steam|steam).,title:^(Steam|steam)$
-        windowrulev2 = workspace 4,class:^(gamescope|steam_app).*
-        windowrulev2 = workspace 4,class:^(heroic)$
-        windowrulev2 = workspace 4,class:^(lutris)$
-        windowrulev2 = workspace 4,class:.*(cemu|yuzu|ryujinx|emulationstation|retroarch).*
-        windowrulev2 = workspace 4,title:.*(cemu|yuzu|ryujinx|emulationstation|retroarch).*
+        windowrule = workspace 4,class:^(Steam|steam)$
+        windowrule = workspace 4,class:^(Steam|steam).,title:^(Steam|steam)$
+        windowrule = workspace 4,class:^(gamescope|steam_app).*
+        windowrule = workspace 4,class:^(heroic)$
+        windowrule = workspace 4,class:^(lutris)$
+        windowrule = workspace 4,class:.*(cemu|yuzu|ryujinx|emulationstation|retroarch).*
+        windowrule = workspace 4,title:.*(cemu|yuzu|ryujinx|emulationstation|retroarch).*
         # Mail
-        windowrulev2 = workspace 5,class:^(thunderbird)$
-        windowrulev2 = workspace 5,class:^(Mailspring)$
+        windowrule = workspace 5,class:^(thunderbird)$
+        windowrule = workspace 5,class:^(Mailspring)$
         # Messaging
-        windowrulev2 = workspace 6,title:^(Slack)$
-        windowrulev2 = workspace 6,title:^(Mattermost)$
-        windowrulev2 = workspace 6,class:^(Caprine)$
-        windowrulev2 = workspace 6,class:^(org.telegram.desktop)$
-        windowrulev2 = workspace 6,class:^(discord)$
-        windowrulev2 = workspace 6,class:^(zoom)$
-        windowrulev2 = workspace 6,class:^(Element)$
+        windowrule = workspace 6,title:^(Slack)$
+        windowrule = workspace 6,title:^(Mattermost)$
+        windowrule = workspace 6,class:^(Caprine)$
+        windowrule = workspace 6,class:^(org.telegram.desktop)$
+        windowrule = workspace 6,class:^(discord)$
+        windowrule = workspace 6,class:^(zoom)$
+        windowrule = workspace 6,class:^(Element)$
         # Media
-        windowrulev2 = workspace 7,class:^(mpv|vlc|mpdevil)$
-        windowrulev2 = workspace 7,class:^(Spotify)$
-        windowrulev2 = workspace 7,title:^(Spotify)$
-        windowrulev2 = workspace 7,title:^(Spotify Free)$
-        windowrulev2 = tile,class:^(Spotify)$
-        windowrulev2 = tile,class:^(Spotify Free)$
-        windowrulev2 = workspace 7,class:^(elisa)$
+        windowrule = workspace 7,class:^(mpv|vlc|mpdevil)$
+        windowrule = workspace 7,class:^(Spotify)$
+        windowrule = workspace 7,title:^(Spotify)$
+        windowrule = workspace 7,title:^(Spotify Free)$
+        windowrule = tile,class:^(Spotify)$
+        windowrule = tile,class:^(Spotify Free)$
+        windowrule = workspace 7,class:^(elisa)$
         #Remote
-        windowrulev2 = workspace 8,class:^(virt-manager|qemu)$
-        windowrulev2 = workspace 8,class:^(gnome-connections)$
-        windowrulev2 = workspace 8,class:^(looking-glass-client)$
+        windowrule = workspace 8,class:^(virt-manager|qemu)$
+        windowrule = workspace 8,class:^(gnome-connections)$
+        windowrule = workspace 8,class:^(looking-glass-client)$
       '';
     };
   };
