c3a7aa685b305314d5c017219a8c0f231dbb4ef2
Matt Camp <matt@aicampground.com>
2026-06-18 21:35:42 -0500

    cf update

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
     services-flake.url = "github:juspay/services-flake";
     crystal-forge = {
-      url = "gitlab:crystal-forge/crystal-forge/TASK-345.1-evaluations-parity";
+      url = "gitlab:crystal-forge/crystal-forge/TASK-348.1-cves-parity";
       inputs.nixpkgs.follows = "unstable";
     };
     opencode-anthropic-auth = {
