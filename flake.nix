4fc4e29fc30532325cbf5646608098077b9ee1f1
Matt Camp <matt@aicampground.com>
2026-07-01 22:45:19 -0500

    cf update

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     services-flake.url = "github:juspay/services-flake";
     crystal-forge = {
       url = "gitlab:crystal-forge/crystal-forge/TASK-376-system-detail-history-logs-refactor";
+      # url = "gitlab:crystal-forge/crystal-forge/TASK-375.4-verified-source-re-evaluation";
       inputs.nixpkgs.follows = "unstable";
     };
     opencode-anthropic-auth = {
