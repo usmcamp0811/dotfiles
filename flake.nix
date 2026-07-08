2be233d628da83e6a2f4e618d3d78be0c320348f
Matt Camp <matt@aicampground.com>
2026-07-07 21:13:18 -0500

    cf update

--- flake.nix
+++ flake.nix
@@ -4,8 +4,7 @@
     process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
     services-flake.url = "github:juspay/services-flake";
     crystal-forge = {
-      # url = "gitlab:crystal-forge/crystal-forge/TASK-376-system-detail-history-logs-refactor";
-      url = "gitlab:crystal-forge/crystal-forge/TASK-378.1-system-events-timeline";
+      url = "gitlab:crystal-forge/crystal-forge/dev";
       inputs.nixpkgs.follows = "unstable";
     };
     opencode-anthropic-auth = {
