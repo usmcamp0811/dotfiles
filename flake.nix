2b48197d9b4c42e1f6d168204e87853baa1350e6
Matt Camp <matt@aicampground.com>
2026-08-05 11:11:08 -0500

    cf update

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
     services-flake.url = "github:juspay/services-flake";
     crystal-forge = {
-      url = "gitlab:crystal-forge/crystal-forge/5fba0782af78fcbac7c73277e954c7d347187892";
+      url = "gitlab:crystal-forge/crystal-forge/ecaf14ea7177553d2a9868994b87663e3b94aae7";
 
       inputs.nixpkgs.follows = "unstable";
     };
