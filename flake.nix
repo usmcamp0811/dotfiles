d0c7cd88002851eb6e22b7db03d007a8b39f27d7
Matt Camp <matt@aicampground.com>
2026-08-15 11:03:05 -0500

    cf update

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
     services-flake.url = "github:juspay/services-flake";
     crystal-forge = {
-      url = "gitlab:crystal-forge/crystal-forge/TASK-412-cf-xccdf-interchange";
+      url = "gitlab:crystal-forge/crystal-forge/TASK-418-cross-framework-requirements";
 
       inputs.nixpkgs.follows = "unstable";
     };
